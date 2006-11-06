6defdaa4259f8eb13bf6ef40602de9e52451834c
acarboni <acarboni@ff403467-3f20-0410-99e5-ff581e626a08>
2006-11-06 14:27:46 +0000

    Added http-client library
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@18 ff403467-3f20-0410-99e5-ff581e626a08

--- csw/cswClient.sh
+++ csw/cswClient.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-java -cp lib/csw-client.jar:lib/csw-common.jar:../web/WEB-INF/lib/jdom.jar:../web/WEB-INF/lib/dlib.jar \
-			org.fao.geonet.csw.client.CswClient
+CP=lib/csw-client.jar:lib/csw-common.jar
+
+for i in ../web/WEB-INF/lib/*.jar ; do
+	CP=${CP}:$i
+done
+
+java -cp $CP org.fao.geonet.csw.client.CswClient
