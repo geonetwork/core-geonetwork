9bb85de0fa323781f65a107ea928644e72229f4b
acarboni <acarboni@ff403467-3f20-0410-99e5-ff581e626a08>
2007-01-26 14:43:22 +0000

    Updated
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@137 ff403467-3f20-0410-99e5-ff581e626a08

---
+++ src/org/fao/gast/lib/Resource.java
@@ -0,0 +1,114 @@
+//=============================================================================
+//===	Copyright (C) 2001-2005 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: GeoNetwork@fao.org
+//==============================================================================
+
+package org.fao.gast.lib;
+
+import jeeves.constants.ConfigFile;
+import jeeves.interfaces.Activator;
+import jeeves.server.resources.ProviderManager;
+import jeeves.server.resources.ResourceManager;
+import org.jdom.Element;
+
+//=============================================================================
+
+public class Resource
+{
+	//--------------------------------------------------------------------------
+	//---
+	//--- Constructor
+	//---
+	//--------------------------------------------------------------------------
+
+	public Resource(String appPath, Element resource) throws Exception
+	{
+		        name      = resource.getChildText(ConfigFile.Resource.Child.NAME);
+		String  provider  = resource.getChildText(ConfigFile.Resource.Child.PROVIDER);
+		Element config    = resource.getChild    (ConfigFile.Resource.Child.CONFIG);
+		Element activator = resource.getChild    (ConfigFile.Resource.Child.ACTIVATOR);
+
+		if (activator != null)
+		{
+			String clas = activator.getAttributeValue(ConfigFile.Activator.Attr.CLASS);
+
+			activ = (Activator) Class.forName(clas).newInstance();
+			activ.startup(appPath, activator);
+		}
+
+		provMan.register(provider, name, config);
+		resMan = new ResourceManager(provMan);
+	}
+
+	//--------------------------------------------------------------------------
+	//---
+	//--- API methods
+	//---
+	//--------------------------------------------------------------------------
+
+	public Object open() throws Exception
+	{
+		return resMan.open(name);
+	}
+
+	//--------------------------------------------------------------------------
+
+	public void close()
+	{
+		try
+		{
+			resMan.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+
+			try
+			{
+				resMan.abort();
+			}
+			catch (Exception ex)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		provMan.end();
+
+		if (activ != null)
+			activ.shutdown();
+	}
+
+	//--------------------------------------------------------------------------
+	//---
+	//--- Variables
+	//---
+	//--------------------------------------------------------------------------
+
+	private String          name;
+	private Activator       activ;
+	private ResourceManager resMan;
+	private ProviderManager provMan = new ProviderManager();
+}
+
+//=============================================================================
+
+
