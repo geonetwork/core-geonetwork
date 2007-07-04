3b666d37feb48e767eb46d33c1acc2d9de88b029
acarboni <acarboni@ff403467-3f20-0410-99e5-ff581e626a08>
2007-07-04 16:37:38 +0000

    Changed structure to accomodate authentication
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@535 ff403467-3f20-0410-99e5-ff581e626a08

--- src/org/fao/geonet/services/main/Forward.java
+++ src/org/fao/geonet/services/main/Forward.java
@@ -26,11 +26,13 @@ package org.fao.geonet.services.main;
 import java.net.URL;
 import java.util.List;
 import jeeves.exceptions.MissingParameterEx;
+import jeeves.exceptions.UserNotFoundEx;
 import jeeves.interfaces.Service;
 import jeeves.server.ServiceConfig;
 import jeeves.server.context.ServiceContext;
 import jeeves.utils.Util;
 import jeeves.utils.XmlRequest;
+import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.lib.Lib;
 import org.jdom.Element;
 
@@ -54,8 +56,15 @@ public class Forward implements Service
 
 	public Element exec(Element params, ServiceContext context) throws Exception
 	{
-		String  url = Util.getParam(params, "url");
-		Element par = Util.getChild(params, "params");
+		Element site = Util.getChild(params, "site");
+		Element par  = Util.getChild(params, "params");
+		Element acc  = site.getChild("account");
+
+		String url  = Util.getParam(site, "url");
+		String type = Util.getParam(site, "type", "generic");
+
+		String username = (acc == null) ? null : Util.getParam(acc, "username");
+		String password = (acc == null) ? null : Util.getParam(acc, "password");
 
 		List list = par.getChildren();
 
@@ -66,10 +75,42 @@ public class Forward implements Service
 
 		XmlRequest req = new XmlRequest(new URL(url));
 
+		//--- does we need to authenticate?
+
+		if (username != null)
+			authenticate(req, username, password, type);
+
 		Lib.net.setupProxy(context, req);
 		req.setRequest(params);
 
 		return req.execute();
+	}
+
+	//--------------------------------------------------------------------------
+
+	private void authenticate(XmlRequest req, String username, String password,
+									  String type) throws Exception
+	{
+		if (!type.equals("geonetwork"))
+			//--- set basic/digest credentials for non geonetwork sites
+			req.setCredentials(username, password);
+
+		else
+		{
+			String addr = req.getAddress();
+			int    pos  = addr.lastIndexOf("/");
+
+			req.setAddress(addr.substring(0, pos +1) + Geonet.Service.XML_LOGIN);
+			req.addParam("username", username);
+			req.addParam("password", password);
+
+			Element response = req.execute();
+
+			if (!response.getName().equals("ok"))
+				throw new UserNotFoundEx(username);
+
+			req.setAddress(addr);
+		}
 	}
 }
 
