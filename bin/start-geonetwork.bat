1464263e8b214ff37a8b848621dcb55232616e6c
acarboni <acarboni@ff403467-3f20-0410-99e5-ff581e626a08>
2007-08-02 14:39:20 +0000

    Fixed, thanks to Archie W.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@698 ff403467-3f20-0410-99e5-ff581e626a08

--- bin/start-geonetwork.bat
+++ bin/start-geonetwork.bat
@@ -1,5 +1,5 @@
 cd ..\jetty
-rm log\*request.log*
-mv log\jeeves.log.*  log\archive
+del log\*request.log*
+move log\jeeves.log.*  log\archive
 
 java -Xmx512m -DSTOP.PORT=8079 -jar start.jar ..\bin\jetty.xml
