1bc4165ec9c03145ee03c813f8d1e0e0078e0c59
acarboni <acarboni@ff403467-3f20-0410-99e5-ff581e626a08>
2007-10-06 16:13:00 +0000

    Moved jetty output to separate log file
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@900 ff403467-3f20-0410-99e5-ff581e626a08

--- bin/start-geonetwork.sh
+++ bin/start-geonetwork.sh
@@ -1,8 +1,9 @@
 cd ../jetty
 rm log/*request.log*
 mv log/jeeves.log.* log/archive
+rm log/jetty.log
 
 # try changing the Xmx parameter if your machine has little RAM
 #java -Xms48m -Xmx256m -DSTOP.PORT=8079 -jar start.jar ../bin/jetty.xml &
 
-java -Xms48m -Xmx512m -DSTOP.PORT=8079 -jar start.jar ../bin/jetty.xml &
+java -Xms48m -Xmx512m -DSTOP.PORT=8079 -jar start.jar ../bin/jetty.xml > log/jetty.log 2>&1 &
