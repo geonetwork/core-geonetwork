7f03a8749b90c9d62f0cc5eedec69dbc1cfe442c
e_tajariol <e_tajariol@ff403467-3f20-0410-99e5-ff581e626a08>
2008-02-16 18:27:04 +0000

    Added serverside markers support.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@1092 ff403467-3f20-0410-99e5-ff581e626a08

--- src/org/wfp/vam/intermap/services/map/Action.java
+++ src/org/wfp/vam/intermap/services/map/Action.java
@@ -30,6 +30,7 @@ import org.jdom.Element;
 import org.wfp.vam.intermap.Constants;
 import org.wfp.vam.intermap.kernel.map.MapMerger;
 import org.wfp.vam.intermap.kernel.map.mapServices.BoundingBox;
+import org.wfp.vam.intermap.kernel.marker.MarkerSet;
 import org.wfp.vam.intermap.util.Util;
 
 //=============================================================================
@@ -54,12 +55,12 @@ public class Action implements Service
 		int mapimgy = Integer.parseInt(params.getChildText("mapimgy"));
 		int mapimgx2 = Integer.parseInt(params.getChildText("mapimgx2"));
 		int mapimgy2 = Integer.parseInt(params.getChildText("mapimgy2"));
-		
+
 //		int width  = Util.parseInt(params.getChildText("width"), MapUtil.getImageWidth(context));
 //		int height = Util.parseInt(params.getChildText("height"), MapUtil.getImageHeight(context));
 		int width  = Integer.parseInt(params.getChildText("width"));
 		int height = Integer.parseInt(params.getChildText("height"));
-		
+
 		MapMerger mm = MapUtil.getMapMerger(context);
 
 		BoundingBox bb = Util.parseBoundingBox(params); // search bb in params
@@ -103,21 +104,23 @@ public class Action implements Service
 			return identify.exec(params, context);
 		}
 
-		// Get the current image size from the user session
-		// 20070622 ETj: already defined
-//		int width = MapUtil.getImageWidth(context);
-//		int height = MapUtil.getImageHeight(context);
-
 		// Merge the images now
 		String imagename = mm.merge(width, height);
 		String url = MapUtil.getTempUrl() + "/" + imagename;
 
-		return new Element("response")
+		// Prepare response
+		Element response = new Element("response")
 			.addContent(new Element("imgUrl").setText(url))
 			.addContent(new Element("scale").setText(mm.getDistScale()))
 			.addContent(mm.getBoundingBox().toElement())
 			.addContent(new Element("width").setText(""+width))
 			.addContent(new Element("height").setText(""+height));
+
+		MarkerSet ms = (MarkerSet)context.getUserSession().getProperty(Constants.SESSION_MARKERSET);
+		if(ms != null)
+			response.addContent(ms.select(mm.getBoundingBox()).toElement());
+
+		return response;
 	}
 }
 
