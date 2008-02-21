62ec95340b32947534e7e8abdd7095987d500986
ticheler <ticheler@ff403467-3f20-0410-99e5-ff581e626a08>
2008-02-21 15:16:15 +0000

    Add and remove parent - child relations between metadata records. (No GUI is provided with this service and the functionality relates to the already existing xml.relation.get search function)
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.2.x@1129 ff403467-3f20-0410-99e5-ff581e626a08

--- src/org/fao/geonet/constants/Params.java
+++ src/org/fao/geonet/constants/Params.java
@@ -44,6 +44,8 @@ public class Params
 	public static final String GROUP        = "group";
 	public static final String GROUPS       = "groups";
 	public static final String ID           = "id";
+    public static final String PARENT_ID    = "parentId";
+    public static final String CHILD_ID     = "childId";
 	public static final String UUID         = "uuid";
 	public static final String URI          = "uri";
 	public static final String NAME         = "name";
