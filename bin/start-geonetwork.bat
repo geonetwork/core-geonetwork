77bb140d593cb18c4d9032ce71c0b813a71e67e4
ticheler <ticheler@ff403467-3f20-0410-99e5-ff581e626a08>
2008-02-21 15:19:28 +0000

    Changed startup scripts for Windows to hide (or allow to hide) the dos window. Patch by Zha Zhuhua
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@1133 ff403467-3f20-0410-99e5-ff581e626a08

--- bin/start-geonetwork.bat
+++ bin/start-geonetwork.bat
@@ -6,4 +6,9 @@ move log\intermap.log.*   log\archive
 rem try changing the Xmx parameter if your machine has little RAM
 rem java -Xms48m -Xmx256m -DSTOP.PORT=8079 -jar start.jar ..\bin\jetty.xml
 
+rem if you want to hide the dos window when GeoNetwork is started, 
+rem comment out the next line and comment the last line
+
 java -Xms48m -Xmx512m -DSTOP.PORT=8079 -jar start.jar ..\bin\jetty.xml
+
+rem start javaw -Xms48m -Xmx512m -DSTOP.PORT=8079 -jar start.jar ..\bin\jetty.xml
