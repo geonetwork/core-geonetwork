1e96efd9faa28195954cf3731f284c8578de5b5a
e_tajariol <e_tajariol@ff403467-3f20-0410-99e5-ff581e626a08>
2008-03-02 20:52:20 +0000

    Temporary workaround preventing IE to visualize (probably broken) URL encoded WMC context.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@1168 ff403467-3f20-0410-99e5-ff581e626a08

--- web/intermap/scripts/im_integration.js
+++ web/intermap/scripts/im_integration.js
@@ -58,6 +58,17 @@ function im_mm_init(wmc, callback)
 		im_mm_fullAoI();		
 	};
     
+	if( wmc && Prototype.Browser.IE )
+	{
+		// FIXME i18n
+		alert("Sorry, but your browser can't handle long URLs.\n\n"+
+			  "If you arrived on this page following a link for viewing a map someone sent you,\n"+
+			  "please be advised that you can't use that feature with your current browser.\n"+
+			  "You are going to see a default map, and not the one you were looking for.\n\n" + 
+			  "Please use the 'Upload a context' button in the Map Viewer and provide a valid context document.");
+		wmc = null;
+	}
+	
 	if(wmc)
 	{
 		im_setWMC(wmc, function(req)
