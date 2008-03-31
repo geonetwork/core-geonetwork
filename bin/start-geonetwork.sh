551bb00a17e47c3758edcf1ae7f68aee3e061a09
ticheler <ticheler@ff403467-3f20-0410-99e5-ff581e626a08>
2008-03-31 09:01:03 +0000

    Start GeoNetwork headless (prevent pop-up Java windows)
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@1227 ff403467-3f20-0410-99e5-ff581e626a08

--- bin/start-geonetwork.sh
+++ bin/start-geonetwork.sh
@@ -5,6 +5,6 @@ mv log/geonetwork.log.* log/archive
 mv log/intermap.log.*   log/archive
 
 # try changing the Xmx parameter if your machine has little RAM
-#java -Xms48m -Xmx256m -DSTOP.PORT=8079 -jar start.jar ../bin/jetty.xml &
+#java -Xms48m -Xmx256m -DSTOP.PORT=8079 -Djava.awt.headless=true -jar start.jar ../bin/jetty.xml &
 
-java -Xms48m -Xmx512m -DSTOP.PORT=8079 -jar start.jar ../bin/jetty.xml > log/output.log 2>&1 &
+java -Xms48m -Xmx512m -DSTOP.PORT=8079 -Djava.awt.headless=true -jar start.jar ../bin/jetty.xml > log/output.log 2>&1 &
