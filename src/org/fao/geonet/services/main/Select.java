d2fe3db68453a9fc96343b6d18a1ca6eb9741319
fxprunayre <fxprunayre@ff403467-3f20-0410-99e5-ff581e626a08>
2009-03-21 05:59:43 +0000

    Add/remove missing files when applying patch for import and selection manager.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@2611 ff403467-3f20-0410-99e5-ff581e626a08

---
+++ src/org/fao/geonet/services/main/Select.java
@@ -0,0 +1,51 @@
+package org.fao.geonet.services.main;
+
+import jeeves.constants.Jeeves;
+import jeeves.interfaces.Service;
+import jeeves.server.ServiceConfig;
+import jeeves.server.UserSession;
+import jeeves.server.context.ServiceContext;
+import jeeves.utils.Util;
+
+import org.fao.geonet.constants.Params;
+import org.fao.geonet.kernel.SelectionManager;
+import org.jdom.Element;
+
+/**
+ * Select a list of elements stored in session
+ * Returns status 
+ */
+
+public class Select implements Service {
+	
+	String init_type;
+	
+	public void init(String appPath, ServiceConfig params) throws Exception {
+		init_type = params.getValue(Params.TYPE);
+	}
+
+	// --------------------------------------------------------------------------
+	// ---
+	// --- Service
+	// ---
+	// --------------------------------------------------------------------------
+
+	public Element exec(Element params, ServiceContext context)
+			throws Exception {
+		
+		String type = Util.getParam(params, Params.TYPE, init_type );
+		
+		// Get the selection manager
+		UserSession session = context.getUserSession();
+		int nbSelected = SelectionManager.updateSelection(type, session, params, context);
+		
+		// send ok
+		Element response = new Element(Jeeves.Elem.RESPONSE);
+		response.addContent(new Element("Selected").setText(""+nbSelected));		
+
+		return response;
+	}
+}
+
+// =============================================================================
+
---
+++ src/org/fao/geonet/services/main/SelectionSearch.java
@@ -0,0 +1,118 @@
+//=============================================================================
+//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: geonetwork@osgeo.org
+//==============================================================================
+
+package org.fao.geonet.services.main;
+
+import java.util.Iterator;
+
+import jeeves.constants.Jeeves;
+import jeeves.interfaces.Service;
+import jeeves.server.ServiceConfig;
+import jeeves.server.UserSession;
+import jeeves.server.context.ServiceContext;
+import org.fao.geonet.GeonetContext;
+import org.fao.geonet.constants.Edit;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.kernel.SelectionManager;
+import org.fao.geonet.kernel.search.MetaSearcher;
+import org.fao.geonet.kernel.search.SearchManager;
+import org.fao.geonet.services.util.MainUtil;
+import org.jdom.Element;
+
+//=============================================================================
+
+public class SelectionSearch implements Service
+{
+	private ServiceConfig _config;
+
+	//--------------------------------------------------------------------------
+	//---
+	//--- Init
+	//---
+	//--------------------------------------------------------------------------
+
+	public void init(String appPath, ServiceConfig config) throws Exception
+	{
+		_config = config;
+	}
+
+	/*
+	 * Get the current search and add an uuid params
+	 * with the list of records contain in the 
+	 * selection manager. 
+	 * 
+	 */
+	public Element exec(Element params, ServiceContext context) throws Exception
+	{
+		GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
+		
+		SearchManager searchMan = gc.getSearchmanager();
+		
+		// possibly close old searcher
+		UserSession  session     = context.getUserSession();
+		MetaSearcher oldSearcher = (MetaSearcher)session.getProperty(Geonet.Session.SEARCH_RESULT);
+
+		if (oldSearcher != null)
+			oldSearcher.close();
+
+		context.info("Get selected metadata");
+		SelectionManager sm = SelectionManager.getManager(session) ;
+
+		// TODO : Get the sortBy params in order to apply on new result list.
+		
+		if (sm != null) {
+			String uuids= "";
+			boolean first = true;
+			for (Iterator<String> iter = sm.getSelection("metadata").iterator(); iter.hasNext();) {
+				String uuid = (String) iter.next();
+				if (first) {
+					uuids = (String) uuid;
+					first = false;
+				}
+				else 
+					uuids = uuids +" or "+ uuid;
+			}
+			context.debug("List of selected uuids: " + uuids);
+			params.addContent(new Element(Geonet.SearchResult.UUID).setText(uuids));
+			sm.close();
+			sm = null;
+		} 
+		
+		// perform the search and save search result into session
+		MetaSearcher searcher;
+
+		context.info("Creating searchers");
+
+		searcher = searchMan.newSearcher(SearchManager.LUCENE, Geonet.File.SEARCH_LUCENE);
+
+		searcher.search(context, params, _config);
+		session.setProperty(Geonet.Session.SEARCH_RESULT, searcher);
+
+		context.info("Getting summary");
+		
+		return searcher.getSummary();
+		
+	}
+}
+
+//=============================================================================
