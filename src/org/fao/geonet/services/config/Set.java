b27c65ed2d556a739bddffcc3691fb1576c42d63
heikkidoeleman <heikkidoeleman@ff403467-3f20-0410-99e5-ff581e626a08>
2009-10-13 06:54:08 +0000

    Local rating - server side handling
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@5363 ff403467-3f20-0410-99e5-ff581e626a08

--- src/org/fao/geonet/services/config/Set.java
+++ src/org/fao/geonet/services/config/Set.java
@@ -119,6 +119,8 @@ public class Set implements Service
 
 		new ConfigEntry(ConfigEntry.Type.BOOL,   true,  "clickablehyperlinks/enable", "system/clickablehyperlinks/enable"),
 		
+		new ConfigEntry(ConfigEntry.Type.BOOL,   true,  "localrating/enable", "system/localrating/enable"),
+
 		new ConfigEntry(ConfigEntry.Type.BOOL,   true,  "proxy/use",                "system/proxy/use"),
 		new ConfigEntry(ConfigEntry.Type.STRING, false, "proxy/host",               "system/proxy/host"),
 		new ConfigEntry(ConfigEntry.Type.INT,    false, "proxy/port",               "system/proxy/port"),
