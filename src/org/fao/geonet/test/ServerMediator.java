88fe359ca1a3513a1b112251972d6b92fc00584d
justb <justb@ff403467-3f20-0410-99e5-ff581e626a08>
2009-10-28 14:08:32 +0000

    GN test framework: checking in all current work-in-progress (still work needed)
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@5389 ff403467-3f20-0410-99e5-ff581e626a08

--- src/org/fao/geonet/test/ServerMediator.java
+++ src/org/fao/geonet/test/ServerMediator.java
@@ -1,110 +1,116 @@
 package org.fao.geonet.test;
 
-import jeeves.constants.ConfigFile;
-import jeeves.server.local.LocalJeeves;
-import org.fao.gast.lib.DatabaseLib;
-import org.fao.gast.lib.Lib;
-import org.fao.gast.lib.Resource;
-import org.fao.gast.lib.XMLLib;
-import org.fao.geonet.util.McKoiDB;
-import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.Document;
+import org.jdom.xpath.XPath;
+import org.fao.gast.lib.XMLLib;
 
-import java.io.File;
+import java.util.List;
 
 /**
  * Provide single API for communicating with a GN server.
  * <p/>
  * The ServerMediator provides a standard API that the protocol
  * test framework can use to send (dispatch) requests and receive
- * responses. For now only a "local Jeeves" version is available, but
+ * responses. For example a "local Jeeves" version LocalJeevesMediator is available, but
  * this class can easily be extended to e.g. provide a HTTP client to
- * communicate with a real remote GN server.
+ * communicate with a real remote GN server, or basically any HTTP server that
+ * supports XML webservices.
  *
  * @author Just van den Broecke - just@justobjects.nl
  */
-public class ServerMediator {
-	// TODO get credentials from config.xml
-	private static final String MCKOI_USER = "geonetwork";
-	private static final String MCKOI_PASSWORD = "geonetwork";
-	private static String ERR = "none";
+public abstract class ServerMediator {
+	static private String error = "none";
+	static private Element configElm;
+
+	private static ServerMediator serverMediator;
 
 	/**
-	 * Dispatch to server using local Jeeves service request.
+	 * Forward to actual Server Mediator.
 	 *
 	 * @param aRequest a server request
 	 * @return a server response element
 	 */
 	static public Element dispatch(Element aRequest) {
-		return LocalJeeves.dispatch(aRequest);
+		return serverMediator.doDispatch(aRequest);
+	}
+
+	/**
+	 * Return Servermediator XML config element.
+	 *
+	 * @return a config element
+	 */
+	static public Element getConfigElm() {
+		return configElm;
 	}
 
 	static public String getError() {
-		return ERR;
+		return error;
 	}
 
+	/**
+	 * Return specific configuration parameter.
+	 *
+	 * @return a config element
+	 */
+	static public String getConfigParam(String aName) throws Exception {
+		XPath xPath = XPath.newInstance("//param[@name=\"" + aName + "\"]");
+		Element paramElm = (Element) xPath.selectSingleNode(getConfigElm());
+		if (paramElm == null) {
+			throw new IllegalArgumentException("Cannot find config parameter: " + aName);
+		}
+
+		return paramElm.getText().trim();
+	}
+
+	/**
+	 * Creates and initializes actual ServerMedaitor.
+	 */
 	static public void init() throws Exception {
-		// Create a new/fresh DB
-		try {
-			dbCreate();
-		} catch (Throwable t) {
-			ERR = "dbCreate err: " + t;
-			throw new Exception("Error creating DB");
-		}
+		init(TestConfig.getConfigPath() + "/server-mediator.xml");
+	}
 
-		// Setup with GN standard data
-		try {
-			dbFill();
-		} catch (Throwable t) {
-			ERR = "dbFill err: " + t;
-			throw new Exception("Error filling DB");
-		}
+	/**
+	 * Creates and initializes actual ServerMedaitor.
+	 */
+	static public void init(String aMediatorConfigFile) throws Exception {
+		// The ServerMediator config
+		configElm = new XMLLib().load(aMediatorConfigFile).getRootElement();
 
-		// Initialize Jeeves engine locally
-		LocalJeeves.init(TestConfig.getAppPath(), TestConfig.getConfigPath(), TestConfig.getBaseUrl());
+		// Factory: create ServerMediator from class name in config
+		String mediatorClassName = configElm.getChildText("class").trim();
+		serverMediator = (ServerMediator) Class.forName(mediatorClassName).newInstance();
+
+		// Initialize new instance
+		serverMediator.doInit();
 	}
 
 	static public boolean isReady() {
-		return LocalJeeves.isRunning();
+		return serverMediator != null && serverMediator.isRunning();
 	}
 
-	static private void dbCreate() throws Exception {
-		// Remove all McKoi files
-		Lib.io.cleanDir(new File(TestConfig.getDBPath()));
-
-		// Create database files
-		McKoiDB mcKoi = new McKoiDB();
-		mcKoi.setConfigFile(TestConfig.getConfigPath() + "mckoi.conf");
-		mcKoi.create(MCKOI_USER, MCKOI_PASSWORD);
+	static public void setError(String e) {
+		error = e;
 	}
 
-	static private void dbFill() throws Exception {
-		// Open a database connection and setup data.
-		Document config = new XMLLib().load(TestConfig.getConfigPath() + "config.xml");
-		Element dbmsElem = retrieveDbms(config);
+	/**
+	 * Dispatch to server using active ServerMediator.
+	 *
+	 * @param aRequest a server request
+	 * @return a server response element
+	 */
+	abstract protected Element doDispatch(Element aRequest);
 
-		Resource resource = new Resource(TestConfig.getAppPath(), dbmsElem);
-		DatabaseLib database = new DatabaseLib(TestConfig.getHomePath());
+	/**
+	 * Initialize actual ServerMediator.
+	 */
+	abstract protected void doInit() throws Exception;
 
-		// TODO: this also wipes Lucene index (I think) in web/geonetwork/WEB-INF !!
-		Lib.init(TestConfig.getHomePath());
-		database.setup(resource, null);
-	}
-
-	static private Element retrieveDbms(Document config) {
-		Element resources = config.getRootElement().getChild(ConfigFile.Child.RESOURCES);
-
-		for (Object res : resources.getChildren(ConfigFile.Resources.Child.RESOURCE)) {
-			Element resource = (Element) res;
-			String enabled = resource.getAttributeValue("enabled");
-
-			if ("true".equals(enabled))
-				return resource;
-		}
-
-		//--- we should not arrive here
-
-		return null;
-	}
+	/**
+	 * Check if actual ServerMediator is running.
+	 *
+	 * @return true if running
+	 */
+	abstract protected boolean isRunning();
 
 }
