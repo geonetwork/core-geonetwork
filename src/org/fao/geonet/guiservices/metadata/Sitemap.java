0b5bf2aaf24764a11b33ef910f39f6038539c40a
josegar74 <josegar74@ff403467-3f20-0410-99e5-ff581e626a08>
2010-04-26 08:49:47 +0000

    #95
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@6031 ff403467-3f20-0410-99e5-ff581e626a08

---
+++ src/org/fao/geonet/guiservices/metadata/Sitemap.java
@@ -0,0 +1,92 @@
+//==============================================================================
+//===
+//=== Sitemap
+//===
+//=============================================================================
+//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: geonetwork@osgeo.org
+//==============================================================================
+
+package org.fao.geonet.guiservices.metadata;
+
+import jeeves.interfaces.Service;
+import jeeves.resources.dbms.Dbms;
+import jeeves.server.ServiceConfig;
+import jeeves.server.context.ServiceContext;
+import jeeves.utils.Util;
+import org.fao.geonet.GeonetContext;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.kernel.AccessManager;
+import org.jdom.Element;
+
+import java.util.Set;
+
+/**
+ * Create a sitemap document with metadata records
+ *
+ * Formats: xml (default if not specified), html
+ *
+ * See http://www.sitemaps.org/protocol.php
+ */
+public class Sitemap implements Service
+{
+    private final String FORMAT_XML = "xml";
+    private final String FORMAT_HTML = "html";
+
+    public void init(String appPath, ServiceConfig config) throws Exception { }
+
+    public Element exec(Element params, ServiceContext context) throws Exception
+    {
+        String format = Util.getParam(params, "format", FORMAT_XML);
+        if (!((format.equalsIgnoreCase(FORMAT_HTML)) ||
+           (format.equalsIgnoreCase(FORMAT_XML)))) {
+
+            format = FORMAT_XML;
+        }
+
+        GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
+        AccessManager am = gc.getAccessManager();
+
+        Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
+
+        Set<String> groups = am.getUserGroups(dbms, context.getUserSession(), context.getIpAddress());
+
+        String query = "SELECT DISTINCT id, uuid, schemaId, changeDate FROM Metadata, OperationAllowed "+
+                            "WHERE id=metadataId AND isTemplate='n' AND operationId=0 AND (";
+
+        String aux = "";
+
+        for (String grpId : groups)
+            aux += " OR groupId="+grpId;
+
+        query += aux.substring(4);
+        query += ") ORDER BY changeDate DESC";
+
+        Element result = dbms.select(query);
+
+        Element formatEl = new Element("format");
+        formatEl.setText(format.toLowerCase());
+
+        result.addContent(formatEl);
+        
+        return result;
+    }
+}
---
+++ web/geonetwork/xsl/portal-sitemap.xsl
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0"
+	xmlns:geonet="http://www.fao.org/geonetwork">
+
+	<xsl:template match="/root">
+		<xsl:variable name="format" select="response/format"/>
+
+        <urlset
+         xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+         xmlns:geo="http://www.google.com/geo/schemas/sitemap/1.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+          <xsl:for-each select="response/record">
+            <xsl:variable name="uuid" select="uuid"/>
+            <xsl:variable name="schemaid" select="schemaid"/>
+            <xsl:variable name="changedate" select="changedate"/>
+									
+			<xsl:value-of select="$format" />
+
+            <url>
+				<loc>
+				<xsl:choose>
+					<xsl:when test="$format='xml'">
+						<xsl:variable name="metadataUrlValue">
+							<xsl:call-template name="metadataXmlDocUrl">
+								<xsl:with-param name="schemaid" select="$schemaid"/>
+								<xsl:with-param name="uuid" select="$uuid"/>
+							</xsl:call-template>
+						</xsl:variable>
+						http://<xsl:value-of select="/root/gui/env/server/host"/>:<xsl:value-of select="/root/gui/env/server/port"/><xsl:value-of select="/root/gui/locService"/>/<xsl:value-of select="$metadataUrlValue"/>
+					</xsl:when>
+					
+					<xsl:otherwise>
+						http://<xsl:value-of select="/root/gui/env/server/host"/>:<xsl:value-of select="/root/gui/env/server/port"/><xsl:value-of select="/root/gui/url"/>/?uuid=<xsl:value-of select="$uuid"/>									
+					</xsl:otherwise>
+				</xsl:choose>
+				</loc>
+				<lastmod><xsl:value-of select="$changedate"/></lastmod>
+                <geo:geo>
+                    <geo:format><xsl:value-of select="$schemaid"/></geo:format>
+                </geo:geo>
+            </url>
+          </xsl:for-each>
+        </urlset>
+    </xsl:template>
+
+	
+	<xsl:template name="metadataXmlDocUrl">
+		<xsl:param name="schemaid" />
+		<xsl:param name="uuid" />
+		
+		<xsl:choose>
+			<xsl:when test="$schemaid='dublin-core'">dc.xml?uuid=<xsl:value-of select="$uuid"/></xsl:when>
+
+			<xsl:when test="$schemaid='fgdc-std'">fgdc.xml?uuid=<xsl:value-of select="$uuid"/></xsl:when>
+			
+			<xsl:when test="$schemaid='iso19115'">iso19115to19139.xml?uuid=<xsl:value-of select="$uuid"/></xsl:when>
+										
+			<xsl:otherwise>iso19139.xml?uuid=<xsl:value-of select="$uuid"/></xsl:otherwise>
+		</xsl:choose>			
+	</xsl:template>
+</xsl:stylesheet>
