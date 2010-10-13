a03e6a33c41a649112533012b9978ea5fabfdf3b
josegar74 <josegar74@ff403467-3f20-0410-99e5-ff581e626a08>
2010-10-13 09:17:27 +0000

    Added support for sqlserver database. Thanks to Mikael Elmquist for provide sql files
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.6.x@6650 ff403467-3f20-0410-99e5-ff581e626a08

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/2.4.3-to-2.6.0/sqlserver.sql
@@ -0,0 +1,32 @@
+ALTER TABLE Settings ALTER COLUMN value TYPE text;
+ALTER TABLE Metadata ADD displayorder int;
+
+INSERT INTO Settings VALUES (90,1,'selectionmanager',NULL);
+INSERT INTO Settings VALUES (91,90,'maxrecords','1000');
+INSERT INTO Settings VALUES (210,1,'localrating',NULL);
+INSERT INTO Settings VALUES (211,210,'enable','false');
+INSERT INTO Settings VALUES (220,1,'downloadservice',NULL);
+INSERT INTO Settings VALUES (221,220,'leave','false');
+INSERT INTO Settings VALUES (222,220,'simple','true');
+INSERT INTO Settings VALUES (223,220,'withdisclaimer','false');
+INSERT INTO Settings VALUES (230,1,'xlinkResolver',NULL);
+INSERT INTO Settings VALUES (231,230,'enable','false');
+INSERT INTO Settings VALUES (600,1,'indexoptimizer',NULL);
+INSERT INTO Settings VALUES (601,600,'enable','true');
+INSERT INTO Settings VALUES (602,600,'at',NULL);
+INSERT INTO Settings VALUES (603,602,'hour','0');
+INSERT INTO Settings VALUES (604,602,'min','0');
+INSERT INTO Settings VALUES (605,602,'sec','0');
+INSERT INTO Settings VALUES (606,600,'interval',NULL);
+INSERT INTO Settings VALUES (607,606,'day','0');
+INSERT INTO Settings VALUES (608,606,'hour','24');
+INSERT INTO Settings VALUES (609,606,'min','0');
+INSERT INTO Settings VALUES (700,1,'oai',NULL);
+INSERT INTO Settings VALUES (701,700,'mdmode','1');
+INSERT INTO Settings VALUES (702,700,'tokentimeout','3600');
+INSERT INTO Settings VALUES (703,700,'cachesize','60');
+INSERT INTO Settings VALUES (720,1,'inspire',NULL);
+INSERT INTO Settings VALUES (721,720,'enable','false');
+
+UPDATE Settings SET value='2.6.0' WHERE name='version';
+UPDATE Settings SET value='0' WHERE name='subVersion';
