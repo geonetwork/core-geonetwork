7ce8422e65cf9d6bda250ad9ddd53d4586a7ce93
josegar74 <josegar74@ff403467-3f20-0410-99e5-ff581e626a08>
2010-11-04 11:24:47 +0000

    Fix for broken searches.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.6.x@6759 ff403467-3f20-0410-99e5-ff581e626a08

--- web/src/main/java/org/fao/geonet/kernel/search/GeoNetworkAnalyzer.java
+++ web/src/main/java/org/fao/geonet/kernel/search/GeoNetworkAnalyzer.java
@@ -1,46 +1,52 @@
+//===	Copyright (C) 2001-2005 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: GeoNetwork@fao.org
+//==============================================================================
+
 package org.fao.geonet.kernel.search;
 
 import org.apache.lucene.analysis.ASCIIFoldingFilter;
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LowerCaseFilter;
-import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.WhitespaceTokenizer;
 
-import java.io.IOException;
 import java.io.Reader;
 
 /**
- * 
  * Default Lucene analyzer for GeoNetwork. Based on WhitespaceAnalyzer but with added
  * LowercaseFilter and ASCIIFoldingFilter to the tokenstream.
- *
+ * <p/>
  * Reason is that if using Standardanalyzer, like GeoNetwork did before, it tokenizes such that the character * is
  * seen as delimiting. That caused queries with wildcards analyzed using StandardAnalyzer to fail.
- * 
  *
  * @author heikki doeleman
  */
-public final class GeoNetworkAnalyzer extends Analyzer {
-    
-         @Override
-         public TokenStream tokenStream(String fieldName, Reader reader) {
-             TokenStream ts = new WhitespaceTokenizer(reader);
-             ts = new LowerCaseFilter(ts);
-             ts = new ASCIIFoldingFilter(ts);
-             return ts;
-         }
+public final class GeoNetworkAnalyzer extends GeoNetworkReusableAnalyzerBase {
 
-         @Override
-         public TokenStream reusableTokenStream(String fieldName, Reader reader) throws IOException {
-           TokenStream ts = (TokenStream) getPreviousTokenStream();
-           if (ts == null) {
-             ts = tokenStream(null, reader);
-             setPreviousTokenStream(ts);
-           }
-           else {
-             ts.reset();
-           }
-           return ts;
-         }
+    @Override
+    protected TokenStreamComponents createComponents(final String fieldName,
+                                                     final Reader reader) {
+
+        final Tokenizer source = new WhitespaceTokenizer(reader);
+        return new TokenStreamComponents(source, new ASCIIFoldingFilter(new LowerCaseFilter(
+                source)));
+    }
 
 }
