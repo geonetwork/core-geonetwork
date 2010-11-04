e39c5ffef187514b76272184be46db797c4eaf63
josegar74 <josegar74@ff403467-3f20-0410-99e5-ff581e626a08>
2010-11-04 09:40:22 +0000

    Fix to check for broken searches.
    Previous implementation required a reindex after any update in index so the changes were searchable
    
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@6757 ff403467-3f20-0410-99e5-ff581e626a08

--- web/src/main/java/org/fao/geonet/kernel/search/GeoNetworkAnalyzer.java
+++ web/src/main/java/org/fao/geonet/kernel/search/GeoNetworkAnalyzer.java
@@ -5,42 +5,37 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.WhitespaceTokenizer;
+import org.apache.lucene.analysis.Tokenizer;
 
 import java.io.IOException;
 import java.io.Reader;
 
 /**
- * 
+ *
  * Default Lucene analyzer for GeoNetwork. Based on WhitespaceAnalyzer but with added
  * LowercaseFilter and ASCIIFoldingFilter to the tokenstream.
  *
  * Reason is that if using Standardanalyzer, like GeoNetwork did before, it tokenizes such that the character * is
  * seen as delimiting. That caused queries with wildcards analyzed using StandardAnalyzer to fail.
- * 
+ *
  *
  * @author heikki doeleman
  */
 public final class GeoNetworkAnalyzer extends Analyzer {
-    
-         @Override
-         public TokenStream tokenStream(String fieldName, Reader reader) {
-             TokenStream ts = new WhitespaceTokenizer(reader);
-             ts = new LowerCaseFilter(ts);
-             ts = new ASCIIFoldingFilter(ts);
-             return ts;
-         }
 
-         @Override
-         public TokenStream reusableTokenStream(String fieldName, Reader reader) throws IOException {
-           TokenStream ts = (TokenStream) getPreviousTokenStream();
-           if (ts == null) {
-             ts = tokenStream(null, reader);
-             setPreviousTokenStream(ts);
-           }
-           else {
-             ts.reset();
-           }
-           return ts;
-         }
+    @Override
+    public TokenStream tokenStream(String fieldName, Reader reader) {
+        return new ASCIIFoldingFilter(new LowerCaseFilter(new WhitespaceTokenizer(reader)));
+    }
 
+    @Override
+    public TokenStream reusableTokenStream(String fieldName, Reader reader) throws IOException {
+        Tokenizer tokenizer = (Tokenizer) getPreviousTokenStream();
+        if (tokenizer == null) {
+            tokenizer = new WhitespaceTokenizer(reader);
+            setPreviousTokenStream(tokenizer);
+        } else
+            tokenizer.reset(reader);
+        return tokenizer;
+    }
 }
