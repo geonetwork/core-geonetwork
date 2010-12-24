3422db66f904cc5fa16da42655bccdaca07ba495
josegar74 <josegar74@ff403467-3f20-0410-99e5-ff581e626a08>
2010-12-24 15:35:12 +0000

    # 376 : Configurable stopwords
    Updated migration scripts
    
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.6.x@7050 ff403467-3f20-0410-99e5-ff581e626a08

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/2.6.0-to-2.6.2/oracle.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/2.6.0-to-2.6.2/oracle.sql
@@ -29,46 +29,6 @@ ALTER TABLE MetadataNotifications ADD FOREIGN KEY (notifierId) REFERENCES Metada
 INSERT INTO Settings VALUES (85,80,'uidAttr','uid');
 INSERT INTO Settings VALUES (240,1,'autofixing',NULL);
 INSERT INTO Settings VALUES (241,240,'enable','true');
-INSERT INTO Settings VALUES (800,1,'indexlanguages',NULL);
-INSERT INTO Settings VALUES (801,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (802,801,'name','danish');
-INSERT INTO Settings VALUES (803,801,'selected','false');
-INSERT INTO Settings VALUES (804,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (805,804,'name','dutch');
-INSERT INTO Settings VALUES (806,804,'selected','false');
-INSERT INTO Settings VALUES (807,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (808,807,'name','english');
-INSERT INTO Settings VALUES (809,807,'selected','true');
-INSERT INTO Settings VALUES (810,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (811,810,'name','finnish');
-INSERT INTO Settings VALUES (812,810,'selected','false');
-INSERT INTO Settings VALUES (813,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (814,813,'name','french');
-INSERT INTO Settings VALUES (815,813,'selected','false');
-INSERT INTO Settings VALUES (816,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (817,816,'name','german');
-INSERT INTO Settings VALUES (818,816,'selected','false');
-INSERT INTO Settings VALUES (819,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (820,819,'name','hungarian');
-INSERT INTO Settings VALUES (821,819,'selected','false');
-INSERT INTO Settings VALUES (822,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (823,822,'name','italian');
-INSERT INTO Settings VALUES (824,822,'selected','false');
-INSERT INTO Settings VALUES (825,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (826,825,'name','norwegian');
-INSERT INTO Settings VALUES (827,825,'selected','false');
-INSERT INTO Settings VALUES (828,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (829,828,'name','portuguese');
-INSERT INTO Settings VALUES (830,828,'selected','false');
-INSERT INTO Settings VALUES (831,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (832,831,'name','russian');
-INSERT INTO Settings VALUES (833,831,'selected','false');
-INSERT INTO Settings VALUES (834,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (835,834,'name','spanish');
-INSERT INTO Settings VALUES (836,834,'selected','false');
-INSERT INTO Settings VALUES (837,800,'indexlanguage',NULL);
-INSERT INTO Settings VALUES (838,837,'name','swedish');
-INSERT INTO Settings VALUES (839,837,'selected','false');
 
 REM == 2.6.2 changes
 
@@ -84,6 +44,16 @@ CREATE TABLE CswServerCapabilitiesInfo
 
 ALTER TABLE CswServerCapabilitiesInfo ADD FOREIGN KEY (langId) REFERENCES Languages (id);
 
+CREATE TABLE IndexLanguages
+  (
+    id            int,
+    languageName  varchar(32)   not null,
+    selected      char(1)       default 'n' not null,
+
+    primary key(id, languageName)
+
+  );
+  
 ALTER TABLE Languages ADD isocode varchar(3);
 
 UPDATE Languages SET isocode = 'eng' where id ='en';
@@ -128,6 +98,19 @@ INSERT INTO CswServerCapabilitiesInfo VALUES (30, 'ru', 'abstract', '');
 INSERT INTO CswServerCapabilitiesInfo VALUES (31, 'ru', 'fees', '');
 INSERT INTO CswServerCapabilitiesInfo VALUES (32, 'ru', 'accessConstraints', '');
 
+INSERT INTO IndexLanguages VALUES (1, 'danish', 'n');
+INSERT INTO IndexLanguages VALUES (2, 'dutch', 'n');
+INSERT INTO IndexLanguages VALUES (3, 'english', 'y');
+INSERT INTO IndexLanguages VALUES (4, 'finnish', 'n');
+INSERT INTO IndexLanguages VALUES (5, 'french', 'n');
+INSERT INTO IndexLanguages VALUES (6, 'german', 'n');
+INSERT INTO IndexLanguages VALUES (7, 'hungarian', 'n');
+INSERT INTO IndexLanguages VALUES (8, 'italian', 'n');
+INSERT INTO IndexLanguages VALUES (9, 'norwegian', 'n');
+INSERT INTO IndexLanguages VALUES (10, 'portuguese', 'n');
+INSERT INTO IndexLanguages VALUES (11, 'russian', 'n');
+INSERT INTO IndexLanguages VALUES (12, 'spanish', 'n');
+INSERT INTO IndexLanguages VALUES (13, 'swedish', 'n');
 
 UPDATE Settings SET value='2.6.2' WHERE name='version';
 UPDATE Settings SET value='0' WHERE name='subVersion';
