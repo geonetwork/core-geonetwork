a1c076e9f9d8f8fbf3f5c2f6158129a88a16eee4
heikkidoeleman <heikkidoeleman@ff403467-3f20-0410-99e5-ff581e626a08>
2011-01-10 16:03:57 +0000

    Changed to use Lucene instead of SQL
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.6.x@7108 ff403467-3f20-0410-99e5-ff581e626a08

--- web/src/main/java/org/fao/geonet/guiservices/metadata/GetByOwner.java
+++ web/src/main/java/org/fao/geonet/guiservices/metadata/GetByOwner.java
@@ -1,115 +1,117 @@
 package org.fao.geonet.guiservices.metadata;
 
-import jeeves.exceptions.OperationNotAllowedEx;
+import jeeves.constants.Jeeves;
 import jeeves.interfaces.Service;
-import jeeves.resources.dbms.Dbms;
 import jeeves.server.ServiceConfig;
 import jeeves.server.context.ServiceContext;
 import org.fao.geonet.GeonetContext;
+import org.fao.geonet.constants.Edit;
 import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.kernel.search.LuceneSearcher;
+import org.fao.geonet.kernel.search.SearchManager;
 import org.jdom.Element;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
- * Retrieves the metadata owned by a user. Depending on user profile:
  *
- *   - Administrator: returns all metadata
- *   - Reviewer, UserAdmin: returns all metadata owned by the user's groups
- *   - Editor: return metadata owned by the user
- *   - Other profiles: throw exception OperationNotAllowedEx
- *
- * Service parameters:
- *
- *   - sortBySelect: allowed values (date, popularity, rating) (optional parameter)
+ * Retrieve all metadata editable by the current user.
  *
  */
 public class GetByOwner implements Service {
-        
-    private static final String SORT_BY       = "sortBy";
 
-    private Element _response;
+	private ServiceConfig _config;
 
-    //--------------------------------------------------------------------------
-    //---
-    //--- Init
-    //---
-    //--------------------------------------------------------------------------
+	public void init(String appPath, ServiceConfig config) throws Exception {
+		_config = config;
+	}
 
-    public void init(String appPath, ServiceConfig config) throws Exception {
-    }
+	public Element exec(Element params, ServiceContext context) throws Exception {
 
-    //--------------------------------------------------------------------------
-    //---
-    //--- Service
-    //---
-    //--------------------------------------------------------------------------
+		Element request = makeSearchRequest(params, context);
+		GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
+		SearchManager searchMan = gc.getSearchmanager();
 
-    public Element exec(Element params, ServiceContext context) throws Exception {
-        GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
-        Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
+        LuceneSearcher searcher = null;
+        try {
+            searcher = (LuceneSearcher) searchMan.newSearcher(SearchManager.LUCENE, Geonet.File.SEARCH_LUCENE);
 
-        String query = null;
+            searcher.search(context, request, _config);
 
-        String ownerId = context.getUserSession().getUserId();
-        String userProfile = context.getUserSession().getProfile();
-        String userId = context.getUserSession().getUserId();
+			Element presentRequest = new Element("request");
+            presentRequest.addContent(new Element("fast").setText("true"));
+			presentRequest.addContent(new Element("from").setText("1"));
+			presentRequest.addContent(new Element("to").setText(searcher.getSize()+""));
 
-        if (userProfile == null) {
-           throw new OperationNotAllowedEx("Unauthorized user attempted to list editable metadata ");
+			List<Element> results = searcher.present(context, presentRequest, _config).getChildren();
+            List<Element> responseList = new ArrayList<Element>();
+
+            for (Element element : results) {
+                if(element.getName().equals("summary")) {
+                    continue;
+                }
+                Element info = element.getChild(Edit.RootChild.INFO, Edit.NAMESPACE);
+                String id = info.getChildText(Edit.Info.Elem.ID);
+                if (id != null && id != "") {
+                    Element md = gc.getDataManager().getMetadata(context, id, false);
+                    responseList.add(md);
+                }
+            }
+            Element response = new Element("response");
+            for (Iterator<Element> i = responseList.iterator(); i.hasNext();) {
+                response.addContent(i.next());
+            }
+            return response;
         }
-
-        // if the user is an admin, return all metadata
-        if(userProfile.equals(Geonet.Profile.ADMINISTRATOR)) {
-            query = "SELECT id FROM Metadata WHERE isHarvested='n'" ;
+        finally {
+            if (searcher != null) searcher.close();
         }
-        // if the user is a reviewer, return all metadata of the user's groups
-        else if(userProfile.equals(Geonet.Profile.REVIEWER) || userProfile.equals(Geonet.Profile.USER_ADMIN)) {
-            query = "SELECT id FROM Metadata "+
-                        "WHERE groupOwner IN (SELECT groupId FROM UserGroups WHERE userId='"+ownerId+
-                        "' AND isHarvested='n')" ;
-        }
-        // if the user is an editor, return metadata owned by this user
-        else if(userProfile.equals(Geonet.Profile.EDITOR) ) {
-            query = "SELECT id FROM Metadata WHERE owner="+ownerId+
-                        " AND isHarvested='n'" ;
-        } else {
-            throw new OperationNotAllowedEx("Unauthorized user " + userId + " attempted to list editable metadata ");
-        }
+	}
 
-        // Sorting
-        String sortBy = sortByParameter(params);
+	private Element makeSearchRequest(Element params, ServiceContext ctx) throws Exception {
 
-        if(sortBy.equals("date")) {
-            query += " ORDER BY changeDate DESC";
-        }
-        else if(sortBy.equals("popularity")) {
-            query += " ORDER BY popularity DESC";
-        }
-        else if(sortBy.equals("rating")) {
-            query += " ORDER BY rating DESC";
-        }
+		// create a search request
+        Element request = new Element(Jeeves.Elem.REQUEST);
 
-        Element result = dbms.select(query);
+        // exclude harvested metadata
+        request.addContent(new Element("isHarvested").setText("n"));
 
-        _response = new Element("response");
+        // include template metadata
+        request.addContent(new Element("template").setText("n"));
+        request.addContent(new Element("template").setText("y"));
+        request.addContent(new Element("template").setText("s"));
 
-        for (Iterator iter = result.getChildren().iterator(); iter.hasNext();) {
-            Element rec = (Element)iter.next();
-            String  id = rec.getChildText("id");
-            Element md = gc.getDataManager().getMetadata(context, id, false);
-            _response.addContent(md);
-        }
+        // restrict to editable metadata
+        request.addContent(new Element("editable").setText("true"));
 
-        Element currentSortBySelect = new Element(SORT_BY);
-        currentSortBySelect.setText(sortBy);
-        _response.addContent(currentSortBySelect);
-        Element response = (Element) _response.clone();
-        return response;
-    }
+        //
+        // sorting
+        //
+
+		String sortBy = sortByParameter(params);
+
+		if ("title".equals(sortBy)) {
+			Element sortOrder = request.getChild("sortOrder");
+			if (sortOrder == null) {
+				sortOrder = new Element("sortOrder");
+				request.addContent(sortOrder);
+			}
+			sortOrder.setText("reverse");
+		}
+		Element sortByElement = request.getChild("sortBy");
+		if (sortByElement == null) {
+			sortByElement = new Element("sortBy");
+			request.addContent(sortByElement);
+		}
+		sortByElement.setText(sortBy);
+
+		return request;
+	}
 
     private String sortByParameter(Element params) {
-		Element sortByEl = params.getChild(SORT_BY);
+		Element sortByEl = params.getChild(Geonet.SearchResult.SORT_BY);
 		String sortBy = null;
 		if (sortByEl == null) {
 			sortBy = "date";
