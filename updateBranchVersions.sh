b38de73088a4bbb6185fe08173826acc74b3b0a4
josegar74 <josegar74@ff403467-3f20-0410-99e5-ff581e626a08>
2011-01-13 08:59:16 +0000

    Update version in installer file. Change in sed parameters.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@7141 ff403467-3f20-0410-99e5-ff581e626a08

--- updateBranchVersions.sh
+++ updateBranchVersions.sh
@@ -3,12 +3,18 @@
 # Usage to update branch from a release 2.6.2 version to 2.6.3-SNAPSHOT version 		
 # In root folder of branch code: ./updateBranchVersion.sh 2.6.2 2.6.3
 
+# Note: In MacOs change seed -i to seed -i .bak to work properly
+
 version="$1"
 new_version="$2"
 
 # Update version in sphinx doc files
-sed -i .bak  "s/${version}/${new_version}-SNAPSHOT/g" docs/eng/users/source/conf.py 
-sed -i .bak  "s/${version}/${new_version}-SNAPSHOT/g" docs/eng/developer/source/conf.py
+sed -i "s/${version}/${new_version}-SNAPSHOT/g" docs/eng/users/source/conf.py 
+sed -i "s/${version}/${new_version}-SNAPSHOT/g" docs/eng/developer/source/conf.py
+
+# Update installer
+sed -i "s/\<property name=\"version\" value=\"${version}\" \/\>/\<property name=\"version\" value=\"${new_version}\" \/\>/g" installer/build.xml
+sed -i "s/\<property name=\"subVersion\" value=\"0\" \/\>/\<property name=\"subVersion\" value=\"SNAPSHOT\" \/\>/g" installer/build.xml
 
 # Update version pom files
-find . -name pom.xml -exec sed -i .bak "s/${version}/${new_version}-SNAPSHOT/g" {} \;
+find . -name pom.xml -exec sed -i "s/${version}/${new_version}-SNAPSHOT/g" {} \;
--- updateReleaseVersions.sh
+++ updateReleaseVersions.sh
@@ -3,11 +3,16 @@
 # Usage to create 2.6.2 release version from 2.6.2-SNAPSHOT
 # In root folder of branch code: ./updateReleaseVersion.sh 2.6.2
 
+# Note: In MacOs change seed -i to seed -i .bak to work properly
+
 version="$1"
 
 # Update version in sphinx doc files
-sed -i .bak  "s/${version}-SNAPSHOT/${version}/g" docs/eng/users/source/conf.py 
-sed -i .bak  "s/${version}-SNAPSHOT/${version}/g" docs/eng/developer/source/conf.py
+sed -i "s/${version}-SNAPSHOT/${version}/g" docs/eng/users/source/conf.py 
+sed -i "s/${version}-SNAPSHOT/${version}/g" docs/eng/developer/source/conf.py
+
+# Update installer
+sed -i "s/\<property name=\"subVersion\" value=\"SNAPSHOT\" \/\>/\<property name=\"subVersion\" value=\"0\" \/\>/g" installer/build.xml
 
 # Update version pom files
-find . -name pom.xml -exec sed -i .bak "s/${version}-SNAPSHOT/${version}/g" {} \;
+find . -name pom.xml -exec sed -i "s/${version}-SNAPSHOT/${version}/g" {} \;
