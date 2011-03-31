09b1d94537d534af24977f765bfd66216ae6c565
heikkidoeleman <heikkidoeleman@ff403467-3f20-0410-99e5-ff581e626a08>
2011-03-31 13:45:00 +0000

    #491 - Custom ElementSet in CSW 2.0.2
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.6.x@7581 ff403467-3f20-0410-99e5-ff581e626a08

--- web/src/main/java/org/fao/geonet/kernel/csw/services/getrecords/SearchController.java
+++ web/src/main/java/org/fao/geonet/kernel/csw/services/getrecords/SearchController.java
@@ -274,7 +274,8 @@ public class SearchController
                 "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns:csw=\"http://www.opengis.net/cat/csw/2.0.2\" xmlns:gmd=\"http://www.isotc211.org/2005/gmd\" xmlns:gco=\"http://www.isotc211.org/2005/gco\" xmlns:srv=\"http://www.isotc211.org/2005/srv\" xmlns:ows=\"http://www.opengis.net/ows\" xmlns:geonet=\"http://www.fao.org/geonetwork\">\n" +
                 "<xsl:output indent=\"yes\"/>\n" +
                 "<xsl:param name=\"displayInfo\"/>\n" +
-                "<xsl:template match=\"gmd:MD_Metadata|*[@gco:isoType='gmd:MD_Metadata']\">\n" +
+                //"<xsl:template match=\"gmd:MD_Metadata|*[@gco:isoType='gmd:MD_Metadata']\">\n" +
+                "<xsl:template match=\"/*\">\n" +
                 "<xsl:variable name=\"info\" select=\"geonet:info\"/>\n" +
                 "<xsl:copy>\n";
 
