c4abc9a8921ace303343d6b38274d103216f4461
fxprunayre <fxprunayre@ff403467-3f20-0410-99e5-ff581e626a08>
2011-09-27 09:40:17 +0000

    Add harvester history table to migration step for 2.7.0
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@8225 ff403467-3f20-0410-99e5-ff581e626a08

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/270/migrate-db-postgis.sql
@@ -0,0 +1,16 @@
+CREATE TABLE HarvestHistory
+  (
+    id             int not null,
+    harvestDate    varchar(30),
+        harvesterUuid  varchar(250),
+        harvesterName  varchar(128),
+        harvesterType  varchar(128),
+    deleted        char(1) default 'n' not null,
+    info           text,
+    params         text,
+
+    primary key(id)
+
+  );
+
+CREATE INDEX HarvestHistoryNDX1 ON HarvestHistory(harvestDate);
