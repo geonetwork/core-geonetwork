548f9a2bf5eaeef6ff639afb0fe2292ef85e0d9f
fxprunayre <fxprunayre@ff403467-3f20-0410-99e5-ff581e626a08>
2012-02-16 13:08:58 +0000

    Widgets / add options to have list of selection as a menu. #774. Thanks Jean Pommier for the patch.
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@8698 ff403467-3f20-0410-99e5-ff581e626a08

--- web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/widgets/MetadataResultsToolbar.js
+++ web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/widgets/MetadataResultsToolbar.js
@@ -48,9 +48,14 @@ GeoNetwork.MetadataResultsToolbar = Ext.extend(Ext.Toolbar, {
     metadataResultsView: undefined,
     
     /** property: config[mdSelectionInfoCmp] 
-     *  Component to use to display selection information
+     *  Component to use to display selection information (number of selected records)
      */
     mdSelectionInfoCmp: undefined,
+    
+    /** api: config[mdSelectionAsMenu] 
+     * ``boolean`` Display metadata selection with a menu or a set of button.
+     */
+    mdSelectionAsMenu: true,
     
     /** api: config[searchBtCmp] 
      *  Search button component use to trigger search
@@ -393,24 +398,39 @@ GeoNetwork.MetadataResultsToolbar = Ext.extend(Ext.Toolbar, {
             scope: this
         });
         
-        /* Selection information status */
-        var mdSelectionInfoCmp = {
-            id: 'md-selection-info',
-            xtype: 'tbtext',
-            text: OpenLayers.i18n('noneSelected')
-        };
         this.catalogue.on('selectionchange', this.updateSelectionInfo, this);
         
-        return [mdSelectionInfoCmp, '|', {
-            xtype: 'tbtext',
-            text: OpenLayers.i18n('select')
-        }, selectAllInPageAction, {
-            xtype: 'tbtext',
-            text: ', '
-        }, selectAllAction, {
-            xtype: 'tbtext',
-            text: ', '
-        }, selectNoneAction];
+        /* Selection information status as a combo or a set of button */
+        if (this.mdSelectionAsMenu) {
+            return [{
+                id: 'md-selection-info',
+                text: OpenLayers.i18n('noneSelected'),
+                menu : {
+                    xtype: 'menu',
+                    items: [
+                        '<b class="menu-title">' + OpenLayers.i18n('select') + '</b>',
+                        selectAllAction, 
+                        selectAllInPageAction, 
+                        selectNoneAction]
+                }
+            }];
+        } else {
+            return [{
+                id: 'md-selection-info',
+                xtype: 'tbtext',
+                text: OpenLayers.i18n('noneSelected')
+            }, '|', {
+                xtype: 'tbtext',
+                text: OpenLayers.i18n('select')
+            }, selectAllInPageAction, {
+                xtype: 'tbtext',
+                text: ', '
+            }, selectAllAction, {
+                xtype: 'tbtext',
+                text: ', '
+            }, selectNoneAction
+            ];
+        }
     },
     /** api: method[updateSelectionInfo] 
      *  Update selection and selection information
