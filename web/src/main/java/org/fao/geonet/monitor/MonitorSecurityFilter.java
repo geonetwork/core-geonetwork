3ac42a36e60952fb5683e94aa2c93eb13c02197d
jesseeichar <jesseeichar@ff403467-3f20-0410-99e5-ff581e626a08>
2012-06-05 18:44:50 +0000

    Added a new profile 'Monitor'
    A 'Monitor' is a user with the Monitor profile which is designed to allow a user
    The logic is essentially: some monitoring can be expensive and a good vector for
    be secured.  With this new Profile (Administrator extends this profile) we can h
    monitor but not other activities.  It is not in the normal straight profile hier
    capabilities
    
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/branches/2.8.x@9182 ff403467-3f20-0410-99e5-ff581e626a08

--- web/src/main/java/org/fao/geonet/monitor/MonitorSecurityFilter.java
+++ web/src/main/java/org/fao/geonet/monitor/MonitorSecurityFilter.java
@@ -1,6 +1,8 @@
 package org.fao.geonet.monitor;
 
 import jeeves.server.UserSession;
+import jeeves.utils.Log;
+
 import org.fao.geonet.constants.Geonet;
 
 import javax.servlet.*;
@@ -16,9 +18,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * User: jeichar
- * Date: 3/29/12
- * Time: 9:04 PM
+ * User: jeichar Date: 3/29/12 Time: 9:04 PM
  */
 public class MonitorSecurityFilter implements Filter {
     private Set<String> whiteList;
@@ -45,14 +45,16 @@ public class MonitorSecurityFilter implements Filter {
             HttpSession httpSession = servletRequest.getSession(false);
             if (httpSession != null) {
                 UserSession userSession = (UserSession) httpSession.getAttribute("session");
-                if (userSession != null && Geonet.Profile.ADMINISTRATOR.equals(userSession.getProfile())) {
+                if (userSession != null &&
+                        (Geonet.Profile.ADMINISTRATOR.equals(userSession.getProfile()) 
+                                || Geonet.Profile.MONITOR.equals(userSession.getProfile()))) {
                     chain.doFilter(request, response);
                     return;
                 }
             }
         }
 
-        final String errorMessage = "Only administrator is permitted to access monitoring data";
+        final String errorMessage = "Only administrator or monitor are permitted to access monitoring data";
         if (response instanceof HttpServletResponse) {
             HttpServletResponse servletResponse = (HttpServletResponse) response;
             servletResponse.sendError(401, errorMessage);
@@ -68,16 +70,18 @@ public class MonitorSecurityFilter implements Filter {
         InetAddress[] remoteHostAddresses = InetAddress.getAllByName(servletRequest.getRemoteHost());
 
         for (String host : whiteList) {
-        	InetAddress[] acceptedAddresses = InetAddress.getAllByName(host);
-
-        	for (InetAddress acceptedAddress : acceptedAddresses) {
-				for (InetAddress remoteHostAddress : remoteHostAddresses) {
-		            if (remoteHostAddress.equals(acceptedAddress)) {
-		                return true;
-		            }
-					
-				}
-			}
+            try {
+                InetAddress[] acceptedAddresses = InetAddress.getAllByName(host);
+                for (InetAddress acceptedAddress : acceptedAddresses) {
+                    for (InetAddress remoteHostAddress : remoteHostAddresses) {
+                        if (remoteHostAddress.equals(acceptedAddress)) {
+                            return true;
+                        }
+                    }
+                }
+            } catch (UnknownHostException e) {
+                Log.error(Log.MONITOR, host + " is not a valid host.  MonitorSecurityFilter's configuration in web.xml is not valid", e);
+            }
         }
         return false;
     }
