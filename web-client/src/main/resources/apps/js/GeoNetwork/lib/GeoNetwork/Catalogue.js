0601475ddb771851eff1180971b34c42ce8ad8ad
fxprunayre <fxprunayre@ff403467-3f20-0410-99e5-ff581e626a08>
2012-06-13 08:31:44 +0000

    Widgets / PermalinkProvider overrides CookieProvider (#945)
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@9248 ff403467-3f20-0410-99e5-ff581e626a08

--- web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/Catalogue.js
+++ web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/Catalogue.js
@@ -1074,7 +1074,6 @@ GeoNetwork.Catalogue = Ext.extend(Ext.util.Observable, {
             return false;
         } else {
             // Reset user cookie information
-            var cookie = Ext.state.Manager.getProvider();
             if (cookie) {
                 cookie.set('user', undefined);
             }
