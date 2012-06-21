2bd74b8740e1cc9f1fb8bc8e910a30ab94195c42
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-06-21 09:03:03 +0200

    fixed file parsing so it parses last line

--- monitoring/nagios/healthcheck.sh
+++ monitoring/nagios/healthcheck.sh
@@ -2,6 +2,12 @@
 
 #set -x
 
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
+
 HOST=$1
 CHECK=$2
 USERNAME=$3
@@ -19,15 +25,20 @@ fi
 curl -s -c /tmp/cookie "$HOST/geonetwork/srv/eng/user.login?username=$USERNAME&password=$PASSWORD" -o /dev/null
 
 CODE=`curl -sL --cookie /tmp/cookie -w "%{http_code}\\n" "$HOST/$CHECK" -o $OUT`
-
 if [ "$CODE" == "200" ]; then 
-    exit 0
-else 
-    exec<$OUT
+    exit $STATE_OK
+else
+    
+    # Add line to file so that last line is processed by while loop
+    echo "" >> $OUT
+    
     FAILURE=""
     while read line; do
         if grep -v -q ": OK" <<<$line ; then
             FAILURE="$FAILURE || $line";
         fi
-    done
+    done < $OUT
+    
+    echo $FAILURE
+    exit $STATE_CRITICAL
 fi
