341828816f1f72a8adc591c3a2b8243fde094ad0
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-06-21 13:17:18 +0200

    got correct jeeves

--- jeeves/src/main/java/jeeves/server/ConfigurationOverrides.java
+++ jeeves/src/main/java/jeeves/server/ConfigurationOverrides.java
@@ -752,12 +752,7 @@ public class ConfigurationOverrides {
                         if (file.exists()) {
                             in = new FileInputStream(file);
                         } else {
-                        	file = new File(resource.replace('/', '\\'));
-                        	if(file.exists()) {
-                        		in = new FileInputStream(file);
-                        	} else {
-                        		throw new IllegalArgumentException("The resource file " + resource + " is not a file and not a web resource: " + url + ".  Perhaps a leading / was forgotten?");
-                        	}
+                            throw new IllegalArgumentException("The resource file " + resource + " is not a file and not a web resource: " + url + ".  Perhaps a leading / was forgotten?");
                         }
                     } else {
                         in = url.openStream();
