3efac24343ddb6df7bb05cc542beb3d1a86bbcbf
Craig Jones <Craig.Jones@utas.edu.au>
2012-06-22 11:59:28 +1000

    Return metadata permissions when using index search (#963)

--- web/src/main/java/org/fao/geonet/kernel/DataManager.java
+++ web/src/main/java/org/fao/geonet/kernel/DataManager.java
@@ -2893,22 +2893,6 @@ public class DataManager {
 		if (version != null)
 			addElement(info, Edit.Info.Elem.VERSION, version);
 
-		// add operations
-		Element operations = accessMan.getAllOperations(context, id, context.getIpAddress());
-		Set<String> hsOper = accessMan.getOperations(context, id, context.getIpAddress(), operations);
-
-		addElement(info, Edit.Info.Elem.VIEW,     			String.valueOf(hsOper.contains(AccessManager.OPER_VIEW)));
-		addElement(info, Edit.Info.Elem.NOTIFY,   			String.valueOf(hsOper.contains(AccessManager.OPER_NOTIFY)));
-		addElement(info, Edit.Info.Elem.DOWNLOAD, 			String.valueOf(hsOper.contains(AccessManager.OPER_DOWNLOAD)));
-		addElement(info, Edit.Info.Elem.DYNAMIC,  			String.valueOf(hsOper.contains(AccessManager.OPER_DYNAMIC)));
-		addElement(info, Edit.Info.Elem.FEATURED, 			String.valueOf(hsOper.contains(AccessManager.OPER_FEATURED)));
-
-
-		if (!hsOper.contains(AccessManager.OPER_DOWNLOAD)) {
-			boolean gDownload = Xml.selectNodes(operations, "guestoperations/record[operationid="+AccessManager.OPER_DOWNLOAD+" and groupid='-1']").size() == 1;
-			addElement(info, Edit.Info.Elem.GUEST_DOWNLOAD, gDownload+"");
-		}
-
 		buildExtraMetadataInfo(context, id, info);
 
         if(accessMan.isVisibleToAll(dbms, id)) {
@@ -3023,6 +3007,21 @@ public class DataManager {
 		if (accessMan.isOwner(context, id)) {
 			addElement(info, Edit.Info.Elem.OWNER, "true");
 		}
+
+		Element operations = accessMan.getAllOperations(context, id, context.getIpAddress());
+		Set<String> hsOper = accessMan.getOperations(context, id, context.getIpAddress(), operations);
+
+		addElement(info, Edit.Info.Elem.VIEW,     			String.valueOf(hsOper.contains(AccessManager.OPER_VIEW)));
+		addElement(info, Edit.Info.Elem.NOTIFY,   			String.valueOf(hsOper.contains(AccessManager.OPER_NOTIFY)));
+		addElement(info, Edit.Info.Elem.DOWNLOAD, 			String.valueOf(hsOper.contains(AccessManager.OPER_DOWNLOAD)));
+		addElement(info, Edit.Info.Elem.DYNAMIC,  			String.valueOf(hsOper.contains(AccessManager.OPER_DYNAMIC)));
+		addElement(info, Edit.Info.Elem.FEATURED, 			String.valueOf(hsOper.contains(AccessManager.OPER_FEATURED)));
+
+		if (!hsOper.contains(AccessManager.OPER_DOWNLOAD)) {
+			boolean gDownload = Xml.selectNodes(operations, "guestoperations/record[operationid="+AccessManager.OPER_DOWNLOAD+" and groupid='-1']").size() == 1;
+			addElement(info, Edit.Info.Elem.GUEST_DOWNLOAD, gDownload+"");
+		}
+
 	}
 
     /**
