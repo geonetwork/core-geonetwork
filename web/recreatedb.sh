f25d25603a975c0a9a6d9f32dd4db25c6882a19f
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-06-22 21:59:03 +0200

    all data added on startup now to make build easier

--- web/recreatedb.sh
+++ web/recreatedb.sh
@@ -25,9 +25,9 @@ cp -R src/main/webapp/WEB-INF/data/codelist $HOME/gc_data/config/
 cp -R src/main/webapp/WEB-INF/data/config/schema_plugins $HOME/gc_data/config/
 
 
-psql -d $DB -c "ALTER TABLE geometry_columns OWNER TO \"www-data\";"
-psql -d $DB -c "ALTER TABLE spatial_ref_sys OWNER TO \"www-data\";"
+#psql -d $DB -c "ALTER TABLE geometry_columns OWNER TO \"www-data\";"
+#psql -d $DB -c "ALTER TABLE spatial_ref_sys OWNER TO \"www-data\";"
 
-for f in `ls sql/*.sql` ; do psql -f $f -d $DB -U www-data > /dev/null; done
+#for f in `ls sql/*.sql` ; do psql -f $f -d $DB -U www-data > /dev/null; done
 
-psql -f geometry_columns.sql -d $DB -U www-data > /dev/null
+#psql -f geometry_columns.sql -d $DB -U www-data > /dev/null
