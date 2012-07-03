f1294d5634084dfd41a36b70047f1071e632c4ae
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-07-03 15:10:51 +0200

    added more log options

--- jeeves/src/main/java/jeeves/server/dispatchers/guiservices/XmlFile.java
+++ jeeves/src/main/java/jeeves/server/dispatchers/guiservices/XmlFile.java
@@ -111,7 +111,8 @@ public class XmlFile implements GuiService
 		try {
 			result = (Element)xmlCache.get().clone();
 		} catch (Exception e) {
-            Log.error(Log.RESOURCES, "Error cloning the cached data",e);
+            Log.error(Log.RESOURCES, "Error cloning the cached data");
+            Log.debug(Log.RESOURCES, "Error cloning the cached data",e);
 			String xmlDefaultLangFilePath = rootPath + File.separator + defaultLang + File.separator + file;
 			xmlCache = new XmlFileCacher(new File(xmlDefaultLangFilePath),servletContext, appPath);
             xmlCaches.put(language, xmlCache);
