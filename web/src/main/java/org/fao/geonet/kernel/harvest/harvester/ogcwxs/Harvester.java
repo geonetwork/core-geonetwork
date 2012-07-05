c9ae790bb49aca6f0132da8fb9862a518da9d618
Craig Jones <Craig.Jones@utas.edu.au>
2012-07-05 14:25:11 +1000

    Fix null pointer exception when harvesting from a WMS server with non-requestable category layers (i.e. layers without a name element)

--- web/src/main/java/org/fao/geonet/kernel/harvest/harvester/ogcwxs/Harvester.java
+++ web/src/main/java/org/fao/geonet/kernel/harvest/harvester/ogcwxs/Harvester.java
@@ -500,13 +500,19 @@ class Harvester
 		boolean loaded 		= false;
 		
 		if (params.ogctype.substring(0,3).equals("WMS")) {
-
+			Element name;
 			if (params.ogctype.substring(3,8).equals("1.3.0")) {
 				Namespace wms = Namespace.getNamespace("http://www.opengis.net/wms");
-				reg.name 	= layer.getChild ("Name", wms).getValue ();
+				name = layer.getChild ("Name", wms);
 			} else {
-				reg.name 	= layer.getChild ("Name").getValue ();
+				name = layer.getChild ("Name");
 			}
+			//--- For the moment, skip non-requestable category layers
+			if (name == null || name.getValue().trim().equals("")) {
+				log.info("  - skipping layer with no name element");
+				return null;
+			}
+			reg.name = name.getValue();
 		} else if (params.ogctype.substring(0,3).equals("WFS")) {
 			Namespace wfs = Namespace.getNamespace("http://www.opengis.net/wfs");
 			reg.name 	= layer.getChild ("Name", wfs).getValue ();
