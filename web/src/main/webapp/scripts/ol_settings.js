9ffe5051432af8bc2c006cabdd4d4ab93da91060
ianwallen <ianwallen@hotmail.com>
2012-07-10 11:35:33 -0700

    Added some missing ext js locales (CA, RU, PT, CN, FI and NO) that seems to be missing - This corrects the calendar so that the proper language is displayed. This issues was discovered while patching Ticket #986

--- web/src/main/webapp/scripts/ol_settings.js
+++ web/src/main/webapp/scripts/ol_settings.js
@@ -16,9 +16,13 @@ OpenLayers.Util.onImageLoadErrorColor = "transparent";
  */
 function getOpenlayersLangCode() {
     switch(Env.lang) {
+        case"cat":a="ca";
+            break;
         case"dut":a="nl";
             break;
         case"eng":a="en";
+            break;
+        case"fin":a="fi";
             break;
         case"fre":a="fr";
             break;
@@ -27,6 +31,8 @@ function getOpenlayersLangCode() {
         case"ita":a="it";
             break;
         case"spa":a="es";
+            break;
+        case"nor":a="no";
             break;
         case"rus":a="ru";
             break;
