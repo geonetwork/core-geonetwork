48a2f68b1ad56f949a99284a88dc768b60ead84a
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-08-02 11:45:42 +0200

    added option to specify bean factory post processors

--- jeeves/src/main/java/jeeves/config/springutil/JeevesContextLoaderListener.java
+++ jeeves/src/main/java/jeeves/config/springutil/JeevesContextLoaderListener.java
@@ -5,21 +5,51 @@ import java.io.File;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
+import jeeves.utils.Log;
+
 import org.geonetwork.config.MigrateConfiguration;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.Lifecycle;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class JeevesContextLoaderListener extends ContextLoaderListener {
-	public JeevesContextLoaderListener() {
-		System.out.println("Created JeevesContextLoader");
-	}
 	
-	
+	private static final String POST_PROCESSOR_INIT_PARAM = "applicationContextPostProcessors";
 	@Override
 	protected void customizeContext(ServletContext servletContext,
 			ConfigurableWebApplicationContext applicationContext) {
+		processBeanFactoryPostProcessorParam(applicationContext, servletContext.getInitParameter(POST_PROCESSOR_INIT_PARAM));
+		String baseURL = servletContext.getContextPath();
+		String webappName;
+		if(baseURL.length()>1) {
+			webappName = baseURL.substring(1)+".";
+		} else {
+			webappName = "";
+		}
+		String key = webappName+POST_PROCESSOR_INIT_PARAM;
+		String param = System.getProperty(key);
+		if(param != null) {
+			processBeanFactoryPostProcessorParam(applicationContext, param);
+		} else {
+			processBeanFactoryPostProcessorParam(applicationContext, "geonetwork."+POST_PROCESSOR_INIT_PARAM);
+		}
+	}
+
+	private void processBeanFactoryPostProcessorParam(
+			ConfigurableWebApplicationContext applicationContext, String param) {
+		if (param != null) {
+		for (String className: param.split(",")) {
+			try {
+				Class<?> class1 = Class.forName(className.trim());
+				BeanFactoryPostProcessor postProcessor = (BeanFactoryPostProcessor) class1.newInstance();
+				applicationContext.addBeanFactoryPostProcessor(postProcessor);
+			} catch (Throwable e) {
+				Log.error(Log.JEEVES, "Unable to create Bean Post processor");
+			}
+		}
+		}
 	}
 	
 	@Override
