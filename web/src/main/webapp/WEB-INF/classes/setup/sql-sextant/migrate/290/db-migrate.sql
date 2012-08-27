e7d4d653f152fbeaab6ca72f22c145bdf90f4b98
François Prunayre <fxprunayre@gmail.com>
2012-08-27 07:57:17 +0200

    Update migration script

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/290/db-migrate.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/290/db-migrate.sql
@@ -449,6 +449,12 @@ UPDATE usergroups SET profile = (SELECT profile from users WHERE id = userid);
 ALTER TABLE usergroups DROP CONSTRAINT usergroups_pkey;
 ALTER TABLE usergroups ADD PRIMARY KEY (userid, profile, groupid);
 
+-- Delete LDAP settings
+DELETE FROM Settings WHERE parentid=86;
+DELETE FROM Settings WHERE parentid=87;
+DELETE FROM Settings WHERE parentid=89;
+DELETE FROM Settings WHERE parentid=80;
+DELETE FROM Settings WHERE id=80;
 
 
 UPDATE Settings SET value='2.9.0' WHERE name='version';
