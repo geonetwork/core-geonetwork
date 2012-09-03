2ceb9253f073425558bcf0bf7ec0512d462f9d0b
François Prunayre <fxprunayre@gmail.com>
2012-09-03 10:38:08 +0200

    Info service could have parameters other than type.

--- web/src/main/java/org/fao/geonet/services/main/Info.java
+++ web/src/main/java/org/fao/geonet/services/main/Info.java
@@ -107,7 +107,7 @@ public class Info implements Service
 			String type = el.getText();
 
 			if (!name.equals("type"))
-				throw new BadParameterEx(name, type);
+				continue;
 
 			if (type.equals("site")) {
 				result.addContent(gc.getSettingManager().get("system", -1));
