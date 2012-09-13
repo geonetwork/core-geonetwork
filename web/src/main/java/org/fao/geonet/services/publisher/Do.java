cc236942e35384c63070a87b372f19e989a85ac8
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-09-13 13:09:55 +0200

    the last commits failed to correctly commit the files I changed for reasons i don't understand so I am very carefully committing again.  This is for remove password and username so that data cannot be downloaded to client.  See http://trac.osgeo.org/geonetwork/ticket/983.  Yet again

--- web/src/main/java/org/fao/geonet/services/publisher/Do.java
+++ web/src/main/java/org/fao/geonet/services/publisher/Do.java
@@ -147,6 +147,12 @@ public class Do implements Service {
 			String url = node.getChildText("adminUrl");
 			String namespacePrefix = node.getChildText("namespacePrefix");
 			String namespaceUrl = node.getChildText("namespaceUrl");
+            String user = node.getChildText("user");
+			String password = node.getChildText("password");
+
+			// sanitize data that will be returned when the list action is requested
+			node.removeChild("user");
+			node.removeChild("password");
 
 			GeoServerNode g = new GeoServerNode(id, name, url, namespacePrefix,
 					namespaceUrl, user, password);
@@ -220,10 +226,6 @@ public class Do implements Service {
 				String[] userInfo = dbUserInfo[0].split(":");
 				String user = userInfo[0];
 				String password = userInfo[1];
-				
-				// sanitize data that will be returned when the list action is requested
-				node.removeChild("user");
-				node.removeChild("password");
 				
 				String[] dbInfo = dbUserInfo[1].split("#");
 				String db = dbInfo[0]; 
