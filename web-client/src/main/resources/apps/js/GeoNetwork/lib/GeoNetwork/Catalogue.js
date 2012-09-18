717f8b05d3b616d2fdf27d2fe298fefbede8cf1e
Jose García <josegar74@gmail.com>
2012-09-18 18:19:08 +0200

    Add missing variable declaration

--- web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/Catalogue.js
+++ web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/Catalogue.js
@@ -1218,6 +1218,8 @@ GeoNetwork.Catalogue = Ext.extend(Ext.util.Observable, {
             return false;
         } else {
             // Reset user cookie information
+            var cookie = Ext.state.Manager.getProvider();
+             
             if (cookie) {
                 cookie.set('user', undefined);
             }
