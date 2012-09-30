1d6fb9b4f8469539b5a6159b22b7e5c0a88d9a31
Ian <ianwallen@hotmail.com>
2012-09-30 08:36:48 -0700

    Fix for Ticket #850 This completes the fixes for ticket #850 - Performed a compare of a 2.6.4 db upgraded to 2.8 to a fresh install of 2.8 and they seem to now be the same. - Note: Reverted previous change regarding 'SP' and 'PO' from migrate-defautl.sql as this was automatically corrected later on down the script - the previous change broke the script. These codes were incorrect in 2.6 and converting to the 3 char codes fixes it.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v270/loc-cat-default.sql
@@ -0,0 +1,17 @@
+-- Take care to table ID (related to other loc files)
+INSERT INTO CswServerCapabilitiesInfo VALUES (53, 'cat', 'title', '');
+INSERT INTO CswServerCapabilitiesInfo VALUES (54, 'cat', 'abstract', '');
+INSERT INTO CswServerCapabilitiesInfo VALUES (55, 'cat', 'fees', '');
+INSERT INTO CswServerCapabilitiesInfo VALUES (56, 'cat', 'accessConstraints', '');
+
+INSERT INTO CategoriesDes VALUES (11,'cat','Z3950 Servers');
+INSERT INTO CategoriesDes VALUES (12,'cat','Registers');
+INSERT INTO CategoriesDes VALUES (13,'cat','Physical Samples');
+
+INSERT INTO StatusValuesDes VALUES (0,'cat','Unknown');
+INSERT INTO StatusValuesDes VALUES (1,'cat','Draft');
+INSERT INTO StatusValuesDes VALUES (2,'cat','Approved');
+INSERT INTO StatusValuesDes VALUES (3,'cat','Retired');
+INSERT INTO StatusValuesDes VALUES (4,'cat','Submitted');
+INSERT INTO StatusValuesDes VALUES (5,'cat','Rejected');
+
---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v270/loc-chi-default.sql
@@ -0,0 +1,12 @@
+-- Take care to table ID (related to other loc files)
+INSERT INTO CategoriesDes VALUES (11,'chi','Z3950 Servers');
+INSERT INTO CategoriesDes VALUES (12,'chi','Registers');
+INSERT INTO CategoriesDes VALUES (13,'chi','Physical Samples');
+
+INSERT INTO StatusValuesDes VALUES (0,'chi','Unknown');
+INSERT INTO StatusValuesDes VALUES (1,'chi','Draft');
+INSERT INTO StatusValuesDes VALUES (2,'chi','Approved');
+INSERT INTO StatusValuesDes VALUES (3,'chi','Retired');
+INSERT INTO StatusValuesDes VALUES (4,'chi','Submitted');
+INSERT INTO StatusValuesDes VALUES (5,'chi','Rejected');
+
