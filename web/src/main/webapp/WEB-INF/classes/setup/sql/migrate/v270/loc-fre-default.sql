4723f32e375cc3bfe133294d33647df23e7756ba
Ian <ianwallen@hotmail.com>
2012-09-30 08:36:48 -0700

    Fix for Ticket #850 This completes the fixes for ticket #850 - Performed a compare of a 2.6.4 db upgraded to 2.8 to a fresh install of 2.8 and they seem to now be the same. - Note: Reverted previous change regarding 'SP' and 'PO' from migrate-defautl.sql as this was automatically corrected later on down the script - the previous change broke the script. These codes were incorrect in 2.6 and converting to the 3 char codes fixes it.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v270/loc-fre-default.sql
@@ -0,0 +1,12 @@
+-- Take care to table ID (related to other loc files)
+INSERT INTO CategoriesDes VALUES (11,'fre','Serveurs Z3950');
+INSERT INTO CategoriesDes VALUES (12,'fre','Annuaires');
+INSERT INTO CategoriesDes VALUES (13,'fre','Echantillons physiques');
+
+INSERT INTO StatusValuesDes VALUES (0,'fre','Inconnu');
+INSERT INTO StatusValuesDes VALUES (1,'fre','Brouillon');
+INSERT INTO StatusValuesDes VALUES (2,'fre','Validé');
+INSERT INTO StatusValuesDes VALUES (3,'fre','Retiré');
+INSERT INTO StatusValuesDes VALUES (4,'fre','A valider');
+INSERT INTO StatusValuesDes VALUES (5,'fre','Rejeté');
+
---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v270/loc-ger-default.sql
@@ -0,0 +1,12 @@
+-- Take care to table ID (related to other loc files)
+INSERT INTO CategoriesDes VALUES (11,'ger','Z3950 Servers');
+INSERT INTO CategoriesDes VALUES (12,'ger','Registers');
+INSERT INTO CategoriesDes VALUES (13,'ger','Physischen Proben');
+
+INSERT INTO StatusValuesDes VALUES (0,'ger','Unknown');
+INSERT INTO StatusValuesDes VALUES (1,'ger','Draft');
+INSERT INTO StatusValuesDes VALUES (2,'ger','Approved');
+INSERT INTO StatusValuesDes VALUES (3,'ger','Retired');
+INSERT INTO StatusValuesDes VALUES (4,'ger','Submitted');
+INSERT INTO StatusValuesDes VALUES (5,'ger','Rejected');
+
