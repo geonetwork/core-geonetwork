51b3142c1630a89c36bc9df0d74cb5a65abeac8f
Ian <ianwallen@hotmail.com>
2012-10-02 14:06:05 -0300

    Fixed bugs in migration script   - ara missing   - rus has bug where it was removing categories.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v280/loc-rus-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v280/loc-rus-default.sql
@@ -1,15 +1,4 @@
 -- Take care to table ID (related to other loc files)
-delete from CategoriesDes where iddes <14;
-INSERT INTO CategoriesDes VALUES (1,'ru','Карты и графика');
-INSERT INTO CategoriesDes VALUES (2,'ru','Наборы данных');
-INSERT INTO CategoriesDes VALUES (3,'ru','Интерактивные ресурсы');
-INSERT INTO CategoriesDes VALUES (4,'ru','Компьютерные программы');
-INSERT INTO CategoriesDes VALUES (5,'ru','Практические ситуации');
-INSERT INTO CategoriesDes VALUES (6,'ru','Материалы конференций');
-INSERT INTO CategoriesDes VALUES (7,'ru','Фотографии');
-INSERT INTO CategoriesDes VALUES (8,'ru','Аудио/Видео');
-INSERT INTO CategoriesDes VALUES (9,'ru','Каталоги/справочники');
-INSERT INTO CategoriesDes VALUES (10,'ru','Другие ресурсы');
 INSERT INTO CategoriesDes VALUES (11,'rus','Z3950 Servers');
 INSERT INTO CategoriesDes VALUES (12,'rus','Registers');
 INSERT INTO CategoriesDes VALUES (13,'rus','Физические образцы');
