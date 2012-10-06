9dda3bbf28b8d82c3f3fff27414cd0fef31bb60e
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-10-06 11:09:04 +0200

    updated sql so that hopefully thepsql exception will be not thrown any more

--- web/src/main/webapp/WEB-INF/classes/setup/sql/data/geometry_columns-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/data/geometry_columns-default.sql
@@ -2,7 +2,7 @@ INSERT INTO geometry_columns VALUES ('', 'public', 'countriesBB', 'the_geom', 2,
 INSERT INTO geometry_columns VALUES ('', 'public', 'countries', 'the_geom', 2, 21781, 'MULTIPOLYGON');
 INSERT INTO geometry_columns VALUES ('', 'public', 'non_validated', 'the_geom', 2, 21781, 'MULTIPOLYGON');
 INSERT INTO geometry_columns VALUES ('', 'public', 'xlinks', 'the_geom', 2, 21781, 'MULTIPOLYGON');
-INSERT INTO geometry_columns VALUES ('', 'public', 'spatialIndex', 'the_geom', 2, 4326, 'MULTIPOLYGON');
+-- INSERT INTO geometry_columns VALUES ('', 'public', 'spatialIndex', 'the_geom', 2, 4326, 'MULTIPOLYGON');
 INSERT INTO geometry_columns VALUES ('', 'public', 'gemeindenBB', 'the_geom', 2, 21781, 'MULTIPOLYGON');
 INSERT INTO geometry_columns VALUES ('', 'public', 'kantoneBB', 'the_geom', 2, 21781, 'MULTIPOLYGON');
 
