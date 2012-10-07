geonetwork/core-geonetwork

commit 705be8513a94f482779c88e4029ce437b47cc4c9

Liferay Theme sextant integration

web-client/src/main/resources/apps/sextant/css/gnmetadatadefault.css
@@ -85,36 +85,36 @@ div.attr {
 }
 
 /* Tab navigation - start */
-div#tabs ul li ul {
+div#tabs-metadata ul li ul {
   list-style-type : none;
   padding-bottom : 25px;
   border-bottom : 2px solid #7EADD9;
   margin : 0;
 }
-div#tabs ul li span.mode {
+div#tabs-metadata ul li span.mode {
   display : none;
 }
 
-div#tabs ul li ul li {
+div#tabs-metadata ul li ul li {
   float : left;
   height : 21px;
   background-color : #F7EEE1;
   margin : 2px 2px 0 2px;
   border : 2px solid #7EADD9;
 }
-div#tabs ul li ul li.active {
+div#tabs-metadata ul li ul li.active {
   padding-top : 2px;
   border-bottom : 2px solid #fff;
   background-color : #fff;
 }
-#tabs a {
+#tabs-metadata a {
   float : left;
   display : block;
   color : #666;
   text-decoration : none;
   padding : 4px;
 }
-#tabs a:hover {
+#tabs-metadata a:hover {
   background : #fff;
 }
 /* Tab navigation - end */

web-client/src/main/resources/apps/css/gnmetadatadefault.css
@@ -85,36 +85,36 @@ div.attr {
 }
 
 /* Tab navigation - start */
-div#tabs ul li ul {
+div#tabs_metadata ul li ul {
   list-style-type : none;
   padding-bottom : 25px;
   border-bottom : 2px solid #7EADD9;
   margin : 0;
 }
-div#tabs ul li span.mode {
+div#tabs_metadata ul li span.mode {
   display : none;
 }
 
-div#tabs ul li ul li {
+div#tabs_metadata ul li ul li {
   float : left;
   height : 21px;
   background-color : #F7EEE1;
   margin : 2px 2px 0 2px;
   border : 2px solid #7EADD9;
 }
-div#tabs ul li ul li.active {
+div#tabs_metadata ul li ul li.active {
   padding-top : 2px;
   border-bottom : 2px solid #fff;
   background-color : #fff;
 }
-#tabs a {
+#tabs_metadata a {
   float : left;
   display : block;
   color : #666;
   text-decoration : none;
   padding : 4px;
 }
-#tabs a:hover {
+#tabs_metadata a:hover {
   background : #fff;
 }
 /* Tab navigation - end */

web-client/src/main/resources/apps/sextant/catalogue.xhtml
@@ -55,7 +55,8 @@
               <h:inputHidden value="#{catalogue.layergroup}" id="layergroup" />
               <h:inputHidden value="#{catalogue.wmsversion}" id="wmsversion" />
               <h:inputHidden value="#{catalogue.getrecordbyidurl}" id="getrecordbyidurl" />
-              <h:inputHidden value="#{catalogue.geonetworkurl}" id="configgeonetwork" />
+              <!-- <h:inputHidden value="#{catalogue.geonetworkurl}" id="configgeonetwork" /> -->
+              <h:inputHidden value="https://localhost:8443/geonetwork" id="configgeonetwork" />
               <h:inputHidden value="#{catalogue.maxextent}" id="configmaxextent" />
               <h:inputHidden value="#{catalogue.cswurl}" id="configcsw" />
               <h:inputHidden value="#{catalogue.what}" id="configwhat" />

web-client/src/main/resources/apps/sextant/css/main.css
@@ -49,6 +49,7 @@ body {
     font-weight: normal;
     float: right;
     padding-right: 25px;
+    text-decoration:underline;
 }
 .search-panel .x-panel-header-text a:hover{
 	color: #FF7930;
@@ -174,6 +175,8 @@ body {
 	font-size: 12px;
 	font-family: Arial,Helvetica,sans-serif;
 	font-weight:700;
+	text-shadow:inherit;
+	background-image:none;
 }
 .x-panel-tbar {
 	height:32px;
@@ -326,6 +329,7 @@ body {
     padding-left: 12px;
     padding-right: 12px;
     font-family: Arial,Helvetica,sans-serif;
+    vertical-align: inherit;
 }
 .result-list li .content div.md-contact {
     text-align: left;
@@ -436,7 +440,9 @@ body {
 .result-list li.md-full div.unabled:hover span.icon {
 	background-position: 0 0 !important;
 }
-
+.result-list li.md-full tbody{
+	vertical-align:inherit;
+}
 div.no-icon-menu a.x-menu-item {
     padding-left: 3px;
 }

web-client/src/main/resources/apps/sextant/js/cat.main.js
@@ -121,8 +121,7 @@ cat.app = function() {
             cls: 'view-win',
             bodyStyle:'padding:10px'
             });
-        win.show(this.resultsView);
-        //win.alignTo(bd, 'tl-br',[30,30,30,30]);
+        win.show();
 	}
 	
 	function edit(metadataId, create, group, child) {
@@ -607,9 +606,9 @@ cat.app = function() {
 						fn: function(o) {
 							var portletContainer = Ext.Element.select('.portlet-content');
 							var height=0
-							if(portletContainer.getCount()==1) {
+							if(portletContainer.getCount()==2) {
 								var d = Ext.get('main-viewport');
-								height=Ext.getBody().getViewSize().height -d.getY() - 100;
+								height=Ext.getBody().getViewSize().height -d.getY();
 							}
 							else if (portletContainer.getCount()>1) {
 								height=400;

commit 8ab623cfe189fc7234102fb8fed0badcf36f19a0

Fix tab layout broken in commit 705be8513a94f482779c88e4029ce437b47cc4c9.

web-client/src/main/resources/apps/sextant/css/gnmetadatadefault.css
@@ -85,36 +85,36 @@ div.attr {
 }
 
 /* Tab navigation - start */
-div#tabs-metadata ul li ul {
+div.tabs-metadata ul li ul {
   list-style-type : none;
   padding-bottom : 25px;
   border-bottom : 2px solid #7EADD9;
   margin : 0;
 }
-div#tabs-metadata ul li span.mode {
+div.tabs-metadata ul li span.mode {
   display : none;
 }
 
-div#tabs-metadata ul li ul li {
+div.tabs-metadata ul li ul li {
   float : left;
   height : 21px;
   background-color : #F7EEE1;
   margin : 2px 2px 0 2px;
   border : 2px solid #7EADD9;
 }
-div#tabs-metadata ul li ul li.active {
+div.tabs-metadata ul li ul li.active {
   padding-top : 2px;
   border-bottom : 2px solid #fff;
   background-color : #fff;
 }
-#tabs-metadata a {
+.tabs-metadata a {
   float : left;
   display : block;
   color : #666;
   text-decoration : none;
   padding : 4px;
 }
-#tabs-metadata a:hover {
+.tabs-metadata a:hover {
   background : #fff;
 }
 /* Tab navigation - end */

web/src/main/webapp/xsl/metadata/tab-utils.xsl
@@ -22,7 +22,7 @@
   <xsl:template name="tab">
     <xsl:param name="schema"/>
     <xsl:param name="tabLink"/>
-    <div id="tabs">
+    <div id="tabs" class="tabs-metadata">
 
       <!-- Tab visibility is managed in config-gui.xml -->
       <!-- simple tab -->