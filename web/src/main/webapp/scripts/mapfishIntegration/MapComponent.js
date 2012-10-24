3c7a6bd500d0b3a37146273f41af49bd3153a5fc
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-10-24 12:05:22 +0200

    updated tile date

--- web/src/main/webapp/scripts/mapfishIntegration/MapComponent.js
+++ web/src/main/webapp/scripts/mapfishIntegration/MapComponent.js
@@ -117,7 +117,7 @@ var MapComponent = OpenLayers.Class({
                 visibility:true,
                 requestEncoding: "REST",
                 dimensions: ['TIME'],
-                params: {'time': '20111027'},
+                params: {'time': '20120809'},
                 formatSuffix: 'jpeg',
                 zoomOffset: 12,
                 serverResolutions: [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1.5, 1, 0.5, 0.25]
