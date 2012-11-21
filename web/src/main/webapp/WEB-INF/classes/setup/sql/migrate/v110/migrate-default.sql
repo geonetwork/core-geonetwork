17b64d3bba565fbf80185323278d4d5d9614bb62
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-11-21 15:46:57 +0100

    removed problematic migratin settings that are not needed anyhow

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v110/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v110/migrate-default.sql
@@ -6,12 +6,5 @@ UPDATE Settings SET value='prefer_locale' where name='only';
 
 INSERT INTO settings VALUES ( 3, 1, 'wmtTimestamp', '20120809');
 
-INSERT INTO Settings VALUES (89,80,'bind',NULL);
-INSERT INTO Settings VALUES (102,86,'subtree','false');
-INSERT INTO Settings VALUES (140,89,'bindDn','cn=fake.name,ou=people,dc=fao,dc=org');
-INSERT INTO Settings VALUES (141,89,'bindPw','fake_password');
-INSERT INTO Settings VALUES (150,80,'anonBind','true');
-
 UPDATE settings SET value='0 0 1 * * ?' where name = 'every';
 
-ALTER TABLE HarvestHistory ADD elapsedTime int;
