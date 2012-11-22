8f7f06e47fb5ab1f8d33048627827de899385012
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-11-22 15:15:17 +0100

    add configuration for both https and http urls in same system since we allow this, even require it sometimes.  Also fix an issue when using cas and updating configuration

--- jeeves/src/main/java/jeeves/config/springutil/JeevesContextLoaderListener.java
+++ jeeves/src/main/java/jeeves/config/springutil/JeevesContextLoaderListener.java
@@ -15,66 +15,70 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class JeevesContextLoaderListener extends ContextLoaderListener {
-	
-	private static final String POST_PROCESSOR_INIT_PARAM = "applicationContextPostProcessors";
-	@Override
-	protected void customizeContext(ServletContext servletContext,
-			ConfigurableWebApplicationContext applicationContext) {
-		processBeanFactoryPostProcessorParam(applicationContext, servletContext.getInitParameter(POST_PROCESSOR_INIT_PARAM));
-		String baseURL = servletContext.getContextPath();
-		String webappName;
-		if(baseURL.length()>1) {
-			webappName = baseURL.substring(1)+".";
-		} else {
-			webappName = "";
-		}
-		String key = webappName+POST_PROCESSOR_INIT_PARAM;
-		String param = System.getProperty(key);
-		if(param != null) {
-			processBeanFactoryPostProcessorParam(applicationContext, param);
-		} else {
-			processBeanFactoryPostProcessorParam(applicationContext, "geonetwork."+POST_PROCESSOR_INIT_PARAM);
-		}
-	}
 
-	private void processBeanFactoryPostProcessorParam(
-			ConfigurableWebApplicationContext applicationContext, String param) {
-		if (param != null) {
-		for (String className: param.split(",")) {
-			try {
-				Class<?> class1 = Class.forName(className.trim());
-				BeanFactoryPostProcessor postProcessor = (BeanFactoryPostProcessor) class1.newInstance();
-				applicationContext.addBeanFactoryPostProcessor(postProcessor);
-			} catch (Throwable e) {
-				Log.error(Log.JEEVES, "Unable to create Bean Post processor");
-			}
-		}
-		}
-	}
-	
-	@Override
-	public void contextInitialized(ServletContextEvent event) {
-		String appPath = event.getServletContext().getRealPath("/");
+    private static final String POST_PROCESSOR_INIT_PARAM = "applicationContextPostProcessors";
 
-		if (!appPath.endsWith(File.separator)) {
-			appPath += File.separator;
-		}
+    @Override
+    protected void customizeContext(ServletContext servletContext, ConfigurableWebApplicationContext applicationContext) {
+        processBeanFactoryPostProcessorParam(applicationContext, servletContext.getInitParameter(POST_PROCESSOR_INIT_PARAM));
+        String baseURL = servletContext.getContextPath();
+        String webappName;
+        if (baseURL.length() > 1) {
+            webappName = baseURL.substring(1) + ".";
+        } else {
+            webappName = "";
+        }
+        String key = webappName + POST_PROCESSOR_INIT_PARAM;
+        String param = System.getProperty(key);
+        if (param != null) {
+            processBeanFactoryPostProcessorParam(applicationContext, param);
+        } else {
+            key = "geonetwork." + POST_PROCESSOR_INIT_PARAM;
+            param = System.getProperty(key);
+            processBeanFactoryPostProcessorParam(applicationContext, param);
+        }
+    }
 
-		String configPath = appPath + "WEB-INF" + File.separator;
+    private void processBeanFactoryPostProcessorParam(ConfigurableWebApplicationContext applicationContext, String param) {
+        if (param != null) {
+            for (String className : param.split(",")) {
+                if (!className.trim().isEmpty()) {
+                    try {
+                        Class<?> class1 = Class.forName(className.trim());
+                        BeanFactoryPostProcessor postProcessor = (BeanFactoryPostProcessor) class1.newInstance();
+                        applicationContext.addBeanFactoryPostProcessor(postProcessor);
+                    } catch (Throwable e) {
+                        Log.error(Log.JEEVES, "Unable to create Bean Post processor: "+className);
+                    }
+                }
+            }
+        }
+    }
 
-		// migrate from old configuration to new spring configuration if needed
-		new MigrateConfiguration().migrate(configPath, configPath, true);
-		
-		super.contextInitialized(event);
-		Lifecycle context = (Lifecycle) WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
-		context.start();
-	}
-	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-		Lifecycle context = (Lifecycle) WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
-		if(context != null) {
-			context.stop();
-		}
-		super.contextDestroyed(event);
-	}
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        String appPath = event.getServletContext().getRealPath("/");
+
+        if (!appPath.endsWith(File.separator)) {
+            appPath += File.separator;
+        }
+
+        String configPath = appPath + "WEB-INF" + File.separator;
+
+        // migrate from old configuration to new spring configuration if needed
+        new MigrateConfiguration().migrate(configPath, configPath, true);
+
+        super.contextInitialized(event);
+        Lifecycle context = (Lifecycle) WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
+        context.start();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        Lifecycle context = (Lifecycle) WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
+        if (context != null) {
+            context.stop();
+        }
+        super.contextDestroyed(event);
+    }
 }
