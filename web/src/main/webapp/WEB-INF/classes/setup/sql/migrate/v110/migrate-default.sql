eccfda8e70eabf96841e4ee472c5788d1b79c68a
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-11-22 16:37:12 +0100

    change secure port to 8493

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v110/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v110/migrate-default.sql
@@ -7,6 +7,7 @@ UPDATE Settings SET value='prefer_locale' where name='only';
 INSERT INTO settings VALUES ( 3, 1, 'wmtTimestamp', '20120809');
 
 UPDATE settings SET value='0 0 1 * * ?' where name = 'every';
+INSERT INTO Settings VALUES (24,20,'securePort','443');
 
 DROP VIEW sharedusers;
 ALTER TABLE users ALTER "password" TYPE character varying(120);
