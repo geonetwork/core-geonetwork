0fedba2a0a9dc2a28059b734cd3e0b84130d511c
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-11-23 17:11:08 +0100

    fixed merge

--- web/src/main/webapp/scripts/config/model.js
+++ web/src/main/webapp/scripts/config/model.js
@@ -65,8 +65,6 @@ ConfigModel.prototype.getConfig_OK = function(node)
 
             AUTODETECT_ENABLE : xml.evalXPath(node, 'autodetect/enable'),
             REQUESTED_LANGUAGE_ONLY : xml.evalXPath(node, 'requestedLanguage/only'),
-            REQUESTED_LANGUAGE_SORTED : xml.evalXPath(node, 'requestedLanguage/sorted'),
-            REQUESTED_LANGUAGE_IGNORED : xml.evalXPath(node, 'requestedLanguage/ignored'),
 
 			DOWNLOADSERVICE_LEAVE         : xml.evalXPath(node, 'downloadservice/leave'),
 			DOWNLOADSERVICE_SIMPLE         : xml.evalXPath(node, 'downloadservice/simple'),
@@ -203,8 +201,6 @@ ConfigModel.updateTemp =
 '	</autodetect>'+
 '	<requestedLanguage>'+
 '        <only>{REQUESTED_LANGUAGE_ONLY}</only>' +
-'        <sorted>{REQUESTED_LANGUAGE_SORTED}</sorted>' +
-'        <ignored>{REQUESTED_LANGUAGE_IGNORED}</ignored>' +
 '	</requestedLanguage>'+
 '	<downloadservice>'+
 '		<leave>{DOWNLOADSERVICE_LEAVE}</leave>'+
