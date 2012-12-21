9c68aa0faab0ec92a9ae52e85ce5231524322011
François Prunayre <fxprunayre@gmail.com>
2012-12-21 12:51:07 +0100

    Update SQL migration script.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/290/db-migrate-v5.1.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/290/db-migrate-v5.1.sql
@@ -7,6 +7,16 @@ CREATE TABLE CustomElementSet
   (
     xpath  varchar(1000) not null
   );
+  
+  
+ALTER TABLE Metadata ALTER COLUMN harvestUri TYPE varchar(455);
+
+ALTER TABLE HarvestHistory ADD elapsedTime int;
 
 INSERT INTO Settings VALUES (960,1,'wiki',NULL);
 INSERT INTO Settings VALUES (961,960,'markup','none');
+INSERT INTO Settings VALUES (962,960,'output','strip');
+INSERT INTO Settings VALUES (963,960,'mefoutput','strip');
+
+INSERT INTO Settings VALUES (964,1,'wysiwyg',NULL);
+INSERT INTO Settings VALUES (965,964,'enable','false');
