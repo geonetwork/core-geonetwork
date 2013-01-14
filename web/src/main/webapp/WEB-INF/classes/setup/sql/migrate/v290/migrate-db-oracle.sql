5c3cd4fd825e3c4472d14682d06dc41620348b63
Ian <ianwallen@hotmail.com>
2013-01-14 12:42:49 -0400

    Fixed bug with the harvestUri being set to 455 when it should be 512 like in the create scripts.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v290/migrate-db-oracle.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v290/migrate-db-oracle.sql
@@ -1,4 +1,4 @@
 ALTER TABLE usergroups DROP PRIMARY KEY;
 ALTER TABLE usergroups ADD PRIMARY KEY (userid, profile, groupid);
 ALTER TABLE Users MODIFY COLUMN password varchar(120) not null;
-ALTER TABLE Metadata MODIFY COLUMN harvestUri varchar(455);
+ALTER TABLE Metadata MODIFY COLUMN harvestUri varchar(512);
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v290/migrate-db-postgis.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v290/migrate-db-postgis.sql
@@ -1,4 +1,4 @@
 ALTER TABLE usergroups DROP CONSTRAINT usergroups_pkey;
 ALTER TABLE usergroups ADD PRIMARY KEY (userid, profile, groupid);
 ALTER TABLE users ALTER "password" TYPE character varying(120);
-ALTER TABLE Metadata ALTER harvestUri TYPE varchar(455);
+ALTER TABLE Metadata ALTER harvestUri TYPE varchar(512);
