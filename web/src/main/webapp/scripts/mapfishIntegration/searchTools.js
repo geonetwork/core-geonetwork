5130251517a95612d65a806ffd5459c0a52523b3
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-01-16 09:18:18 +0100

    fixed a potential javascript error when there is an empty native coords element

--- web/src/main/webapp/scripts/mapfishIntegration/searchTools.js
+++ web/src/main/webapp/scripts/mapfishIntegration/searchTools.js
@@ -486,6 +486,9 @@ var searchTools = {
               var chinput = Ext.get("ch03_"+ref);
               var wgsinput = Ext.get("wgs84_"+ref);
               var nativeCoordsEl = Ext.get("native_"+ref);
+              if(!nativeCoordsEl || !nativeCoordsEl.dom.innerText || nativeCoordsEl.dom.innerText.split(/:|,/).slice(1,5).length < 4)  {
+                nativeCoordsEl = undefined;
+              }
               var cheInputs, wgsInputs;
               if (chinput && nativeCoordsEl) {
                 var nativeCoords = nativeCoordsEl ? nativeCoordsEl.dom.innerText.split(/:|,/).slice(1,5) : undefined;
