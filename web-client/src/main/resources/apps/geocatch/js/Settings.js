6d690e2cbe3d534ee8c7cdf7fbb666c640e02561
María Arias de Reyna <maria.arias@geocat.net>
2013-01-18 11:37:52 +0100

    Changing to black the color of bbox on map results

--- web-client/src/main/resources/apps/geocatch/js/Settings.js
+++ web-client/src/main/resources/apps/geocatch/js/Settings.js
@@ -76,7 +76,7 @@ Ext.BLANK_IMAGE_URL = '../../apps/js/ext/resources/images/default/s.gif';
 GeoNetwork.Settings.results = {
     // Parameters to set bounding box highlighter colors
     // Use a custom single color for bounding box
-    featurecolor: 'orange',
+    featurecolor: 'black',
     // Use a random color map with 2 colors
     //colormap: GeoNetwork.Util.generateColorMap(2),
     // Use a default color map with 10 colors
