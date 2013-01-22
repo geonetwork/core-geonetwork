96312581292360a41eb6d8484e31203c4dc93316
François Prunayre <fxprunayre@gmail.com>
2013-01-22 08:22:35 +0100

    Add validation status to migration script.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/290/db-migrate-v5.1.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/290/db-migrate-v5.1.sql
@@ -20,3 +20,28 @@ INSERT INTO Settings VALUES (963,960,'mefoutput','strip');
 
 INSERT INTO Settings VALUES (964,1,'wysiwyg',NULL);
 INSERT INTO Settings VALUES (965,964,'enable','false');
+
+
+
+INSERT INTO StatusValues VALUES  (0,'unknown','y');
+INSERT INTO StatusValues VALUES  (1,'draft','y');
+INSERT INTO StatusValues VALUES  (2,'approved','y');
+INSERT INTO StatusValues VALUES  (3,'retired','y');
+INSERT INTO StatusValues VALUES  (4,'submitted','y');
+INSERT INTO StatusValues VALUES  (5,'rejected','y');
+
+INSERT INTO StatusValuesDes VALUES (0,'eng','Unknown');
+INSERT INTO StatusValuesDes VALUES (1,'eng','Draft');
+INSERT INTO StatusValuesDes VALUES (2,'eng','Approved');
+INSERT INTO StatusValuesDes VALUES (3,'eng','Retired');
+INSERT INTO StatusValuesDes VALUES (4,'eng','Submitted');
+INSERT INTO StatusValuesDes VALUES (5,'eng','Rejected');
+
+INSERT INTO StatusValuesDes VALUES (0,'fre','Inconnu');
+INSERT INTO StatusValuesDes VALUES (1,'fre','Brouillon');
+INSERT INTO StatusValuesDes VALUES (2,'fre','Validé');
+INSERT INTO StatusValuesDes VALUES (3,'fre','Retiré');
+INSERT INTO StatusValuesDes VALUES (4,'fre','A valider');
+INSERT INTO StatusValuesDes VALUES (5,'fre','Rejeté');
+
+
