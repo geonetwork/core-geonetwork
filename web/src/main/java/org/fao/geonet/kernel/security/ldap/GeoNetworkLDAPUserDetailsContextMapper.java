6105bf10d938f4e3096da6b126f8c3165ac2a32e
Florent gravin <florent.gravin@camptocamp.com>
2013-02-12 18:48:50 +0100

    #15253 -Avoid null pointer if no listesiteweb are defined

--- web/src/main/java/org/fao/geonet/kernel/security/ldap/GeoNetworkLDAPUserDetailsContextMapper.java
+++ web/src/main/java/org/fao/geonet/kernel/security/ldap/GeoNetworkLDAPUserDetailsContextMapper.java
@@ -185,7 +185,10 @@ public class GeoNetworkLDAPUserDetailsContextMapper implements
 				// combination of group and profile pair.
 				ArrayList<String> privileges = userInfo.get(mapping.get("privilege")[0]);
 				if(userDetails.getOrganisation().equals("IFREMER")) {
-					privileges.add("SXT_IFREMER_RegisteredUser");
+					if(privileges == null) {
+						privileges=new ArrayList<String>();
+					}
+					privileges.add("SXT5_IFREMER_RegisteredUser");
 				}
 				if (privileges != null) {
 					Set<String> profileList = new HashSet<String>();
