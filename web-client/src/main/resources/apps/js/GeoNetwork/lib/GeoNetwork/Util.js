289967cad7c01f1c35aef1e4fe7411de03f67297
François Prunayre <fxprunayre@gmail.com>
2013-04-11 13:30:09 +0200

    Fix IE7 error on map initialization in editor.

--- web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/Util.js
+++ web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/Util.js
@@ -22,6 +22,32 @@
  */
 Ext.namespace('GeoNetwork');
 
+//Required for IE6, 7, 8
+//https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+     Function.prototype.bind = function (oThis) {
+         if (typeof this !== "function") {
+           // closest thing possible to the ECMAScript 5 internal IsCallable function
+           throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+         }
+      
+         var aArgs = Array.prototype.slice.call(arguments, 1), 
+             fToBind = this, 
+             fNOP = function () {},
+             fBound = function () {
+               return fToBind.apply(this instanceof fNOP && oThis
+                                      ? this
+                                      : oThis,
+                                    aArgs.concat(Array.prototype.slice.call(arguments)));
+             };
+      
+         fNOP.prototype = this.prototype;
+         fBound.prototype = new fNOP();
+      
+         return fBound;
+    };
+}
+
 GeoNetwork.Lang = {};
 
 GeoNetwork.Util = {
