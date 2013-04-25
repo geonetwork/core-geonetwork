e84e5d9c5972cde8947822271fe2e4e9d07f5d84
François Prunayre <fxprunayre@gmail.com>
2013-04-25 13:30:23 +0200

    Add missing SQL statement required for CSW virtual admin.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/create/create-db-postgis.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/create/create-db-postgis.sql
@@ -478,6 +478,30 @@ CREATE TABLE Thesaurus
     primary key(id)
   );
 
+CREATE TABLE Services
+  (
+  
+    id         int,
+    name       varchar(64)   not null,
+    class       varchar(1048)   not null,
+    description       varchar(1048),
+        
+    primary key(id)
+  );
+  
+
+CREATE TABLE ServiceParameters
+  (
+    id         int,
+    service     int,
+    name       varchar(64)   not null,
+    value       varchar(1048)   not null,
+    
+    primary key(id),
+        
+    foreign key(service) references Services(id)
+  );
+
 -- ======================================================================
   
 CREATE TABLE spatialindex
