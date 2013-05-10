2d16f074a4a74259e16df59e91dae26abc7f3538
Heikki Doeleman <tropicano@gmail.com>
2013-05-10 08:33:10 +0200

    Unit test new authentication

--- web/src/test/java/org/fao/geonet/kernel/security/GeonetworkAuthenticationProviderTest.java
+++ web/src/test/java/org/fao/geonet/kernel/security/GeonetworkAuthenticationProviderTest.java
@@ -14,6 +14,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Random;
+
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
@@ -22,160 +24,75 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 /**
+ * Test GeonetworkAuthenticationProvider.
+ *
  * @author heikki doeleman
  */
-public class GeonetworkAuthenticationProviderTest extends TestCase {
-
+public class GeonetworkAuthenticationProviderTest {
+    /**
+     * The class under test.
+     */
     private GeonetworkAuthenticationProvider geonetworkAuthenticationProvider;
+
+    //
+    // Test doubles
+    //
 
     private UsernamePasswordAuthenticationToken authentication;
     private Dbms dbms;
-    private ResourceManager resourceManager;
     private ApplicationContext applicationContext;
-    private PasswordEncoder encoder;
 
+    /**
+     * Creates a GeonetworkAuthenticationProvider using a mock ApplicationContext that uses a mock ResourceManager to
+     * provide a mock Dbms. This method is invoked before each @Test method.
+     *
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception{
         dbms = mock(Dbms.class);
-        resourceManager = mock(ResourceManager.class);
+        ResourceManager resourceManager = mock(ResourceManager.class);
         when(resourceManager.openDirect(Geonet.Res.MAIN_DB)).thenReturn(dbms);
 
         applicationContext = mock(ApplicationContext.class);
-
         when(applicationContext.getBean(ResourceManager.class)).thenReturn(resourceManager);
+
         geonetworkAuthenticationProvider = new GeonetworkAuthenticationProvider();
         geonetworkAuthenticationProvider.setApplicationContext(applicationContext);
     }
 
-    @Test(expected = UsernameNotFoundException.class)
-    public void testUserNotFound() throws Exception{
-        Element response = new Element("response");
-        when(dbms.select(anyString(), anyString())).thenReturn(response);
-        try {
-            geonetworkAuthenticationProvider.retrieveUser("xxx", authentication);
-            TestCase.fail("Expected UsernameNotFoundException");
-        }
-        catch (UsernameNotFoundException x) {}
-    }
-
-    @Test
-    public void testUserFound() throws Exception{
-        userFoundSetup();
-
-        geonetworkAuthenticationProvider.retrieveUser("xxx", authentication);
-        TestCase.assertNotNull(geonetworkAuthenticationProvider.retrieveUser("xxx", authentication));
-    }
-
-    @Test
-    public void testFindUserWithAuthenticationToken() throws Exception{
-        userFoundSetup();
-        authentication = mock(UsernamePasswordAuthenticationToken.class);
-        when(authentication.getCredentials()).thenReturn("old password");
-
-        TestCase.assertNotNull(geonetworkAuthenticationProvider.retrieveUser("xxx", authentication));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testAuthenticateWithoutToken() throws Exception {
-        userFoundSetup();
-        try {
-            geonetworkAuthenticationProvider.authenticate(authentication);
-            TestCase.fail("Expected IllegalArgumentException");
-        }
-        catch(IllegalArgumentException x){}
-    }
-
-    @Test(expected = BadCredentialsException.class)
-    public void testAuthenticateWithTokenWithoutCredentials() throws Exception {
-        userFoundSetup();
-        authentication = mock(UsernamePasswordAuthenticationToken.class);
-        try {
-            geonetworkAuthenticationProvider.authenticate(authentication);
-            TestCase.fail("Expected BadCredentialsException");
-        }
-        catch(BadCredentialsException x) {}
-    }
-
-    @Test
-    public void testAuthenticateWithTokenWithCredentials() throws Exception {
-        GeonetworkAuthenticationProvider spy = authenticationSetup(true);
-        TestCase.assertNotNull(spy.authenticate(authentication));
-    }
-
-    @Test(expected = BadCredentialsException.class)
-    public void testAuthenticateWithTokenWithWrongCredentials() throws Exception {
-        GeonetworkAuthenticationProvider spy = authenticationSetup(false);
-        try {
-            spy.authenticate(authentication);
-            TestCase.fail("Expected BadCredentialsException");
-        }
-        catch(BadCredentialsException x) {}
-    }
-
     /**
-     * Sets up things for mock authentication tests.
+     * Makes dbms not find a user.
      *
-     * @param OK whether the authentication succeeds
-     * @return Spy on GeonetworkAuthenticationProvider
      * @throws Exception
      */
-    private GeonetworkAuthenticationProvider authenticationSetup(boolean OK) throws Exception{
-        userFoundSetup();
-        mockEncoderSetup(OK);
-        mockAuthenticationSetup();
-        return spyOnAuthenticateSetup();
+    private void userNotFoundSetup() throws Exception{
+        Element response = new Element("response");
+        when(dbms.select(anyString(), anyString())).thenReturn(response);
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void testUserNotFound() throws Exception{
+        userNotFoundSetup();
+
+        geonetworkAuthenticationProvider.retrieveUser("username", authentication);
     }
 
     /**
-     * Sets up a mock authentication.
-     */
-    private void mockAuthenticationSetup() {
-        authentication = mock(UsernamePasswordAuthenticationToken.class);
-        when(authentication.getCredentials()).thenReturn("password");
-    }
-
-    /**
-     * Makes applicationContext use a mock encoder.
+     * Minimalist representation of a user retrieved from the DB.
      *
-     * @param match whether a password matches
+     * @return user in XML
      */
-    private void mockEncoderSetup(boolean match) {
-        encoder = mock(PasswordEncoder.class);
-        when(encoder.matches(anyString(), anyString())).thenReturn(match);
-        when(applicationContext.getBean(PasswordUtil.ENCODER_ID)).thenReturn(encoder);
-    }
-    /**
-     * Creates mock GeonetworkUser.
-     *
-     * @return
-     */
-    private GeonetworkUser mockUserSetup() {
-        GeonetworkUser user = mock(GeonetworkUser.class);
-        when(user.isAccountNonLocked()).thenReturn(true);
-        when(user.isEnabled()).thenReturn(true);
-        when(user.isAccountNonExpired()).thenReturn(true);
-        when(user.getPassword()).thenReturn("password");
-        when(user.isCredentialsNonExpired()).thenReturn(true);
-        return user;
+    private Element userXML() {
+        Element record = new Element("record");
+        Element id = new Element("id");
+        id.setText(Integer.toString(new Random().nextInt()));
+        record.addContent(id);
+        return record;
     }
 
     /**
-     * Makes GAP use a mock user on authenticate.
-     *
-     * @return
-     */
-    private GeonetworkAuthenticationProvider spyOnAuthenticateSetup() {
-        GeonetworkAuthenticationProvider spy = spy(geonetworkAuthenticationProvider);
-        GeonetworkUser user = mockUserSetup();
-
-        doReturn(user).when(spy).retrieveUser(anyString(), any(UsernamePasswordAuthenticationToken.class));
-        spy.setApplicationContext(applicationContext);
-
-        return spy;
-    }
-
-    /**
-     * Mocks a user found.
+     * Makes dbms find a user.
      *
      * @throws Exception
      */
@@ -188,16 +105,123 @@ public class GeonetworkAuthenticationProviderTest extends TestCase {
         when(dbms.select(anyString(), anyString())).thenReturn(response);
     }
 
+    @Test
+    public void testUserFound() throws Exception{
+        userFoundSetup();
+
+        geonetworkAuthenticationProvider.retrieveUser("username", authentication);
+        TestCase.assertNotNull("User should be found",
+                geonetworkAuthenticationProvider.retrieveUser("username", authentication));
+    }
+
+    @Test
+    public void testFindUserWithAuthenticationToken() throws Exception{
+        userFoundSetup();
+        authentication = mock(UsernamePasswordAuthenticationToken.class);
+        when(authentication.getCredentials()).thenReturn("password");
+
+        TestCase.assertNotNull("User with authentication token should be found",
+                geonetworkAuthenticationProvider.retrieveUser("username", authentication));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAuthenticateWithoutToken() throws Exception {
+        userFoundSetup();
+
+        geonetworkAuthenticationProvider.authenticate(authentication);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAuthenticateWithTokenWithoutCredentials() throws Exception {
+        userFoundSetup();
+        authentication = mock(UsernamePasswordAuthenticationToken.class);
+
+        geonetworkAuthenticationProvider.authenticate(authentication);
+    }
+
     /**
+     * Makes applicationContext use a mock encoder. The mock encoder either approves or rejects any password.
      *
-     * @return
+     * @param match whether a password matches
      */
-    private Element userXML() {
-        Element record = new Element("record");
-        Element id = new Element("id");
-        id.setText("666");
-        record.addContent(id);
-        return record;
+    private void mockEncoderSetup(boolean match) {
+        PasswordEncoder encoder = mock(PasswordEncoder.class);
+        when(encoder.matches(anyString(), anyString())).thenReturn(match);
+        when(applicationContext.getBean(PasswordUtil.ENCODER_ID)).thenReturn(encoder);
+    }
+
+    /**
+     * Sets up a mock authentication that can return a password.
+     */
+    private void mockAuthenticationSetup() {
+        authentication = mock(UsernamePasswordAuthenticationToken.class);
+        when(authentication.getCredentials()).thenReturn("password");
+    }
+
+    /**
+     * Creates mock GeonetworkUser. The mock user can return a password and has all Spring's UserDetails flags set to be
+     * a valid user (not locked, not disabled, not expired, credentials not expired).
+     *
+     * @return mock user
+     */
+    private GeonetworkUser mockUserSetup() {
+        GeonetworkUser user = mock(GeonetworkUser.class);
+        when(user.isAccountNonLocked()).thenReturn(true);
+        when(user.isEnabled()).thenReturn(true);
+        when(user.isAccountNonExpired()).thenReturn(true);
+        when(user.isCredentialsNonExpired()).thenReturn(true);
+        when(user.getPassword()).thenReturn("password");
+        return user;
+    }
+
+    /**
+     * Makes GAP use a mock user on authenticate. Uses a spy because we need to stub only one method (retrieveUser) of
+     * the class under test while using the real thing for its other methods.
+     *
+     * @param userFound whether the user is found
+     * @return GeonetworkAuthenticationProvider spy
+     */
+    private GeonetworkAuthenticationProvider spyOnAuthenticateSetup(boolean userFound) {
+        GeonetworkAuthenticationProvider spy = spy(geonetworkAuthenticationProvider);
+        GeonetworkUser user = userFound ? mockUserSetup() : null ;
+
+        doReturn(user).when(spy).retrieveUser(anyString(), any(UsernamePasswordAuthenticationToken.class));
+        spy.setApplicationContext(applicationContext);
+
+        return spy;
+    }
+
+    /**
+     * Sets up a GeonetworkAuthenticationProvider for authentication tests.
+     *
+     * @param userFound whether the user is found
+     * @param OK whether the authentication succeeds
+     * @return Spy on GeonetworkAuthenticationProvider
+     * @throws Exception
+     */
+    private GeonetworkAuthenticationProvider authenticationSetup(boolean userFound, boolean OK) throws Exception{
+        mockEncoderSetup(OK);
+        mockAuthenticationSetup();
+        return spyOnAuthenticateSetup(userFound);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAuthenticateWithUserNotFound() throws Exception {
+        geonetworkAuthenticationProvider = authenticationSetup(false, true);
+        geonetworkAuthenticationProvider.authenticate(authentication);
+    }
+
+    @Test
+    public void testAuthenticateWithTokenWithCorrectCredentials() throws Exception {
+        geonetworkAuthenticationProvider = authenticationSetup(true, true);
+        TestCase.assertNotNull("Authentication with correct credentials should succeed",
+                geonetworkAuthenticationProvider.authenticate(authentication));
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAuthenticateWithTokenWithWrongCredentials() throws Exception {
+        geonetworkAuthenticationProvider = authenticationSetup(true, false);
+        geonetworkAuthenticationProvider.authenticate(authentication);
     }
 
 }
