7d7231b00e859ec8323a7e4fec7699ca4f7e15ec
María Arias de Reyna <maria.arias@geocat.net>
2013-05-22 15:49:08 +0200

    Merge pull request #92 from Delawen/2.10.x in master
    Minor tweaks to HTML5UI

--- web-client/src/main/resources/apps/html5ui/js/App.js
+++ web-client/src/main/resources/apps/html5ui/js/App.js
@@ -68,7 +68,7 @@ GeoNetwork.app = function() {
                         '<div class="tag-cloud">',
                            '<a href="#" onclick="javascript:catalogue.kvpSearch(\'fast=' + catalogue.metadataStore.fast + '&summaryOnly=0&from=1&to=20&hitsPerPage=20&' + 
                            'themekey' + 
-                                '={value}\', app.loadResults, null, null);" alt="{value}">{value} ({count})</a>', 
+                                '={value}\', app.loadResults, null, null);app.searchApp.firstSearch=true;showSearch();" alt="{value}">{value} ({count})</a>', 
                         '</div>', 
                     '</tpl>')
         });
