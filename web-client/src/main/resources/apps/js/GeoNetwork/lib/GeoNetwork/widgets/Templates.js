9309918c5f5472ab34285f317bde79b4e8cbbc14
François Prunayre <fxprunayre@gmail.com>
2013-05-24 09:28:07 +0200

    Widgets / Fix uuid to call to addWMSLayer.

--- web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/widgets/Templates.js
+++ web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/widgets/Templates.js
@@ -173,7 +173,7 @@ GeoNetwork.Templates.THUMBNAIL = new Ext.XTemplate(
                     '<tpl for="links">',
                     '<tpl if="values.type == \'application/vnd.ogc.wms_xml\'">',
                     // FIXME : ref to app
-                        '<a href="#" class="md-mn addLayer" title="{title}" alt="{title}" onclick="app.switchMode(\'1\', true);app.getIMap().addWMSLayer([[\'{title}\', \'{href}\', \'{name}\', \'{uuid}\']]);">&nbsp;</a>',
+                        '<a href="#" class="md-mn addLayer" title="{title}" alt="{title}" onclick="app.switchMode(\'1\', true);app.getIMap().addWMSLayer([[\'{title}\', \'{href}\', \'{name}\', \'{parent.uuid}\']]);">&nbsp;</a>',
                     '</tpl>',
                     '</tpl>',
                 '</div>',
