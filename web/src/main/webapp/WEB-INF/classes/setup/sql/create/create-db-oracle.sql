74de67168951221a91dd47dd48fd7d89662ddac3
Ian <ianwallen@hotmail.com>
2013-05-27 11:38:29 -0300

    SQL table name error (from last commit) - "service" should be "services"

--- web/src/main/webapp/WEB-INF/classes/setup/sql/create/create-db-oracle.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/create/create-db-oracle.sql
@@ -447,7 +447,7 @@ REM CREATE INDEX MetadataNDX1 ON Metadata(uuid);
 CREATE INDEX MetadataNDX2 ON Metadata(source);
 CREATE INDEX MetadataNDX3 ON Metadata(owner);
 
-ALTER TABLE ServiceParameters ADD FOREIGN KEY (service) REFERENCES service (id);
+ALTER TABLE ServiceParameters ADD FOREIGN KEY (service) REFERENCES services (id);
 ALTER TABLE CategoriesDes ADD FOREIGN KEY (idDes) REFERENCES Categories (id);
 ALTER TABLE CategoriesDes ADD FOREIGN KEY (langId) REFERENCES Languages (id);
 ALTER TABLE Groups ADD FOREIGN KEY (referrer) REFERENCES Users (id);
