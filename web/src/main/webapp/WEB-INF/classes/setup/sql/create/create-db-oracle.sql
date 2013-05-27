5edb7e52f47dc107044b8c0db41d5480be07fa68
Ian <ianwallen@hotmail.com>
2013-05-27 11:49:45 -0300

    Removed blank lines in Oracle statements so that it can be executed from sqlplus if required.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/create/create-db-oracle.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/create/create-db-oracle.sql
@@ -408,12 +408,10 @@ CREATE TABLE Thesaurus
 
 CREATE TABLE Services
   (
-  
     id         int,
     name       varchar(64)   not null,
     class       varchar(1048)   not null,
     description       varchar(1048),
-        
     primary key(id)
   );
   
@@ -424,7 +422,6 @@ CREATE TABLE ServiceParameters
     service     int,
     name       varchar(64)   not null,
     value       varchar(1048)   not null,
-    
     primary key(id)
   );
 
