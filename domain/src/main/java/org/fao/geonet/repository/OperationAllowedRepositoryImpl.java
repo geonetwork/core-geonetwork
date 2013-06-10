aa8cbcb93b1865201c2197c48d2e670ba351cebb
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-06-10 19:47:29 +0200

    fixed operationallowed findby metadata id (string)

--- domain/src/main/java/org/fao/geonet/repository/OperationAllowedRepositoryImpl.java
+++ domain/src/main/java/org/fao/geonet/repository/OperationAllowedRepositoryImpl.java
@@ -9,21 +9,22 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 import org.fao.geonet.domain.OperationAllowed;
+import org.fao.geonet.domain.OperationAllowedId_;
 import org.fao.geonet.domain.OperationAllowed_;
 
-public class OperationAllowedImpl implements OperationAllowedRepositoryCustom {
+public class OperationAllowedRepositoryImpl implements OperationAllowedRepositoryCustom {
 
     @PersistenceContext
     EntityManager _entityManager;
     @Override
-    public List<OperationAllowed> findByMetadataId(String metadataId) {
+    public List<OperationAllowed> findByMetadataId (String metadataId) {
         CriteriaBuilder builder = _entityManager.getCriteriaBuilder();
         CriteriaQuery<OperationAllowed> query = builder.createQuery(OperationAllowed.class);
-
+        
         int iMdId = Integer.parseInt(metadataId);
         Root<OperationAllowed> root = query.from(OperationAllowed.class);
         
-        query.where(builder.equal(builder.literal(iMdId), root.get(OperationAllowed_.id)));
+        query.where(builder.equal(builder.literal(iMdId), root.get(OperationAllowed_.id).get(OperationAllowedId_.metadataId)));
         return _entityManager.createQuery(query).getResultList();
     }
 
