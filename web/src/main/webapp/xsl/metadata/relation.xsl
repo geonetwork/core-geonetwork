geonetwork/core-geonetwork

commit 11fb06b098193dea6e49522bafc4aedb420a3ba2

Widgets / Relation manager / Do not display resource with no link. Use URL as default label if none found.

web/src/main/webapp/xsl/metadata/relation.xsl
@@ -49,7 +49,7 @@
       </relation>
     </xsl:for-each>
     
-    <xsl:for-each select="*/descendant::*[name(.) = 'gmd:onLine']/*">
+    <xsl:for-each select="*/descendant::*[name(.) = 'gmd:onLine']/*[gmd:linkage/gmd:URL!='']">
       <relation type="onlinesrc">
         
         <!-- Compute title based on online source info-->
@@ -57,10 +57,12 @@
           <xsl:variable name="title" select="if (../@uuidref) then util:getIndexField(string(/root/gui/app/path), string(../@uuidref), '_title', string(/root/gui/language)) else ''"/>
           <xsl:value-of select="if ($title = '' and ../@uuidref) then ../@uuidref else $title"/><xsl:text> </xsl:text>
           <xsl:value-of select="if (gmd:name/gco:CharacterString != '') 
-                                  then gmd:name/gco:CharacterString 
-                                  else if (gmd:name/gmx:MimeFileType != '')
-                                  then gmd:name/gmx:MimeFileType
-                                  else gmd:description/gco:CharacterString"/>
+            then gmd:name/gco:CharacterString 
+            else if (gmd:name/gmx:MimeFileType != '')
+            then gmd:name/gmx:MimeFileType
+            else if (gmd:description/gco:CharacterString != '')
+            then gmd:description/gco:CharacterString
+            else gmd:linkage/gmd:URL"/>
           <xsl:value-of select="if (gmd:protocol/*) then concat(' (', gmd:protocol/*, ')') else ''"/>
         </xsl:variable>
         
@@ -71,7 +73,6 @@
         <abstract><xsl:value-of select="gmd:description/gco:CharacterString"/></abstract>
       </relation>
     </xsl:for-each>
-  </xsl:template>
   
 
   <!-- In Lucene only mode, metadata are retrieved from 

commit 59712b0bf0dc5aa42bfa7e8a2589e98b7cf0d682

Fix wrong tag introduced in 11fb06b098193dea6e49522bafc4aedb420a3ba2.

web/src/main/webapp/xsl/metadata/relation.xsl
@@ -73,7 +73,7 @@
         <abstract><xsl:value-of select="gmd:description/gco:CharacterString"/></abstract>
       </relation>
     </xsl:for-each>
-  
+  </xsl:template>
 
   <!-- In Lucene only mode, metadata are retrieved from 
   the index and pass as a simple XML with one level element.