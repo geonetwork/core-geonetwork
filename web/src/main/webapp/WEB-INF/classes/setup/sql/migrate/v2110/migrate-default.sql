1687416d6a81baf3ed709dcd96d7e72a10e4c3c5
François Prunayre <fxprunayre@gmail.com>
2013-08-09 18:22:26 +0200

    Migration / Fix error when migrating harvesters.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2110/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2110/migrate-default.sql
@@ -2,7 +2,7 @@ INSERT INTO HarvesterSettings VALUES  (1,NULL,'harvesting',NULL);
 -- Copy all harvester's root nodes config
 INSERT INTO HarvesterSettings SELECT id, 1, name, value FROM Settings WHERE parentId = 2;
 -- Copy all harvester's properties (Greater than last 2.10.1 settings ie. keepMarkedElement)
-INSERT INTO HarvesterSettings SELECT * FROM Settings WHERE id > 958;
+INSERT INTO HarvesterSettings SELECT * FROM Settings WHERE id > 958 AND parentId > 2;
 -- Drop harvester config from Settings table
 DELETE FROM Settings WHERE id > 958;
 DELETE FROM Settings WHERE id=2;
