d2b00bf197bcef1dc17513d723c7d684390d0b87
François Prunayre <fxprunayre@gmail.com>
2013-08-12 08:34:57 +0200

    Settings / Log error if missing setting in table.

--- core/src/main/java/org/fao/geonet/kernel/setting/SettingManager.java
+++ core/src/main/java/org/fao/geonet/kernel/setting/SettingManager.java
@@ -171,7 +171,16 @@ public class SettingManager {
         if (Log.isDebugEnabled(Geonet.SETTINGS)) {
             Log.debug(Geonet.SETTINGS, "Requested setting with name: " + path);
         }
-        String value = settings.get(path).getValue();
+        SettingEntry se = settings.get(path);
+        if (se == null) {
+            // TODO : When a settings is not available in the settings table
+            // we end here. It could be relevant to add a list of default
+            // settings and populate the settings table when the settings is 
+            // missing (due to bad migration for example).
+            Log.error(Geonet.SETTINGS, "  Requested setting with name: " + path + "  not found. Add it to the settings table.");
+            return null;
+        }
+        String value = se.getValue();
         if (value == null) {
             Log.warning(Geonet.SETTINGS, "  Requested setting with name: " + path + " but null value found. Check the settings table.");
         }
@@ -187,11 +196,16 @@ public class SettingManager {
         Element env = new Element("settings");
         for (int i = 0; i < keys.length; i++) {
             String key = keys[i];
-            String value = settings.get(key).getValue();
-            if (value != null) {
-                Element setting = new Element("setting");
-                setting.setAttribute("name", key).setAttribute("value", value);
-                env.addContent(setting);
+            SettingEntry se = settings.get(key);
+            if (se == null) {
+                Log.error(Geonet.SETTINGS, "  Requested setting with name: " + key + " not found. Add it to the settings table.");
+            } else {
+                String value = se.getValue();
+                if (value != null) {
+                    Element setting = new Element("setting");
+                    setting.setAttribute("name", key).setAttribute("value", value);
+                    env.addContent(setting);
+                }
             }
         }
         return env;
