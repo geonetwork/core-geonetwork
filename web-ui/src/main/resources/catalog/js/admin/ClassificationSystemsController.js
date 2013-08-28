b0a4a07f842677df2975c1ab47821feb365861a8
Arnaud De Groof <arnaud.degroof@spacebel.be>
2013-08-28 13:59:00 +0200

    Classification system UI update

--- web-ui/src/main/resources/catalog/js/admin/ClassificationSystemsController.js
+++ web-ui/src/main/resources/catalog/js/admin/ClassificationSystemsController.js
@@ -32,11 +32,73 @@
 
       $scope.categories = null;
       $scope.categorySelected = {id: $routeParams.categoryId};
+      
+      $scope.categoryUpdated = false;
      
       $scope.selectCategory = function (c) {
     	  console.log(c);
     	  $scope.categorySelected = c;
-      }
+      };
+      
+            
+      /**
+       * Delete a category  
+       */
+      $scope.deleteCategory = function(id) {
+    	  
+    	  console.log($scope.categoryId);
+        $http.get($scope.url + 'admin.category.remove?id=' +
+        		 id)
+        .success(function(data) {
+        	  $scope.unselectCategory();
+        	  loadCategories();
+            })
+        .error(function(data) {
+              $rootScope.$broadcast('StatusUpdated', {
+                title: $translate('categoryDeleteError'),
+                error: data,
+                timeout: 0,
+                type: 'danger'});
+            });
+      };
+      
+      /**
+       * Save a category  
+       */
+      $scope.saveCategory = function(formId) {  	  
+          $http.get($scope.url + 'admin.category.update?' + $(formId).serialize())
+          .success(function(data) {
+                $scope.unselectCategory();
+                loadCategories();
+                $rootScope.$broadcast('StatusUpdated', {
+                  msg: $translate('groupUpdated'),
+                  timeout: 2,
+                  type: 'success'});
+              })
+          .error(function(data) {
+                $rootScope.$broadcast('StatusUpdated', {
+                  title: $translate('groupUpdateError'),
+                  error: data,
+                  timeout: 0,
+                  type: 'danger'});
+              });
+        };
+      
+     $scope.addCategory = function() {
+          $scope.unselectCategory();
+          $scope.categorySelected = {
+            '@id': '',
+            name: ''
+          };
+        };
+      
+        $scope.unselectCategory =function() {
+        	$scope.categorySelected = {};
+        }
+        $scope.updatingCategory = function() {
+            $scope.cateroryUpdated = true;
+          };
+      
       function loadCategories() {
         $http.get('xml.info@json?type=categories').success(function(data) {
           $scope.categories = data.categories;
