872bee57d6aa005ef3e998285278de71fe6110f4
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-08-29 14:00:48 +0200

    fix test with iso language repository

---
+++ domain/src/main/java/org/fao/geonet/repository/IsoLanguageRepository.java
@@ -0,0 +1,30 @@
+package org.fao.geonet.repository;
+
+import org.fao.geonet.domain.IsoLanguage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+/**
+ * Data Access object for accessing {@link IsoLanguage} entities.
+ * 
+ * @author Jesse
+ */
+public interface IsoLanguageRepository extends JpaRepository<IsoLanguage, Integer>, JpaSpecificationExecutor<IsoLanguage> {
+    /**
+     * Find all the IsoLanguages based on the code (the longer code).
+     *
+     * @param code the required code
+     * @return all the IsoLanguages based on the code (the longer code).
+     */
+    List<IsoLanguage> findAllByCode(String code);
+
+    /**
+     * Find all the IsoLanguages based on the short code.
+     *
+     * @param code the required code
+     * @return all the IsoLanguages based on the short code.
+     */
+    List<IsoLanguage> findAllByShortCode(String code);
+}
---
+++ domain/src/test/java/org/fao/geonet/repository/IsoLanguageRepositoryTest.java
@@ -0,0 +1,76 @@
+package org.fao.geonet.repository;
+
+
+import org.fao.geonet.domain.IsoLanguage;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
+
+@Transactional
+public class IsoLanguageRepositoryTest extends AbstractSpringDataTest {
+
+    @Autowired
+    IsoLanguageRepository _repo;
+
+    AtomicInteger _inc = new AtomicInteger();
+    @Test
+    public void testFindOne() {
+        IsoLanguage isolang1 = newIsoLanguage();
+        isolang1 = _repo.save(isolang1);
+
+        IsoLanguage isolang2 = newIsoLanguage();
+        isolang2 = _repo.save(isolang2);
+
+
+        assertEquals(isolang2, _repo.findOne(isolang2.getId()));
+        assertEquals(isolang1, _repo.findOne(isolang1.getId()));
+    }
+
+    @Test
+    public void testFindByCode() {
+        IsoLanguage isolang1 = newIsoLanguage();
+        isolang1 = _repo.save(isolang1);
+
+        IsoLanguage isolang2 = newIsoLanguage();
+        isolang2 = _repo.save(isolang2);
+
+        List<IsoLanguage> histories = _repo.findAllByCode(isolang1.getCode());
+
+        assertEquals(isolang1.getCode(), histories.get(0).getCode());
+
+        histories = _repo.findAllByCode(isolang2.getCode());
+
+        assertEquals(isolang2.getCode(), histories.get(0).getCode());
+    }
+
+    @Test
+    public void testFindByShortCode() {
+        IsoLanguage isolang1 = newIsoLanguage();
+        isolang1 = _repo.save(isolang1);
+
+        IsoLanguage isolang2 = newIsoLanguage();
+        isolang2 = _repo.save(isolang2);
+
+        List<IsoLanguage> histories = _repo.findAllByShortCode(isolang1.getShortCode());
+
+        assertEquals(isolang1.getShortCode(), histories.get(0).getShortCode());
+
+        histories = _repo.findAllByShortCode(isolang2.getShortCode());
+
+        assertEquals(isolang2.getShortCode(), histories.get(0).getShortCode());
+    }
+
+    private IsoLanguage newIsoLanguage() {
+        int val = _inc.incrementAndGet();
+        IsoLanguage lang = new IsoLanguage();
+        lang.setCode("cd"+val);
+        lang.setShortCode("c" + val);
+        return lang;
+    }
+
+}
