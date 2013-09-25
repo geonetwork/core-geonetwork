1d37071d8bb090ce7bdb1e815612ee9ea788b20d
François Prunayre <fxprunayre@gmail.com>
2013-09-25 17:30:52 +0200

    Fix if no theme defined for record.

--- web-client/src/main/resources/apps/sextant/js/cat.MetadataResultsView.js
+++ web-client/src/main/resources/apps/sextant/js/cat.MetadataResultsView.js
@@ -63,10 +63,10 @@ cat.MetadataResultsView = Ext.extend(GeoNetwork.MetadataResultsView, {
     	if(type == 'wms') {
 	    	var c = link.split('|');
 	    	
-	    	var theme = this.getStore().getAt(this.curId).get("sextantTheme")[0].value;
+	    	var theme = this.getStore().getAt(this.curId).get("sextantTheme")[0];
 	    	if(theme) {
 	    		var translationStore = Ext.getCmp('E_sextantTheme').storeLabel;
-	    		var idx = translationStore.find('name',theme, null, false);
+	    		var idx = translationStore.find('name',theme.value, null, false);
 	            if(idx >= 0 && translationStore.getAt(idx).get('label')) {
 	            	Ext.get(Ext.query('input[id*=layergroup]')[0]).dom.value = 
 	            		translationStore.getAt(idx).get('label');
