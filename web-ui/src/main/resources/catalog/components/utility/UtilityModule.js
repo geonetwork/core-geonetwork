cdf6dbb38196a110a69199e7d952eda59a93a7a9
Gravin <florent.gravin@camptocamp.com>
2013-11-07 00:18:11 +0100

    reset files that shouldnt have been commited

--- web-ui/src/main/resources/catalog/components/utility/UtilityModule.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityModule.js
@@ -2,10 +2,8 @@
   goog.provide('gn_utility');
 
   goog.require('gn_utility_service');
-  goog.require('gn_utility_directive');
 
   angular.module('gn_utility', [
-    'gn_utility_service',
-    'gn_utility_directive'
+    'gn_utility_service'
   ]);
 })();
