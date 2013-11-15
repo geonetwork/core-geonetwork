4c7b8faad7209fb8f3c0d51ec26a43788f359fe7
François Prunayre <fxprunayre@gmail.com>
2013-11-15 17:03:00 +0100

    Refactor / merge thesaurus directives.

--- web-ui/src/main/resources/catalog/components/thesaurus/ThesaurusDirective.js
+++ web-ui/src/main/resources/catalog/components/thesaurus/ThesaurusDirective.js
@@ -1,7 +1,74 @@
 (function() {
-  goog.provide('gn_keyword_selector');
+  goog.provide('gn_thesaurus_directive');
 
-  var module = angular.module('gn_keyword_selector', []);
+  var module = angular.module('gn_thesaurus_directive', []);
+
+  /**
+     *
+     *
+     */
+  module.directive('gnThesaurusSelector',
+      ['$http', '$rootScope', '$timeout',
+       'gnThesaurusService', 'gnMetadataManagerService',
+       function($http, $rootScope, $timeout,
+       gnThesaurusService, gnMetadataManagerService) {
+
+         return {
+           restrict: 'A',
+           replace: true,
+           transclude: true,
+           scope: {
+             mode: '@gnThesaurusSelector',
+             elementName: '@',
+             elementRef: '@',
+             domId: '@'
+           },
+           templateUrl: '../../catalog/components/thesaurus/' +
+           'partials/thesaurusselector.html',
+           link: function(scope, element, attrs) {
+             scope.thesaurus = {};
+             scope.snippet = null;
+             scope.snippetRef = null;
+
+             // TODO: Remove from list existing thesaurus
+             // in the record ?
+             gnThesaurusService.getThesaurusList().then(
+             function(data) {
+               // TODO: Sort them
+               scope.thesaurus = data;
+             });
+
+             scope.add = function() {
+               $rootScope.$broadcast('AddElement',
+                   scope.elementRef, scope.elementName, scope.domId, 'before');
+             };
+
+             scope.addThesaurus = function(thesaurusIdentifier) {
+               gnThesaurusService
+               .getThesaurusSnippet(thesaurusIdentifier).then(
+               function(data) {
+                 // Add the fragment to the form
+                 scope.snippet = '<' + scope.elementName +
+                 " xmlns:gmd='http://www.isotc211.org/2005/gmd'>" +
+                 data + '</' + scope.elementName + '>';
+                 scope.snippetRef = '_X' + scope.elementRef +
+                 '_' + scope.elementName.replace(':', 'COLON');
+
+                 // FIXME : time out may not be the best options.
+                 // We need to have the form updated before saving
+                 // edits
+                 $timeout(function() {
+                   // Save the metadata and refresh the form
+                   $rootScope.$broadcast('SaveEdits', true);
+                 }, 200);
+
+               });
+               return false;
+             };
+           }
+         };
+       }]);
+
 
   /**
      *
@@ -27,11 +94,9 @@
              currentTransformation: '@'
            },
            templateUrl: '../../catalog/components/thesaurus/' +
-           'keywordselector/partials/' +
-           'keywordselector.html',
+           'partials/keywordselector.html',
            link: function(scope, element, attrs) {
 
-             console.log(scope);
              scope.max = 200;
              scope.filter = null;
              scope.results = null;
--- web-ui/src/main/resources/catalog/components/thesaurus/ThesaurusModule.js
+++ web-ui/src/main/resources/catalog/components/thesaurus/ThesaurusModule.js
@@ -3,13 +3,11 @@
 
 
 
-  goog.require('gn_keyword_selector');
-  goog.require('gn_thesaurus_selector');
   goog.require('gn_thesaurus_service');
+  goog.require('gn_thesaurus_directive');
 
   angular.module('gn_thesaurus', [
     'gn_thesaurus_service',
-    'gn_thesaurus_selector',
-    'gn_keyword_selector'
+    'gn_thesaurus_directive'
   ]);
 })();
