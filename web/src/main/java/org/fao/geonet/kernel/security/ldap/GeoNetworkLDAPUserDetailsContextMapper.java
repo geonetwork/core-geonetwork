05c1868ea7edbe8f9f5eafc11ed2c13ef68de0b2
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2013-12-09 11:39:11 +0100

    fix proposal for Mantis bug #15253
    Re-plugging back the bean ldapUserContextMapper onto
    GeoNetworkLDAPUserDetailsContextMapper java class.

--- web/src/main/java/org/fao/geonet/kernel/security/ldap/GeoNetworkLDAPUserDetailsContextMapper.java
+++ web/src/main/java/org/fao/geonet/kernel/security/ldap/GeoNetworkLDAPUserDetailsContextMapper.java
@@ -25,7 +25,6 @@ package org.fao.geonet.kernel.security.ldap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
