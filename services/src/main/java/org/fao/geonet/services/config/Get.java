ad6c9dc7a5d93130257ba6a55bf406d71c8a21bd
María Arias de Reyna <maria.arias@geocat.net>
2013-12-19 15:36:22 +0100

    Return xml by default

--- services/src/main/java/org/fao/geonet/services/config/Get.java
+++ services/src/main/java/org/fao/geonet/services/config/Get.java
@@ -23,35 +23,60 @@
 
 package org.fao.geonet.services.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jeeves.interfaces.Service;
+import jeeves.server.ServiceConfig;
+import jeeves.server.context.ServiceContext;
+
+import org.fao.geonet.GeonetContext;
+import org.fao.geonet.Util;
+import org.fao.geonet.services.config.bean.Setting;
+import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.kernel.setting.SettingManager;
 import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //=============================================================================
 
-@Controller
-@RequestMapping(value = "/srv/eng")
 /**
- *  Return the list of languages
- *
+ * Return the list of languages
+ * 
  */
-public class Get {
+@Controller
+public class Get implements Service {
 
 	@Autowired
 	private SettingManager sm;
 
-	@RequestMapping(value = "/admin.config.list")
+	@RequestMapping(value = "/{lang}/admin.config.list", produces="application/xml")
 	public @ResponseBody
-	Element exec(
+	List<Setting> exec(
+			@PathVariable String lang,
 			@RequestParam(required = false, defaultValue = "false") Boolean asTree)
 			throws Exception {
 
 		Element system = sm.getAllAsXML(asTree);
-		return system;
+		XMLOutputter outp = new XMLOutputter();
+		String s = outp.outputString(system);
+
+		System.out.println("---");
+		System.out.println(s);
+		System.out.println("---");
+
+		List<Setting> res = new ArrayList<Setting>();
+		Setting set = new Setting();
+		set.setName("sdf");
+		res.add(set);
+
+		return res;
 	}
 
 	public SettingManager getSm() {
@@ -60,5 +85,18 @@ public class Get {
 
 	public void setSm(SettingManager sm) {
 		this.sm = sm;
+	}
+
+	public void init(String appPath, ServiceConfig params) throws Exception {
+	}
+
+	public Element exec(Element params, ServiceContext context)
+			throws Exception {
+		GeonetContext gc = (GeonetContext) context
+				.getHandlerContext(Geonet.CONTEXT_NAME);
+		boolean asTree = Util.getParam(params, "asTree", "true").equals("true");
+
+		Element system = gc.getBean(SettingManager.class).getAllAsXML(asTree);
+		return system;
 	}
 }
