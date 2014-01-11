faff86db219ee72232f36e9ba922308db227ec54
François Prunayre <fxprunayre@gmail.com>
2014-01-11 12:25:36 +0100

    Add utility function for myocean use case.

--- web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/widgets/editor/EditorTools.js
+++ web-client/src/main/resources/apps/js/GeoNetwork/lib/GeoNetwork/widgets/editor/EditorTools.js
@@ -117,6 +117,22 @@ GeoNetwork.editor.EditorTools = {
             });
         }
         scope.newMetadataWindow.show();
+    },
+	// Return an array of the selected opion values
+	// select is an HTML select element
+    getSelectValues: function (select) {
+	   var result = [];
+	   var options = select && select.options;
+	   var opt;
+	
+	   for (var i=0, iLen=options.length; i<iLen; i++) {
+	     opt = options[i];
+	
+	     if (opt.selected) {
+	       result.push(opt.value || opt.text);
+	     }
+	   }
+	   return result;
     }
 };
 
