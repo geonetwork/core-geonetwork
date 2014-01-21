5e32f256cc4a61ba7b23d0326b396255e1799ecb
François Prunayre <fxprunayre@gmail.com>
2014-01-21 15:42:09 +0100

    Add server side selection mode.

--- web-ui/src/main/resources/catalog/components/search/searchmanager/SearchResultsDirective.js
+++ web-ui/src/main/resources/catalog/components/search/searchmanager/SearchResultsDirective.js
@@ -3,8 +3,8 @@
 
   var module = angular.module('gn_search_form_results_directive', []);
 
-  module.directive('gnSearchFormResults', [
-    function() {
+  module.directive('gnSearchFormResults', ['gnSearchManagerService',
+    function(gnSearchManagerService) {
 
       var activeClass = 'active';
 
@@ -53,9 +53,66 @@
                   scope.selection.push(md);
                 }
               };
+            } else {
+              scope.select = function(md) {
+                if (scope.options.selection.mode.indexOf('multiple') >= 0) {
+                  if (md['geonet:info'].selected === false) {
+                    md['geonet:info'].selected = true;
+                    gnSearchManagerService.select(md['geonet:info'].uuid)
+                      .then(updateSelectionNumber);
+                  } else {
+                    md['geonet:info'].selected = false;
+                    gnSearchManagerService.unselect(md['geonet:info'].uuid)
+                      .then(updateSelectionNumber);
+                  }
+                }
+                else {
+                  // TODO: clear selection ?
+                  console.log("Single selection is not supported in remote mode.");
+//                  md['geonet:info'].selected = true;
+//                  gnSearchManagerService.select(md['geonet:info'].uuid)
+//                    .then(updateSelectionNumber);
+                }
+              };
             }
           }
-
+          
+          var updateSelectionNumber = function (data) {
+            scope.selection = {
+                length: data[0]
+            };
+          };
+          
+          scope.selectAll = function (all) {
+            angular.forEach(scope.searchResults.records, function (md) {
+              md['geonet:info'].selected = all;
+            });
+            if (all) {
+              gnSearchManagerService.selectAll().then(updateSelectionNumber);
+            } else {
+              gnSearchManagerService.selectNone().then(updateSelectionNumber);
+            }
+          }
+          
+          /**
+           * If local, selection is handled in an array on the client
+           * if not, selection is handled on server side and
+           * search results contains information if a record is selected or not.
+           */
+          scope.isSelected = function (md) {
+            var targetUuid = md['geonet:info'].uuid;
+            var selected = false;
+            if (scope.options.selection.mode.indexOf('local') >= 0) {
+              angular.forEach(scope.selection, function (md) {
+                if (md['geonet:info'].uuid === targetUuid) {
+                  selected = true;
+                }
+              });
+            } else {
+              selected = md['geonet:info'].selected;
+            }
+            return selected;
+          }
           // Event on new search result
           // compute page number for pagination
           scope.$watchCollection('searchResults.records', function() {
@@ -71,7 +128,7 @@
           scope.paginationInfo = {
             pages: -1,
             currentPage: 1,
-            hitsPerPage: 2
+            hitsPerPage: 5
           };
         }
       };
