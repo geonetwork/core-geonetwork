1af7339e2ccce1a3af122390ad309db51ce83cdd
mtreguer <mickael.treguer@ifremer.fr>
2014-01-31 17:35:06 +0100

    fixed function for mantis #18570

--- web-client/src/main/resources/apps/sextant/js/cat.ipc.js
+++ web-client/src/main/resources/apps/sextant/js/cat.ipc.js
@@ -3,39 +3,71 @@ Ext.namespace('cat');
 cat.ipc = function() {
 	
 	var getViewerMenuItem = function() {
-		var result = null;
-		var navItems = Ext.each(Ext.query("#navigation a"), function(){
-			if (this.innerHTML.toUpperCase().indexOf("GEOVIEWER") != -1) {	
-				result = [];
-				result["obj"] = this;
-				result["key"] = "Geoviewer";
-			} else if (this.innerHTML.toUpperCase().indexOf("VIEWER") != -1) {	
-				result = [];
-				result["obj"] = this;
-				result["key"] = "Viewer";
-			} else if (this.innerHTML.toUpperCase().indexOf("VISUALISATEUR") != -1) {
-				result = [];
-				result["obj"] = this;
-				result["key"] = "Visualisateur";
-			}
-		});
-		return result;
+            //first looking for the function defined in liferay's hook,
+            //if not found then use the redefined method from sextant with last changes.
+            if(window.getViewerMenuItem){
+                return window.getViewerMenuItem();
+            }else {
+                var navItems = document.getElementById("navigation").getElementsByTagName("a");
+                var result = [];
+                for (var i = 0; i < navItems.length; i++) {
+                    var item = navItems[i];
+                    var navItem;
+                    if(item.innerHTML.indexOf("<span>") != -1){
+                        navItem = item.getElementsByTagName("span")[0];
+                    }else {
+                        navItem = item;
+                    }
+                    if (navItem.innerHTML.toUpperCase().indexOf("GEOVIEWER") != -1) {
+                        result["obj"] = navItem;
+                        result["key"] = "Geoviewer";
+                        return result;
+                    }else if (navItem.innerHTML.toLowerCase().indexOf("géoviewer") != -1) {
+                        result["obj"] = navItem;
+                        result["key"] = "Géoviewer";
+                        return result;
+                    } else if (navItem.innerHTML.toUpperCase().indexOf("VIEWER") != -1) {
+                        result["obj"] = navItem;
+                        result["key"] = "Viewer";
+                        return result;
+                    } else if (navItem.innerHTML.toUpperCase().indexOf("VISUALISEUR") != -1) {
+                        result["obj"] = navItem;
+                        result["key"] = "Visualiseur";
+                        return result;
+                    }
+                }
+                return null;
+            }
 	};
 	
 	var getPanierMenuItem = function() {
-		var result = null;
-		var navItems = Ext.each(Ext.query("#navigation a"), function(){
-			if (this.innerHTML.toUpperCase().indexOf("PANIER") != -1) {	
-				result = [];
-				result["obj"] = this;
-				result["key"] = "Panier";
-			} else if (this.innerHTML.toUpperCase().indexOf("BASKET") != -1) {
-				result = [];
-				result["obj"] = this;
-				result["key"] = "Basket";
-			}
-		});
-		return result;
+            //first looking for the function defined in liferay's hook,
+            //if not found then use the redefined method from sextant with last changes.
+            if(window.getPanierMenuItem){
+		return window.getPanierMenuItem();
+            }else {
+                var navItems = document.getElementById("navigation").getElementsByTagName("a");
+                var result = [];
+                for (var i = 0; i < navItems.length; i++) {
+                    var item = navItems[i];
+                    var navItem;
+                    if(item.innerHTML.indexOf("<span>") != -1){
+                        navItem = item.getElementsByTagName("span")[0];
+                    }else {
+                        navItem = item;
+                    }
+                    if (navItem.innerHTML.toUpperCase().indexOf("PANIER") != -1) {
+                        result["obj"] = navItem;
+                        result["key"] = "Panier";
+                        return result;
+                    } else if (navItem.innerHTML.toUpperCase().indexOf("BASKET") != -1) {
+                        result["obj"] = navItem;
+                        result["key"] = "Basket";
+                        return result;
+                    }
+                }
+                return null;
+            }
 	};
 	
 	return	{
@@ -46,7 +78,7 @@ cat.ipc = function() {
 				var nbLayersInput = Ext.query("span[id*=nbPanierLayers]")[0];
 				var nbLayers = nbLayersInput.innerHTML;
 				if (nbLayers != '0') {
-					panierMenuItem["obj"].innerHTML = panierMenuItem["key"] + " (" + nbLayers + ")";
+					panierMenuItem["obj"].innerHTML = panierMenuItem["key"] + " (+" + nbLayers + ")";
 				}
 			}
 			
