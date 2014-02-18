3bebd3d66b10ec8f7d6d44b53f1f00d45a04839a
François Prunayre <fxprunayre@gmail.com>
2014-02-18 11:51:19 +0100

    Module to generate JS documentation.

---
+++ web-ui-docs/config.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-ngdocs');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
+    ngdocs : {
+      options : {
+        dest: 'target/jsdocs',
+        scripts : [ 'angular.js', '../src.js' ],
+        html5Mode : true,
+        startPage : '/api'
+      },
+      api : {
+        src : [ '../web-ui/src/main/resources/catalog/components/**/*.js' ],
+        title : 'API Documentation'
+      },
+    },
+    connect : {
+      options : {
+        keepalive : true
+      },
+      server : {}
+    },
+    clean : [ 'target/jsdocs' ]
+  });
+
+//  grunt.registerTask('default', [ 'clean', 'ngdocs', 'connect' ]);
+  grunt.registerTask('default', [ 'clean', 'ngdocs']);
+
+};
