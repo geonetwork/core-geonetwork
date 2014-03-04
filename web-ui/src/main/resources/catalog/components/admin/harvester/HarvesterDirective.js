04b1d4efe588c835aa49434b0ed4a3807379ad59
François Prunayre <fxprunayre@gmail.com>
2014-03-04 10:35:08 +0100

    Harvester admin / Fix JS error on saving CSW config with no filter. Set default icon.

--- web-ui/src/main/resources/catalog/components/admin/harvester/HarvesterDirective.js
+++ web-ui/src/main/resources/catalog/components/admin/harvester/HarvesterDirective.js
@@ -23,13 +23,14 @@
             'identification.html',
         link: function(scope, element, attrs) {
           scope.lang = 'eng'; // FIXME
-          $http.get('admin.harvester.info@json?type=icons')
+          $http.get('admin.harvester.info@json?type=icons', {cache: true})
           .success(function(data) {
                 scope.icons = data[0];
               });
           // $http.get('admin.usergroups.list@json?id=' + 1)
           //          .success(function(data) {
-          $http.get('admin.group.list@json').success(function(data) {
+          $http.get('admin.group.list@json', {cache: true})
+            .success(function(data) {
             scope.groups = data !== 'null' ? data : null;
           });
           scope.setIcon = function(i) {
@@ -145,7 +146,8 @@
                }
              };
              function loadGroups() {
-               $http.get('info@json?type=groups').success(function(data) {
+               $http.get('info@json?type=groups', {cache: true})
+                 .success(function(data) {
                  scope.groups = data !== 'null' ? data.group : null;
                });
              }
