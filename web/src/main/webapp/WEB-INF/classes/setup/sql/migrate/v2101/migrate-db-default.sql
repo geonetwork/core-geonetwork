33b78eb7bf7b791acc5a13dbf92d07db018ff202
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-03-19 06:51:42 +0100

    update database migration script to be all REPORT_ONLY schematron rules.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2101/migrate-db-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2101/migrate-db-default.sql
@@ -72,16 +72,17 @@ ALTER TABLE public.schematrondes
   OWNER TO "www-data";
 
 
+
 INSERT INTO public.schematron (id, file, schemaname) VALUES (1, 'schematron-rules-inspire-strict.disabled.xsl', 'iso19139.che');
 INSERT INTO public.schematron (id, file, schemaname) VALUES (2, 'schematron-rules-bgdi.required.xsl', 'iso19139.che');
 INSERT INTO public.schematron (id, file, schemaname) VALUES (3, 'schematron-rules-geobasisdatensatz.required.xsl', 'iso19139.che');
 
-INSERT INTO public.schematroncriteriagroup (name, schematronid, requirement) VALUES ('Lichtenstein', 1, 'REQUIRED');
-INSERT INTO public.schematroncriteriagroup (name, schematronid, requirement) VALUES ('BGDI', 2, 'REQUIRED');
-INSERT INTO public.schematroncriteriagroup (name, schematronid, requirement) VALUES ('Geobasisdatensatz', 3, 'REQUIRED');
+INSERT INTO public.schematroncriteriagroup (name, schematronid, requirement) VALUES ('DefaultInspireStrict', 1, 'REPORT_ONLY');
+INSERT INTO public.schematroncriteriagroup (name, schematronid, requirement) VALUES ('BGDI', 2, 'REPORT_ONLY');
+INSERT INTO public.schematroncriteriagroup (name, schematronid, requirement) VALUES ('Geobasisdatensatz', 3, 'REPORT_ONLY');
 
--- INSERT INTO public.schematroncriteria (id, type, value, group_name, group_schematronid) VALUES (1, 'GROUP', '', 'Lichtenstein', 1);
 INSERT INTO public.schematroncriteria (id, type, value, group_name, group_schematronid) VALUES (1, 'XPATH', '*//gmd:keyword/gco:CharacterString/text() = ''BGDI''__OR__*//gmd:keyword/gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString/text() = ''BGDI''', 'BGDI', 2);
 INSERT INTO public.schematroncriteria (id, type, value, group_name, group_schematronid) VALUES (2, 'GROUP', '42', 'BGDI', 2);
 INSERT INTO public.schematroncriteria (id, type, value, group_name, group_schematronid) VALUES (3, 'XPATH', '*//gmd:keyword/gco:CharacterString/text() = ''Geobasisdatensatz''__OR__*//gmd:keyword/gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString/text() = ''Geobasisdatensatz''', 'Geobasisdatensatz', 3);
 INSERT INTO public.schematroncriteria (id, type, value, group_name, group_schematronid) VALUES (4, 'GROUP', '42', 'Geobasisdatensatz', 3);
+INSERT INTO public.schematroncriteria (id, type, value, group_name, group_schematronid) VALUES (5, 'ALWAYS_ACCEPT', '', 'DefaultInspireStrict', 1);
