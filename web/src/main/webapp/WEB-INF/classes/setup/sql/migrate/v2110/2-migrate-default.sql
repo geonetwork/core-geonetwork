6ac0df163636dd8e1371afe06a12251698c2a904
François Prunayre <fxprunayre@gmail.com>
2014-04-22 08:34:48 +0200

    SQL Migration / Update dc editor config.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2110/2-migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2110/2-migrate-default.sql
@@ -172,7 +172,7 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES 
   ('map/proj4js', '[{"code":"EPSG:2154","value":"+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"}]', 0, 9591, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES
-  ('metadata/editor/schemaConfig', '{"iso19110":{"defaultTab":"default","displayToolTip":false,"related":{"display":true,"readonly":true,"categories":["dataset"]},"validation":{"display":true}},"iso19139":{"defaultTab":"inspire","displayToolTip":false,"related":{"display":true,"categories":[]},"suggestion":{"display":true},"validation":{"display":true}},"dublin-core":{"defaultTab":"default","related":{"display":true,"readonly":true,"categories":[]},}}', 0, 10000, 'n');
+  ('metadata/editor/schemaConfig', '{"iso19110":{"defaultTab":"default","displayToolTip":false,"related":{"display":true,"readonly":true,"categories":["dataset"]},"validation":{"display":true}},"iso19139":{"defaultTab":"inspire","displayToolTip":false,"related":{"display":true,"categories":[]},"suggestion":{"display":true},"validation":{"display":true}},"dublin-core":{"defaultTab":"default","related":{"display":true,"readonly":false,"categories":["parent"]},}}', 0, 10000, 'n');
 
 
 ALTER TABLE StatusValues ADD displayorder int;
