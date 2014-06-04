0ec2c34312da75543fcb6b60184d69d3e349be5c
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-06-04 21:13:44 +0200

    Migrated Category services to MVC

--- services/src/test/java/org/fao/geonet/services/category/UpdateTest.java
+++ services/src/test/java/org/fao/geonet/services/category/UpdateTest.java
@@ -1,16 +1,15 @@
 package org.fao.geonet.services.category;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import jeeves.server.context.ServiceContext;
-
 import org.fao.geonet.domain.MetadataCategory;
-import org.fao.geonet.domain.Pair;
 import org.fao.geonet.exceptions.MissingParameterEx;
 import org.fao.geonet.repository.MetadataCategoryRepository;
 import org.fao.geonet.services.AbstractServiceIntegrationTest;
-import org.jdom.Element;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.fao.geonet.services.category.CategoryUpdateResponse.Operation;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test for Category Update service
@@ -20,48 +19,41 @@ import org.junit.Test;
 
 public class UpdateTest extends AbstractServiceIntegrationTest {
 
-	@Test
-    public void updateCategoryTest() throws Exception {
-    	Update updateCategoryController = new Update();
 
-    	@SuppressWarnings("unchecked")
-		Element params = createParams(Pair.write("id", "1"), Pair.write("name", "updatecategorytest_renamed"));
-    	ServiceContext srvctx = createServiceContext();
-    	Element ret = updateCategoryController.exec(params, srvctx);
-    	assertNotNull(ret.getChild("operation"));
-    	assertTrue(ret.getChildText("operation").equals("updated"));
+    @Autowired
+    private MetadataCategoryRepository categoryRepository;
+    @Autowired
+    private Update updateCategoryController;
+
+    @Test
+    public void updateCategoryTest() throws Exception {
+
+        final String categoryName = "updatecategorytest_renamed";
+        CategoryUpdateResponse ret = updateCategoryController.exec(1, categoryName);
+    	assertEquals(1, ret.getOperations().size());
+    	assertTrue(ret.getOperations().contains(Operation.updated));
     	// Checks that the category has correctly been saved using JPA
-        final MetadataCategoryRepository categoryRepository = srvctx.getBean(MetadataCategoryRepository.class);
         MetadataCategory category = categoryRepository.findOne(1);
-        assertTrue(category.getName().equals("updatecategorytest_renamed"));
+        assertTrue(category.getName().equals(categoryName));
     }
 
 	@Test
     public void createCategoryTest() throws Exception {
-    	Update updateCategoryController = new Update();
 
-    	@SuppressWarnings("unchecked")
-		Element params = createParams(Pair.write("name", "createdcategory_updatetest"));
-    	ServiceContext srvctx = createServiceContext();
-    	Element ret = updateCategoryController.exec(params, srvctx);
-    	assertNotNull(ret.getChild("operation"));
+        final String name = "createdcategory_updatetest";
+    	CategoryUpdateResponse ret = updateCategoryController.exec(null, name);
+    	assertEquals(1, ret.getOperations().size());
     	
-    	assertTrue(ret.getChildText("operation").equals("added"));
+    	assertTrue(ret.getOperations().contains(Operation.added));
     	// Checks that the category has correctly been saved using JPA
-        final MetadataCategoryRepository categoryRepository = srvctx.getBean(MetadataCategoryRepository.class);
-        MetadataCategory category = categoryRepository.findOneByName("createdcategory_updatetest");
+        MetadataCategory category = categoryRepository.findOneByName(name);
         assertTrue(category != null);
     }
 
 	@Test
     public void badParameterCategoryTest() throws Exception {
-    	Update updateCategoryController = new Update();
-
-    	@SuppressWarnings("unchecked")
-		Element params = createParams(Pair.write("not_expected_parameter", "not_expected_string"), Pair.write("id", "42"));
-    	ServiceContext srvctx = createServiceContext();
     	try {
-    		updateCategoryController.exec(params, srvctx);
+    		updateCategoryController.exec(42, null);
     	} catch (Throwable e) {
     		// Should complain of a missing parameter "name"
     		assertTrue (e instanceof MissingParameterEx);
