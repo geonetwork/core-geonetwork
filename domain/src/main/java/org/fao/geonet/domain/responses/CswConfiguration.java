a7205eaedd03e087ac94efc6eb607f6cf5566426
Jose Garcia <jose.garcia@geocat.net>
2014-06-04 20:33:56 +0200

    CSW services migrated to Spring MVC

---
+++ domain/src/main/java/org/fao/geonet/domain/responses/CswConfiguration.java
@@ -0,0 +1,43 @@
+package org.fao.geonet.domain.responses;
+
+
+import org.fao.geonet.domain.CswCapabilitiesInfoField;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(name = "cswcapabilitiesinfofield")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CswConfiguration {
+
+    private boolean cswEnabled;
+    private boolean cswMetadataPublic;
+    private int cswContactId;
+
+    @XmlElement(name = "record")
+    private List<CswCapabilitiesInfoField> capabilitiesInfoFields;
+
+    public void setCswEnabled(boolean cswEnabled) {
+        this.cswEnabled = cswEnabled;
+    }
+
+    public void setCswMetadataPublic(boolean cswMetadataPublic) {
+        this.cswMetadataPublic = cswMetadataPublic;
+    }
+
+    public int getCswContactId() {
+        return cswContactId;
+    }
+
+    public void setCswContactId(int cswContactId) {
+        this.cswContactId = cswContactId;
+    }
+
+    public List<CswCapabilitiesInfoField> getCapabilitiesInfoFields() {
+        return capabilitiesInfoFields;
+    }
+
+    public void setCapabilitiesInfoFields(List<CswCapabilitiesInfoField> capabilitiesInfoFields) {
+        this.capabilitiesInfoFields = capabilitiesInfoFields;
+    }
+}
