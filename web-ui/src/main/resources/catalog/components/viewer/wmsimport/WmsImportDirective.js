4bee2c20071d3890f28848e342a419df0aae2e73
Gravin <florent.gravin@camptocamp.com>
2014-06-18 22:54:39 +0200

    set wmsimport as directive

--- web-ui/src/main/resources/catalog/components/viewer/wmsimport/WmsImportDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/wmsimport/WmsImportDirective.js
@@ -24,11 +24,23 @@
       scope: {
         map: '=gnWmsImportMap'
       },
-      link: function (scope, element, attrs) {
-        scope.gnOwsCapabilities = gnOwsCapabilities;
-        scope.selection = [];
+      controller: function($scope){
+        this.addLayer = function(getCapLayer) {
 
-        scope.params = {};
+          if (getCapLayer) {
+              var layer = getCapLayer;
+              return gnMap.addWmsToMap($scope.map, {
+                    LAYERS: layer.name
+                  }, {
+                    url: $scope.url,
+                    label: layer.title,
+                    extent: gnOwsCapabilities.getLayerExtentFromGetCap($scope.map, layer)
+                  }
+              );
+          }
+        };
+      },
+      link: function (scope, element, attrs) {
 
         //TODO: remove
         scope.servicesList = [
@@ -37,53 +49,10 @@
           'http://www.ifremer.fr/services/wms/oceanographie_physique'
         ];
         scope.load = function (url) {
-          scope.searchFilter = '';
-          scope.selection = [];
           gnOwsCapabilities.getCapabilities(url)
             .then(function (layers) {
               scope.layers = layers;
             });
-        };
-
-        scope.select = function (layer) {
-          if (scope.selection.indexOf(layer) < 0) {
-            scope.selection.push(layer);
-          }
-          else {
-            scope.selection.splice(scope.selection.indexOf(layer), 1);
-          }
-        };
-
-        scope.addLayer = function(getCapLayer, isPreview) {
-
-          if (getCapLayer) {
-
-            try {
-              var layer = getCapLayer;
-
-              return gnMap.addWmsToMap(scope.map,
-                {
-                  LAYERS: layer.name
-                },
-                {
-                  url: scope.params.url,
-                  label: layer.title,
-                  extent: gnOwsCapabilities.getLayerExtentFromGetCap(scope.map, layer)
-                }
-              );
-
-            } catch (e) {
-              scope.userMessage = $translate('add_wms_layer_failed') +
-                e.message;
-              return null;
-            }
-          }
-        };
-
-        scope.loadLayers = function(layers) {
-          angular.forEach(layers, function(layer) {
-            scope.addLayer(layer);
-          });
         };
       }
     };
@@ -95,18 +64,18 @@
    *
    * @description
    * Directive to manage a collection of nested layers from
-   * the capabilities document.
+   * the capabilities document. This directive works with
+   * gnCapTreeElt directive.
    */
   module.directive('gnCapTreeCol', [
     function () {
       return {
-        restrict: "E",
+        restrict: 'E',
         replace: true,
         scope: {
-          collection: '=',
-          selection: '='
+          collection: '='
         },
-        template: "<ul><gn-cap-tree-elt ng-repeat='member in collection' member='member' selection='selection'></gn-cap-tree-elt></ul>"
+        template: "<ul class='list-group'><gn-cap-tree-elt ng-repeat='member in collection' member='member'></gn-cap-tree-elt></ul>"
       }
     }]);
 
@@ -124,24 +93,20 @@
     function ($compile) {
     return {
       restrict: "E",
+      require: '^gnWmsImport',
       replace: true,
       scope: {
         member: '='
       },
-      template: "<li data-ng-click='select(member)'>{{member.title}}</li>",
+      template: "<li class='list-group-item'><label><input type='checkbox' data-ng-model='inmap' data-ng-change='select()'>{{member.title}}</label></li>",
       link: function (scope, element, attrs, controller) {
         if (angular.isArray(scope.member.nestedLayers)) {
-          element.append("<gn-cap-tree-col collection='member.nestedLayers' selection='selection'></gn-cap-tree-col>");
-          $compile(element.contents())(scope)
+          element.append("<gn-cap-tree-col class='list-group' collection='member.nestedLayers'></gn-cap-tree-col>");
+          $compile(element.contents())(scope);
         }
-        scope.select = function (layer) {
-          if (scope.selection.indexOf(layer) < 0) {
-            scope.selection.push(layer);
-          }
-          else {
-            scope.selection.splice(scope.selection.indexOf(layer), 1);
-          }
-        };
+        scope.select = function() {
+          controller.addLayer(scope.member);
+        }
       }
     }
   }]);
