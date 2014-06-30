bbc1163268e4ffa9abe99842627207e803791107
Simon Pigot <Simon.Pigot@csiro.au>
2014-06-30 22:08:41 +1000

    Include search on status for metadata workflow

--- web-client/src/main/resources/apps/html5ui/js/search/SearchApp.js
+++ web-client/src/main/resources/apps/html5ui/js/search/SearchApp.js
@@ -238,6 +238,8 @@ GeoNetwork.searchApp = function() {
                 		catalogue.services.getSources, catalogue.services.logoUrl, true);
         		var groupField = GeoNetwork.util.SearchFormTools.getGroupField(
                 		catalogue.services.getGroups, true);
+        		var statusField = GeoNetwork.util.SearchFormTools.getStatusField(
+                		catalogue.services.getStatus, true);
         		var metadataTypeField = GeoNetwork.util.SearchFormTools
                 		.getMetadataTypeField(true);
         		var categoryField = GeoNetwork.util.SearchFormTools.getCategoryField(
@@ -266,7 +268,7 @@ GeoNetwork.searchApp = function() {
 						    .getServiceTypeField(true);
 
             advancedCriteria.push(themekeyField, orgNameField,
-										catalogueField, groupField, metadataTypeField, 
+										catalogueField, groupField, statusField, metadataTypeField, 
 										categoryField, validField, spatialTypes, denominatorField,
                     ownerField, isHarvestedField, siteId);
 
