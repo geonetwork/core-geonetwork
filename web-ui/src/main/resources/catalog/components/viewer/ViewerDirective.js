fbe40dc747824c8aabd4667a2939f21604a2f74c
Florent gravin <florent.gravin@camptocamp.com>
2014-07-09 17:09:57 +0200

    add directive to manage tool buttons

--- web-ui/src/main/resources/catalog/components/viewer/ViewerDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/ViewerDirective.js
@@ -21,18 +21,84 @@
         link: function (scope, element, attrs) {
         }
       };
-    }])
-    .directive('gnMap',
-    function () {
+    }]);
+  module.directive('goBtnGroup', function() {
+    return {
+      restrict: 'A',
+      controller: function($scope) {
+        var buttonScopes = [];
+
+        this.activate = function(btnScope) {
+          angular.forEach(buttonScopes, function(b) {
+            if (b != btnScope) {
+              b.ngModelSet(b, false);
+            }
+          });
+        };
+
+        this.addButton = function(btnScope) {
+          buttonScopes.push(btnScope);
+        };
+      }
+    };
+  })
+      .directive('goBtn', function($parse) {
+        return {
+          require: ['?^goBtnGroup','ngModel'],
+          restrict: 'A',
+          //replace: true,
+          scope: true,
+          link: function (scope, element, attrs, ctrls) {
+            var buttonsCtrl = ctrls[0], ngModelCtrl = ctrls[1];
+            var ngModelGet = $parse(attrs['ngModel']);
+            var cls = attrs['goBtn'];
+            scope.ngModelSet = ngModelGet.assign;
+
+            if(buttonsCtrl) buttonsCtrl.addButton(scope);
+
+            //ui->model
+            element.bind('click', function () {
+              scope.$apply(function () {
+                ngModelCtrl.$setViewValue(!ngModelCtrl.$viewValue);
+                ngModelCtrl.$render();
+              });
+            });
+
+            //model -> UI
+            ngModelCtrl.$render = function () {
+              if (buttonsCtrl && ngModelCtrl.$viewValue) {
+                buttonsCtrl.activate(scope);
+              }
+              if(cls != '') {
+                element.toggleClass(cls, ngModelCtrl.$viewValue);
+              }
+            };
+          }
+        };
+      });
+
+  module.directive('gnViewerToolbutton', ['gnHttp',
+    function (gnHttp) {
       return {
         restrict: 'A',
-        scope: {
-          map: '=gnMapMap'
-        },
         link: function (scope, element, attrs) {
-          scope.map.setTarget('map');
+          element.bind('click', function() {
+            if (element.hasClass('active')) {
+              console.log('toolbutton inactive');
+              element.removeClass('active');
+              $(element.attr('rel')).addClass('force-hide')
+            } else {
+              $('.btn').removeClass('active');
+              element.addClass('active');
+              $('.panel-tools').addClass('force-hide');
+              $(element.attr('rel')).removeClass('force-hide')
+            }
+
+            scope.$apply(function() {
+            });
+          });
+
         }
       };
-    });
-
+    }])
 })();
--- web-ui/src/main/resources/catalog/components/viewer/ViewerModule.js
+++ web-ui/src/main/resources/catalog/components/viewer/ViewerModule.js
@@ -77,58 +77,8 @@
       ['$scope', 'gnMeasure',
         function($scope, gnMeasure) {
           $scope.mInteraction = gnMeasure.create($scope.map, $scope.measureObj, $scope);
+          $scope.activeTools = {};
         }
       ]);
 
-  module.directive('goBtnGroup', function() {
-        return {
-          restrict: 'A',
-          controller: function($scope) {
-            var buttonScopes = [];
-
-            this.activate = function(btnScope) {
-              angular.forEach(buttonScopes, function(b) {
-                if (b != btnScope) {
-                  b.ngModelSet(b, false);
-                }
-              });
-            };
-
-            this.addButton = function(btnScope) {
-              buttonScopes.push(btnScope);
-            };
-          }
-        };
-      })
-      .directive('goBtn', function($parse) {
-        return {
-          require: ['?^goBtnGroup','ngModel'],
-          restrict: 'A',
-          //replace: true,
-          scope: true,
-          link: function (scope, element, attrs, ctrls) {
-            var buttonsCtrl = ctrls[0], ngModelCtrl = ctrls[1];
-            var ngModelGet = $parse(attrs['ngModel']);
-            scope.ngModelSet = ngModelGet.assign;
-
-            if(buttonsCtrl) buttonsCtrl.addButton(scope);
-
-            //ui->model
-            element.bind('click', function () {
-              scope.$apply(function () {
-                ngModelCtrl.$setViewValue(!ngModelCtrl.$viewValue);
-                ngModelCtrl.$render();
-              });
-            });
-
-            //model -> UI
-            ngModelCtrl.$render = function () {
-              if (buttonsCtrl && ngModelCtrl.$viewValue) {
-                buttonsCtrl.activate(scope);
-              }
-              element.toggleClass('active', ngModelCtrl.$viewValue);
-            };
-          }
-        };
-      });
 })();
