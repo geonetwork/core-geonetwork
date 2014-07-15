c978cd1d4a6a15fffe7273982a0cd0cc2f96c911
etj <etj@geo-solutions.it>
2014-07-15 20:22:58 +0200

    Restore Shibboleth auth on 2.10.x

---
+++ web/src/main/java/org/fao/geonet/kernel/security/shibboleth/ShibbolethPreAuthFilter.java
@@ -0,0 +1,139 @@
+/*
+ *  Copyright (C) 2014 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.fao.geonet.kernel.security.shibboleth;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import jeeves.server.ProfileManager;
+import jeeves.server.resources.ResourceManager;
+import jeeves.utils.Log;
+import jeeves.utils.SerialFactory;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.kernel.security.GeonetworkUser;
+import org.fao.geonet.kernel.security.shibboleth.ShibbolethUserUtils.MinimalUser;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+/**
+ * Preauthentication Filter for Shibboleth.
+ *
+ * See {@link org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter}
+ *
+ * @author ETj (etj at geo-solutions.it)
+ */
+public class ShibbolethPreAuthFilter extends GenericFilterBean implements ApplicationContextAware {
+    
+	private ApplicationContext applicationContext;
+
+    private ShibbolethUserConfiguration configuration;
+
+    public ShibbolethPreAuthFilter() {
+        if (Log.isDebugEnabled(Geonet.LOG_AUTH)) {
+            Log.debug(Geonet.LOG_AUTH, "Setting up Shibboleth pre-auth filter");
+        }
+    }
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+        if (Log.isDebugEnabled(Geonet.LOG_AUTH)) {
+            Log.debug(Geonet.LOG_AUTH, "Performing Shibboleth pre-auth check. Existing auth is " + SecurityContextHolder.getContext().getAuthentication());
+        }
+
+        String username = "UNIDENTIFIED";
+        try {
+            Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
+            MinimalUser minimal =  MinimalUser.create(request, configuration);
+
+            if(minimal != null) { // for logging only
+                username = minimal.getUsername();
+
+                if (Log.isDebugEnabled(Geonet.LOG_AUTH)) {
+                    Log.debug(Geonet.LOG_AUTH, "Found Shibboleth credentials for user " + username);
+                }
+            } else {
+                if (Log.isDebugEnabled(Geonet.LOG_AUTH)) {
+                    Log.debug(Geonet.LOG_AUTH, "No Shibboleth credentials found");
+                }
+            }
+
+            if(currentUser == null && // not in any user context
+                    minimal != null) {   // valid headers found
+
+                if (Log.isDebugEnabled(Geonet.LOG_AUTH)) {
+                    Log.debug(Geonet.LOG_AUTH, "Trying to authenticate via Shibboleth...");
+                }
+
+//                if (! currentUser.getName().equals(minimal.getUsername()) ) {
+//                    Log.info(Geonet.LOG_AUTH, "Pre-authenticated principal has changed from "
+//                            + currentUser.getName()+" to "
+//                            + minimal.getUsername() + " and will be reauthenticated");
+//                }
+
+                ResourceManager resourceManager = applicationContext.getBean(ResourceManager.class);
+                ProfileManager profileManager = applicationContext.getBean(ProfileManager.class);
+                SerialFactory serialFactory = applicationContext.getBean(SerialFactory.class);
+                GeonetworkUser user = ShibbolethUserUtils.setupUser(request, resourceManager, profileManager, serialFactory, configuration);
+
+                if(user != null) {
+
+                    if (Log.isDebugEnabled(Geonet.LOG_AUTH)) {
+                        Log.debug(Geonet.LOG_AUTH, "Shibboleth user found " + user.getUsername() + " with authorities: " + user.getAuthorities());
+                    }
+
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities() ) ;
+                    auth.setDetails(user);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+
+                    Log.info(Geonet.LOG_AUTH, "User '"+user.getUsername()+"' properly authenticated via Shibboleth");
+                } else {
+                    Log.warning(Geonet.LOG_AUTH, "Error in GN shibboleth precedures handling user '" + username);
+                }
+            }
+        } catch (Exception ex) {
+            Log.warning(Geonet.LOG_AUTH, "Error during Shibboleth login for user " + username + ": " + ex.getMessage(), ex );
+        }
+
+		chain.doFilter(request, response);
+	}
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    public void setConfiguration(ShibbolethUserConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+
+}
--- web/src/main/java/org/fao/geonet/services/login/ShibLogin.java
+++ web/src/main/java/org/fao/geonet/services/login/ShibLogin.java
@@ -1,5 +1,5 @@
 //=============================================================================
-//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===	Copyright (C) 2001-2014 Food and Agriculture Organization of the
 //===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
 //===	and United Nations Environment Programme (UNEP)
 //===
@@ -23,31 +23,25 @@
 
 package org.fao.geonet.services.login;
 
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-
-import jeeves.exceptions.UserLoginEx;
-import jeeves.interfaces.Service;
-import jeeves.resources.dbms.Dbms;
-import jeeves.server.ProfileManager;
 import jeeves.server.ServiceConfig;
 import jeeves.server.context.ServiceContext;
-import jeeves.utils.Util;
-
-import org.fao.geonet.GeonetContext;
-import org.fao.geonet.constants.Geonet;
-import org.fao.geonet.kernel.security.GeonetworkUser;
-import org.fao.geonet.kernel.setting.SettingManager;
-import org.fao.geonet.lib.Lib;
 import org.fao.geonet.services.NotInReadOnlyModeService;
 import org.jdom.Element;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 //=============================================================================
 
 /**
+ * This service does nothing.
+ *
+ * This service used to handle the logic for performing shibboleth login.
+ * Since now the authentication procedures are driven by Spring, this logic has been moved to
+ * a class that conforms to the Spring specifications.
+ *
+ * @see org.fao.geonet.kernel.security.shibboleth.ShibbolethPreAuthFilter
+ *
+ * <b>Previous doc follows</b>
+ * <hr/>
+ *
  * <code>ShibLogin</code> processes the result of a Shibboleth (or other external 
  * authentication system) login. The user will have already been challenged for 
  * userid and password and will have had their credentials placed in the HTTP 
@@ -56,11 +50,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
  * @author James Dempsey <James.Dempsey@csiro.au>
  * @version $Revision: 1629 $
  */
+@Deprecated
 public class ShibLogin extends NotInReadOnlyModeService
 {
-	private static final String VIA_SHIBBOLETH = "Via Shibboleth";
-	private static final String SHIBBOLETH_FLAG = "SHIBBOLETH";
-
 
 	//--------------------------------------------------------------------------
 	//---
@@ -78,151 +70,10 @@ public class ShibLogin extends NotInReadOnlyModeService
 	//---
 	//--------------------------------------------------------------------------
 
-	/* (non-Javadoc)
-	 * @see jeeves.interfaces.Service#exec(org.jdom.Element, jeeves.server.context.ServiceContext)
-	 */
 	public Element serviceSpecificExec(Element params, ServiceContext context) throws Exception
 	{
-		// Get the header keys to lookup from the settings
-		GeonetContext  gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
-		SettingManager sm = gc.getSettingManager();
-		String prefix = "system/shib";
-		String usernameKey = sm.getValue(prefix + "/attrib/username");
-		String surnameKey = sm.getValue(prefix + "/attrib/surname");
-		String firstnameKey = sm.getValue(prefix + "/attrib/firstname");
-		String profileKey = sm.getValue(prefix + "/attrib/profile");
-        String groupKey = sm.getValue(prefix + "/attrib/group");
-        String defGroup =  sm.getValue(prefix +"/defaultGroup");
-
-		// Read in the data from the headers
-		Map<String, String> headers = context.getHeaders();
-		String username = Util.getHeader(headers, usernameKey, "");
-		String surname = Util.getHeader(headers, surnameKey, "");
-		String firstname = Util.getHeader(headers, firstnameKey, "");
-		String profile = Util.getHeader(headers, profileKey, "");
-        String group    = Util.getHeader(headers, groupKey, "");
-	
-		// Make sure the profile name is an exact match
-		profile = context.getProfileManager().getCorrectCase(profile);
-		if (profile.equals(""))
-		{
-			profile = ProfileManager.GUEST;
-		}
-
-        if (group.equals("")) {
-            group = defGroup;
-        }
-
-		Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
-
-		// Create or update the user 
-		if (username != null && username.length() > 0)
-		{
-			if (username.length() > 256) // only accept the first 256 chars
-			{
-				username = username.substring(0, 256);
-			}
-			updateUser(context, dbms, username, surname, firstname, profile, group);
-		}
-
-		//--- attempt to load user from db
-
-		String query = "SELECT * FROM Users WHERE username = ? ";
-
-		List list = dbms.select(query, username).getChildren();
-
-		if (list.size() == 0)
-			throw new UserLoginEx(username);
-
-		Element userEl = (Element) list.get(0);
-	
-		GeonetworkUser user = new GeonetworkUser(context.getProfileManager(), username, userEl);
-		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), null, user.getAuthorities() ) ;
-		authentication.setDetails(user);
-
-		if(SecurityContextHolder.getContext() == null) {
-			SecurityContextHolder.createEmptyContext();
-		}
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-
-		context.info("User '"+ username +"' logged in as '"+ user.getProfile() +"'");
-
-		return new Element("ok");
-	}
-
-
-	//--------------------------------------------------------------------------
-
-	/**
-	 * Update the user to match the provided details, or create a new record
-	 * for them if they don't have one already.
-	 * 
-	 * @param context The Jeeves ServiceContext
-	 * @param dbms The database connection.
-	 * @param username The user's username, must not be null.
-	 * @param surname The surname of the user
-	 * @param firstname The first name of the user.
-	 * @param profile The name of the user type.
-	 * @throws SQLException If the record cannot be saved.
-	 */
-	private void updateUser(ServiceContext context, Dbms dbms, String username,
-			String surname, String firstname, String profile, String group) throws SQLException
-	{
-        boolean groupProvided = ((group != null) && (!(group.equals(""))));
-        int groupId = -1;
-        int userId = -1;
-
-        if (groupProvided) {
-            String query = "SELECT id FROM Groups WHERE name=?";
-
-            List list  = dbms.select(query, group).getChildren();
-
-            if (list.isEmpty()) {
-                groupId = context.getSerialFactory().getSerial(dbms, "Groups");
-
-                query = "INSERT INTO GROUPS(id, name) VALUES(?,?)";
-                dbms.execute(query, groupId, group);
-                Lib.local.insert(dbms, "Groups", groupId, group);
-
-            } else {
-                String gi = ((Element) list.get(0)).getChildText("id");
-
-                groupId = Integer.valueOf(gi).intValue();
-            }
-        }
-		//--- update user information into the database
-
-		String query = "UPDATE Users SET name=?, surname=?, profile=?, password=?, authtype=? WHERE username=?";
-
-		int res = dbms.execute(query, firstname, surname, profile, VIA_SHIBBOLETH, SHIBBOLETH_FLAG, username);
-
-		//--- if the user was not found --> add it
-
-		if (res == 0)
-		{
-			userId = context.getSerialFactory().getSerial(dbms, "Users");
-
-			query = 	"INSERT INTO Users(id, username, name, surname, profile, password, authtype) "+
-						"VALUES(?,?,?,?,?,?,?)";
-
-			dbms.execute(query, userId, username, firstname, surname, profile, VIA_SHIBBOLETH, SHIBBOLETH_FLAG);
-
-            if (groupProvided) {
-                String query2 = "SELECT count(*) as numr FROM UserGroups WHERE groupId=? and userId=?";
-                List list  = dbms.select(query2, groupId, userId).getChildren();
-
-                String count = ((Element) list.get(0)).getChildText("numr");
-
-                 if (count.equals("0")) {
-                     query = "INSERT INTO UserGroups(userId, groupId) "+
-                             "VALUES(?,?)";
-                     dbms.execute(query, userId, groupId);
-
-                 }
-            }
-		}
-
-		dbms.commit();
-	}
+		context.info("Shibboleth login service does nothing.");
+		return new Element("error");
+    }
 
 }
