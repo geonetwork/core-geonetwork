b9396a807936ea2ee35180aee0d685c36b2dc489
Florent gravin <florent.gravin@camptocamp.com>
2014-08-07 11:15:40 +0200

    Manage 'auto' for ncwms

--- web-ui/src/main/resources/catalog/components/viewer/ncwms/NcwmsDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/ncwms/NcwmsDirective.js
@@ -27,36 +27,7 @@
         link: function (scope, element, attrs) {
 
           var drawInteraction, featureOverlay;
-          scope.constants = gnNcWmsConst;
-
-          // 2010-12-06T12:00:00.000Z/2010-12-31T12:00:00.000Z
-          var parseTimeSeries = function(s) {
-            s = s.trim();
-            var as = s.split('/');
-            scope.tsfromD = moment(new Date(as[0])).format('YYYY-MM-DD');
-            scope.tstoD = moment(new Date(as[1])).format('YYYY-MM-DD');
-          };
-
-          var getDimensionValue = function(ncInfo, name) {
-            var value;
-            if (angular.isArray(ncInfo.Dimension)) {
-              for (var i = 0; i < ncInfo.Dimension.length; i++) {
-                if (ncInfo.Dimension[i].name == name) {
-                  value = ncInfo.Dimension[i].values;
-                  break;
-                }
-              }
-            }
-            else if (angular.isObject(ncInfo.Dimension) &&
-                ncInfo.Dimension.name == name) {
-              value = ncInfo.Dimension.values[0] ||
-                  ncInfo.Dimension.values;
-            }
-            return value;
-          };
-          parseTimeSeries(getDimensionValue(scope.layer.ncInfo, 'time'));
-
-          scope.elevations = getDimensionValue(scope.layer.ncInfo, 'elevation').split(',');
+          var map = scope.map;
 
           /**
            * Just manage active button in ui.
@@ -145,26 +116,57 @@
 
             scope.params = scope.layer.getSource().getParams() || {};
 
-            var colors = scope.layer.getSource().getParams().COLORSCALERANGE.split(',');
-            scope.colorscalerange = [colors[0], colors[1]];
+            scope.colorRange = {
+              step: 1
+            };
+            scope.timeSeries = gnNcWms.parseTimeSeries(gnNcWms.getDimensionValue(scope.layer.ncInfo, 'time'));
+            scope.elevations = gnNcWms.getDimensionValue(scope.layer.ncInfo, 'elevation').split(',');
+
+
+            // Get maxExtent color ranges
+            gnNcWms.getColorRangesBounds(scope.layer,
+                scope.layer.ncInfo.EX_GeographicBoundingBox.join(',')).success(function(data) {
+
+                  var min = Number((data.min).toFixed(5));
+                  var max = Number((data.max).toFixed(5));
+
+                  angular.extend(scope.colorRange, {min: min, max: max});
+                  scope.colorscalerange = [min, max];
+                  scope.onColorscaleChange(scope.colorscalerange);
+            });
 
             if(angular.isUndefined(scope.params.LOGSCALE)) {
               scope.params.LOGSCALE = false;
             }
           };
 
+          /**
+           *  Get bounds of color range depending on the current extent.
+           *  Called when user wlick on 'auto' button.
+           *  Update the slider values to this bounds.
+           */
+          scope.setAutoColorranges = function(evt) {
+            $(evt.target).addClass('fa fa-spinner');
+            gnNcWms.getColorRangesBounds(scope.layer,
+                ol.proj.transform(map.getView().calculateExtent(map.getSize()),
+                    map.getView().getProjection(), 'EPSG:4326').join(',')).success(function(data) {
+                  scope.colorscalerange = [data.min, data.max];
+                  scope.onColorscaleChange(scope.colorscalerange);
+                  $(evt.target).removeClass('fa fa-spinner');
+                });
+          };
+
+          /**
+           * Call when the input of the double slider get change.
+           * The input is an array of 2 values. It updates the layer
+           * with `COLORSCALERANGE` params and refreshes it.
+           * @param v the colorange array
+           */
           scope.onColorscaleChange = function(v) {
-            var colorange;
-            if(angular.isString(v) && v == 'auto') {
-              colorange = v;
-            }
-            else if(angular.isArray(v) && v.length == 2) {
+            if(angular.isArray(v) && v.length == 2) {
               colorange = v[0] + ',' + v[1];
-            }
-            if(colorange) {
               scope.params.COLORSCALERANGE = colorange;
-              scope.updateLayerParams();
-            }
+              scope.updateLayerParams();}
           };
 
           scope.updateLayerParams = function() {
