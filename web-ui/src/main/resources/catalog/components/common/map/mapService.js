3b7cbaf0dbd2a442d5e6bc31db9594b961cb6fd2
Florent gravin <florent.gravin@camptocamp.com>
2014-08-07 11:16:59 +0200

    Add missing 5th coordinates to polygon from extent

--- web-ui/src/main/resources/catalog/components/common/map/mapService.js
+++ web-ui/src/main/resources/catalog/components/common/map/mapService.js
@@ -62,7 +62,8 @@
                      [extent[0], extent[1]],
                      [extent[0], extent[3]],
                      [extent[2], extent[3]],
-                     [extent[2], extent[1]]
+                     [extent[2], extent[1]],
+                     [extent[0], extent[1]]
               ]
             ];
           },
