ab8ed3618b1705b229526e99ad36a4e6512e2978
Florent gravin <florent.gravin@camptocamp.com>
2014-09-12 16:05:42 +0200

    Add controller and config for geocat view

---
+++ web-ui/src/main/resources/catalog/js/custom/geocat/config.js
@@ -0,0 +1,75 @@
+(function() {
+
+  goog.provide('gn_search_geocat_config');
+
+  var module = angular.module('gn_search_geocat_config', []);
+
+  module.config(['gnSearchSettings',
+
+    function(searchSettings) {
+
+      searchSettings.olStyles = {
+        drawBbox: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'rgba(255,0,0,1)',
+            width: 2
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(255,0,0,0.3)'
+          })
+        }),
+        mdExtent: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'orange',
+            width: 2
+          })
+        }),
+        mdExtentHighlight:new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'orange',
+            width: 3
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(255,255,0,0.3)'
+          })
+        })
+      };
+
+      searchSettings.searchMap = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: [280274.03240585705, 6053178.654789996],
+          zoom: 0
+        })
+      });
+
+      /** Facets configuration */
+      searchSettings.facetsConfig = {
+        keyword: 'keywords',
+        orgName: 'orgNames',
+        denominator: 'denominator',
+        format: 'formats',
+        createDateYear: 'createDateYears'
+      };
+
+      /* Pagination configuration */
+      searchSettings.paginationInfo = {
+        hitsPerPage: 3
+      };
+
+      /* Custom templates for search result views */
+      searchSettings.resultViewTpls = [{
+        tplUrl: '../../catalog/components/search/resultsview/partials/viewtemplates/title.html',
+        tooltip: 'Simple',
+        icon: 'fa-list'
+      }, {
+        tplUrl: '../../catalog/components/search/resultsview/partials/viewtemplates/thumb.html',
+        tooltip: 'Thumbnails',
+        icon: 'fa-th-list'
+      }];
+    }]);
+})();
