ec073bfe592dfc80f21398561ecdf88189f0ba03
Florent gravin <florent.gravin@camptocamp.com>
2014-09-16 17:13:45 +0200

    delegate locale management to GnLocale in GnModule

--- web-ui/src/main/resources/catalog/js/GnModule.js
+++ web-ui/src/main/resources/catalog/js/GnModule.js
@@ -1,18 +1,8 @@
 (function() {
   goog.provide('gn');
 
-
-
-
-
-
-
-
-
-
-
-
   goog.require('gn_cat_controller');
+  goog.require('gn_locale');
   goog.require('gn_form_fields_directive');
   goog.require('gn_language_switcher');
   goog.require('gn_map');
@@ -26,8 +16,7 @@
 
   var module = angular.module('gn', [
     'ngRoute',
-    'pascalprecht.translate',
-    'angular-md5',
+    'gn_locale',
     'gn_language_switcher',
     'gn_utility',
     'gn_search_manager',
@@ -40,50 +29,5 @@
     'gn_needhelp',
     'gn_alert'
   ]);
-
-  module.constant('$LOCALES', ['core']);
-
-
-  module.factory('localeLoader', ['$http', '$q', function($http, $q) {
-    return function(options) {
-      var allPromises = [];
-      angular.forEach(options.locales, function(value, index) {
-        var langUrl = options.prefix +
-            options.key + '-' + value + options.suffix;
-
-        var deferredInst = $q.defer();
-        allPromises.push(deferredInst.promise);
-
-        $http({
-          method: 'GET',
-          url: langUrl
-        }).success(function(data, status, header, config) {
-          deferredInst.resolve(data);
-        }).error(function(data, status, header, config) {
-          deferredInst.reject(options.key);
-        });
-      });
-
-      // Finally, create a single promise containing all the promises
-      // for each app module:
-      var deferred = $q.all(allPromises);
-      return deferred;
-    };
-  }]);
-
-
-  // TODO: could be improved instead of putting this in all main modules ?
-  module.config(['$translateProvider', '$LOCALES',
-                 function($translateProvider, $LOCALES) {
-      $translateProvider.useLoader('localeLoader', {
-        locales: $LOCALES,
-        prefix: '../../catalog/locales/',
-        suffix: '.json'
-      });
-
-      var lang = location.href.split('/')[5].substring(0, 2) || 'en';
-      $translateProvider.preferredLanguage(lang);
-      moment.lang(lang);
-    }]);
 
 })();
