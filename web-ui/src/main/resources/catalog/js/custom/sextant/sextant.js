6e6b237e78f60a965bc88128292a7364c067a16d
Florent gravin <florent.gravin@camptocamp.com>
2014-09-16 11:54:53 +0200

    add sextant custom view

---
+++ web-ui/src/main/resources/catalog/js/custom/sextant/sextant.js
@@ -0,0 +1,82 @@
+(function() {
+
+  goog.provide('gn_search_sextant');
+
+  goog.require('gn_search');
+  goog.require('gn_search_sextant_config');
+
+  var module = angular.module('gn_search_sextant', ['gn_search', 'gn_search_sextant_config']);
+
+  module.controller('gnsSextant', [
+    '$scope',
+    '$location',
+    'suggestService',
+    '$http',
+    'gnSearchSettings',
+    'gnMap',
+    function($scope, $location, suggestService, $http, gnSearchSettings, gnMap) {
+
+      var viewerMap = gnSearchSettings.viewerMap;
+      var searchMap = gnSearchSettings.searchMap;
+
+      $scope.mainTabs = {
+        home :{
+          title: 'Home',
+          titleInfo: '',
+          active: true
+        },
+        search: {
+          title: 'Search',
+          titleInfo: '',
+          active: false
+        },
+        map:{
+          title: 'Map',
+          active: false
+        }};
+
+      $scope.addLayerToMap = function(number) {
+        $scope.mainTabs.map.titleInfo = '  (+' + number + ')';
+      };
+
+      $scope.$on('addLayerFromMd', function(evt, getCapLayer) {
+        gnMap.addWmsToMapFromCap(viewerMap, getCapLayer);
+      });
+
+      $scope.displayMapTab = function() {
+        if(viewerMap.getSize()[0] == 0 || viewerMap.getSize()[1] == 0){
+          setTimeout(function(){
+            viewerMap.updateSize();
+          }, 0);
+        }
+        $scope.mainTabs.map.titleInfo = '';
+      };
+
+///////////////////////////////////////////////////////////////////
+      $scope.getAnySuggestions = function(val) {
+        var url = suggestService.getUrl(val, 'anylight',
+            ('STARTSWITHFIRST'));
+
+        return $http.get(url, {
+        }).then(function(res){
+          return res.data[1];
+        });
+      };
+
+      $scope.$watch('searchObj.advancedMode', function(val) {
+        if(val && (searchMap.getSize()[0] == 0 || searchMap.getSize()[1] == 0)){
+          setTimeout(function(){
+            searchMap.updateSize();
+          }, 0);
+        }
+      });
+
+///////////////////////////////////////////////////////////////////
+
+      angular.extend($scope.searchObj, {
+        advancedMode: false,
+        viewerMap: viewerMap,
+        searchMap: searchMap
+      });
+    }]);
+})();
