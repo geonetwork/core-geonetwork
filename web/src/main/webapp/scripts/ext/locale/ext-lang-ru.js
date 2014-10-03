d8876ebd42046b02244ec1f2b300f94677d50e7e
Peter Riisager <peter.riisager@atkinsglobal.com>
2014-10-03 08:31:22 +0200

    Maven build runs

--- web/src/main/webapp/scripts/ext/locale/ext-lang-ru.js
+++ web/src/main/webapp/scripts/ext/locale/ext-lang-ru.js
@@ -3,8 +3,8 @@
  * Copyright(c) 2006-2009 Ext JS, LLC
  * licensing@extjs.com
  * http://www.extjs.com/license
- */
-﻿/*
+ *
+﻿ *
  * Russian translation
  * By ZooKeeper (utf-8 encoding)
  * 6 November 2007
