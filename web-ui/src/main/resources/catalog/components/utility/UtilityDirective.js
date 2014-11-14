05789aa603edea1e84801548624a0f1a33bf9ba9
Antoine Abt <antoine@abt.im>
2014-11-14 12:21:20 +0100

    Add ddCollapseText directive from https://github.com/doukasd/AngularJS-Components

--- web-ui/src/main/resources/catalog/components/utility/UtilityDirective.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityDirective.js
@@ -462,4 +462,52 @@
         }
       };
     }]);
+
+    module.directive('ddTextCollapse', ['$compile', function($compile) {
+      return {
+        restrict: 'A',
+        scope: true,
+        link: function(scope, element, attrs) {
+          // start collapsed
+          scope.collapsed = false;
+          // create the function to toggle the collapse
+          scope.toggle = function() {
+            scope.collapsed = !scope.collapsed;
+          };
+          // wait for changes on the text
+          attrs.$observe('ddTextCollapseText', function(text) {
+            // get the length from the attributes
+            var maxLength = scope.$eval(attrs.ddTextCollapseMaxLength);
+            if (text.length > maxLength) {
+              // split the text in two parts, the first always showing
+              var firstPart = String(text).substring(0, maxLength);
+              var secondPart = String(text).substring(maxLength, text.length);
+              // create some new html elements to hold the separate info
+              var firstSpan = $compile('<span>' + firstPart + '</span>')(scope);
+              var secondSpan = $compile('<span ng-if="collapsed">' + secondPart + '</span>')(scope);
+              var moreIndicatorSpan = $compile('<span ng-if="!collapsed">... </span>')(scope);
+              var lineBreak = $compile('<br ng-if="collapsed">')(scope);
+              var toggleButton = $compile(
+                '<span class="collapse-text-toggle" ng-click="toggle()">' +
+                '  <span ng-show="collapsed" translate>less</span>' +
+                '  <span ng-show="!collapsed" translate>more</span>' +
+                '</span>'
+                )(scope);
+              // remove the current contents of the element
+              // and add the new ones we created
+              element.empty();
+              element.append(firstSpan);
+              element.append(secondSpan);
+              element.append(moreIndicatorSpan);
+              element.append(lineBreak);
+              element.append(toggleButton);
+            }
+            else {
+              element.empty();
+              element.append(text);
+            }
+          });
+        }
+      };
+    }]);
 })();
