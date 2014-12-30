74efe24617fb1e08ec61882c9f9f0a3ba829255b
François Prunayre <fxprunayre@gmail.com>
2014-12-30 12:11:35 +0100

    Add SQL for region API settings.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.0.x/6.0.0/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.0.x/6.0.0/migrate-db-postgres.sql
@@ -1,1 +1,6 @@
 ALTER TABLE schematron RENAME file TO filename;
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/background', 'osm', 0, 9590, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/width', '500', 0, 9590, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/summaryWidth', '500', 0, 9590, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/mapproj', 'EPSG:3857', 0, 9590, 'n');
