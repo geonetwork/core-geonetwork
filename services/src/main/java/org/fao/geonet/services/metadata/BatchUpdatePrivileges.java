8118cd087e8a2f99925f32d7fa8d5aaec0142a05
Florent gravin <florent.gravin@camptocamp.com>
2015-01-12 19:31:11 +0100

    allow _content_type attribute to privileges service

--- services/src/main/java/org/fao/geonet/services/metadata/BatchUpdatePrivileges.java
+++ services/src/main/java/org/fao/geonet/services/metadata/BatchUpdatePrivileges.java
@@ -29,6 +29,7 @@ import jeeves.server.UserSession;
 import jeeves.server.context.ServiceContext;
 import org.fao.geonet.GeonetContext;
 import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.constants.Params;
 import org.fao.geonet.domain.Metadata;
 import org.fao.geonet.domain.Profile;
 import org.fao.geonet.kernel.AccessManager;
@@ -114,7 +115,8 @@ public class BatchUpdatePrivileges extends NotInReadOnlyModeService {
                     for (Element el : list) {
                         String name = el.getName();
 
-                        if (name.startsWith("_")) {
+                        if (name.startsWith("_") &&
+                                !Params.CONTENT_TYPE.equals(name)) {
                             StringTokenizer st = new StringTokenizer(name, "_");
 
                             String groupId = st.nextToken();
