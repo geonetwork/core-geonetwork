cfbce8782219724fb3fcc917c448133f828a6bd1
François Prunayre <fxprunayre@gmail.com>
2015-01-12 11:45:28 +0100

    Withheld configuration is now per schema defined (#23547).

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/5.2.x/5.2.4/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/5.2.x/5.2.4/migrate-db-postgres.sql
@@ -1,3 +1,9 @@
 UPDATE Settings SET value =
   '{"useOSM":true,"context":"","layer":{"url":"http://www2.demis.nl/mapserver/wms.asp?","layers":"Countries","version":"1.1.1"},"projection":"EPSG:3857","projectionList":[{"code":"EPSG:4326","label":"WGS84 (EPSG:4326)"},{"code":"EPSG:3857","label":"Google mercator (EPSG:3857)"}]}', 0, 9590, 'n')
   WHERE name = 'map/config';
+
+
+-- See https://github.com/geonetwork/core-geonetwork/commit/8ec082bc48c613b53acb06963d29c9a734482ba9
+-- withheld element filter is now configured as a per schema basis
+DELETE FROM Settings WHERE name = 'system/hidewithheldelements/enable';
+DELETE FROM Settings WHERE name = 'system/hidewithheldelements/keepMarkedElement';
