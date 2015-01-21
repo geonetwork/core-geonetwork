4856d854c413414af578c3d54b452aa0c3a50e21
María Arias de Reyna <maria.arias@geocat.net>
2015-01-21 16:56:06 +0100

    Make the editor reload when a new resource is added #690

--- web-ui/src/main/resources/catalog/components/admin/BatchService.js
+++ web-ui/src/main/resources/catalog/components/admin/BatchService.js
@@ -49,9 +49,11 @@
           gnEditor.save(false, true)
                 .then(function() {
                 gnHttp.callService('processMd', params).then(function(data) {
-                  var snippet = $(data.data);
-                  gnEditor.refreshEditorForm(snippet);
-                  defer.resolve(data);
+                  gnHttp.callService('edit', params).then(function(data) {
+                    var snippet = $(data.data);
+                    gnEditor.refreshEditorForm(snippet);
+                    defer.resolve(data);
+                  });
                 });
               });
           return defer.promise;
