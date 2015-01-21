d0c7f3383cca08f013ccb1a712ef9c28e70c5075
François Prunayre <fxprunayre@gmail.com>
2015-01-21 16:39:34 +0100

    Update version number to 3.0.0-SNAPSHOT.

--- docs/eng/users/source/conf.py
+++ docs/eng/users/source/conf.py
@@ -44,9 +44,9 @@ copyright = u'2010 Open Source Geospatial Foundation'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.11.0-SNAPSHOT'
+version = '3.0.0-SNAPSHOT'
 # The full version, including alpha/beta/rc tags.
-release = '2.11.0-SNAPSHOT'
+release = '3.0.0-SNAPSHOT'
 
 language = 'en'
 
