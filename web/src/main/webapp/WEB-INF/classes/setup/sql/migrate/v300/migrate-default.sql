e75f0b306b4b2509a91ff956258bc2221a13a460
Jose Garcia <jose.garcia@geocat.net>
2015-01-27 12:23:28 +0100

    Update migration scripts, related to #588 and #666

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/migrate-default.sql
@@ -6,3 +6,8 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/summaryWidth', '500', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/mapproj', 'EPSG:3857', 0, 9590, 'n');
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/proxy/ignorehostlist', NULL, 0, 560, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atom', 'disabled', 0, 7230, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atomSchedule', '0 0 0/24 ? * *', 0, 7240, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atomProtocol', 'INSPIRE-ATOM', 0, 7250, 'y');
