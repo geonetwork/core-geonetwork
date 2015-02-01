91f5f44b17eaf4c7a89eba37122f892fd161284b
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-02-01 13:20:32 +0100

    In status admin page add drop down with the index error message and details.

--- web-ui/src/main/resources/catalog/js/admin/DashboardStatusController.js
+++ web-ui/src/main/resources/catalog/js/admin/DashboardStatusController.js
@@ -21,7 +21,41 @@
         $scope.healthy = false;
         $scope.healthcheck = data;
       });
+      $scope.indexMessageTitle = function (md) {
+        return md.idxMsg.split("|")[0];
+      };
+      $scope.indexMessageReason = function (md) {
+        return md.idxMsg.split("|")[1];
+      };
+      $scope.indexMessageDetail = function (md) {
+        var maxLine = 80,
+            indentPattern = /(\s*).*/,
+            detail = md.idxMsg.split("|")[2],
+            lines = detail.split("\n");
 
+        detail = "";
+
+        var nextSpace = function (line) {
+          for (var j = maxLine; j < line.length; j++) {
+            if (' ' === line.charAt(j)) {
+              return j;
+            }
+          }
+          return line.length;
+        };
+
+        for (var i = 0; i < lines.length; i++) {
+          var line = lines[i];
+          var indent = indentPattern.exec(line)[1] + "    ";
+          while(line.length > maxLine) {
+            var ns = nextSpace(line);
+            detail += line.substring(0, ns) + "\n";
+            line = indent + line.substring(ns);
+          }
+          detail += line + "\n";
+        }
+        return detail;
+      };
       $scope.searchObj = {
         params: {
           _indexingError: 1,
