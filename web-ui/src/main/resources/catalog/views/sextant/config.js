c8c842671a08c0090c5a41c959e70fe6f2c2b6af
Florent gravin <florent.gravin@camptocamp.com>
2015-02-24 17:18:15 +0100

    update formatter list

--- web-ui/src/main/resources/catalog/views/sextant/config.js
+++ web-ui/src/main/resources/catalog/views/sextant/config.js
@@ -169,10 +169,7 @@
       searchSettings.formatter = {
         defaultUrl: 'md.format.xml?xsl=full_view&id=',
         list: [
-          {label: 'inspire', url: 'md.format.xml?xsl=xsl-view' +
-                '&view=inspire&id='},
-          {label: 'full', url: 'md.format.xml?xsl=xsl-view&view=advanced&id='},
-          {label: 'groovy', url: 'md.format.xml?xsl=full_view&id='}
+          {label: 'full', url: 'md.format.xml?xsl=full_view&id='}
         ]
         // TODO: maybe formatter config should depends
         // on the metadata schema.
