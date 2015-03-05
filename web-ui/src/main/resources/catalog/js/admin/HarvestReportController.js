8c9fb2ec5076b3882a1047038cae8593793fa886
bmaire <benoit.maire@capgemini.com>
2015-03-05 19:08:26 +0100

    add export CSV

--- web-ui/src/main/resources/catalog/js/admin/HarvestReportController.js
+++ web-ui/src/main/resources/catalog/js/admin/HarvestReportController.js
@@ -13,6 +13,25 @@
   module.controller('GnHarvestReportController', [
 	'$scope', '$http', '$translate', '$injector', '$rootScope', 'gnUtilityService',
 	function($scope, $http, $translate, $injector, $rootScope) {
-		
+		$scope.csvReport = function(event) {
+			var json = [];// [$translate('harvestName'), $translate('serviceUrl'), $translate('harvesterType'), $translate('totalNonTemplateMetadata')];
+			var table = document.getElementById("harvestReport");
+			var names = [];
+			for (var i = 0, row; row = table.rows[i]; i++) {
+			  var obj = {};
+			  for (var j = 0, col; col = row.cells[j]; j++) {
+			    if (i==0) {
+			      names[j] = col.innerText;   
+				} else {
+			      obj[names[j]] = col.innerText;
+				}
+			  }
+			  if (i!=0) {
+			    json[i-1] = obj;
+			  }
+			}
+
+			$scope.csvExport(json, event);
+		}
 	}]);
 })();
