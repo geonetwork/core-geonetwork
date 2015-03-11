415e1fbb7607c5d570600763c5bb28d6a5633bb1
François Prunayre <fxprunayre@gmail.com>
2015-03-11 17:43:46 +0100

    Map viewer / Add simple layer search #855.

---
+++ web-ui/src/main/resources/catalog/components/viewer/searchlayerformap/SearchLayerForMapDirective.js
@@ -0,0 +1,74 @@
+(function() {
+  goog.provide('gn_searchlayerformap_directive');
+
+  var module = angular.module('gn_searchlayerformap_directive', [
+  ]);
+
+  /**
+   * @ngdoc directive
+   * @name gn_searchlayerformap_directive.directive:gnSearchLayerForMap
+   *
+   * @description
+   * Panel to search for WMS layer describe in the catalog
+   */
+  module.directive('gnSearchLayerForMap', [
+    'gnOwsCapabilities',
+    'gnMap',
+    '$translate',
+    'Metadata',
+    'gnRelatedResources',
+    'gnSearchSettings',
+    function(gnOwsCapabilities, gnMap, $translate, Metadata,
+             gnRelatedResources, gnSearchSettings) {
+      return {
+        restrict: 'A',
+        replace: true,
+        templateUrl: '../../catalog/components/viewer/searchlayerformap/' +
+            'partials/searchlayerformap.html',
+        scope: {
+          map: '=gnSearchLayerForMap'
+        },
+        controller: ['$scope', function($scope) {
+          $scope.searchObj = {
+            permalink: false,
+            hitsperpageValues: gnSearchSettings.hitsperpageValues,
+            sortbyValues: gnSearchSettings.sortbyValues,
+            params: {
+              protocol: 'OGC:WMS*',
+              from: 1,
+              to: 9
+            }
+          };
+
+          $scope.paginationInfo = {
+            hitsPerPage: gnSearchSettings.hitsperpageValues[0]
+          };
+
+        }],
+        link: function(scope, element, attrs) {
+          scope.filterTopic = function(topic) {
+            delete scope.searchObj.params.topicCat;
+            scope.searchObj.params.topicCat = topic;
+          };
+          scope.addToMap = function(link) {
+            gnRelatedResources.getAction('WMS')(link);
+          };
+          scope.zoomToLayer = function(md) {
+            var extent = gnMap.getBboxFromMd(md);
+            if (extent) {
+              var proj = scope.map.getView().getProjection();
+              extent = ol.extent.containsExtent(proj.getWorldExtent(), extent) ?
+                  ol.proj.transformExtent(extent, 'EPSG:4326', proj) :
+                  proj.getExtent();
+              scope.map.getView().fitExtent(extent, scope.map.getSize());
+            }
+          };
+          scope.getMetadata = function(md) {
+            var m = new Metadata(md);
+            m.relevantLinks = m.getLinksByType('WMS');
+            return m;
+          };
+        }
+      };
+    }]);
+})();
