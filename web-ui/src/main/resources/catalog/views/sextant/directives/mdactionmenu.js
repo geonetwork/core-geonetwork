2c0798156decad22ad65f9cf4ec7ca5773196d62
Florent gravin <florent.gravin@camptocamp.com>
2015-03-30 14:40:17 +0200

    use sextant mdaction directive in formatter

---
+++ web-ui/src/main/resources/catalog/views/sextant/directives/mdactionmenu.js
@@ -0,0 +1,30 @@
+(function() {
+
+  goog.provide('sxt_mdactionmenu');
+
+  var module = angular.module('sxt_mdactionmenu', []);
+
+
+  module.directive('sxtMdActionsMenu', ['gnMetadataActions', '$http',
+    '$location', 'Metadata',
+    function(gnMetadataActions, $http, $location, Metadata) {
+      return {
+        restrict: 'A',
+        replace: true,
+        templateUrl: '../../catalog/views/sextant/directives/' +
+            'partials/mdactionmenu.html',
+        link: function linkFn(scope, element, attrs) {
+          scope.mdService = gnMetadataActions;
+          scope.md = scope.$eval(attrs.sxtMdActionsMenu);
+          if (!scope.md) {
+            var url = $location.url();
+            var uuid = url.substring(url.lastIndexOf('/') + 1);
+            $http.get('q?_uuid=' + uuid + '&fast=index&_content_type=json&buildSummary=false').success (function (resp) {
+              scope.md = new Metadata(resp.metadata);
+            });
+          }
+        }
+      };
+    }
+  ]);
+})();
