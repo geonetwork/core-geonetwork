836b8142127a4617d9f186442776c7fdd6d96e8d
Florent gravin <florent.gravin@camptocamp.com>
2015-04-08 11:46:47 +0200

    update facet config

--- web-ui/src/main/resources/catalog/components/search/facets/FacetsDirective.js
+++ web-ui/src/main/resources/catalog/components/search/facets/FacetsDirective.js
@@ -95,9 +95,9 @@
           for (var i = 0; i < groups.length; i++) {
             var o = groups[i];
             if (o.name == f['@name']) {
-              f['@name'] = o.label[lang];
+              f['@label'] = o.label[lang];
             }
-            f['name'] = f['@name'];
+            f['name'] = f['@label'];
           }
         });
       };
