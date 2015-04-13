188605c945a976b8325371368dc683127cce5b1d
Jeroen Bloemscheer <jeroen.bloemscheer@cgi.com>
2015-04-13 12:22:24 +0200

    Reverted DB migration issues

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2100/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2100/migrate-default.sql
@@ -10,10 +10,10 @@ DELETE FROM Settings WHERE id=80;
 
 -- New settings 
 INSERT INTO Settings VALUES (24,20,'securePort','8443');
-INSERT INTO Settings VALUES (1000956,1,'hidewithheldelements',NULL);
-INSERT INTO Settings VALUES (1000957,1000956,'enable','false');
-INSERT INTO Settings VALUES (1000958,1000956,'keepMarkedElement','true');
---INSERT INTO Settings VALUES (1000955,1000956,'ignored','true');
+INSERT INTO Settings VALUES (956,1,'hidewithheldelements',NULL);
+INSERT INTO Settings VALUES (957,956,'enable','false');
+INSERT INTO Settings VALUES (958,956,'keepMarkedElement','true');
+INSERT INTO Settings VALUES (955,956,'ignored','true');
 
 -- Version update
 UPDATE Settings SET value='2.10.0' WHERE name='version';
