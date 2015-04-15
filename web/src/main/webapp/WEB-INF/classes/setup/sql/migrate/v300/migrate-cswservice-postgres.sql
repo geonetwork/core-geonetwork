cce07b75dda25b385434e58f073af84ad2c8578c
François Prunayre <fxprunayre@gmail.com>
2015-04-15 13:51:55 +0200

    Add missing ;.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/migrate-cswservice-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/migrate-cswservice-postgres.sql
@@ -3,6 +3,6 @@ UPDATE ServiceParameters SET occur='+';
 
 create sequence serviceparameter_id_seq start with 1 increment by 1;
 alter table serviceparameters add column id integer;
-UPDATE serviceparameters SET ID=nextval('serviceparameter_id_seq')
+UPDATE serviceparameters SET ID=nextval('serviceparameter_id_seq');
 
-ALTER TABLE SERVICEPARAMETERS ADD PRIMARY KEY (id)
+ALTER TABLE SERVICEPARAMETERS ADD PRIMARY KEY (id);
