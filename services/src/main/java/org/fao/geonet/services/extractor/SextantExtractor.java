af730c710e5cb79f5fea1ef1f7eaa5ecf4d9bdb0
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2015-04-15 18:12:00 +0200

    Extractor - creating pretty-printed XML files in the correct directories
    Conflicts:
    	services/pom.xml

--- services/src/main/java/org/fao/geonet/services/extractor/SextantExtractor.java
+++ services/src/main/java/org/fao/geonet/services/extractor/SextantExtractor.java
@@ -1,9 +1,13 @@
 package org.fao.geonet.services.extractor;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.UUID;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -11,9 +15,11 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import jeeves.server.UserSession;
 import jeeves.server.context.ServiceContext;
 import jeeves.server.dispatchers.ServiceManager;
 
+import org.apache.commons.io.FileUtils;
 import org.fao.geonet.kernel.DataManager;
 import org.fao.geonet.services.extractor.mapping.LayerSpec;
 import org.json.JSONObject;
@@ -44,44 +50,72 @@ public class SextantExtractor {
 	private DataManager dataManager;
 	@Autowired
 	private ServiceManager serviceManager;
+	@Autowired
+	private String extractorPath;
 
-	// TODO: to be customized
-	private String path = "/tmp";
+	private File intranetPath, extranetPath, anonymousPath;
+
+	@PostConstruct
+	public void init() throws Exception {
+		// ensures directories are created
+		File exPathF  = new File(extractorPath + File.separator + "folders" + File.separator + "aTraiter");
+		intranetPath  = new File(exPathF, "intranet");
+		extranetPath  = new File(exPathF, "extranet");
+		anonymousPath = new File(exPathF, "anonymous");
+		FileUtils.forceMkdir(intranetPath);
+		FileUtils.forceMkdir(extranetPath);
+		FileUtils.forceMkdir(anonymousPath);
+	}
 
 	@RequestMapping(value = "/{lang}/extractor.doExtract", method = RequestMethod.POST)
 	public HttpEntity<byte[]> exec(@RequestBody LayerSpec[] jsonLayers, HttpServletRequest request) throws Exception {
 		JSONObject status = new JSONObject();
-		XmlMapper xmlMapper = new XmlMapper();
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			XmlMapper xmlMapper = new XmlMapper();
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-		out.write("<extract xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"extracteur.xsd\">".getBytes());
-		final ServiceContext serviceContext = ServiceContext.get();
+			out.write("<extract xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"extracteur.xsd\">"
+					.getBytes());
+			final ServiceContext serviceContext = ServiceContext.get();
 
-		out.write(String.format("<user lastname=\"%s\" firstname=\"%s\" mail=\"%s\" is_ifremer=\"%s\""
-				+ " uidNumber=\"%s\" login=\"%s\" />",
-				serviceContext.getUserSession().getName(),
-				serviceContext.getUserSession().getSurname(),
-				serviceContext.getUserSession().getEmailAddr(),
-				"true", // TODO ...
-				serviceContext.getUserSession().getUserId(),
-				serviceContext.getUserSession().getUsername()).getBytes());
-		out.write("<layers>".getBytes());
+			UserSession us = serviceContext.getUserSession();
+			boolean isAuthenticated = us.isAuthenticated();
 
-		for (LayerSpec l : jsonLayers) {
-			xmlMapper.writeValue(out, l);
+			if (isAuthenticated) {
+				out.write(String.format(
+						"<user lastname=\"%s\" firstname=\"%s\" mail=\"%s\" is_ifremer=\"%s\""
+								+ " uidNumber=\"%s\" login=\"%s\" />", us.getName(), us.getSurname(),
+						us.getEmailAddr(), us.getPrincipal().getOrganisation().equals("ifremer"), us.getUserId(),
+						us.getUsername()).getBytes());
+			}
+			out.write("<layers>".getBytes());
+			for (LayerSpec l : jsonLayers) {
+				xmlMapper.writeValue(out, l);
+			}
+			out.write("</layers>".getBytes());
+			out.write("</extract>".getBytes());
+			// Indent using the available XML lib
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			StreamResult result = new StreamResult(new StringWriter());
+			StreamSource source = new StreamSource(new StringReader(out.toString()));
+			transformer.transform(source, result);
+			String xmlString = result.getWriter().toString();
+
+			if (isAuthenticated) {
+				// TODO: intranet vs extranet ?
+				FileUtils.writeStringToFile(new File(anonymousPath, us.getEmailAddr() + "_" + UUID.randomUUID()
+						+ ".xml"), xmlString);
+			} else {
+				FileUtils.writeStringToFile(new File(anonymousPath, "anonymous_" + UUID.randomUUID() + ".xml"),
+						xmlString);
+			}
+			status.put("success", true);
+		} catch (Exception e) {
+			status.put("success", false);
+			status.put("reason", e.getMessage());
 		}
-		out.write("</layers></extract>".getBytes());
-
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-		//initialize StreamResult with File object to save to file
-		StreamResult result = new StreamResult(new StringWriter());
-		StreamSource source = new StreamSource(new StringReader(out.toString()));
-		transformer.transform(source, result);
-		String xmlString = result.getWriter().toString();
-		System.out.println(xmlString);
-
-		status.put("success", true);
 		return new HttpEntity<byte[]>(status.toString(4).getBytes());
 	}
 
