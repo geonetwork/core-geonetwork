f1189558240c4d85882995ad4ff73c9c0d692abb
François Prunayre <fxprunayre@gmail.com>
2015-04-16 03:49:05 +0200

    Language switcher should display not translated language name.

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -37,6 +37,9 @@
       // TODO : get list from server side
       $scope.langs = {'eng': 'en', 'dut': 'du', 'fre': 'fr',
         'ger': 'ge', 'kor': 'ko', 'spa': 'es'};
+      // Lang names to be displayed in language selector
+      $scope.langLabels = {'eng': 'English', 'dut': 'Nederlands',
+        'fre': 'Français', 'ger': 'Deutsch', 'kor': '한국의', 'spa': 'Español'};
       $scope.url = '';
       $scope.base = '../../catalog/';
       $scope.proxyUrl = gnGlobalSettings.proxyUrl;
