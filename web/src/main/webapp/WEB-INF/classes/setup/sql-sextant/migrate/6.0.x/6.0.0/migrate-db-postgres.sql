3dab912001a2c683ae00e97cb6d1ce14362e724d
François Prunayre <fxprunayre@gmail.com>
2015-04-16 11:54:23 +0200

    Add draft migration script for v6.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.0.x/6.0.0/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.0.x/6.0.0/migrate-db-postgres.sql
@@ -1,9 +1,33 @@
 ALTER TABLE schematron RENAME file TO filename;
 
+ALTER TABLE ServiceParameters ADD COLUMN occur varchar(1) default '+';
+UPDATE ServiceParameters SET occur='+';
+
+create sequence serviceparameter_id_seq start with 1 increment by 1;
+alter table serviceparameters add column id integer;
+UPDATE serviceparameters SET ID=nextval('serviceparameter_id_seq');
+
+ALTER TABLE SERVICEPARAMETERS ADD PRIMARY KEY (id);
+
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/background', 'osm', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/width', '500', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/summaryWidth', '500', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/mapproj', 'EPSG:3857', 0, 9590, 'n');
-INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadata/prefergrouplogo', 'y', 0, 9111, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/proxy/ignorehostlist', NULL, 0, 560, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atom', 'disabled', 0, 7230, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atomSchedule', '0 0 0/24 ? * *', 0, 7240, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atomProtocol', 'INSPIRE-ATOM', 0, 7250, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadata/prefergrouplogo', 'false', 2, 9111, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('map/isMapViewerEnabled', 'true', 2, 9592, 'n');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadata/allThesaurus', 'false', 2, 9160, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/ui/defaultView', 'sextant', 0, 10100, 'n');
 
+INSERT INTO settings (name, value, datatype, position, internal) VALUES ('system/server/log','log4j',0,250,'y');
+
+
+UPDATE Settings SET value='3.0.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
