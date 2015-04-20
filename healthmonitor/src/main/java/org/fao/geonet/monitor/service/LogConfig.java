faddbb4afc2a4ef8a0496cf903fec31c7d4d23eb
bmaire <benoit.maire@capgemini.com>
2015-04-20 11:08:07 +0200

    reindent with spaces

--- healthmonitor/src/main/java/org/fao/geonet/monitor/service/LogConfig.java
+++ healthmonitor/src/main/java/org/fao/geonet/monitor/service/LogConfig.java
@@ -22,127 +22,127 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * get last lines of log
- * @author bmaire
- *
- */
+* get last lines of log
+* @author bmaire
+*
+*/
 
 @Controller("/log")
 public class LogConfig {
-	private FileAppender fileAppender = null;
-	
-	private final String fileAppenderName = "fileAppender";
-	private final int maxLines = 200;
-	
-	//--------------------------------------------------------------------------
-	// ---
-	// --- Init
-	// ---
-	// --------------------------------------------------------------------------
-	@PostConstruct
-	public void init() throws Exception {
-		fileAppender = (FileAppender) Logger.getLogger(Geonet.GEONETWORK).getAppender(fileAppenderName);
-		
-		if (fileAppender == null) {
-			fileAppender = (FileAppender) Logger.getLogger(Log.JEEVES).getAppender(fileAppenderName);
-		}	
-
-	}
-	
-	@RequestMapping(value = "/{lang}/log/file", produces = {
-            MediaType.APPLICATION_OCTET_STREAM_VALUE})
-	@ResponseBody
-	public void getLog(HttpServletResponse response) throws IOException {
-		if (fileAppender != null) {
-			File file = new File(fileAppender.getFile());
-			
-			// create ZIP FILE
-			
-			String fname = String.valueOf(Calendar.getInstance().getTimeInMillis());
-		
-			// set headers for the response
-			response.setContentType("application/zip");
-			response.setContentLength((int) file.length());
-			String headerKey = "Content-Disposition";
-			String headerValue = String.format("attachment; filename=\"%s\"","export-log-" + fname + ".zip");
-			response.setHeader(headerKey, headerValue);
-		    
-			int read = 0;
-		    byte[] bytes = new byte[1024];
-		    ZipOutputStream zos = null;
-	    	ZipEntry ze;
-		    InputStream in = null;
-		    try {
-		    	zos = new ZipOutputStream(response.getOutputStream());
-				ze = new ZipEntry(file.getName());
-				zos.putNextEntry(ze);
-				in=new FileInputStream(file);
-		        while ((read = in.read(bytes)) != -1) {
-		            zos.write(bytes, 0, read);
-		        }
-		    } finally {
-		        in.close();
-		        zos.flush();
-		        zos.close(); 
-		    }
-		}
-	}
-	
-	@RequestMapping(value = "/{lang}/log/activity", produces = {
-            MediaType.TEXT_PLAIN_VALUE})
-	@ResponseBody
-	public String activity() {
-		String lastActivity = null;
-		
-	    if (fileAppender != null) {
-	    	lastActivity = readLastLines(new File(fileAppender.getFile()));
-	    }		
-        return lastActivity;
-	}
-	
-	private String readLastLines(File file) {
-		RandomAccessFile fileHandler = null;
-		try {
-			fileHandler = new RandomAccessFile( file, "r" );
-			long fileLength = fileHandler.length() - 1;
-			StringBuilder sb = new StringBuilder();
+    private FileAppender fileAppender = null;
     
-			int line = 0;
-			int readByte;
-			long filePointer;
-			for(filePointer = fileLength; filePointer != -1; filePointer--){
-				fileHandler.seek( filePointer );
-				readByte = fileHandler.readByte();
-				if( readByte == 0xA ) {
-					if (filePointer < fileLength) {
-						line = line + 1;
-					}
-				} else if(readByte == 0xD ) {
-					if (filePointer < fileLength-1) {
-						line = line + 1;
-					}
-				}
-				if (line >= maxLines) {
-				    break;
-				}
-				sb.append( ( char ) readByte );
-			}
+    private final String fileAppenderName = "fileAppender";
+    private final int maxLines = 200;
+    
+    //--------------------------------------------------------------------------
+    // ---
+    // --- Init
+    // ---
+    // --------------------------------------------------------------------------
+    @PostConstruct
+    public void init() throws Exception {
+        fileAppender = (FileAppender) Logger.getLogger(Geonet.GEONETWORK).getAppender(fileAppenderName);
+        
+        if (fileAppender == null) {
+            fileAppender = (FileAppender) Logger.getLogger(Log.JEEVES).getAppender(fileAppenderName);
+        }    
+        
+    }
+    
+    @RequestMapping(value = "/{lang}/log/file", produces = {
+        MediaType.APPLICATION_OCTET_STREAM_VALUE})
+    @ResponseBody
+    public void getLog(HttpServletResponse response) throws IOException {
+        if (fileAppender != null) {
+            File file = new File(fileAppender.getFile());
+            
+            // create ZIP FILE
+            
+            String fname = String.valueOf(Calendar.getInstance().getTimeInMillis());
+            
+            // set headers for the response
+            response.setContentType("application/zip");
+            response.setContentLength((int) file.length());
+            String headerKey = "Content-Disposition";
+            String headerValue = String.format("attachment; filename=\"%s\"","export-log-" + fname + ".zip");
+            response.setHeader(headerKey, headerValue);
+            
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            ZipOutputStream zos = null;
+            ZipEntry ze;
+            InputStream in = null;
+            try {
+                zos = new ZipOutputStream(response.getOutputStream());
+                ze = new ZipEntry(file.getName());
+                zos.putNextEntry(ze);
+                in=new FileInputStream(file);
+                while ((read = in.read(bytes)) != -1) {
+                    zos.write(bytes, 0, read);
+                }
+            } finally {
+                in.close();
+                zos.flush();
+                zos.close(); 
+            }
+        }
+    }
+    
+    @RequestMapping(value = "/{lang}/log/activity", produces = {
+        MediaType.TEXT_PLAIN_VALUE})
+    @ResponseBody
+    public String activity() {
+        String lastActivity = null;
+        
+        if (fileAppender != null) {
+            lastActivity = readLastLines(new File(fileAppender.getFile()));
+        }          
+        return lastActivity;
+    }
+    
+    private String readLastLines(File file) {
+        RandomAccessFile fileHandler = null;
+        try {
+            fileHandler = new RandomAccessFile( file, "r" );
+            long fileLength = fileHandler.length() - 1;
+            StringBuilder sb = new StringBuilder();
+            
+            int line = 0;
+            int readByte;
+            long filePointer;
+            for(filePointer = fileLength; filePointer != -1; filePointer--){
+                fileHandler.seek( filePointer );
+                readByte = fileHandler.readByte();
+                if( readByte == 0xA ) {
+                    if (filePointer < fileLength) {
+                        line = line + 1;
+                    }
+                } else if(readByte == 0xD ) {
+                    if (filePointer < fileLength-1) {
+                        line = line + 1;
+                    }
+                }
+                if (line >= maxLines) {
+                    break;
+                }
+                sb.append( ( char ) readByte );
+            }
 
-		    String lastLine = sb.reverse().toString();
-		    return lastLine;
-		} catch( java.io.FileNotFoundException e ) {
-			Log.error(Geonet.GEONETWORK, e.getMessage());
-		    return null;
-		} catch( java.io.IOException e ) {
-			Log.error(Geonet.GEONETWORK, e.getMessage());
-		    return null;
-		} finally {
-		    if (fileHandler != null )
-		        try {
-		            fileHandler.close();
-		        } catch (IOException e) {
-		        	Log.error(Geonet.GEONETWORK, e.getMessage());
-		        }
-		}
-	}
+            String lastLine = sb.reverse().toString();
+            return lastLine;
+        } catch( java.io.FileNotFoundException e ) {
+            Log.error(Geonet.GEONETWORK, e.getMessage());
+            return null;
+        } catch( java.io.IOException e ) {
+            Log.error(Geonet.GEONETWORK, e.getMessage());
+            return null;
+        } finally {
+            if (fileHandler != null )
+            try {
+                fileHandler.close();
+            } catch (IOException e) {
+                Log.error(Geonet.GEONETWORK, e.getMessage());
+            }
+        }
+    }
 }
