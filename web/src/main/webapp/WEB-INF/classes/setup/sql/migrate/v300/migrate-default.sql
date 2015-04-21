2e7580f700a8d42c2172398acd2d04dfb07d046f
François Prunayre <fxprunayre@gmail.com>
2015-04-21 09:23:46 +0200

    Migration / Add statements to drop iso19115 and fgdc-std which are now unsupported. Commented out for the time being as it does not break the app and users can drop them manually if they're confident with the removal.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/migrate-default.sql
@@ -1,6 +1,3 @@
-UPDATE Settings SET value='3.0.0' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
-
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/background', 'osm', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/width', '500', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/summaryWidth', '500', 0, 9590, 'n');
@@ -19,3 +16,16 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 INSERT INTO settings (name, value, datatype, position, internal) VALUES ('system/server/log','log4j.xml',0,250,'y');
 
+UPDATE Settings SET value='3.0.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+
+-- Drop unsupported metadata schema
+-- DELETE FROM operationallowed WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM metadatarating WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM metadatafiledownloads WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM metadatafileuploads WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM metadatanotifications WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM metadatastatus WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM validation WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std'));
+-- DELETE FROM metadata WHERE schemaid in ('iso19115', 'fgdc-std');
