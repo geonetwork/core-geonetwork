559c3645daddd94204be25413fa6d92f90f4602d
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-05-06 09:44:57 +0200

    Add index.html and index.csv to MEF export to make export of large number of MD easier to consume.

--- core/src/main/java/org/fao/geonet/kernel/mef/MEF2Exporter.java
+++ core/src/main/java/org/fao/geonet/kernel/mef/MEF2Exporter.java
@@ -24,29 +24,48 @@
 package org.fao.geonet.kernel.mef;
 
 import jeeves.server.context.ServiceContext;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
 import org.fao.geonet.Constants;
 import org.fao.geonet.GeonetContext;
 import org.fao.geonet.ZipUtil;
 import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.domain.Metadata;
 import org.fao.geonet.domain.MetadataRelation;
+import org.fao.geonet.domain.MetadataType;
 import org.fao.geonet.domain.Pair;
 import org.fao.geonet.domain.ReservedOperation;
 import org.fao.geonet.kernel.DataManager;
 import org.fao.geonet.kernel.mef.MEFLib.Format;
 import org.fao.geonet.kernel.mef.MEFLib.Version;
+import org.fao.geonet.kernel.search.IndexAndTaxonomy;
+import org.fao.geonet.kernel.search.LuceneIndexField;
+import org.fao.geonet.kernel.search.NoFilterFilter;
+import org.fao.geonet.kernel.search.SearchManager;
 import org.fao.geonet.lib.Lib;
 import org.fao.geonet.repository.MetadataRelationRepository;
 import org.fao.geonet.utils.IO;
+import org.fao.geonet.utils.Xml;
+import org.jdom.Element;
 
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import static com.google.common.xml.XmlEscapers.xmlContentEscaper;
 import static org.fao.geonet.Constants.CHARSET;
+import static org.fao.geonet.constants.Geonet.IndexFieldNames.LOCALE;
+import static org.fao.geonet.constants.Geonet.IndexFieldNames.UUID;
 import static org.fao.geonet.kernel.mef.MEFConstants.FILE_INFO;
 import static org.fao.geonet.kernel.mef.MEFConstants.FILE_METADATA;
 import static org.fao.geonet.kernel.mef.MEFConstants.MD_DIR;
@@ -70,18 +89,129 @@ class MEF2Exporter {
 			Format format, boolean skipUUID, Path stylePath, boolean resolveXlink, boolean removeXlinkAttribute) throws Exception {
 
 		Path file = Files.createTempFile("mef-", ".mef");
-        try (FileSystem zipFs = ZipUtil.createZipFs(file)) {
+        SearchManager searchManager = context.getBean(SearchManager.class);
+        String contextLang = context.getLanguage() == null ? Geonet.DEFAULT_LANGUAGE : context.getLanguage();
+        try (
+                FileSystem zipFs = ZipUtil.createZipFs(file);
+                IndexAndTaxonomy indexReaderAndTaxonomy = searchManager.getNewIndexReader(contextLang);
+        ) {
+            StringBuilder csvBuilder = new StringBuilder("\"schema\";\"uuid\";\"id\";\"type\";\"isHarvested\";\"title\";\"abstract\"\n");
+            Element html = new Element("html").addContent(new Element("head").addContent(Arrays.asList(
+                    new Element("title").setText("Export Index"),
+                    new Element("link").setAttribute("rel", "stylesheet").
+                            setAttribute("href", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"),
+                    new Element("style").setText("body {\n"
+                                                 + "  padding-left: 10px;\n"
+                                                 + "}\n"
+                                                 + "p.abstract {\n"
+                                                 + "  font-style: italic;\n"
+                                                 + "}\n"
+                                                 + ".entry {\n"
+                                                 + "  padding: 20px;\n"
+                                                 + "  margin: 20px 0;\n"
+                                                 + "  border: 1px solid #eee;\n"
+                                                 + "  border-left-width: 5px;\n"
+                                                 + "  border-radius: 3px;\n"
+                                                 + "  border-left-color: #1b809e;\n"
+                                                 + "}\n"
+                                                 + ".entry:hover {\n"
+                                                 + "  background-color: #f5f5f5;\n"
+                                                 + "}\n")
+            )));
+            Element body = new Element("body");
+            html.addContent(body);
             for (Object uuid1 : uuids) {
                 String uuid = (String) uuid1;
+                IndexSearcher searcher = new IndexSearcher(indexReaderAndTaxonomy.indexReader);
+                BooleanQuery query = new BooleanQuery();
+                query.add(new BooleanClause(new TermQuery(new Term(UUID, uuid)), BooleanClause.Occur.MUST));
+                query.add(new BooleanClause(new TermQuery(new Term(LOCALE, contextLang)), BooleanClause.Occur.SHOULD));
+                TopDocs topDocs = searcher.search(query, NoFilterFilter.instance(), 5);
+                String mdSchema = null, mdTitle = null, mdAbstract = null, id = null, isHarvested = null;
+                MetadataType mdType = null;
+
+                for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                    Document doc = searcher.doc(scoreDoc.doc);
+                    String locale = doc.get(Geonet.IndexFieldNames.LOCALE);
+                    if (mdSchema == null) {
+                        mdSchema = doc.get(Geonet.IndexFieldNames.SCHEMA);
+                    }
+                    if (mdTitle == null || contextLang.equals(locale)) {
+                        mdTitle = doc.get(LuceneIndexField.TITLE);
+                    }
+                    if (mdAbstract == null || contextLang.equals(locale)) {
+                        mdAbstract = doc.get(LuceneIndexField.ABSTRACT);
+                    }
+                    if (id == null) {
+                        id = doc.get(LuceneIndexField.ID);
+                    }
+                    if (isHarvested == null) {
+                        isHarvested = doc.get(Geonet.IndexFieldNames.IS_HARVESTED);
+                    }
+                    if (mdType == null) {
+                        String tmp = doc.get(LuceneIndexField.IS_TEMPLATE);
+                        mdType = MetadataType.lookup(tmp.charAt(0));
+                    }
+
+                }
+
+                if (mdType == null) {
+                    mdType = MetadataType.METADATA;
+                }
+                csvBuilder.append('"').append(cleanForCsv(mdSchema)).append("\";\"").
+                        append(cleanForCsv(uuid)).append("\";\"").
+                        append(cleanForCsv(id)).append("\";\"").
+                        append(mdType.toString()).append("\";\"").
+                        append(cleanForCsv(isHarvested)).append("\";\"").
+                        append(cleanForCsv(mdTitle)).append("\";\"").
+                        append(cleanForCsv(mdAbstract)).append("\"\n");
+
+                body.addContent(new Element("div").setAttribute("class", "entry").addContent(Arrays.asList(
+                        new Element("h4").setAttribute("class", "title").addContent(
+                                new Element("a").setAttribute("href", uuid).setText(cleanXml(mdTitle))),
+                        new Element("p").setAttribute("class", "abstract").setText(cleanXml(mdAbstract)),
+                        new Element("table").setAttribute("class", "table").addContent(Arrays.asList(
+                                new Element("thead").addContent(
+                                        new Element("tr").addContent(Arrays.asList(
+                                                new Element("th").setText("ID"),
+                                                new Element("th").setText("UUID"),
+                                                new Element("th").setText("Type"),
+                                                new Element("th").setText("isHarvested")
+                                        ))),
+                                new Element("tbody").addContent(
+                                        new Element("tr").addContent(Arrays.asList(
+                                                new Element("td").setAttribute("class", "id").setText(id),
+                                                new Element("td").setAttribute("class", "uuid").setText(xmlContentEscaper().escape
+                                                        (uuid)),
+                                                new Element("td").setAttribute("class", "type").setText(mdType.toString()),
+                                                new Element("td").setAttribute("class", "isHarvested").setText(isHarvested)
+                                        )))
+                        ))
+                )));
                 createMetadataFolder(context, uuid, zipFs, skipUUID, stylePath,
                         format, resolveXlink, removeXlinkAttribute);
             }
+            Files.write(zipFs.getPath("/index.csv"), csvBuilder.toString().getBytes(Constants.CHARSET));
+            Files.write(zipFs.getPath("/index.html"), Xml.getString(html).getBytes(Constants.CHARSET));
         }
-
 		return file;
 	}
 
-	/**
+    private static String cleanXml(String xmlTextContent) {
+        if (xmlTextContent != null) {
+            return xmlContentEscaper().escape(xmlTextContent);
+        }
+        return "-";
+    }
+
+    private static String cleanForCsv(String csvColumnText) {
+        if (csvColumnText != null) {
+            return csvColumnText.replace("\"", "'");
+        }
+        return "-";
+    }
+
+    /**
 	 * Create a metadata folder according to MEF {@link Version} 2
 	 * specification. If current record is based on an ISO profil, the
 	 * stylesheet /convert/to19139.xsl is used to map to ISO. Both files are
