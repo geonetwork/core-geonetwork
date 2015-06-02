22b1acbac8084539f45f37c548932fda7e41d005
François Prunayre <fxprunayre@gmail.com>
2015-06-02 17:02:33 +0200

    Open medsea or emodnet view when editing.

--- web-ui/src/main/resources/catalog/js/edit/EditorController.js
+++ web-ui/src/main/resources/catalog/js/edit/EditorController.js
@@ -182,12 +182,14 @@
                 var schemaCustomConfig = {
                   // Example : open ISO19139 record having
                   // standardName containing medsea in advanced mode
-                  //'iso19139': function (md) {
-                  //  if (md.standardName && md.standardName.match(/medsea/i)) {
-                  //    return 'identificationInfo';
-                  //  }
-                  //  return defaultTab;
-                  //}
+                  'iso19139': function (md) {
+                    if (md.standardName && md.standardName.match(/medsea/i)) {
+                      return 'characteristics';
+                    } else if (md.standardName && md.standardName.match(/emodnet - hydrography/i)) {
+                      return 'emodnetHydrography-what';
+                    }
+                    return defaultTab;
+                  }
                 };
                 if (schemaCustomConfig) {
                   var fn = schemaCustomConfig[$scope.mdSchema];
