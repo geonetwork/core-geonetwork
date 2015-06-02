dd7b6f35c47f35f84cfea2533c07a16663549d8c
François Prunayre <fxprunayre@gmail.com>
2015-06-02 17:02:11 +0200

    New settings to be added to Sextant db settings.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.0.x/6.0.0/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.0.x/6.0.0/migrate-db-postgres.sql
@@ -28,6 +28,7 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 INSERT INTO settings (name, value, datatype, position, internal) VALUES ('system/server/log','log4j.xml',0,250,'y');
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('map/is3DModeAllowed', 'false', 2, 9593, 'n');
 
 UPDATE Settings SET value='3.0.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
