ea3884a8961d591672088430baed3b641c0b723f
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-06-17 11:07:37 +0200

    Make PDF Export more robust when images are missing
    scale images in PDF when images are too large.

--- services/src/main/java/org/fao/geonet/services/metadata/format/ImageReplacedElementFactory.java
+++ services/src/main/java/org/fao/geonet/services/metadata/format/ImageReplacedElementFactory.java
@@ -1,5 +1,7 @@
 package org.fao.geonet.services.metadata.format;
 
+import com.google.common.collect.Sets;
+import com.google.common.io.Files;
 import com.itextpdf.text.Image;
 import org.apache.commons.io.IOUtils;
 import org.fao.geonet.constants.Geonet;
@@ -9,6 +11,7 @@ import org.xhtmlrenderer.extend.ReplacedElement;
 import org.xhtmlrenderer.extend.ReplacedElementFactory;
 import org.xhtmlrenderer.extend.UserAgentCallback;
 import org.xhtmlrenderer.layout.LayoutContext;
+import org.xhtmlrenderer.pdf.EmptyReplacedElement;
 import org.xhtmlrenderer.pdf.ITextFSImage;
 import org.xhtmlrenderer.pdf.ITextImageElement;
 import org.xhtmlrenderer.render.BlockBox;
@@ -17,8 +20,11 @@ import org.xhtmlrenderer.simple.extend.FormSubmissionListener;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Set;
+import javax.imageio.ImageIO;
 
 public class ImageReplacedElementFactory implements ReplacedElementFactory {
+    private static Set<String> imgFormatExts = null;
     private final ReplacedElementFactory superFactory;
     private String baseURL;
 
@@ -38,10 +44,9 @@ public class ImageReplacedElementFactory implements ReplacedElementFactory {
         String nodeName = element.getNodeName();
         String src = element.getAttribute("src");
         if ("img".equals(nodeName) && src.contains("region.getmap.png")) {
-            InputStream input = null;
+            StringBuilder builder = new StringBuilder(baseURL);
             try {
                 String[] parts = src.split("\\?|&");
-                StringBuilder builder = new StringBuilder(baseURL);
                 builder.append(parts[0]);
                 builder.append('?');
                 for (int i = 1; i < parts.length ; i++) {
@@ -53,29 +58,77 @@ public class ImageReplacedElementFactory implements ReplacedElementFactory {
                     builder.append('=');
                     builder.append(URLEncoder.encode(param[1], "UTF-8"));
                 }
-                input = new URL(builder.toString()).openStream();
-                byte[] bytes = IOUtils.toByteArray(input);
-                Image image = Image.getInstance(bytes);
-
-                float factor = layoutContext.getDotsPerPixel();
-                image.scaleAbsolute(image.getPlainWidth() * factor, image.getPlainHeight() * factor);
-                FSImage fsImage = new ITextFSImage(image);
-                
-                if(cssHeight > -1 && cssWidth > -1) {
-                    fsImage.scale(cssWidth, cssHeight);
-                }
-                
-                return new ITextImageElement(fsImage);
             } catch (Exception e) {
-                Log.error(Geonet.GEONETWORK, "Error writing metadata to PDF", e);
-            } finally {
-                if (input != null) {
-                    org.apache.commons.io.IOUtils.closeQuietly(input);
+                Log.warning(Geonet.GEONETWORK, "Error writing metadata to PDF", e);
+            }
+            float factor = layoutContext.getDotsPerPixel();
+            return loadImage(layoutContext, box, userAgentCallback, cssWidth, cssHeight, builder.toString(), factor);
+        } else if ("img".equals(nodeName) && isSupportedImageFormat(src)) {
+            float factor = layoutContext.getDotsPerPixel();
+            return loadImage(layoutContext, box, userAgentCallback, cssWidth, cssHeight, src, factor);
+        }
+
+        try {
+            return superFactory.createReplacedElement(layoutContext, box, userAgentCallback, cssWidth, cssHeight);
+        } catch (Throwable e) {
+            return new EmptyReplacedElement(cssWidth, cssHeight);
+        }
+    }
+
+    private boolean isSupportedImageFormat(String imgUrl) {
+        String ext = Files.getFileExtension(imgUrl);
+        return ext.trim().isEmpty() || getSupportedExts().contains(ext);
+    }
+
+    private static Set<String> getSupportedExts() {
+        if (imgFormatExts == null) {
+            synchronized (ImageReplacedElementFactory.class) {
+                if (imgFormatExts == null) {
+                    imgFormatExts = Sets.newHashSet();
+                    for (String ext : ImageIO.getReaderFileSuffixes()) {
+                        imgFormatExts.add(ext.toLowerCase());
+                    }
                 }
             }
         }
 
-        return superFactory.createReplacedElement(layoutContext, box, userAgentCallback, cssWidth, cssHeight);
+        return imgFormatExts;
+    }
+
+    private ReplacedElement loadImage(LayoutContext layoutContext, BlockBox box, UserAgentCallback userAgentCallback,
+                                      int cssWidth, int cssHeight, String url, float scaleFactor) {
+        InputStream input = null;
+        try {
+            input = new URL(url).openStream();
+            byte[] bytes = IOUtils.toByteArray(input);
+            Image image = Image.getInstance(bytes);
+
+            image.scaleAbsolute(image.getPlainWidth() * scaleFactor, image.getPlainHeight() * scaleFactor);
+            FSImage fsImage = new ITextFSImage(image);
+
+            if(cssHeight > -1 && cssWidth > -1) {
+                fsImage.scale(cssWidth, cssHeight);
+            }
+            int maxWidth = (int) (900 * scaleFactor);
+            if (fsImage.getWidth() > maxWidth) {
+                int ratio = fsImage.getWidth() / maxWidth;
+                fsImage.scale(maxWidth, fsImage.getHeight() / ratio);
+            }
+
+            return new ITextImageElement(fsImage);
+        } catch (Exception e) {
+            Log.error(Geonet.GEONETWORK, "Error writing metadata to PDF", e);
+
+            try {
+                return superFactory.createReplacedElement(layoutContext, box, userAgentCallback, cssWidth, cssHeight);
+            } catch (Throwable e2) {
+                return new EmptyReplacedElement(cssWidth, cssHeight);
+            }
+        } finally {
+            if (input != null) {
+                IOUtils.closeQuietly(input);
+            }
+        }
     }
 
     @Override
