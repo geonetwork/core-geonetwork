277a6e797f0b362b9ac23e9c38378a31ddc106d3
Jose Garcia <jose.garcia@geocat.net>
2015-07-02 10:29:05 +0200

    Fix ThesaurusTest

--- core/src/test/java/org/fao/geonet/kernel/ThesaurusTest.java
+++ core/src/test/java/org/fao/geonet/kernel/ThesaurusTest.java
@@ -37,7 +37,7 @@ public class ThesaurusTest extends AbstractThesaurusBasedTest {
         Files.deleteIfExists(file);
 
         this.writableThesaurus = new Thesaurus(isoLangMapper, file.getFileName().toString(), null, null, Geonet.CodeList.LOCAL,
-                file.getFileName().toString(), file, null, true);
+                file.getFileName().toString(), file, "", true);
         writableThesaurus.initRepository();
     }
     
