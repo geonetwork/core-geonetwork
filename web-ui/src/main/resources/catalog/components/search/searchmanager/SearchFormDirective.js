geonetwork/core-geonetwork

commit ad676d93859d8caa2058935b64c822fc6bf5600b

search: load first search after login

web-ui/src/main/resources/catalog/components/search/searchmanager/SearchFormDirective.js
@@ -95,73 +95,75 @@
      */
     this.triggerSearchFn = function(keepPagination) {
 
-      $scope.searching++;
-      angular.extend($scope.searchObj.params, defaultParams);
+      $scope.userLoginPromise.finally(function() {
+        $scope.searching++;
+        angular.extend($scope.searchObj.params, defaultParams);
 
-      // Set default pagination if not set
-      if ((!keepPagination &&
-          !$scope.searchObj.permalink) ||
-          (angular.isUndefined($scope.searchObj.params.from) ||
-          angular.isUndefined($scope.searchObj.params.to))) {
-        self.resetPagination();
-      }
+        // Set default pagination if not set
+        if ((!keepPagination &&
+            !$scope.searchObj.permalink) ||
+            (angular.isUndefined($scope.searchObj.params.from) ||
+                angular.isUndefined($scope.searchObj.params.to))) {
+          self.resetPagination();
+        }
 
-      // Set default sortBy
-      if (angular.isUndefined($scope.searchObj.params.sortBy)) {
-        angular.extend($scope.searchObj.params, $scope.searchObj.sortbyDefault);
-      }
+        // Set default sortBy
+        if (angular.isUndefined($scope.searchObj.params.sortBy)) {
+          angular.extend($scope.searchObj.params, $scope.searchObj.sortbyDefault);
+        }
 
-      // Don't add facet extra params to $scope.params but
-      // compute them each time on a search.
-      var params = angular.copy($scope.searchObj.params);
+        // Don't add facet extra params to $scope.params but
+        // compute them each time on a search.
+        var params = angular.copy($scope.searchObj.params);
 
-      if ($scope.currentFacets.length > 0) {
-        angular.extend(params,
-            gnFacetService.getParamsFromFacets($scope.currentFacets));
-      }
+        if ($scope.currentFacets.length > 0) {
+          angular.extend(params,
+              gnFacetService.getParamsFromFacets($scope.currentFacets));
+        }
 
-      var finalParams = angular.copy(params);
-      if(hiddenParams) {
-        for(var p in hiddenParams) {
-          if(!finalParams.hasOwnProperty(p)) {
-            finalParams[p] = hiddenParams[p];
+        var finalParams = angular.copy(params);
+        if(hiddenParams) {
+          for(var p in hiddenParams) {
+            if(!finalParams.hasOwnProperty(p)) {
+              finalParams[p] = hiddenParams[p];
+            }
           }
         }
-      }
-      gnSearchManagerService.gnSearch(finalParams).then(
-          function(data) {
-            $scope.searching--;
-            $scope.searchResults.records = [];
-            for (var i = 0; i < data.metadata.length; i++) {
-              $scope.searchResults.records.push(new Metadata(data.metadata[i]));
-            }
-            $scope.searchResults.count = data.count;
-            $scope.searchResults.facet = data.facet;
-
-            // compute page number for pagination
-            if ($scope.searchResults.records.length > 0 &&
-                $scope.hasPagination) {
-
-              var paging = $scope.paginationInfo;
-
-              // Means the `from` and `to` params come from permalink
-              if ((paging.currentPage - 1) *
-                  paging.hitsPerPage + 1 != params.from) {
-                paging.currentPage = (params.from - 1) / paging.hitsPerPage + 1;
+        gnSearchManagerService.gnSearch(finalParams).then(
+            function(data) {
+              $scope.searching--;
+              $scope.searchResults.records = [];
+              for (var i = 0; i < data.metadata.length; i++) {
+                $scope.searchResults.records.push(new Metadata(data.metadata[i]));
               }
+              $scope.searchResults.count = data.count;
+              $scope.searchResults.facet = data.facet;
 
-              paging.resultsCount = $scope.searchResults.count;
-              paging.to = Math.min(
-                  data.count,
-                  paging.currentPage * paging.hitsPerPage
-                  );
-              paging.pages = Math.ceil(
-                  $scope.searchResults.count /
-                  paging.hitsPerPage, 0
-                  );
-              paging.from = (paging.currentPage - 1) * paging.hitsPerPage + 1;
-            }
-          });
+              // compute page number for pagination
+              if ($scope.searchResults.records.length > 0 &&
+                  $scope.hasPagination) {
+
+                var paging = $scope.paginationInfo;
+
+                // Means the `from` and `to` params come from permalink
+                if ((paging.currentPage - 1) *
+                    paging.hitsPerPage + 1 != params.from) {
+                  paging.currentPage = (params.from - 1) / paging.hitsPerPage + 1;
+                }
+
+                paging.resultsCount = $scope.searchResults.count;
+                paging.to = Math.min(
+                    data.count,
+                        paging.currentPage * paging.hitsPerPage
+                );
+                paging.pages = Math.ceil(
+                        $scope.searchResults.count /
+                        paging.hitsPerPage, 0
+                );
+                paging.from = (paging.currentPage - 1) * paging.hitsPerPage + 1;
+              }
+            });
+      });
     };
 
     /**

web-ui/src/main/resources/catalog/js/CatController.js
@@ -279,6 +279,7 @@
                 );
               });
         });
+        $scope.userLoginPromise = userLogin;
 
 
         // Retrieve main search information

commit 92ca35ca2942be72355a3edaff320d3f97a3946d

Editor / Fix subtemplate search.
Related to issue introduced by
https://github.com/camptocamp/sextant-geonetwork/commit/ad676d93859d8caa2058935b64c822fc6bf5600b#comments

web-ui/src/main/resources/catalog/components/search/searchmanager/SearchFormDirective.js
@@ -95,7 +95,7 @@
      */
     this.triggerSearchFn = function(keepPagination) {
 
-      $scope.userLoginPromise.finally(function() {
+      function searchFn() {
         $scope.searching++;
         $scope.searchObj.params = angular.extend({},
           $scope.searchObj.defaultParams || defaultParams,
@@ -104,9 +104,9 @@
 
         // Set default pagination if not set
         if ((!keepPagination &&
-            !$scope.searchObj.permalink) ||
-            (angular.isUndefined($scope.searchObj.params.from) ||
-                angular.isUndefined($scope.searchObj.params.to))) {
+          !$scope.searchObj.permalink) ||
+          (angular.isUndefined($scope.searchObj.params.from) ||
+          angular.isUndefined($scope.searchObj.params.to))) {
           self.resetPagination();
         }
 
@@ -121,7 +121,7 @@
 
         if ($scope.currentFacets.length > 0) {
           angular.extend(params,
-              gnFacetService.getParamsFromFacets($scope.currentFacets));
+            gnFacetService.getParamsFromFacets($scope.currentFacets));
         }
 
         var finalParams = angular.copy(params);
@@ -133,41 +133,47 @@
           }
         }
         gnSearchManagerService.gnSearch(finalParams).then(
-            function(data) {
-              $scope.searching--;
-              $scope.searchResults.records = [];
-              for (var i = 0; i < data.metadata.length; i++) {
-                $scope.searchResults.records.push(new Metadata(data.metadata[i]));
+          function(data) {
+            $scope.searching--;
+            $scope.searchResults.records = [];
+            for (var i = 0; i < data.metadata.length; i++) {
+              $scope.searchResults.records.push(new Metadata(data.metadata[i]));
+            }
+            $scope.searchResults.count = data.count;
+            $scope.searchResults.facet = data.facet;
+            $scope.searchResults.dimension = data.dimension;
+
+            // compute page number for pagination
+            if ($scope.searchResults.records.length > 0 &&
+              $scope.hasPagination) {
+
+              var paging = $scope.paginationInfo;
+
+              // Means the `from` and `to` params come from permalink
+              if ((paging.currentPage - 1) *
+                paging.hitsPerPage + 1 != params.from) {
+                paging.currentPage = (params.from - 1) / paging.hitsPerPage + 1;
               }
-              $scope.searchResults.count = data.count;
-              $scope.searchResults.facet = data.facet;
-              $scope.searchResults.dimension = data.dimension;
 
-              // compute page number for pagination
-              if ($scope.searchResults.records.length > 0 &&
-                  $scope.hasPagination) {
-
-                var paging = $scope.paginationInfo;
-
-                // Means the `from` and `to` params come from permalink
-                if ((paging.currentPage - 1) *
-                    paging.hitsPerPage + 1 != params.from) {
-                  paging.currentPage = (params.from - 1) / paging.hitsPerPage + 1;
-                }
-
-                paging.resultsCount = $scope.searchResults.count;
-                paging.to = Math.min(
-                    data.count,
-                    paging.currentPage * paging.hitsPerPage
-                    );
-                paging.pages = Math.ceil(
-                    $scope.searchResults.count /
-                        paging.hitsPerPage, 0
-                    );
-                paging.from = (paging.currentPage - 1) * paging.hitsPerPage + 1;
-              }
-            });
-      });
+              paging.resultsCount = $scope.searchResults.count;
+              paging.to = Math.min(
+                data.count,
+                paging.currentPage * paging.hitsPerPage
+              );
+              paging.pages = Math.ceil(
+                $scope.searchResults.count /
+                paging.hitsPerPage, 0
+              );
+              paging.from = (paging.currentPage - 1) * paging.hitsPerPage + 1;
+            }
+          });
+      };
+      
+      if ($scope.userLoginPromise && $scope.userLoginPromise.finally) {
+      $scope.userLoginPromise.finally(searchFn);
+      } else {
+        searchFn();
+      }
     };
 
     /**