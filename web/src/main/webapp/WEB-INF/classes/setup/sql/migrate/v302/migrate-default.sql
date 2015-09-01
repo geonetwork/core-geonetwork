3fffd416fe34b673d6c11ec6d3ab2095573c7f14
François Prunayre <fxprunayre@gmail.com>
2015-09-01 18:44:25 +0200

    Deprecate shib.user.login ? #1156
    See http://geonetwork-opensource.org/manuals/trunk/eng/users/administrator-guide/managing-users-and-groups/authentication-mode.html#configuring-shibboleth for Spring Security configuration.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v302/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v302/migrate-default.sql
@@ -1,6 +1,8 @@
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/draftWhenInGroup', '', 0, 100002, 'n');
 
+DELETE FROM Settings WHERE name LIKE 'system/shib/%';
+
 -- ALTER TABLE groups ALTER COLUMN email TYPE varchar(128);
 
 UPDATE Settings SET value='3.0.2' WHERE name='system/platform/version';
