5a81cac0689c1168931c243b0993248e3db19ccb
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-09-10 13:05:05 +0200

    Fixed import error message on IE

--- web-ui/src/main/resources/catalog/js/edit/ImportController.js
+++ web-ui/src/main/resources/catalog/js/edit/ImportController.js
@@ -46,8 +46,9 @@
         $scope.importing = false;
         var response = new DOMParser().parseFromString(
             data.jqXHR.responseText, 'text/xml');
+        var msgEl = response.getElementsByTagName('message')[0];
         var report = {
-          message: response.getElementsByTagName('message')[0].innerHTML
+          message: msgEl.innerHTML ? msgEl.innerHTML : msgEl.textContent
         };
         $scope.reports.push(report);
       };
