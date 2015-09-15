9b0dab582650f3dbf50a2ac044e31b26f589d7f9
François Prunayre <fxprunayre@gmail.com>
2015-09-16 01:01:15 +0200

    Add Czech translation #1192.

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -46,10 +46,11 @@
       $scope.nodeId = tokens[4];
       // TODO : get list from server side
       $scope.langs = {'eng': 'en', 'dut': 'du', 'fre': 'fr',
-        'ger': 'ge', 'kor': 'ko', 'spa': 'es'};
+        'ger': 'ge', 'kor': 'ko', 'spa': 'es', 'cze': 'cz'};
       // Lang names to be displayed in language selector
       $scope.langLabels = {'eng': 'English', 'dut': 'Nederlands',
-        'fre': 'Français', 'ger': 'Deutsch', 'kor': '한국의', 'spa': 'Español'};
+        'fre': 'Français', 'ger': 'Deutsch', 'kor': '한국의',
+        'spa': 'Español', 'cze': 'Czech'};
       $scope.url = '';
       $scope.base = '../../catalog/';
       $scope.proxyUrl = gnGlobalSettings.proxyUrl;
