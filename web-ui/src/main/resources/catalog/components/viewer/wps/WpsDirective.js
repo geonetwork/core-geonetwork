79717a53cf49ed166864dee7d5a1e52219be759e
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2015-09-24 17:56:16 +0200

    Parse gnWpsProcessForm defaults as KVP params

--- web-ui/src/main/resources/catalog/components/viewer/wps/WpsDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/wps/WpsDirective.js
@@ -20,12 +20,69 @@
    */
   module.directive('gnWpsProcessForm', [
     'gnWpsService',
+    'gnUrlUtils',
     '$timeout',
-    function(gnWpsService, $timeout) {
+    function(gnWpsService, gnUrlUtils, $timeout) {
 
       var inputTypes = {
         string: 'text',
         float: 'number'
+      };
+
+      var parseKvpParams = function(str) {
+        var escaper = function(match, p1) {
+          return '='+gnUrlUtils.encodeUriQuery(p1);
+        };
+        str = str.replace(/=\[([^&]*)\]/gi, escaper);
+
+        var queryParams = gnUrlUtils.parseKeyValue(str);
+        var defaults = {};
+        for ( var prop in queryParams ) {
+          if ( queryParams.hasOwnProperty( prop ) ) {
+            defaults[prop.toLowerCase()] = queryParams[prop];
+          }
+        }
+
+        var parseDataInputs = function(value) {
+          var datainputs = {};
+
+          if (value === undefined) {
+            return datainputs;
+          }
+
+          value.split(';').map(function(str) {
+            var list = str.split('@');
+
+            var input = list[0].split('=');
+            var identifier = input[0];
+            var value = input[1];
+            list = list.slice(1);
+
+            var attributes = {};
+            list.map(function(attribute) {
+              keyValue = attribute.split('=');
+              attributes[keyValue[0].toLowerCase()] = keyValue[1];
+            });
+
+            datainputs[identifier] = {
+              value: value,
+              attributes: attributes
+            };
+          });
+          return datainputs;
+        };
+
+        defaults.datainputs = parseDataInputs(defaults.datainputs);
+        defaults.responsedocument = parseDataInputs(defaults.responsedocument);
+
+        return defaults;
+      };
+
+      var toBool = function(str, defaultVal) {
+        if (str === undefined) {
+          return defaultVal;
+        }
+        return str.toLowerCase() === 'true';
       };
 
       return {
@@ -42,7 +99,7 @@
         },
 
         link: function(scope, element, attrs) {
-          var defaults = scope.defaults || {};
+          var defaults = parseKvpParams(scope.defaults);
 
           scope.describeState = 'sended';
           scope.executeState = '';
@@ -57,18 +114,27 @@
                   scope.processDescription = response.processDescription[0];
                   angular.forEach(scope.processDescription.dataInputs.input,
                       function(input) {
-                        if (input.literalData) {
+                        var value;
+                        var defaultValue;
+                        var datainput = defaults.datainputs[input.identifier.value];
+                        if (datainput != undefined) {
+                          defaultValue = datainput.value;
+                        }
+
+                        if (input.literalData != undefined) {
+
                           // Input type
                           input.type = inputTypes[input.literalData.dataType.value];
 
                           // Default value
-                          var value = undefined;
                           if (input.literalData.defaultValue != undefined) {
                             value = input.literalData.defaultValue;
                           }
-                          if (defaults[input.identifier.value] != undefined) {
-                            value = defaults[input.identifier.value];
+                          if (defaultValue != undefined) {
+                            value = defaultValue;
                           }
+
+                          // Format conversion
                           switch (input.literalData.dataType.value) {
                             case 'float':
                               value = parseFloat(value); break;
@@ -77,27 +143,41 @@
                           }
                           input.value = value;
                         }
-                        if (input.boundingBoxData) {
+
+                        if (input.boundingBoxData != undefined) {
                           input.value = '';
+                          if (defaultValue) {
+                            input.value = defaultValue.split(',').slice(0, 4).join(',');
+                          }
                         }
                       }
                   );
 
                   angular.forEach(scope.processDescription.processOutputs.output,
                       function(output) {
-                        output.value = true;
                         output.asReference = true;
+
+                        var outputDefault = defaults.responsedocument[output.identifier.value]
+                        if (outputDefault) {
+                          output.value = true;
+                          var defaultAsReference = outputDefault.attributes['asreference'];
+                          if (defaultAsReference !== undefined) {
+                            output.asReference = toBool(defaultAsReference);
+                          }
+                        }
                       }
                   );
-                  scope.outputsVisible = false;
+                  if (scope.processDescription.processOutputs.output.length == 1) {
+                    scope.processDescription.processOutputs.output[0].value = true;
+                  }
+                  scope.outputsVisible = true;
 
                   scope.responseDocument = {
-                    lineage: false,
-                    storeExecuteResponse: true,
-                    status: false
+                    lineage: toBool(defaults.lineage, false),
+                    storeExecuteResponse: toBool(defaults.storeexecuteresponse, false),
+                    status: toBool(defaults.status, false)
                   };
-
-                  scope.optionsVisible = false;
+                  scope.optionsVisible = true;
                 }
               },
               function(response) {
@@ -221,6 +301,7 @@
               scope.responseDocument.storeExecuteResponse = true;
             }
           };
+
         }
       };
     }
