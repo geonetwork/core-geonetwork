863e5bc2f840f7bbbaf133904456a4a2db2ac332
María Arias de Reyna <maria.arias@geocat.net>
2015-10-09 13:27:12 +0200

    Setup SubscriptionClient

--- openwis-services-client/src/main/java/org/openwis/subscription/client/SubscriptionClient.java
+++ openwis-services-client/src/main/java/org/openwis/subscription/client/SubscriptionClient.java
@@ -1,10 +1,10 @@
 package org.openwis.subscription.client;
 
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-import org.springframework.ws.soap.client.core.SoapActionCallback;
+import java.util.List;
 
 import javax.xml.bind.JAXBElement;
-import java.util.List;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 /**
  * Client for CacheIndex web service.
@@ -21,14 +21,15 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
     public ProcessedRequest retrieveLastProcessedRequest(long subscriptionId) {
         ObjectFactory objFact = new ObjectFactory();
 
-        FindLastProcessedRequest request =  objFact.createFindLastProcessedRequest();
+        FindLastProcessedRequest request = objFact
+                .createFindLastProcessedRequest();
         request.setSubscriptionId(subscriptionId);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createFindLastProcessedRequest(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        FindLastProcessedRequestResponse responseType = (FindLastProcessedRequestResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createFindLastProcessedRequest(request));
+        FindLastProcessedRequestResponse responseType = (FindLastProcessedRequestResponse) response
+                .getValue();
 
         return responseType.getReturn();
     }
@@ -40,17 +41,15 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
      * @return
      */
     public Subscription retrieveSubscription(long subscriptionId) {
-       ObjectFactory objFact = new ObjectFactory();
+        ObjectFactory objFact = new ObjectFactory();
 
-        GetSubscription request =  objFact.createGetSubscription();
+        GetSubscription request = objFact.createGetSubscription();
         request.setSubscriptionId(subscriptionId);
 
-
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createGetSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        GetSubscriptionResponse responseType = (GetSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(objFact.createGetSubscription(request));
+        GetSubscriptionResponse responseType = (GetSubscriptionResponse) response
+                .getValue();
 
         return responseType.getReturn();
     }
@@ -63,72 +62,73 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
      * @param sort
      * @return
      */
-    public List<Subscription> retrieveSubscriptionsByUsers(int firstResult, int maxResults, SortDirection sort) {
+    public List<Subscription> retrieveSubscriptionsByUsers(int firstResult,
+            int maxResults, SortDirection sort) {
         ObjectFactory objFact = new ObjectFactory();
 
-        GetSubscriptionsByUsers request =  objFact.createGetSubscriptionsByUsers();
+        GetSubscriptionsByUsers request = objFact
+                .createGetSubscriptionsByUsers();
         request.setFirstResult(firstResult);
         request.setMaxResults(maxResults);
         request.setSortDirection(sort);
 
-
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createGetSubscriptionsByUsers(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        GetSubscriptionsByUsersResponse responseType = (GetSubscriptionsByUsersResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createGetSubscriptionsByUsers(request));
+        GetSubscriptionsByUsersResponse responseType = (GetSubscriptionsByUsersResponse) response
+                .getValue();
 
         return responseType.getReturn();
     }
 
-
-    // TODO: Check to implement (getSubscriptionsByUsersCount) if required and clarify parameters
+    // TODO: Check to implement (getSubscriptionsByUsersCount) if required and
+    // clarify parameters
     public int retrieveSubscriptionsByUsersCount() {
 
         return -1;
     }
-
 
     /**
      * Creates a metadata subscription.
      *
      * @param metadataUrn
      * @param subscription
-     * @return  Subscription id.
+     * @return Subscription id.
      */
-    public Long createSubscription(String metadataUrn, Subscription subscription) {
+    public Long createSubscription(String metadataUrn,
+            Subscription subscription) {
         ObjectFactory objFact = new ObjectFactory();
 
-        CreateSubscription request =  objFact.createCreateSubscription();
+        CreateSubscription request = objFact.createCreateSubscription();
         request.setMetadataUrn(metadataUrn);
         request.setSubscription(subscription);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createCreateSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        CreateSubscriptionResponse responseType = (CreateSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createCreateSubscription(request));
+        CreateSubscriptionResponse responseType = (CreateSubscriptionResponse) response
+                .getValue();
 
         return responseType.getReturn();
     }
 
-
     /**
      * Deletes a subscription.
      *
-     * @param subscriptionId    Subscription identifier.
+     * @param subscriptionId
+     *            Subscription identifier.
      */
     public void deleteSubscription(Long subscriptionId) {
         ObjectFactory objFact = new ObjectFactory();
 
-        DeleteSubscription request =  objFact.createDeleteSubscription();
+        DeleteSubscription request = objFact.createDeleteSubscription();
         request.setSubscriptionId(subscriptionId);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createDeleteSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        DeleteSubscriptionResponse responseType = (DeleteSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createDeleteSubscription(request));
+        DeleteSubscriptionResponse responseType = (DeleteSubscriptionResponse) response
+                .getValue();
     }
 
     /**
@@ -140,14 +140,14 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
     public Subscription updateSubscription(Subscription subscription) {
         ObjectFactory objFact = new ObjectFactory();
 
-        UpdateSubscription request =  objFact.createUpdateSubscription();
+        UpdateSubscription request = objFact.createUpdateSubscription();
         request.setSubscription(subscription);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createUpdateSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        UpdateSubscriptionResponse responseType = (UpdateSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createUpdateSubscription(request));
+        UpdateSubscriptionResponse responseType = (UpdateSubscriptionResponse) response
+                .getValue();
 
         return responseType.getReturn();
     }
@@ -163,66 +163,63 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
      * @return
      */
     public Subscription updateSubscriptionConfig(Long subscriptionId,
-                                                 Frequency frequency,
-                                                 Dissemination primaryDissemination,
-                                                 Dissemination secondaryDissemination,
-                                                 String startingDate) {
+            Frequency frequency, Dissemination primaryDissemination,
+            Dissemination secondaryDissemination, String startingDate) {
         ObjectFactory objFact = new ObjectFactory();
 
-        UpdateSubscriptionConfig request =  objFact.createUpdateSubscriptionConfig();
+        UpdateSubscriptionConfig request = objFact
+                .createUpdateSubscriptionConfig();
         request.setSubscriptionId(subscriptionId);
         request.setFrequency(frequency);
         request.setPrimaryDissemination(primaryDissemination);
         request.setSecondaryDissemination(secondaryDissemination);
         request.setStartingDate(startingDate);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createUpdateSubscriptionConfig(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        UpdateSubscriptionConfigResponse responseType = (UpdateSubscriptionConfigResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createUpdateSubscriptionConfig(request));
+        UpdateSubscriptionConfigResponse responseType = (UpdateSubscriptionConfigResponse) response
+                .getValue();
 
         return responseType.getReturn();
     }
 
-
-
     /**
      * Resumes a subscription.
      *
-     * @param subscriptionId    Subscription identifier.
+     * @param subscriptionId
+     *            Subscription identifier.
      */
     public void resumeSubscription(Long subscriptionId) {
         ObjectFactory objFact = new ObjectFactory();
 
-        ResumeSubscription request =  objFact.createResumeSubscription();
+        ResumeSubscription request = objFact.createResumeSubscription();
         request.setSubscription(subscriptionId);
 
-
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createResumeSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        ResumeSubscriptionResponse responseType = (ResumeSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createResumeSubscription(request));
+        ResumeSubscriptionResponse responseType = (ResumeSubscriptionResponse) response
+                .getValue();
     }
-
 
     /**
      * Suspends a subscription.
      *
-     * @param subscriptionId    Subscription identifier.
+     * @param subscriptionId
+     *            Subscription identifier.
      */
     public void suspendSubscription(Long subscriptionId) {
         ObjectFactory objFact = new ObjectFactory();
 
-        SuspendSubscription request =  objFact.createSuspendSubscription();
+        SuspendSubscription request = objFact.createSuspendSubscription();
         request.setSubscription(subscriptionId);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createSuspendSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        SuspendSubscriptionResponse responseType = (SuspendSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createSuspendSubscription(request));
+        SuspendSubscriptionResponse responseType = (SuspendSubscriptionResponse) response
+                .getValue();
     }
 
     /**
@@ -230,21 +227,20 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
      *
      * @param activateBackup
      */
-    public void setBackup(boolean activateBackup, String dateFrom, String deploymentName) {
+    public void setBackup(boolean activateBackup, String dateFrom,
+            String deploymentName) {
         ObjectFactory objFact = new ObjectFactory();
 
-        SetBackup request =  objFact.createSetBackup();
+        SetBackup request = objFact.createSetBackup();
         request.setActivateBackup(activateBackup);
         request.setDateFrom(dateFrom);
         request.setDeploymentName(deploymentName);
 
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createSetBackup(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        SetBackupResponse responseType = (SetBackupResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(objFact.createSetBackup(request));
+        SetBackupResponse responseType = (SetBackupResponse) response
+                .getValue();
     }
-
 
     /**
      * Checks a user subscription.
@@ -254,15 +250,14 @@ public class SubscriptionClient extends WebServiceGatewaySupport {
     public void checkUserSubscription(String user) {
         ObjectFactory objFact = new ObjectFactory();
 
-        CheckUserSubscription request =  objFact.createCheckUserSubscription();
+        CheckUserSubscription request = objFact.createCheckUserSubscription();
         request.setUser(user);
 
-
-        JAXBElement response = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(
-                objFact.createCheckUserSubscription(request),
-                new SoapActionCallback(
-                        "http://localhost:8088/mockProductMetadataServiceSoapBinding"));
-        CheckUserSubscriptionResponse responseType = (CheckUserSubscriptionResponse) response.getValue();
+        JAXBElement response = (JAXBElement) getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        objFact.createCheckUserSubscription(request));
+        CheckUserSubscriptionResponse responseType = (CheckUserSubscriptionResponse) response
+                .getValue();
     }
 
     /**
