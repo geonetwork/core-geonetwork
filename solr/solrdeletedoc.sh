30d083060402c86f4c2511c721514d0a1e1c05f9
Florent gravin <florent.gravin@camptocamp.com>
2015-11-04 19:06:28 +0100

    change to srv-catalog in clean script

--- solr/solrdeletedoc.sh
+++ solr/solrdeletedoc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl http://localhost:8984/solr/catalog/update \
+curl http://localhost:8984/solr/srv-catalog/update \
     --data '<delete><query>*:*</query></delete>' \
     -H 'Content-type:text/xml; charset=utf-8'
 
-curl http://localhost:8984/solr/catalog/update \
+curl http://localhost:8984/solr/srv-catalog/update \
     --data '<commit/>' \
     -H 'Content-type:text/xml; charset=utf-8'
