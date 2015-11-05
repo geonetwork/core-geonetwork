f12010afcc98ed1d82c01498266b2917e6f7bbc2
François Prunayre <fxprunayre@gmail.com>
2015-11-05 08:21:00 +0100

    Add a route for testing. Disabled by default.

--- camel-harvesters/wfsfeature-harvester/src/main/java/org/fao/geonet/harvester/wfsfeatures/HarvesterRouteBuilder.java
+++ camel-harvesters/wfsfeature-harvester/src/main/java/org/fao/geonet/harvester/wfsfeatures/HarvesterRouteBuilder.java
@@ -1,5 +1,6 @@
 package org.fao.geonet.harvester.wfsfeatures;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.w3c.dom.Document;
@@ -12,9 +13,17 @@ public class HarvesterRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        final String url = "http://geoservices.brgm.fr/risques?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=BASIAS_LOCALISE&maxFeatures=10000";
 
 //        final String url = "http4://geoservices.brgm.fr/risques?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=BASIAS_LOCALISE&maxFeatures=10000";
         //http://visi-sextant.ifremer.fr/cgi-bin/sextant/wfs/bgmb?REQUEST=GetFeature&SERVICE=WFS&VERSION=1.1.0&TypeName=SISMER_prelevements&maxFeatures=100 [
+
+        from("timer://start?repeatCount=1").autoStartup(true)
+                .log(LoggingLevel.DEBUG, LOGGER_NAME, "Harvesting ${body.url} one time.")
+                .setHeader(Exchange.HTTP_URI, simple(url))
+                .setProperty("mduuid", simple(""))
+                .setProperty("linkage", simple("test"))
+                .to("direct:index-wfs");
 
         from("spring-event:default")
                 .filter(body().startsWith("http"))
@@ -23,7 +32,11 @@ public class HarvesterRouteBuilder extends RouteBuilder {
                 .setProperty("mduuid", simple("${body.uuid}"))
                 .setProperty("linkage", simple("${body.linkage}"))
                 .setBody(simple(""))
-                .to("http4://temp")
+                .to("direct:index-wfs");
+
+        // TODO drop feature before adding new one ?
+        from("direct:index-wfs")
+                .to("http4://wfs-server-url")
                 .convertBodyTo(Document.class)
                 .split()
                     .xpath("//gml:featureMember/*|//wfs2:member/*", WfsNamespaces.wfsNamespaces.getNamespaces())
