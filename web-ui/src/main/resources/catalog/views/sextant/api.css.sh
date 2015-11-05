6a3caa9dca54704f73511db70f99576418f5e047
Antoine Abt <antoine@abt.im>
2015-11-05 15:32:46 +0100

    Fixes API classes to prevent MQ in fixed containers

--- web-ui/src/main/resources/catalog/views/sextant/api.css.sh
+++ web-ui/src/main/resources/catalog/views/sextant/api.css.sh
@@ -1,6 +1,7 @@
 lessc less/gn_search_sextant.less api.css
 sed 's/calc(\(.*\))/~"calc(\1)"/' api.css > less/tmp.css
 lessc less/sxt-api.less tmp.css
+sed -i 's/.gn .sxt-max-sm/.gn.sxt-max-sm/' tmp.css > api.css
 sed 's/.gn .sxt-max-md/.gn.sxt-max-md/' tmp.css > api.css
 sed  's/.gn .gn-img-modal/.gn-img-modal/' api.css
 rm tmp.css less/tmp.css
