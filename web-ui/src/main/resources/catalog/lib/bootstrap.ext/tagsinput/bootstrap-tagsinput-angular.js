geonetwork/core-geonetwork

commit 5b34ab7cc362db7b24bf43c3336b973b147aed55

Placeholder fix proposed by Juan
The placeholder was showing empty so adding this JQuery function we can add or recuperate the value of an attribute of an element
http://api.jquery.com/attr/ so we ask the values of tagsinput

web-ui/src/main/resources/catalog/lib/bootstrap.ext/tagsinput/bootstrap-tagsinput-angular.js
@@ -26,6 +26,10 @@ angular.module('bootstrap-tagsinput', [])
           scope.model = [];
 
         var select = $('select', element);
+        
+        if (attrs.placeholder) {
++          select.attr('placeholder', attrs.placeholder);
++        }
 
         select.tagsinput({
           typeahead : {

commit 453261443745099a71bc280b3fdd79b1eb8a967e

Fix WRO4J JS error introduced in https://github.com/geonetwork/core-geonetwork/commit/5b34ab7cc362db7b24bf43c3336b973b147aed55.

web-ui/src/main/resources/catalog/lib/bootstrap.ext/tagsinput/bootstrap-tagsinput-angular.js
@@ -28,8 +28,8 @@ angular.module('bootstrap-tagsinput', [])
         var select = $('select', element);
         
         if (attrs.placeholder) {
-+          select.attr('placeholder', attrs.placeholder);
-+        }
+          select.attr('placeholder', attrs.placeholder);
+        }
 
         select.tagsinput({
           typeahead : {