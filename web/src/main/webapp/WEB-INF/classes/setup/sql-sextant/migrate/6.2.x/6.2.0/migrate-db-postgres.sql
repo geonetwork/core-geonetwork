ed54302a157d70c10c6d247b3c573cfc8951e1a4
François Prunayre <fx.prunayre@gmail.com>
2016-01-04 20:39:49 +0100

    Add migration for iso19139.sextant.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.0/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.0/migrate-db-postgres.sql
@@ -1,6 +1,7 @@
 UPDATE Users SET enabled = true;
 UPDATE Mapservers set pushstyleinworkspace = 'n';
 
+UPDATE metadata SET schemaid = 'iso19139' WHERE schemaid = 'iso19139.sextant';
 
 UPDATE Settings SET value='3.0.4' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
