c5b607fcc5ce9d246035a5a8416650ffb7cfc271
François Prunayre <fx.prunayre@gmail.com>
2016-01-27 14:17:33 +0100

    Sextant 6.2.x is now aligned to GN3.1.x.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.0/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.0/migrate-db-postgres.sql
@@ -3,5 +3,9 @@ UPDATE Mapservers set pushstyleinworkspace = 'n';
 
 UPDATE metadata SET schemaid = 'iso19139' WHERE schemaid = 'iso19139.sextant';
 
-UPDATE Settings SET value='3.0.4' WHERE name='system/platform/version';
+
+-- TODO : resource link change cf. MetadataResourceDatabaseMigration
+
+
+UPDATE Settings SET value='3.1.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
