ab3f664a9d067b25ca1592cde0aa8b43a98fa9e9
María Arias de Reyna <maria.arias@geocat.net>
2016-02-05 15:32:45 +0100

    Setting for how much time the editing lock should last (if should)

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v304/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v304/migrate-default.sql
@@ -1,2 +1,4 @@
 UPDATE Settings SET value='3.0.4' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/lock', '-1', 1, 100003, 'n');
