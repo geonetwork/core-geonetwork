5996b73ff2bb023c892bfe8e347007f93c481006
Jose Garcia <jose.garcia@geocat.net>
2016-02-25 12:17:17 +0100

    Rename field 'key' for HarvesterData table as 'key' is a reserved work, to avoid issues when creating the table by Hibernate is some databases like MySql

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v304/migrate-mysql.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v304/migrate-mysql.sql
@@ -1,4 +1,4 @@
 UPDATE Settings SET value='3.0.4' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
-ALTER TABLE HarvesterData RENAME COLUMN "key" TO keyvalue;
+ALTER TABLE HarvesterData CHANGE `key` keyvalue varchar(255);
