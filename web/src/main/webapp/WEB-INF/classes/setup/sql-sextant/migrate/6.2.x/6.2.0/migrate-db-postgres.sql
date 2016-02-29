dbab67833f18ddf725b078519dc3204888925cec
François Prunayre <fx.prunayre@gmail.com>
2016-02-29 10:28:15 +0100

    Migration for 6.2.1.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.0/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.0/migrate-db-postgres.sql
@@ -1,11 +1,7 @@
 UPDATE Users SET enabled = true;
 UPDATE Mapservers set pushstyleinworkspace = 'n';
 
-UPDATE metadata SET schemaid = 'iso19139' WHERE schemaid = 'iso19139.sextant';
-
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/feedback/mailServer/tls', 'false', 2, 644, 'y');
-
--- TODO : resource link change cf. MetadataResourceDatabaseMigration
 
 
 UPDATE Settings SET value='3.1.0' WHERE name='system/platform/version';
---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.1/migrate-db-postgres.sql
@@ -0,0 +1,8 @@
+
+UPDATE metadata SET schemaid = 'iso19139' WHERE schemaid = 'iso19139.sextant';
+
+-- TODO : resource link change cf. MetadataResourceDatabaseMigration
+
+
+UPDATE Settings SET value='3.1.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
