453e1427752e29dbd564c4da1c68049f08b52267
François Prunayre <fx.prunayre@gmail.com>
2016-03-02 19:43:45 +0100

    Add tooltip on heatmap count.

--- web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
@@ -45,18 +45,49 @@
 
           scope.isHeatMapVisible = false;
           scope.heatmapLayer = null;
-          var source;
+          scope.source = null;
           if (scope.map) {
-            source = new ol.source.Vector();
+            scope.source = new ol.source.Vector();
             scope.isHeatMapVisible = true;
             scope.heatmapLayer = new ol.layer.Heatmap({
-              source: source,
+              source: scope.source,
               radius: 40,
               blur: 50,
               opacity: .8,
               visible: scope.isHeatMapVisible
             });
             scope.map.addLayer(scope.heatmapLayer);
+
+            $('body').append('<div id="heatmap-info" data-content=""' +
+              'style="position: absolute; z-index: 100;"/>');
+            var info = $('#heatmap-info');
+            var displayFeatureInfo = function(pixel) {
+              var feature = scope.map.forEachFeatureAtPixel(pixel,
+                function(feature, layer) {
+                  if (layer == scope.heatmapLayer) {
+                    return feature;
+                  }
+                });
+              if (feature) {
+                info.css({
+                  left: pixel[0] + 'px',
+                  top: (pixel[1] + 45) + 'px'
+                });
+                info.tooltip('hide')
+                    .attr('data-original-title', feature.get('count'))
+                    .tooltip('show');
+              } else {
+                info.tooltip('hide');
+              }
+            };
+
+            scope.map.on('pointermove', function(evt) {
+              if (evt.dragging) {
+                info.tooltip('hide');
+                return;
+              }
+              displayFeatureInfo(scope.map.getEventPixel(evt.originalEvent));
+            });
           }
 
           /**
@@ -354,8 +385,8 @@
 
 
           function resetHeatMap() {
-            if (source) {
-              source.clear();
+            if (scope.source) {
+              scope.source.clear();
             }
             scope.map.un('moveend', refreshHeatmap);
           }
@@ -371,8 +402,8 @@
             if (n != o) {
               // TODO: May contains multiple heatmaps
               if (angular.isArray(n.geom)) {
-                source.clear();
-                source.addFeatures(
+                scope.source.clear();
+                scope.source.addFeatures(
                     gnSolrService.heatmapToFeatures(
                     n.geom,
                     scope.map.getView().getProjection())
