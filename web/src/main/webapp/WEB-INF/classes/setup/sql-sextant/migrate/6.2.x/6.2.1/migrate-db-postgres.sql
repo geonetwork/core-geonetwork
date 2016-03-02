f1fd878de2e5fb973cb6efcf3c80a2af02d137ba
François Prunayre <fx.prunayre@gmail.com>
2016-03-02 15:51:16 +0100

    Update SQL migration with new setting.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.1/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.1/migrate-db-postgres.sql
@@ -2,6 +2,7 @@
 UPDATE metadata SET schemaid = 'iso19139' WHERE schemaid = 'iso19139.sextant';
 
 -- TODO : resource link change cf. MetadataResourceDatabaseMigration
++INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/xlinkResolver/ignore', 'operatesOn,featureCatalogueCitation,Anchor,source', 0, 2312, 'n');
 
 
 UPDATE Settings SET value='3.1.0' WHERE name='system/platform/version';
