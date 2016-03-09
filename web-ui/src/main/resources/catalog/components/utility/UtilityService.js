15ac11930363aaf89a8e25aafe31a92b951c8645
François Prunayre <fx.prunayre@gmail.com>
2016-03-09 16:23:52 +0100

    JSLint.

--- web-ui/src/main/resources/catalog/components/utility/UtilityService.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityService.js
@@ -298,10 +298,12 @@
     };
 
     function randomUuid() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-      });
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+          /[xy]/g,
+          function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+          });
     }
     return {
       scrollTo: scrollTo,
