390c41e0d9fe693bbc642e8901fd82443944e829
Jose Garcia <jose.garcia@geocat.net>
2016-03-18 14:40:12 +0100

    Update version to 3.0.4-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v304/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v304/migrate-default.sql
@@ -1,2 +1,2 @@
 UPDATE Settings SET value='3.0.4' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
