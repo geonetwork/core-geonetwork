bdab941fd623eaf63f38b0dbe7c6a24154221ec5
Antoine Abt <antoine@abt.im>
2016-04-11 18:08:33 +0200

    Remove old GFI code

--- web-ui/src/main/resources/catalog/components/viewer/gfi/GetFeatureInfoDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/gfi/GetFeatureInfoDirective.js
@@ -105,137 +105,17 @@
    * GFI results as an array in a popup.
    * The template could be overriden using `gfiTemplateURL` constant.
    */
-  module.directive('gnGfi', [
-    '$http',
-    'gfiTemplateURL',
+  module.directive('gnGfi', [ '$http', 'gfiTemplateURL',
     function($http, gfiTemplateURL) {
-
       return {
         restrict: 'A',
         scope: {
           map: '='
         },
         controller: 'gnGfiController',
-        templateUrl: gfiTemplateURL,
-        link: function(scope, element, attrs) {
-
-          var map = scope.map;
-          var mapElement = $(map.getTarget());
-          mapElement.find('.ol-overlaycontainer-stopevent').append(element);
-
-          var format = new ol.format.WMSGetFeatureInfo();
-          var fo = new ol.layer.Vector({
-            source: new ol.source.Vector({
-              useSpatialIndex: false
-            }),
-            map: map
-          });
-
-          scope.results = fo.getSource().getFeatures();
-          scope.pending = 0;
-          var overlay = new ol.Overlay({
-            positioning: 'center-center',
-            position: undefined,
-            element: $('<span class="marker">+</span>')[0]
-          });
-          map.addOverlay(overlay);
-
-          scope.close = function() {
-            fo.getSource().clear();
-            scope.pending = 0;
-            overlay.setPosition(undefined);
-          };
-
-          map.on('singleclick', function(e) {
-
-            for (var i = 0; i < map.getInteractions().getArray().length; i++) {
-              var interaction = map.getInteractions().getArray()[i];
-              if ((interaction instanceof ol.interaction.Draw ||
-                  interaction instanceof ol.interaction.Select) &&
-                  interaction.getActive()) {
-                return;
-              }
-            }
-
-            fo.getSource().clear();
-            var layers = map.getLayers().getArray().filter(function(layer) {
-              return layer.getSource() instanceof ol.source.ImageWMS ||
-                  layer.getSource() instanceof ol.source.TileWMS;
-            });
-
-            layers.forEach(function(layer) {
-              if (!layer.getVisible()) {
-                return;
-              }
-              var uri = layer.getSource().getGetFeatureInfoUrl(e.coordinate,
-                  map.getView().getResolution(),
-                  map.getView().getProjection(), {
-                    INFO_FORMAT: layer.ncInfo ? 'text/xml' :
-                        'application/vnd.ogc.gml'
-                  });
-              var coordinate = e.coordinate;
-              var proxyUrl = '../../proxy?url=' + encodeURIComponent(uri);
-              scope.pending += 1;
-              return $http.get(proxyUrl).success(function(response) {
-                var features;
-                if (layer.ncInfo) {
-                  var doc = ol.xml.parse(response);
-                  var props = {};
-                  ['longitude', 'latitude',
-                    'time', 'value'].forEach(function(v) {
-                    var node = doc.getElementsByTagName(v);
-                    if (node && node.length > 0) {
-                      props[v] = ol.xml.getAllTextContent(node[0], true);
-                    }
-                  });
-                  if (props.value && props.value != 'none') {
-                    features = [new ol.Feature(props)];
-                  }
-                } else {
-                  features = format.readFeatures(response);
-                }
-                if (features) {
-                  features.forEach(function(f) {
-                    f.layer = layer.get('label');
-                  });
-                  fo.getSource().getFeaturesCollection().extend(features);
-                  if (features.length > 0) {
-                    overlay.setPosition(coordinate);
-                  } else {
-                    overlay.setPosition();
-                  }
-                }
-                scope.pending = Math.max(0, scope.pending - 1);
-              }).error(function() {
-                scope.pending = Math.max(0, scope.pending - 1);
-              });
-            }, this);
-
-          });
-
-          scope.$watch('pending', function(v) {
-            mapElement.toggleClass('gn-gfi-loading', (v !== 0));
-          });
-
-        }
+        templateUrl: gfiTemplateURL
       };
-
-    }])
-
-  .filter('attributes', function() {
-        return function(properties) {
-          var props = {};
-          var exclude = ['FID', 'boundedBy', 'the_geom', 'thegeom'];
-          Object.keys(properties).forEach(function(k) {
-            if (exclude.indexOf(k) !== -1) return;
-            if (properties[k]) {
-              props[k] = properties[k].toString();
-            }
-          });
-          return props;
-        };
-      });
-
+  }]);
 
   geonetwork.GnGfiController = function($scope, gnFeaturesTableManager) {
 
