206cff1b9c52b415404fb15a75aa7fda91f3247f
François Prunayre <fx.prunayre@gmail.com>
2016-04-12 19:38:21 +0200

    Restore swagger config.

---
+++ services/src/main/java/org/fao/geonet/api/SwaggerConfig.java
@@ -0,0 +1,124 @@
+/*
+ * =============================================================================
+ * ===	Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * ===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * ===	and United Nations Environment Programme (UNEP)
+ * ===
+ * ===	This program is free software; you can redistribute it and/or modify
+ * ===	it under the terms of the GNU General Public License as published by
+ * ===	the Free Software Foundation; either version 2 of the License, or (at
+ * ===	your option) any later version.
+ * ===
+ * ===	This program is distributed in the hope that it will be useful, but
+ * ===	WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * ===	General Public License for more details.
+ * ===
+ * ===	You should have received a copy of the GNU General Public License
+ * ===	along with this program; if not, write to the Free Software
+ * ===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ * ===
+ * ===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * ===	Rome - Italy. email: geonetwork@osgeo.org
+ * ==============================================================================
+ */
+
+package org.fao.geonet.api;
+
+import com.google.common.base.Predicate;
+
+import com.fasterxml.classmate.TypeResolver;
+
+import org.fao.geonet.domain.UserSecurity;
+import org.jdom.Element;
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.builders.PathSelectors.regex;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+@EnableWebMvc
+@Configuration
+@Service
+@ComponentScan(basePackages = {"org.fao.geonet.api"})
+@EnableSwagger2 //Loads the spring beans required by the framework
+public class SwaggerConfig {
+    @Autowired
+    private TypeResolver typeResolver;
+
+    private Docket doc;
+
+    private Predicate<String> paths() {
+        return or(
+            regex("/api/" + API.VERSION_0_1 + ".*")
+        );
+    }
+
+    public void loadApi() {
+        this.doc = new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(new ApiInfo(
+                "GeoNetwork Api Documentation (beta)",
+                "Learn how to access the catalog using the GeoNetwork REST API.",
+                API.VERSION_0_1,
+                "urn:tos",
+                API.CONTACT_EMAIL,
+                "GPL 2.0",
+                "http://www.gnu.org/licenses/old-licenses/gpl-2.0.html"))
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(paths())
+            .build()
+            .pathMapping("/srv/")
+            .directModelSubstitute(LocalDate.class, String.class)
+            .directModelSubstitute(UserSecurity.class, Object.class)
+            .directModelSubstitute(Element.class, Object.class)
+            .genericModelSubstitutes(ResponseEntity.class)
+            .alternateTypeRules(
+                newRule(typeResolver.resolve(DeferredResult.class,
+                    typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                    typeResolver.resolve(WildcardType.class)))
+            .useDefaultResponseMessages(false)
+//                .globalResponseMessage(RequestMethod.GET,
+//                        newArrayList(new ResponseMessageBuilder()
+//                                .code(500)
+//                                .message("500 message")
+//                                .responseModel(new ModelRef("Error"))
+//                                .build()))
+            .securitySchemes(newArrayList(new BasicAuth("ba")))
+//                .securityContexts(newArrayList(securityContext()))
+//                .enableUrlTemplating(true)
+//                .globalOperationParameters(
+//                        newArrayList(new ParameterBuilder()
+//                                .name("_content_type")
+//                                .description("Description of someGlobalParameter")
+//                                .modelRef(new ModelRef("string"))
+//                                .parameterType("query")
+//                                .required(true)
+//                                .build()))
+        ;
+    }
+
+    @Bean
+    public Docket geonetworkApi() {
+        // TODO: Add not id in pathMapping
+        loadApi();
+        return this.doc;
+    }
+}
