d745be7ba28583a75f2c638f5eab1e3cfc01e2a5
François Prunayre <fx.prunayre@gmail.com>
2016-04-13 13:52:36 +0200

    Fix WFS feature search.

--- web-ui/src/main/resources/catalog/components/solr/SolrRequest.js
+++ web-ui/src/main/resources/catalog/components/solr/SolrRequest.js
@@ -124,7 +124,7 @@
   };
 
   geonetwork.GnSolrRequest.prototype.buildSolrUrl = function(params) {
-    return this.urlUtils.append(this.config.url + '/query',
+    return this.urlUtils.append(this.config.url + '/features',
         this.urlUtils.toKeyValue(params));
   };
 
