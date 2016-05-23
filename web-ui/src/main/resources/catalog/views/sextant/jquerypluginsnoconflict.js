9a497493b8fc7836ff49b9f081eaaf12ad2b040f
Florent gravin <florent.gravin@camptocamp.com>
2016-05-23 17:36:15 +0200

    #29145 - Fix conflicts with jquery plugins (coriolis)

---
+++ web-ui/src/main/resources/catalog/views/sextant/jquerypluginsnoconflict.js
@@ -0,0 +1,7 @@
+for(var p in window.sxtApiJQuery.fn) {
+  if(!jQuery.fn.hasOwnProperty(p)) {
+    jQuery.fn[p] = window.sxtApiJQuery.fn[p];
+  }
+
+  jQuery.fancybox = window.sxtApiJQuery.fancybox;
+}
