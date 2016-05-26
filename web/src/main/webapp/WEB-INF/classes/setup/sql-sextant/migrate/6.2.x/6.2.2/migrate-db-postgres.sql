b7cf76128629b488d703dc4332b1ff97fa8cce01
mtreguer <mickael.treguer@ifremer.fr>
2016-05-26 13:47:07 +0200

    Update migrate-db-postgres.sql

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.2/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.2.x/6.2.2/migrate-db-postgres.sql
@@ -1,5 +1,5 @@
 
 UPDATE metadata SET data = REPLACE(data, '>WFS<', '>OGC:WFS<') WHERE data LIKE '%>WFS<%';
 UPDATE metadata SET data = REPLACE(data, '>WCS<', '>OGC:WCS<') WHERE data LIKE '%>WCS<%';
-UPDATE metadata SET data = REPLACE(data, 'WWW:LINK-1.0-http--link', 'WWW:LINK') WHERE data LIKE '%WWW:LINK-1.0-http--link%';
-UPDATE metadata SET data = REPLACE(data, 'OGC:WMS-1.1.1-http-get-map', 'OGC:WMS') WHERE data LIKE '%OGC:WMS-1.1.1-http-get-map%';
+UPDATE metadata SET data = REPLACE(data, '>WWW:LINK-1.0-http--link<', '>WWW:LINK<') WHERE data LIKE '%>WWW:LINK-1.0-http--link<%';
+UPDATE metadata SET data = REPLACE(data, '>OGC:WMS-1.1.1-http-get-map<', '>OGC:WMS<') WHERE data LIKE '%>OGC:WMS-1.1.1-http-get-map<%';
