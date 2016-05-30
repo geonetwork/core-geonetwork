9b00fcf80abb9dd8228f9c579cfdb46e436cef00
Florent gravin <florent.gravin@camptocamp.com>
2016-05-30 17:33:04 +0200

    #29457 - Fix temp chrome issue by adding __id random parameter to each api requests to avoid caching

--- web-ui/src/main/resources/catalog/views/sextant/interceptors.js
+++ web-ui/src/main/resources/catalog/views/sextant/interceptors.js
@@ -4,20 +4,43 @@
 
   var module = angular.module('sxt_interceptors', []);
 
+  var isChrome = /Chrome/.test(navigator.userAgent) &&
+      /Google Inc/.test(navigator.vendor);
+
   module.factory('sxtInterceptor', ['gnGlobalSettings',
     function(gnGlobalSettings) {
 
-    return {
-      'request': function(config) {
-        if(config.url.indexOf('http://') < 0 && config.url.indexOf('https://') < 0) {
-          if(!config.url.match(/(partials\/).*(.html)$/)) {
-            config.url = gnGlobalSettings.gnUrl + config.url;
+      return {
+        'request': function(config) {
+          if(config.url.indexOf('http://') < 0 &&
+              config.url.indexOf('https://') < 0) {
+            if(!config.url.match(/(partials\/).*(.html)$/)) {
+
+              // Temp for chrome to avoid CORS caching issue
+              if(isChrome && config.cache != true
+                  && config.url != 'q'
+                  && config.url.indexOf('qi?') != 0
+                  && config.url.indexOf('q?') != 0 ) {
+
+                var param = {
+                  __id: Math.floor(Math.random() * (99999999999 - 0))
+                };
+                config.params = angular.extend({}, config.params, param);
+              }
+
+              if(config.url[0] == 'q') {
+                console.log(config.url);
+              }
+
+              // Use api url
+              config.url = gnGlobalSettings.gnUrl + config.url;
+
+            }
           }
+          return config;
         }
-        return config;
-      }
-    };
-  }]);
+      };
+    }]);
 
   module.config([
     '$httpProvider',
@@ -27,5 +50,5 @@
       if(gnGlobalSettings.gnUrl) {
         $httpProvider.interceptors.push('sxtInterceptor');
       }
-  }]);
+    }]);
 })();
