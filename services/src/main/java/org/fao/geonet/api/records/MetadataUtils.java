377f07f8c84ae15b776fda5288387b47a4bb0eea
François Prunayre <fx.prunayre@gmail.com>
2016-06-02 13:08:07 +0200

    Editor / Add editing of related documents  (#1564)
    * Migrate related service to Spring and API.
    
    * md.relations, xml.relations is now /api/records/<uuid>/related with type parameter (array of String).
    * Editor / Add support for editing a link or thumbnail
    * Schema / Needs update in extract-relations.xsl in all schemas
    
    * Migrate related service / Update RelatedDirective.
    
    * Add testing for onlinesrc-add.xsl.
    
    * Editor / Related / Basic support for multilingual resources when not an OGC protocol.
    
    * Editor / Related / Support multilingual editing of OGC links.
    
    * Fix test.
    
    * Migrate related service to Spring and API.
    
    * md.relations, xml.relations is now /api/records/<uuid>/related with type parameter (array of String).
    * Editor / Add support for editing a link or thumbnail
    * Schema / Needs update in extract-relations.xsl in all schemas
    
    * Migrate related service / Update RelatedDirective.
    
    * Add testing for onlinesrc-add.xsl.
    
    * Editor / Related / Basic support for multilingual resources when not an OGC protocol.
    
    * Editor / Related / Support multilingual editing of OGC links.
    
    * Fix test.
    
    * ISO19139 / Add function and app profile to online resource panel.
    
    * Better support multilingual records.
    
    * Add support for ISO19115-3.

---
+++ services/src/main/java/org/fao/geonet/api/records/MetadataUtils.java
@@ -0,0 +1,258 @@
+/*
+ * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+package org.fao.geonet.api.records;
+
+import com.google.common.base.Joiner;
+
+import org.fao.geonet.GeonetContext;
+import org.fao.geonet.api.records.model.related.RelatedItemType;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.domain.ReservedOperation;
+import org.fao.geonet.kernel.DataManager;
+import org.fao.geonet.kernel.SchemaManager;
+import org.fao.geonet.kernel.schema.AssociatedResource;
+import org.fao.geonet.kernel.schema.AssociatedResourcesSchemaPlugin;
+import org.fao.geonet.kernel.schema.SchemaPlugin;
+import org.fao.geonet.kernel.search.MetaSearcher;
+import org.fao.geonet.kernel.search.SearchManager;
+import org.fao.geonet.kernel.search.SearcherType;
+import org.fao.geonet.lib.Lib;
+import org.fao.geonet.services.metadata.Show;
+import org.fao.geonet.services.relations.Get;
+import org.fao.geonet.utils.Log;
+import org.jdom.Content;
+import org.jdom.Element;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import jeeves.constants.Jeeves;
+import jeeves.server.ServiceConfig;
+import jeeves.server.context.ServiceContext;
+
+/**
+ *
+ */
+public class MetadataUtils {
+    public static final boolean forEditing = false, withValidationErrors = false, keepXlinkAttributes = false;
+
+    public static Element getRelated(ServiceContext context, int iId, String uuid,
+                                     RelatedItemType[] type,
+                                     int from_, int to_, boolean fast_)
+        throws Exception {
+        final String id = String.valueOf(iId);
+        final String from = "" + from_;
+        final String to = "" + to_;
+        final String fast = "" + fast_;
+        GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
+        DataManager dm = gc.getBean(DataManager.class);
+        Element relatedRecords = new Element("relations");
+        List<RelatedItemType> listOfTypes = new ArrayList<RelatedItemType>(Arrays.asList(type));
+
+        // Get the cached version (use by classic GUI)
+        Element md = Show.getCached(context.getUserSession(), id);
+        if (md == null) {
+            // Get from DB
+            md = dm.getMetadata(context, id, forEditing, withValidationErrors,
+                keepXlinkAttributes);
+        }
+
+        String schemaIdentifier = dm.getMetadataSchema(id);
+        SchemaPlugin instance = SchemaManager.getSchemaPlugin(schemaIdentifier);
+        AssociatedResourcesSchemaPlugin schemaPlugin = null;
+        if (instance instanceof AssociatedResourcesSchemaPlugin) {
+            schemaPlugin = (AssociatedResourcesSchemaPlugin) instance;
+        }
+
+        // Search for children of this record
+        if (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.children)) {
+            relatedRecords.addContent(search(uuid, "children", context, from, to, fast));
+        }
+
+        // Get parent record from this record
+        if (schemaPlugin != null && (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.parent))) {
+            Set<String> listOfUUIDs = schemaPlugin.getAssociatedParentUUIDs(md);
+            if (listOfUUIDs.size() > 0) {
+                String joinedUUIDs = Joiner.on(" or ").join(listOfUUIDs);
+                relatedRecords.addContent(search(joinedUUIDs, "parent", context, from, to, fast));
+            }
+        }
+
+        // Get aggregates from this record
+        if (schemaPlugin != null && (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.siblings))) {
+            Element response = new Element("response");
+
+            Set<AssociatedResource> listOfAssociatedResources = schemaPlugin.getAssociatedResourcesUUIDs(md);
+
+            if (listOfAssociatedResources != null) {
+                for (AssociatedResource resource : listOfAssociatedResources) {
+                    Element sibContent = getRecord(resource.getUuid(), context, dm);
+                    if (sibContent != null) {
+                        Element sibling = new Element("sibling");
+                        sibling.setAttribute("initiative", resource.getInitiativeType());
+                        sibling.setAttribute("association", resource.getAssociationType());
+                        response.addContent(sibling.addContent(sibContent));
+                    }
+                }
+            }
+            relatedRecords.addContent(new Element("siblings").addContent(response));
+        }
+
+        // Search for records where an aggregate point to this record
+        if (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.associated)) {
+            relatedRecords.addContent(search(uuid, "associated", context, from, to, fast));
+        }
+
+        // Search for services
+        if (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.services)) {
+            relatedRecords.addContent(search(uuid, "services", context, from, to, fast));
+        }
+
+        // Related record from uuiref attributes in metadata record
+        if (schemaPlugin != null && (
+            listOfTypes.size() == 0 ||
+                listOfTypes.contains(RelatedItemType.datasets) ||
+                listOfTypes.contains(RelatedItemType.fcats) ||
+                listOfTypes.contains(RelatedItemType.sources)
+        )) {
+            // Get datasets related to service search
+            if (listOfTypes.size() == 0 ||
+                listOfTypes.contains(RelatedItemType.datasets)) {
+                Set<String> listOfUUIDs = schemaPlugin.getAssociatedDatasetUUIDs(md);
+                if (listOfUUIDs != null && listOfUUIDs.size() > 0) {
+                    String joinedUUIDs = Joiner.on(" or ").join(listOfUUIDs);
+                    relatedRecords.addContent(search(joinedUUIDs, "datasets", context, from, to, fast));
+                }
+            }
+            // if source, return source datasets defined in the current record
+            if (listOfTypes.size() == 0 ||
+                listOfTypes.contains(RelatedItemType.sources)) {
+                Set<String> listOfUUIDs = schemaPlugin.getAssociatedSourceUUIDs(md);
+                if (listOfUUIDs != null && listOfUUIDs.size() > 0) {
+                    String joinedUUIDs = Joiner.on(" or ").join(listOfUUIDs);
+                    relatedRecords.addContent(search(joinedUUIDs, "sources", context, from, to, fast));
+                }
+            }
+            // if fcat
+            if (listOfTypes.size() == 0 ||
+                listOfTypes.contains(RelatedItemType.fcats)) {
+                Set<String> listOfUUIDs = schemaPlugin.getAssociatedFeatureCatalogueUUIDs(md);
+                if (listOfUUIDs != null && listOfUUIDs.size() > 0) {
+                    String joinedUUIDs = Joiner.on(" or ").join(listOfUUIDs);
+                    relatedRecords.addContent(search(joinedUUIDs, "fcats", context, from, to, fast));
+                }
+            }
+        }
+
+        //
+        if (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.hassources)) {
+            // Return records where this record is a source dataset
+            relatedRecords.addContent(search(uuid, "hassource", context, from, to, fast));
+        }
+
+        // Relation table is preserved for backward compatibility but should not be used anymore.
+        if (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.related)) {
+            // Related records could be feature catalogue defined in relation table
+            relatedRecords.addContent(new Element("related").addContent(Get.getRelation(iId, "full", context)));
+            // Or feature catalogue define in feature catalogue citation
+            relatedRecords.addContent(search(uuid, "hasfeaturecat", context, from, to, fast));
+        }
+
+        // XSL transformation is used on the metadata record to extract
+        // distribution information or thumbnails
+        if (md != null && (listOfTypes.size() == 0 ||
+            listOfTypes.contains(RelatedItemType.onlines) ||
+            listOfTypes.contains(RelatedItemType.thumbnails))) {
+            relatedRecords.addContent(new Element("metadata").addContent((Content) md.clone()));
+        }
+
+        return relatedRecords;
+    }
+
+
+    private static Element search(String uuid, String type, ServiceContext context, String from, String to, String fast) throws Exception {
+        GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
+        SearchManager searchMan = gc.getBean(SearchManager.class);
+
+        // perform the search
+        if (Log.isDebugEnabled(Geonet.SEARCH_ENGINE))
+            Log.debug(Geonet.SEARCH_ENGINE, "Searching for: " + type);
+
+        try (MetaSearcher searcher = searchMan.newSearcher(SearcherType.LUCENE, Geonet.File.SEARCH_LUCENE)) {
+            // Creating parameters for search, fast only to retrieve uuid
+            Element parameters = new Element(Jeeves.Elem.REQUEST);
+            if ("children".equals(type))
+                parameters.addContent(new Element("parentUuid").setText(uuid));
+            else if ("services".equals(type))
+                parameters.addContent(new Element("operatesOn").setText(uuid));
+            else if ("hasfeaturecat".equals(type))
+                parameters.addContent(new Element("hasfeaturecat").setText(uuid));
+            else if ("hassource".equals(type))
+                parameters.addContent(new Element("hassource").setText(uuid));
+            else if ("associated".equals(type))
+                parameters.addContent(new Element("agg_associated").setText(uuid));
+            else if ("datasets".equals(type) || "fcats".equals(type) ||
+                "sources".equals(type) || "siblings".equals(type) ||
+                "parent".equals(type))
+                parameters.addContent(new Element("uuid").setText(uuid));
+
+            parameters.addContent(new Element("fast").addContent("index"));
+            parameters.addContent(new Element("sortBy").addContent("title"));
+            parameters.addContent(new Element("sortOrder").addContent("reverse"));
+            parameters.addContent(new Element("buildSummary").addContent("false"));
+            parameters.addContent(new Element("from").addContent(from));
+            parameters.addContent(new Element("to").addContent(to));
+
+            ServiceConfig config = new ServiceConfig();
+            searcher.search(context, parameters, config);
+
+            Element response = new Element(type);
+            Element relatedElement = searcher.present(context, parameters, config);
+            response.addContent(relatedElement);
+            return response;
+        }
+    }
+
+    private static Element getRecord(String uuid, ServiceContext context, DataManager dm) {
+        Element content = null;
+        try {
+            String id = dm.getMetadataId(uuid);
+            Lib.resource.checkPrivilege(context, id, ReservedOperation.view);
+            content = dm.getMetadata(context, id, forEditing, withValidationErrors, keepXlinkAttributes);
+        } catch (Exception e) {
+            if (Log.isDebugEnabled(Geonet.SEARCH_ENGINE))
+                Log.debug(Geonet.SEARCH_ENGINE, "Metadata " + uuid + " record is not visible for user.");
+        }
+        return content;
+    }
+}
---
+++ services/src/main/java/org/fao/geonet/api/records/model/related/RelatedItemType.java
@@ -0,0 +1,38 @@
+
+package org.fao.geonet.api.records.model.related;
+
+
+import java.util.Arrays;
+
+public enum RelatedItemType {
+
+    children,
+    parent,
+    siblings,
+    associated,
+    services,
+    datasets,
+    fcats,
+    sources,
+    hassources,
+    related,
+    onlines,
+    thumbnails;
+
+    public String value() {
+        return name();
+    }
+
+
+    public static RelatedItemType find(String lookupName) {
+        for (RelatedItemType value : values()) {
+            if(value.name().equals(lookupName)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException(String.format(
+            "Invalid related type value '%s'. Allowed values are: %s",
+            lookupName, values()
+        ));
+    }
+}
