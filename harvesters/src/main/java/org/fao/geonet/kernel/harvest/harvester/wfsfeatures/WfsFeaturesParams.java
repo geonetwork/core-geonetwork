b5bbfb3c0e08677cf729c73e29d7c0219c0d9901
Simon Pigot <Simon.Pigot@csiro.au>
2016-06-08 20:30:00 +1000

    Fix for #1583

--- harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/wfsfeatures/WfsFeaturesParams.java
+++ harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/wfsfeatures/WfsFeaturesParams.java
@@ -1,24 +1,24 @@
 //=============================================================================
-//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
-//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
-//===	and United Nations Environment Programme (UNEP)
+//===  Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===  United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===  and United Nations Environment Programme (UNEP)
 //===
-//===	This program is free software; you can redistribute it and/or modify
-//===	it under the terms of the GNU General Public License as published by
-//===	the Free Software Foundation; either version 2 of the License, or (at
-//===	your option) any later version.
+//===  This program is free software; you can redistribute it and/or modify
+//===  it under the terms of the GNU General Public License as published by
+//===  the Free Software Foundation; either version 2 of the License, or (at
+//===  your option) any later version.
 //===
-//===	This program is distributed in the hope that it will be useful, but
-//===	WITHOUT ANY WARRANTY; without even the implied warranty of
-//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-//===	General Public License for more details.
+//===  This program is distributed in the hope that it will be useful, but
+//===  WITHOUT ANY WARRANTY; without even the implied warranty of
+//===  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===  General Public License for more details.
 //===
-//===	You should have received a copy of the GNU General Public License
-//===	along with this program; if not, write to the Free Software
-//===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//===  You should have received a copy of the GNU General Public License
+//===  along with this program; if not, write to the Free Software
+//===  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
 //===
-//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
-//===	Rome - Italy. email: geonetwork@osgeo.org
+//===  Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===  Rome - Italy. email: geonetwork@osgeo.org
 //==============================================================================
 
 package org.fao.geonet.kernel.harvest.harvester.wfsfeatures;
@@ -27,7 +27,10 @@ import org.fao.geonet.Util;
 import org.fao.geonet.exceptions.BadInputEx;
 import org.fao.geonet.kernel.DataManager;
 import org.fao.geonet.kernel.harvest.harvester.AbstractParams;
+import org.fao.geonet.utils.Xml;
 import org.jdom.Element;
+
+import java.util.List;
 
 //=============================================================================
 
@@ -77,6 +80,18 @@ public class WfsFeaturesParams extends AbstractParams {
         super(dm);
     }
 
+    private String getQuery(Element params) {
+        String query = "";
+        Element q = Util.getChild(params, "query");
+        if (q != null) {
+          List<Element> qu = q.getChildren();
+          if (qu.size() == 1) {
+            query = Xml.getString(qu.get(0));
+          }
+        }
+        return query;
+    }
+
     public void create(Element node) throws BadInputEx {
         super.create(node);
 
@@ -86,7 +101,7 @@ public class WfsFeaturesParams extends AbstractParams {
         url = Util.getParam(site, "url", "");
         icon = Util.getParam(site, "icon", "wfs.gif");
         lang = Util.getParam(opt, "lang", "");
-        query = Util.getParam(opt, "query", "");
+        query = getQuery(opt);
         outputSchema = Util.getParam(opt, "outputSchema", "");
         stylesheet = Util.getParam(opt, "stylesheet", "");
         streamFeatures = Util.getParam(opt, "streamFeatures", true);
@@ -104,7 +119,7 @@ public class WfsFeaturesParams extends AbstractParams {
         url = Util.getParam(site, "url", url);
         icon = Util.getParam(site, "icon", "wfs.gif");
         lang = Util.getParam(opt, "lang", lang);
-        query = Util.getParam(opt, "query", "");
+        query = getQuery(opt);
         outputSchema = Util.getParam(opt, "outputSchema", "");
         stylesheet = Util.getParam(opt, "stylesheet", "");
         streamFeatures = Util.getParam(opt, "streamFeatures", streamFeatures);
