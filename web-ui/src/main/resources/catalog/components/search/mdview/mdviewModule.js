13356f16ac6f604ab1ead1e1bf0348e205f75a9f
María Arias de Reyna <maria.arias@geocat.net>
2016-06-15 10:20:16 +0200

    Feedback form for metadata

--- web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
+++ web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
@@ -27,10 +27,12 @@
 
   goog.require('gn_mdview_directive');
   goog.require('gn_mdview_service');
+  goog.require('md_feedback');
 
   var module = angular.module('gn_mdview', [
     'gn_mdview_service',
-    'gn_mdview_directive'
+    'gn_mdview_directive',
+    'md_feedback'
   ]);
 
   module.controller('GnMdViewController', [
