geonetwork/core-geonetwork

commit 01e8f76c74df5f127c91c2009c453865b83541c2

Fixes #1555

core/src/main/java/org/fao/geonet/listeners/security/CheckUsername.java
@@ -1,0 +1,40 @@
+/**
+ * 
+ */
+package org.fao.geonet.listeners.security;
+
+import org.fao.geonet.domain.User;
+import org.fao.geonet.events.user.UserEvent;
+
+/**
+ * Check that the username is correctly scaped.
+ * 
+ * @author delawen
+ * 
+ * 
+ */
+
+public class CheckUsername {
+
+
+  public void check(UserEvent event) {
+    User user = event.getUser();
+
+    if (user.getUsername().contains("<")) {
+      user.setUsername(user.getUsername().replaceAll("<", ""));
+    }
+
+    if (user.getSurname().contains("<")) {
+      user.setSurname(user.getSurname().replaceAll("<", ""));
+    }
+
+    if (user.getName().contains("<")) {
+      user.setName(user.getName().replaceAll("<", ""));
+    }
+
+    if (user.getOrganisation().contains("<")) {
+      user.setOrganisation(user.getOrganisation().replaceAll("<", ""));
+    }
+  }
+
+}

core/src/main/java/org/fao/geonet/listeners/security/CheckUsernameCreation.java
@@ -1,0 +1,27 @@
+/**
+ * 
+ */
+package org.fao.geonet.listeners.security;
+
+import org.fao.geonet.events.user.UserCreated;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * Check that the username is correctly scaped on creation.
+ * 
+ * @author delawen
+ * 
+ * 
+ */
+
+@Service
+public class CheckUsernameCreation extends CheckUsername
+    implements ApplicationListener<UserCreated> {
+
+  @Override
+  public void onApplicationEvent(UserCreated event) {
+    check(event);
+  }
+
+}

core/src/main/java/org/fao/geonet/listeners/security/CheckUsernameUpdate.java
@@ -1,0 +1,27 @@
+/**
+ * 
+ */
+package org.fao.geonet.listeners.security;
+
+import org.fao.geonet.events.user.UserUpdated;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * Check that the username is correctly scaped on update.
+ * 
+ * @author delawen
+ * 
+ * 
+ */
+
+@Service
+public class CheckUsernameUpdate extends CheckUsername
+    implements ApplicationListener<UserUpdated> {
+
+  @Override
+  public void onApplicationEvent(UserUpdated event) {
+    check(event);
+  }
+
+}

events/src/main/java/org/fao/geonet/events/hooks/user/UserModified.java
@@ -43,13 +43,11 @@ public class UserModified implements GeonetworkEntityListener<User>,
      */
     @Override
     public void handleEvent(PersistentEventType type, User entity) {
-        if (entity.getId() == 0
-                && (type == PersistentEventType.PrePersist || type == PersistentEventType.PreUpdate)) {
+        if (type == PersistentEventType.PrePersist) {
             this.eventPublisher.publishEvent(new UserCreated(entity));
-        } else if ((type == PersistentEventType.PostPersist || type == PersistentEventType.PostUpdate)
-                && entity.getId() != 0) {
+        } else if (type == PersistentEventType.PreUpdate) {
             this.eventPublisher.publishEvent(new UserUpdated(entity));
-        } else if (type == PersistentEventType.PostRemove) {
+        } else if (type == PersistentEventType.PreRemove) {
             this.eventPublisher.publishEvent(new UserDeleted(entity));
         }
     }

events/src/main/java/org/fao/geonet/events/user/UserUpdated.java
@@ -4,10 +4,9 @@
 package org.fao.geonet.events.user;
 
 import org.fao.geonet.domain.User;
-import org.springframework.context.ApplicationEvent;
 
 /** Event launched when a user gets updated **/
-public class UserUpdated extends ApplicationEvent {
+public class UserUpdated extends UserEvent {
 
     private static final long serialVersionUID = -4051701002450179299L;
 

commit 0b11edd5eb5d2bb265cc844d955b87a831d12083

Tests are working again. Broken by 01e8f76c74df5f127c91c2009c453865b83541c2.

core/src/main/java/org/fao/geonet/listeners/security/CheckUsername.java
@@ -27,32 +27,28 @@ import org.fao.geonet.events.user.UserEvent;
 
 /**
  * Check that the username is correctly scaped.
- * 
+ *
  * @author delawen
- * 
- * 
  */
-
 public class CheckUsername {
 
     public void check(UserEvent event) {
         User user = event.getUser();
 
-        if (user.getUsername().contains("<")) {
+        if (user.getUsername() != null && user.getUsername().contains("<")) {
             user.setUsername(user.getUsername().replaceAll("<", ""));
         }
 
-        if (user.getSurname().contains("<")) {
+        if (user.getSurname() != null && user.getSurname().contains("<")) {
             user.setSurname(user.getSurname().replaceAll("<", ""));
         }
 
-        if (user.getName().contains("<")) {
+        if (user.getName() != null && user.getName().contains("<")) {
             user.setName(user.getName().replaceAll("<", ""));
         }
 
-        if (user.getOrganisation().contains("<")) {
+        if (user.getOrganisation() != null && user.getOrganisation().contains("<")) {
             user.setOrganisation(user.getOrganisation().replaceAll("<", ""));
         }
     }
-
 }
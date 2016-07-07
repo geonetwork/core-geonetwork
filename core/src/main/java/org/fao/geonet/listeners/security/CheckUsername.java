0b11edd5eb5d2bb265cc844d955b87a831d12083
Francois Prunayre <fx.prunayre@gmail.com>
2016-07-07 18:11:47 +0200

    Tests are working again. Broken by 01e8f76c74df5f127c91c2009c453865b83541c2.

--- core/src/main/java/org/fao/geonet/listeners/security/CheckUsername.java
+++ core/src/main/java/org/fao/geonet/listeners/security/CheckUsername.java
@@ -27,32 +27,28 @@ import org.fao.geonet.events.user.UserEvent;
 
 /**
  * Check that the username is correctly scaped.
- * 
+ *
  * @author delawen
- * 
- * 
  */
-
 public class CheckUsername {
 
     public void check(UserEvent event) {
         User user = event.getUser();
 
-        if (user.getUsername().contains("<")) {
+        if (user.getUsername() != null && user.getUsername().contains("<")) {
             user.setUsername(user.getUsername().replaceAll("<", ""));
         }
 
-        if (user.getSurname().contains("<")) {
+        if (user.getSurname() != null && user.getSurname().contains("<")) {
             user.setSurname(user.getSurname().replaceAll("<", ""));
         }
 
-        if (user.getName().contains("<")) {
+        if (user.getName() != null && user.getName().contains("<")) {
             user.setName(user.getName().replaceAll("<", ""));
         }
 
-        if (user.getOrganisation().contains("<")) {
+        if (user.getOrganisation() != null && user.getOrganisation().contains("<")) {
             user.setOrganisation(user.getOrganisation().replaceAll("<", ""));
         }
     }
-
 }
