e9e592cd0fc447017f392a1a703e5ef090cf85bf
Jose Garcia <jose.garcia@geocat.net>
2016-07-20 20:29:33 +0200

    API / MapServers - tests

--- services/src/main/java/org/fao/geonet/api/mapservers/MapServersApi.java
+++ services/src/main/java/org/fao/geonet/api/mapservers/MapServersApi.java
@@ -164,7 +164,7 @@ public class MapServersApi {
     @PreAuthorize("hasRole('Reviewer')")
     @ApiResponses(value = {
         @ApiResponse(code = 201, message = "Mapserver created."),
-        @ApiResponse(code = 404, message = "Bad parameters.") ,
+        @ApiResponse(code = 400, message = "Bad parameters.") ,
         @ApiResponse(code = 403, message = ApiParams.API_RESPONSE_NOT_ALLOWED_ONLY_REVIEWER)
     })
     @ResponseStatus(HttpStatus.CREATED)
