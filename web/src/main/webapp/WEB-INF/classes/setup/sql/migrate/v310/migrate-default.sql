1ad5b831b3a689cfc98382e9d82cfd36425acc31
François Prunayre <fx.prunayre@gmail.com>
2016-07-28 12:37:17 +0200

    During migration insure datatype for JSON settings is set to 3. Older version was changing it to 1 ie. String during validation.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v310/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v310/migrate-default.sql
@@ -15,3 +15,8 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 DELETE FROM Settings WHERE name = 'system/z3950/enable';
 DELETE FROM Settings WHERE name = 'system/z3950/port';
 DELETE FROM Settings WHERE name = 'system/removedMetadata/dir';
+
+
+UPDATE Settings SET datatype = 3 WHERE name = 'map/config';
+UPDATE Settings SET datatype = 3 WHERE name = 'map/proj4js';
+UPDATE Settings SET datatype = 3 WHERE name = 'metadata/editor/schemaConfig';
