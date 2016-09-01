7112be9baf8aed00ec5952d151718bd67fb52159
Francois Prunayre <fx.prunayre@gmail.com>
2016-09-01 17:24:33 +0200

    BODC vocabularies / Update titles after download.

--- web/src/main/webapp/WEB-INF/data/config/update-bodc-codelists.sh
+++ web/src/main/webapp/WEB-INF/data/config/update-bodc-codelists.sh
@@ -2,22 +2,20 @@
 
 # Script to download BODC vocabularies.
 
-# After download, thesaurus titles need to be manually updated for P01, P02, P03.
-# eg.
-#-<skos:prefLabel>Agreed Parameter Groups (P03)</skos:prefLabel>
-#-<dc:title>Agreed Parameter Groups (P03)</dc:title>
-#+<skos:prefLabel>SeaDataNet Agreed Parameter Groups</skos:prefLabel>
-#+<dc:title>SeaDataNet Agreed Parameter Groups</dc:title>
-
-
 cd codelist/external/thesauri/feature-type
 wget --output-document=NVS.L02.rdf http://vocab.nerc.ac.uk/collection/L02/current/
 cd ../../../..
 
 cd codelist/external/thesauri/parameter
 wget --output-document=NVS.P01.rdf http://vocab.nerc.ac.uk/collection/P01/current/
+sed -i 's/BODC Parameter Usage Vocabulary/Parameter Usage Vocabulary \(P01\)/g' NVS.P01.rdf
+
 wget --output-document=NVS.P02.rdf http://vocab.nerc.ac.uk/collection/P02/current/
+sed -i 's/SeaDataNet Parameter Discovery Vocabulary/Parameter Discovery Vocabulary \(P02\)/g' NVS.P02.rdf
+
 wget --output-document=NVS.P03.rdf http://vocab.nerc.ac.uk/collection/P03/current/
+sed -i 's/SeaDataNet Agreed Parameter Groups/Agreed Parameter Groups \(P03\)/g' NVS.P03.rdf
+
 wget --output-document=NVS.P35.rdf http://vocab.nerc.ac.uk/collection/P35/current/
 cd ../../../..
 
