87b480b4f1654763771abadfc1425d654e1c2e44
María Arias de Reyna <maria.arias@geocat.net>
2016-09-28 18:01:26 +0200

    Adding migration scripts for 3.0.5 and 3.0.6-SNAPSHOT

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v305/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v305/migrate-default.sql
@@ -1,5 +1,5 @@
 UPDATE Settings SET value='3.0.5' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/allowPublishInvalidMd', 'true', 2, 100003, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/automaticUnpublishInvalidMd', 'false', 2, 100004, 'n');
