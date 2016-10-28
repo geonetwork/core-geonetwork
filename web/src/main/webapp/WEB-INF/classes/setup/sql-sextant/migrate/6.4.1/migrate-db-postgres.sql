33fd595682d314cdb7aca740be39cd8abde9a34f
Francois Prunayre <fx.prunayre@gmail.com>
2016-10-28 11:20:17 +0200

    Add SQL migration for 3.2.1.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.4.1/migrate-db-postgres.sql
@@ -0,0 +1,8 @@
+
+UPDATE Settings SET value = 'true' WHERE name = 'system/inspire/enable';
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/allowPublishInvalidMd', 'true', 2, 100003, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/automaticUnpublishInvalidMd', 'false', 2, 100004, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/forceValidationOnMdSave', 'false', 2, 100005, 'n');
+
+UPDATE Settings SET value='3.2.1' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
