b0bf1483f4c48887d955c922b04844cf460b7640
Florent Gravin <florent.gravin@camptocamp.com>
2016-11-07 12:39:17 +0100

    Put bingMap key in DB admin settings (#1746)

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v321/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v321/migrate-default.sql
@@ -1,2 +1,6 @@
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES
+  ('map/bingKey', 'AnElW2Zqi4fI-9cYx1LHiQfokQ9GrNzcjOh_p_0hkO1yo78ba8zTLARcLBIf8H6D', 2, 9595, 'n');
+
 UPDATE Settings SET value='3.2.1' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
