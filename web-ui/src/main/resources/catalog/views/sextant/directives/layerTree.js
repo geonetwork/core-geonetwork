793a4cc6d48d5fdc34b9132dd671d763f1f1c062
Antoine Abt <antoine@abt.im>
2017-01-31 14:48:08 +0100

    Revert "layertree - remove useless watchers"
    This reverts commit c14dfe97fe97000c35c8c0f393051c48c42f0e78.

--- web-ui/src/main/resources/catalog/views/sextant/directives/layerTree.js
+++ web-ui/src/main/resources/catalog/views/sextant/directives/layerTree.js
@@ -192,7 +192,7 @@
                 }
               }
               debounce--;
-            }, 100, false);
+            }, 100);
           };
 
           scope.map.getLayers().on('change:length', buildTree);
@@ -229,6 +229,7 @@
             scope.layerFilter.q = '';
             scope.filterLayers();
           };
+
         }
       };
     }]);
@@ -243,21 +244,7 @@
           map: '=map'
         },
         template: "<ul class='sxt-layertree-node'><sxt-layertree-elt ng-repeat='member" +
-            " in ::collection' member='member' map='map'></sxt-layertree-elt></ul>"
-      };
-    }]);
-
-  module.directive('sxtLayertreeColRoot', [
-    function() {
-      return {
-        restrict: 'E',
-        replace: true,
-        scope: {
-          collection: '=',
-          map: '=map'
-        },
-        template: "<ul class='sxt-layertree-node'><sxt-layertree-elt ng-repeat='member" +
-        " in collection' member='member' map='map'></sxt-layertree-elt></ul>"
+            " in collection' member='member' map='map'></sxt-layertree-elt></ul>"
       };
     }]);
 
@@ -278,7 +265,7 @@
           var el = element;
           if (angular.isArray(scope.member.nodes)) {
             element.append("<sxt-layertree-col class='list-group' " +
-                "collection='::member.nodes' map='map'></sxt-layertree-col>");
+                "collection='member.nodes' map='map'></sxt-layertree-col>");
             $compile(element.contents())(scope);
           }
           scope.toggleNode = function(evt) {
