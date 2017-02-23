2546c1b115ddd560f1af061a7aba909efedf24d6
Francois Prunayre <fx.prunayre@gmail.com>
2017-02-23 17:50:42 +0100

    Configure properly db migration for 330.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v330/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v330/migrate-default.sql
@@ -49,6 +49,6 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 
 
-UPDATE Settings SET value='3.4.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='3.3.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
