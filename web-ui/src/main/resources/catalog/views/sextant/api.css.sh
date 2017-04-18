e70425523be685234bbcd2759d67983bfd113f60
Olivier Guyot <olivier.guyot@camptocamp.com>
2017-04-18 14:38:39 +0200

    Color scheme switching for API mode
    * fixed colors as vars in less files
    * adjusted api.css compile script & main less file to use themes

--- web-ui/src/main/resources/catalog/views/sextant/api.css.sh
+++ web-ui/src/main/resources/catalog/views/sextant/api.css.sh
@@ -3,7 +3,11 @@
 # install less with node first.
 # sudo npm install -g less
 
-lessc less/gn_search_sextant.less api.css
+# first compute the theme to use
+themename=${1:-"default"}
+echo "Theme file used: themes/$themename.less"
+
+lessc less/gn_search_sextant.less api.css --modify-var="theme-name=$themename"
 sed 's/calc(\(.*\))/~"calc(\1)"/' api.css > less/tmp.css
 lessc less/sxt-api.less tmp.css
 sed -i 's/.gn .sxt-max-sm/.gn.sxt-max-sm/' tmp.css > api.css
