4c8a95eb608ae03fbb5e851c92713c0b6922732e
Francois Prunayre <fx.prunayre@gmail.com>
2017-05-29 16:38:27 +0200

    SQL updates for 4.6.4 version.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.4.6/6.4.6/migrate-db-postgres.sql
@@ -0,0 +1,1 @@
+UPDATE metadata SET data = replace(data, 'Average vertical sampling  and description of specific vertical sampling schema (100 words max)', 'Average vertical sampling') WHERE schemaid = 'iso19115-3';
