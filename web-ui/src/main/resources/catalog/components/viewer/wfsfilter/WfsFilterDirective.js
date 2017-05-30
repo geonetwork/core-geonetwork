27d044bf915070a1447fe79c784df11adaf9b739
Florent gravin <florent.gravin@camptocamp.com>
2017-05-30 09:59:50 +0200

    ui : optimise some treatments on vector layer, filter for only vectors

--- web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
@@ -112,7 +112,9 @@
                     if (layer == scope.heatmapLayer) {
                       return feature;
                     }
-                  });
+                  }, undefined, function(layer) {
+                  return layer instanceof ol.layer.Vector;
+                });
               if (feature) {
                 var mapTop = scope.map.getTarget().getBoundingClientRect().top;
                 info.css({
@@ -142,6 +144,11 @@
            * all different feature types.
            */
           function init() {
+
+            var source = scope.layer.getSource();
+            if(!source || !(source instanceof ol.source.ImageWMS || source instanceof ol.source.TileWMS)) {
+              return;
+            }
 
             angular.extend(scope, {
               fields: [],
