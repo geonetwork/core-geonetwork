d1a5e3a433a4708845510a0f64692185451c2b90
María Arias de Reyna <delawen@gmail.com>
2017-07-06 09:59:25 +0200

    Revert "Index status of records with disabled workflow."
    This reverts commit 4d2b33e929a75404db52e0f340ddb4c6336476b0.

--- core/src/main/java/org/fao/geonet/kernel/DataManager.java
+++ core/src/main/java/org/fao/geonet/kernel/DataManager.java
@@ -738,10 +738,6 @@ public class DataManager implements ApplicationEventPublisherAware {
                 moreFields.add(SearchManager.makeField(Geonet.IndexFieldNames.STATUS, status, true, true));
                 String statusChangeDate = stat.getId().getChangeDate().getDateAndTime();
                 moreFields.add(SearchManager.makeField(Geonet.IndexFieldNames.STATUS_CHANGE_DATE, statusChangeDate, true, true));
-            } else {
-                // _status
-                // 0 : unknown or workflow not enabled.
-                moreFields.add(SearchManager.makeField(Geonet.IndexFieldNames.STATUS, Params.Status.UNKNOWN, true, true));
             }
 
             // getValidationInfo
