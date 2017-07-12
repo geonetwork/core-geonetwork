1b32af2932aaac7a2495845ddd0df418f97ec5aa
Jose Garcia <jose@Joses-MacBook-Pro.local>
2017-07-12 13:54:48 +0200

    Disable CSRF token for CSW POST requests - Make the excluded urls configurable #2071

---
+++ core/src/main/java/org/fao/geonet/security/web/csrf/GeonetworkCsrfSecurityRequestMatcher.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+package org.fao.geonet.security.web.csrf;
+
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * RequestMatcher to exclude the CSRF token from requests.
+ *
+ * Useful to exclude CSW POST requests for GetRecords.
+ *
+ * @author Jose García
+ */
+public class GeonetworkCsrfSecurityRequestMatcher implements RequestMatcher {
+    private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+    private RegexRequestMatcher unprotectedMatcher;
+
+    public GeonetworkCsrfSecurityRequestMatcher(Set<String> unprotectedUrlPatterns) {
+        unprotectedMatcher = new RegexRequestMatcher(String.join("|", unprotectedUrlPatterns), null);
+    }
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        if(allowedMethods.matcher(request.getMethod()).matches()){
+            return false;
+        }
+
+        return !unprotectedMatcher.matches(request);
+    }
+}
