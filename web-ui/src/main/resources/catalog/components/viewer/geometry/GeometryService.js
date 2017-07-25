212be934f0b82024504d8ed3f061f1c9de7f61da
Olivier Guyot <olivier.guyot@camptocamp.com>
2017-07-25 17:46:21 +0200

    added new options to the geometry tool (handle synced input & crs)

--- web-ui/src/main/resources/catalog/components/viewer/geometry/GeometryService.js
+++ web-ui/src/main/resources/catalog/components/viewer/geometry/GeometryService.js
@@ -134,6 +134,102 @@
 
         return this._layer;
       };
+
+      /**
+       * @ngdoc method
+       * @methodOf gn_geometry.service:gnGeometryService
+       * @name gnGeometryService#printGeometryOutput
+       *
+       * @description
+       * This prints the output of a geometry tool according to options given
+       *
+       * @param {ol.Map} map open layers map
+       * @param {ol.Feature} feature feature to output
+       * @param {Object} options
+       * @param {string} options.crs default is EPSG:4326
+       * @param {string} options.format default is GML
+       * @param {bool} options.outputAsFeatures default is false
+       * @return {string | Object} output as string or object
+       */
+      this.printGeometryOutput = function(map, feature, options) {
+        var options = angular.extend({
+          crs: 'EPSG:4326',
+          format: 'gml'
+        }, options);
+
+        // clone & transform geom
+        var outputGeom = feature.getGeometry().clone().transform(
+          map.getView().getProjection(),
+          options.outputCrs || 'EPSG:4326'
+        );
+        var outputFeature = null;
+        if (options.outputAsFeatures) {
+          outputFeature = new ol.Feature({
+            id: 'geometry-tool-output',
+            geometry: outputGeom
+          });
+        }
+
+        // set id on feature
+        feature.setId('geometry-tool-output');
+
+        var formatLabel = options.format.toLowerCase();
+        var format;
+        var outputValue;
+        switch (formatLabel) {
+          case 'json':
+          case 'geojson':
+            format = new ol.format.GeoJSON();
+            if (options.outputAsFeatures) {
+              outputValue = format.writeFeatures([outputFeature]);
+            } else {
+              outputValue = format.writeGeometry(outputGeom);
+            }
+            break;
+
+          case 'wkt':
+            format = new ol.format.WKT();
+            if (options.outputAsFeatures) {
+              outputValue = format.writeFeatures([outputFeature]);
+            } else {
+              outputValue = format.writeGeometry(outputGeom);
+            }
+            break;
+
+          case 'gml':
+            format = new ol.format.GML({
+              featureNS: 'http://mapserver.gis.umn.edu/mapserver',
+              featureType: 'features'
+            });
+
+            if (options.outputAsFeatures) {
+              outputValue = '<wfs:FeatureCollection ' +
+                'xmlns:wfs="http://www.opengis.net/wfs">' +
+                format.writeFeatures([outputFeature]) +
+                '</wfs:FeatureCollection>';
+            } else {
+              outputValue = format.writeGeometryNode(
+                outputGeom).innerHTML;
+            }
+            break;
+
+          // no valid format specified: output as object + give warning
+          default:
+            console.warn('No valid output format specified for ' +
+              'gn-geometry-tool (value=' + options.format + '); ' +
+              'outputting geometry as object');
+
+          case 'object':
+            if (options.outputAsFeatures) {
+              outputValue = [outputFeature];
+            } else {
+              outputValue = outputGeom;
+            }
+            break;
+        }
+
+        return outputValue;
+      };
     }
   ]);
 })();
