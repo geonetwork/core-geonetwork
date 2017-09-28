6e0348cc16146ab39b5c011522bfd0d0ecf8d7b4
Florent gravin <florent.gravin@camptocamp.com>
2017-09-28 16:20:38 +0200

    Go to history.back() when closing editor

--- web-ui/src/main/resources/catalog/js/edit/EditorController.js
+++ web-ui/src/main/resources/catalog/js/edit/EditorController.js
@@ -454,16 +454,7 @@
           // when the editor was not opened by a script.
         }
 
-        var lastPage = document.referrer;
-        if(lastPage && /\/catalog\.search/.test(lastPage)) {
-          var debug = /(&|\?)debug/.test(window.location) ? '?debug' : '';
-          var url = lastPage.split('?')[0] + debug + '#/metadata/' +
-            gnCurrentEdit.uuid;
-          window.location = url;
-        }
-        else {
-          window.history.back();
-        }
+        window.history.back();
       };
 
       $scope.cancel = function(refreshForm) {
