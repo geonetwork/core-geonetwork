97778ff6d29576fc905a6a9fc1edc332c9e71908
Olivier Guyot <olivier.guyot@camptocamp.com>
2017-11-06 16:26:06 +0100

    added heatmap directive with placeholder behaviour

---
+++ web-ui/src/main/resources/catalog/components/viewer/heatmap/HeatmapDirective.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+(function() {
+    goog.provide('gn_heatmap_directive');
+
+    var module = angular.module('gn_heatmap_directive', [
+    ]);
+
+    /**
+     * @ngdoc directive
+     * @name gn_heatmap.directive:gnHeatmap
+     *
+     * @description
+     * Given a feature type, this directive will query the ElasticSearch backend
+     * to render a heatmap of features on the map.
+     * The heatmap is actually several box features which gives info when
+     * hovered (feature count, etc.). These features are redrawn on every map
+     * move.
+     */
+    module.directive('gnHeatmap', [
+      function() {
+        return {
+          restrict: 'E',
+          scope: {
+            map: '<',
+            featureType: '<',
+            enabled: '<'
+          },
+          bindToController: true,
+          controllerAs: 'ctrl',
+          controller: ['$scope', function($scope) {
+            var ctrl = this;
+
+            // create a vector layer to hold the features
+            ctrl.features = new ol.Collection();
+            ctrl.layer = new ol.layer.Vector({
+              source: new ol.source.Vector({
+                features: ctrl.features
+              })
+            });
+            ctrl.map.addLayer(ctrl.layer);
+
+            // watch "enabled" param to show/hide layer
+            $scope.$watch("ctrl.enabled", function(newValue, oldValue) {
+              ctrl.layer.setVisible(!!newValue);
+            });
+
+            // refresh features on map move
+            ctrl.map.on('moveend', function() {
+              if (!ctrl.enabled) {
+                return;
+              }
+              console.log('refresh heatmap, features: ' + ctrl.featureType);
+            });
+          }],
+          link: function(scope, element, attrs) {
+
+          }
+        };
+      }]);
+
+  })();
