a7148f6b5968ff69e6bec4c5e751f40f59913ef4
Francois Prunayre <fx.prunayre@gmail.com>
2017-11-14 12:09:44 +0100

    Add new thesarusu for download.

--- web/src/main/webapp/WEB-INF/data/config/update-bodc-codelists.sh
+++ web/src/main/webapp/WEB-INF/data/config/update-bodc-codelists.sh
@@ -6,6 +6,8 @@ cd codelist/external/thesauri/feature-type
 wget --output-document=NVS.L02.rdf http://vocab.nerc.ac.uk/collection/L02/current/
 cd ../../../..
 
+
+
 cd codelist/external/thesauri/parameter
 wget --output-document=NVS.P01.rdf http://vocab.nerc.ac.uk/collection/P01/current/
 sed -i 's/BODC Parameter Usage Vocabulary/Parameter Usage Vocabulary \(P01\)/g' NVS.P01.rdf
@@ -17,14 +19,31 @@ wget --output-document=NVS.P03.rdf http://vocab.nerc.ac.uk/collection/P03/curren
 sed -i 's/SeaDataNet Agreed Parameter Groups/Agreed Parameter Groups \(P03\)/g' NVS.P03.rdf
 
 wget --output-document=NVS.P35.rdf http://vocab.nerc.ac.uk/collection/P35/current/
-
 wget --output-document=NVS.A05.rdf http://vocab.nerc.ac.uk/collection/A05/current/
 cd ../../../..
+
+
 
 cd codelist/external/thesauri/reference-geographical-area
 wget --output-document=NVS.C19.rdf http://vocab.nerc.ac.uk/collection/C19/current/
 cd ../../../..
 
+
+
 cd codelist/external/thesauri/use-limitation
 wget --output-document=NVS.L08.rdf http://vocab.nerc.ac.uk/collection/L08/current/
 cd ../../../..
+
+
+
+cd codelist/external/thesauri/theme
+wget --output-document=NVS.L11.rdf http://vocab.nerc.ac.uk/collection/L11/current/
+wget --output-document=NVS.L23.rdf http://vocab.nerc.ac.uk/collection/L23/current/
+wget --output-document=NVS.L05.rdf http://vocab.nerc.ac.uk/collection/L05/current/
+# TODO: LO5POS is derived from L05
+cd ../../../..
+
+
+# TODO: Replace XSL header
+# <?xml-stylesheet href="/VocabV2/skosrdf2html.xsl" type="text/xsl" media="screen"?>
+# not supported by Sesame library
