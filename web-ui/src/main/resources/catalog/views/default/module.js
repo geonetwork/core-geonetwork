3b02da2c28187ef5bceeee1a2bac3f04a55fa344
Pascal Like <antonio.cerciello@gmail.com>
2017-11-15 10:17:21 +0100

    Default search parameter in Ui config

--- web-ui/src/main/resources/catalog/views/default/module.js
+++ web-ui/src/main/resources/catalog/views/default/module.js
@@ -367,7 +367,7 @@
           sortBy: gnSearchSettings.sortBy || 'relevance'
         },
         params: {
-          'facet.q': '',
+          'facet.q': gnSearchSettings.defaultSearchString || '',
           resultType: gnSearchSettings.facetsSummaryType || 'details',
           sortBy: gnSearchSettings.sortBy || 'relevance'
         },
