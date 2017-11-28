3133de3a279baa3bf59fbfc9be6dbc041dc03b8b
François Prunayre <fx.prunayre@gmail.com>
2017-11-28 08:44:37 +0100

    On FF period is not displayed #2362

--- web-ui/src/main/resources/catalog/components/utility/UtilityDirective.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityDirective.js
@@ -358,10 +358,9 @@
               // Moment will properly parse YYYY, YYYY-MM,
               // YYYY-MM-DDTHH:mm:ss which are the formats
               // used in the common metadata standards.
-              // By the way check Z
-              var date = null, suffix = 'Z';
-              if (originalDate.indexOf(suffix,
-                  originalDate.length - suffix.length) !== -1) {
+              // By the way check Z which may be used in GML times
+              var date = null;
+              if (originalDate.match("[Zz]$") !== null) {
                 date = moment(originalDate, 'YYYY-MM-DDtHH-mm-SSSZ');
               } else {
                 date = moment(originalDate);
