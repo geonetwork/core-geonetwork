2f3e01fbe249ce0c840c3762f8d3d7fd71f29d71
Francois Prunayre <fx.prunayre@gmail.com>
2017-12-08 07:59:14 +0100

    For the time being, use OSM in editor to avoid #2413.

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -187,9 +187,9 @@
             'layers': []
           },
           'map-editor': {
-            'context': '../../map/config-viewer.xml',
+            'context': '',
             'extent': [0, 0, 0, 0],
-            'layers': []
+            'layers': [{'type':'osm'}]
           }
         },
         'geocoder': 'https://secure.geonames.org/searchJSON',
