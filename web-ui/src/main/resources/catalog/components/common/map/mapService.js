6c89a8c5da95251eb336eb9a808747513d40ce6b
Juan Luis Rodríguez Ponce <juanluisrp@geocat.net>
2017-12-19 11:45:13 +0100

    Fix error when printing WMTS layers using REST
    Since GN uses Mapfish-print v2.1.2 and it contains some bugs when
    printing a WMTS layer when REST is used this commit is a workaround that
    processes some parameters in the client side to allow Mapfish print the
    layer.
    For example it needs a `dimensions` parameter, even when it should be
    optional. Another change is that Mapfish v2.1.2 doesn't replace the
    `{style}` and `{layer}` parameters in the URL, so we need to do that
    in the Javascript code.

--- web-ui/src/main/resources/catalog/components/common/map/mapService.js
+++ web-ui/src/main/resources/catalog/components/common/map/mapService.js
@@ -1602,6 +1602,9 @@
                 for (var i = 0; i < layer.ResourceURL.length; i++) {
                   urls.push(layer.ResourceURL[i].template);
                 }
+                if (layer.ResourceURL.length > 0) {
+                  url = encodeURI(layer.ResourceURL[0].template);
+                }
 
                 angular.extend(sourceConfig, {
                   urls: urls,
