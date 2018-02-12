bc7a34e906a3d325430c2129b64546daa1bd2d90
María Arias de Reyna <maria.arias@geocat.net>
2018-02-12 11:56:14 +0100

    Proxy was not correctly added to layers that need it

--- web-ui/src/main/resources/catalog/components/common/map/mapService.js
+++ web-ui/src/main/resources/catalog/components/common/map/mapService.js
@@ -661,15 +661,6 @@
                   angular.extend(config, {crossOrigin: 'anonymous'}) : config);
             }
 
-            // Set proxy for Cesium to load
-            // layers not accessible with CORS headers
-            // This is optional if the WMS provides CORS
-            if (gnViewerSettings.cesiumProxy) {
-              source.set('olcs.proxy', function(url) {
-                return gnGlobalSettings.proxyUrl + encodeURIComponent(url);
-              });
-            }
-
             var layerOptions = {
               url: options.url,
               type: 'WMS',
@@ -1207,6 +1198,15 @@
                     name, capObj, md && md.getUuid && md.getUuid()),
                     olL;
 
+                // Check if require proxy
+                var tmp_url = url.split('/');
+                tmp_url = tmp_url[0] + '/' + tmp_url[1] + '/' + tmp_url[2] + '/';
+                
+                if ($.inArray(tmp_url, gnGlobalSettings.requireProxy) !== -1) {
+                  url = gnGlobalSettings.proxyUrl +
+                      encodeURIComponent(url);
+                }
+
                 if (!capL) {
                   // If layer not found in the GetCapabilities
                   // Try to add the layer from the metadata
@@ -1308,6 +1308,14 @@
             return gnOwsCapabilities.getWMSCapabilities(url).
                 then(function(capObj) {
 
+                  // Check if require proxy
+                  var tmp_url = capObj.url.split('/');
+                  tmp_url = tmp_url[0] + '/' + tmp_url[1] + '/' + tmp_url[2] + '/';
+                  
+                  if ($.inArray(tmp_url, gnGlobalSettings.requireProxy) !== -1) {
+                    capObj.url = gnGlobalSettings.proxyUrl +
+                        encodeURIComponent(capObj.url);
+                  }
                   var createdLayers = [];
 
                   var layers = capObj.layers || capObj.Layer;
