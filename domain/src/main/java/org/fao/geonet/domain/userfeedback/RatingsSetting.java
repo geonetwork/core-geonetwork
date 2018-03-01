d54af4b74924f399d4f5fc3ff856c43b06ccd9e9
Antonio C <antonio.cerciello@geocat.net>
2018-03-01 15:27:02 +0100

    GUF, Update migration scripts

--- domain/src/main/java/org/fao/geonet/domain/userfeedback/RatingsSetting.java
+++ domain/src/main/java/org/fao/geonet/domain/userfeedback/RatingsSetting.java
@@ -5,7 +5,7 @@ package org.fao.geonet.domain.userfeedback;
 
 public class RatingsSetting {
 
-    public final static String OFF = "";
+    public final static String OFF = "off";
     public final static String BASIC = "basic";
     public final static String ADVANCED = "advanced";
 
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v342/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v342/migrate-default.sql
@@ -1,6 +1,9 @@
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/remotevalidation/url', '', 0, 7211, 'n');
 UPDATE Settings SET internal='n' WHERE name='system/inspire/enable';
 
+UPDATE Settings SET name = 'system/localrating/enable', datatype = 0, value = 'off' WHERE id = 2110 and value = 'n';
+UPDATE Settings SET name = 'system/localrating/enable', datatype = 0, value = 'basic' WHERE id = 2110 and value = 'y';
+
 UPDATE Settings SET value='3.4.2' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
