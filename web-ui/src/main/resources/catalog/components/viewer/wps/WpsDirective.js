cd4a79f2ca9feff7a555d15a744758a6d9e8e098
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-03-12 16:52:56 +0100

    added described callback on the WPS directive

--- web-ui/src/main/resources/catalog/components/viewer/wps/WpsDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/wps/WpsDirective.js
@@ -82,7 +82,8 @@
         scope: {
           map: '=',
           wpsLink: '=',
-          wfsLink: '='
+          wfsLink: '=',
+          describedCallback: '&'
         },
         templateUrl: function(elem, attrs) {
           return attrs.template ||
@@ -423,6 +424,11 @@
                     }
                     scope.loadedDescriptions[processKey] =
                     angular.extend({}, scope.processDescription);
+                  }
+
+                  // described callback
+                  if (scope.describedCallback) {
+                    scope.describedCallback();
                   }
                 },
                 function(response) {
