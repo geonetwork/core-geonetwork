e37b56c6cdb172f676ef5cb25c28359f36ecb3a4
Francois Prunayre <fx.prunayre@gmail.com>
2018-04-26 10:58:20 +0200

    User Feedback / Fix migration and SQL scripts.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/data/loc-chi-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/data/loc-chi-default.sql
@@ -526,9 +526,9 @@ INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (5,'chi','Rejected');
 INSERT INTO SelectionsDes (iddes, langid, label) VALUES (0,'chi','Preferred records');
 INSERT INTO SelectionsDes (iddes, langid, label) VALUES (1,'chi','Watch list');
 
-INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (-1,'eng', 'Average');
-INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (0,'eng', 'Completeness#Is the information on this page complete enough to know what you can expect from this dataset?');
-INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (1,'eng', 'Discoverability#Was it easy to find this information page?');
-INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (2,'eng', 'Readability#Was it easy to read and understand the contents of this page?');
-INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (3,'eng', 'Data quality#Does the dataset contain the information you expected, the dataset has enough accuracy, the data is valid/up-to-date?');
-INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (4,'eng', 'Service quality#The dataset is provided as a service or mediatype that is easy to work with?');
+INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (-1,'chi', 'Average');
+INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (0,'chi', 'Completeness#Is the information on this page complete enough to know what you can expect from this dataset?');
+INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (1,'chi', 'Discoverability#Was it easy to find this information page?');
+INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (2,'chi', 'Readability#Was it easy to read and understand the contents of this page?');
+INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (3,'chi', 'Data quality#Does the dataset contain the information you expected, the dataset has enough accuracy, the data is valid/up-to-date?');
+INSERT INTO GUF_RatingCriteriaDes (iddes, langid, label) VALUES (4,'chi', 'Service quality#The dataset is provided as a service or mediatype that is easy to work with?');
