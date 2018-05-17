2aa49909342c3f9728cfb93a63da740b1be9ae2e
François Prunayre <fx.prunayre@gmail.com>
2018-05-17 11:00:04 +0200

    INSPIRE / Add support to load codelists from Registry (#2741)
    * Registry / Add client side selection of targeted codelist.
    
    * Registry / Add spinner while loading collections. Load dependency.
    
    * Registry / Add helper and use of default INSPIRE registry.
    
    * Registry / Select language depending on current UI lang.
    
    * Registry / Add conversion from Registry XML to SKOS.
    
    * Thesaurus API porting in KeywordsAPI and upload form registry
    
    * Update documentation for upload functions
    
    * Thesaurus API improvement
    
    * Can not mix Multipart request with simple form. Moving to POST for upload and PUT for others
    * Remove Jeeves response that can not be handled by Jackson mapping. Return simple message and proper status code
    * Improve some error messages
    
    * Registry / Use new thesaurus API.
    
    * Registry / Priority dataset is not a codelist but a metadata-codelist thing. Add support to that. Do not duplicate labels - when translations are not available the registry response return the english term.
    
    * Registry / Fix response format to avoid JSON parsing error on client side.
    
    * Registry / Validation status id is not the same in all Registry instances.
    
    * Registry / Add spinner while loading and handle better errors or empty collections.
    
    * Registry / Add skos narrower and broader relations.
    
    * Registry / Add support to simple class eg. INSPIRE theme.
    
    * Registry / Identify all simple types. Specific case of INSPIRE theme thesaurus title. Better error reporting.
    
    * Registry / Code style.
    
    * Missing translations and API error management

--- web-ui/src/main/resources/catalog/components/admin/registry/RegistryModule.js
+++ web-ui/src/main/resources/catalog/components/admin/registry/RegistryModule.js
@@ -22,41 +22,12 @@
  */
 
 (function() {
-  goog.provide('gn_classification_controller');
+  goog.provide('gn_registry');
 
+  goog.require('gn_registry_directive');
+  goog.require('gn_registry_service');
 
-  goog.require('gn_categories_controller');
-  goog.require('gn_thesaurus_controller');
-  goog.require('gn_registry');
-
-  var module = angular.module('gn_classification_controller',
-      ['gn_thesaurus_controller', 'gn_categories_controller', 'gn_registry']);
-
-
-  /**
-   *
-   */
-  module.controller('GnClassificationController',
-      ['$scope', '$http',
-       function($scope, $http) {
-
-          $scope.pageMenu = {
-           folder: 'classification/',
-           defaultTab: 'thesaurus',
-           tabs:
-           [{
-             type: 'thesaurus',
-             label: 'manageThesaurus',
-             icon: 'fa-book',
-             href: '#/classification/thesaurus'
-           },{
-             type: 'categories',
-             label: 'manageCategory',
-             icon: 'fa-tags',
-             href: '#/classification/categories'
-           }]
-          };
-
-       }]);
-
+  angular.module('gn_registry', [
+    'gn_registry_directive', 'gn_registry_service'
+  ]);
 })();
