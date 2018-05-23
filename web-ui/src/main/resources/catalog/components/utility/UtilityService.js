71486517d9a6b7cfa78e7c7767d327dae0f5f72b
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2018-05-23 10:33:47 +0200

    allow use of gnLocalized filter from gnRelatedService
    there are place where gnLocalized filter is called with no dom available, so with no scope available, so with no lang available, and in this case have to look for lang using what is set thru gn_locale services.

--- web-ui/src/main/resources/catalog/components/utility/UtilityService.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityService.js
@@ -351,15 +351,16 @@
   /**
    * Return the object value in requested lang or the first value.
    */
-  module.filter('gnLocalized', function() {
+  module.filter('gnLocalized', ['gnGlobalSettings', function(gnGlobalSettings) {
     return function(obj, lang) {
+    lang = lang || gnGlobalSettings.iso3lang;
       if (angular.isObject(obj)) {
         return obj[lang] ? obj[lang] : (obj[Object.keys(obj)[0]] || '');
       } else {
         return '';
       }
     };
-  });
+  }]);
 
   module.factory('gnRegionService', [
     '$q',
