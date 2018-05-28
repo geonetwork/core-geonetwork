227987c90656356240b094bbe10ba4c348a3513c
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-05-28 15:18:06 +0200

    #41968 add login button next to tab switcher

--- web-ui/src/main/resources/catalog/views/sextant/directives/tabswitcher.js
+++ web-ui/src/main/resources/catalog/views/sextant/directives/tabswitcher.js
@@ -5,15 +5,18 @@
   var module = angular.module('sxt_tabswitcher', []);
 
 
-  module.directive('sxtTabSwitcher', [
-    function() {
+  module.directive('sxtTabSwitcher', ['gnGlobalSettings',
+    function(gnGlobalSettings) {
       return {
         restrict: 'E',
         replace: true,
         templateUrl: '../../catalog/views/sextant/directives/' +
             'partials/tabswitcher.html',
-        link: function linkFn(scope) {
+        link: function linkFn(scope, attrs) {
           scope.isVisible = Object.keys(scope.mainTabs).length > 1;
+          scope.allowLogin = typeof sxtSettings !== 'undefined' ?
+            sxtSettings.allowLogin : true;
+          scope.signInUrl = gnGlobalSettings.gnUrl + 'info?casLogin';
         }
       };
     }
