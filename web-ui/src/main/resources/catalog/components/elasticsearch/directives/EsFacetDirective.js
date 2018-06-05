144d6ef79b292102e7fce35c733f9f961c5a5746
Florent gravin <florent.gravin@camptocamp.com>
2018-06-05 16:12:02 +0200

    es facet highlight serach params

--- web-ui/src/main/resources/catalog/components/elasticsearch/directives/EsFacetDirective.js
+++ web-ui/src/main/resources/catalog/components/elasticsearch/directives/EsFacetDirective.js
@@ -27,10 +27,27 @@
   var module = angular.module('gn_es_facet_directive', []);
 
 
+  /**
+   * All facet panel
+   * @constructor
+   */
   var FacetsController = function() {
-
+    this.fLvlCollapse = {};
   };
 
+  FacetsController.prototype.$onInit = function() {
+  };
+
+  FacetsController.prototype.collapseAll = function() {
+    angular.forEach(this.fLvlCollapse, function(v, k) {
+      this.fLvlCollapse[k] = true;
+    }.bind(this));
+  };
+  FacetsController.prototype.expandAll = function() {
+    angular.forEach(this.fLvlCollapse, function(v, k) {
+      this.fLvlCollapse[k] = false;
+    }.bind(this));
+  };
 
   module.directive('esFacets', [
     'gnFacetConfigService', 'gnLangs',
@@ -41,144 +58,59 @@
         controller: FacetsController,
         bindToController: true,
         scope: {
-          list: '<esFacets'
+          list: '<esFacets',
+          sParams: '<params',
+          type: '<facetType'
         },
         templateUrl: function(elem, attrs) {
           return attrs.template || '../../catalog/components/elasticsearch/directives/' +
             'partials/facets.html';
         },
         link: function(scope, element, attrs) {
-
-          scope.fLvlCollapse = {};
-          scope.collapseAll = function() {
-            angular.forEach(scope.fLvlCollapse, function(v, k) {
-              scope.fLvlCollapse[k] = true;
-            });
-          };
-          scope.expandAll = function() {
-            angular.forEach(scope.fLvlCollapse, function(v, k) {
-              scope.fLvlCollapse[k] = false;
-            });
-          };
-
-          var hasOverridenConfig =
-            angular.isArray(scope.facetList) &&
-            scope.facetList.length > 0;
-          scope.getLabel = function(facet) {
-            if (hasOverridenConfig) {
-              for (var i = 0; i < scope.facetList.length; i++) {
-                var f = scope.facetList[i];
-                if (facet['@name'] === f.key) {
-                  var newLabel = f.labels && f.labels[gnLangs.getCurrent()];
-                  if (newLabel) {
-                    return facet['@label'] = newLabel;
-                  }
-                }
-              }
-            }
-            return facet['@label'];
-          };
-
-          scope.tabs = null;
-          scope.activeTab = null;
-          scope.isTabMode = angular.isDefined(attrs['tabField']);
-          scope.initialValues = null;
-          if (scope.tabField) {
-            // Init the facet to use for tabs the first time
-            // search is triggered
-            scope.$watch('dimension', function(n, o) {
-              if (n !== o && scope.dimension.length > 0) {
-                angular.forEach(scope.dimension, function(value) {
-                  if (value['@name'] === scope.tabField) {
-                    if (scope.tabs == null) {
-                      scope.initialValues = value.category;
-                    } else {
-                      angular.extend(value.category, scope.initialValues);
-                    }
-                    scope.tabs = value;
-                    scope.categoryKey = scope.getLabel(scope.tabs);
-                    angular.forEach(scope.tabs.category, function(c) {
-                      c.isSelected = scope.activeTab === c;
-                    });
-                  }
-                });
-              }
-            });
-
-
-            scope.buildPath = function(category, $event) {
-              return gnFacetConfigService.buildPath(scope.path, category);
-            };
-
-            scope.buildLabel = gnFacetConfigService.buildLabel;
-
-            scope.filter = function(category, $event) {
-              category.isSelected = !category.isSelected;
-              if (category.isSelected) {
-                scope.activeTab = category;
-              } else {
-                scope.activeTab = null;
-              }
-              angular.forEach(scope.tabs.category, function(c) {
-                c.isSelected = scope.activeTab === c;
-              });
-              gnFacetConfigService.filter(scope, category, category.isSelected);
-            };
-
-            scope.isOnDrillDownPath = function(category, $event) {
-              return gnFacetConfigService.isOnDrillDownPath(scope, category);
-            };
-
-            scope.isInFilter = function(category, $event) {
-              return gnFacetConfigService.isInFilter(scope, category);
-            };
-          }
-
-          scope.isDisplayed = function(facet) {
-            if (hasOverridenConfig) {
-              // Check if the facet should be displayed
-              for (var i = 0; i < scope.facetList.length; i++) {
-                var f = scope.facetList[i];
-                if (facet['@name'] === f.key) {
-                  var newLabel = f.labels && f.labels[gnLangs.getCurrent()];
-                  if (newLabel) {
-                    facet['@label'] = newLabel;
-                  }
-                  return true;
-                }
-              }
-            } else {
-              return true;
-            }
-            return false;
-          };
-
-          // Facet is collapsed if not in current search criteria
-          scope.isFacetsCollapse = function(facetKey) {
-            return !angular.isDefined(scope.params[facetKey]);
-          };
-
-          // Load facet configuration to know which index field
-          // correspond to which dimension.
-          gnFacetConfigService.loadConfig(scope.facetType).
-          then(function(data) {
-            scope.facetConfig = {
-              config: data,
-              map:  {}
-            };
-
-            angular.forEach(scope.facetConfig.config, function(key) {
-              scope.facetConfig.map[key.label] = key.name;
-            });
-          });
         }
       };
     }]);
 
-  var FacetController = function() {
 
+  /**
+   * One facet block
+   * @param $scope
+   * @constructor
+   */
+  var FacetController = function($scope) {
+    this.$scope = $scope;
   };
 
+  FacetController.prototype.$onInit = function() {
+  };
+
+  FacetController.prototype.filter = function(facet, item) {
+    var value = this.facetsCtrl.sParams[facet.name];
+    if(value) {
+      if(angular.isArray(value)) {
+        value.push(item.name);
+      }
+      else {
+        console.warn('array');
+      }
+    }
+    else {
+      this.facetsCtrl.sParams[facet.name] = item.name;
+    }
+    this.$scope.$emit('resetSearch', this.facetsCtrl.sParams);
+  };
+
+  FacetController.prototype.isInSearch = function(facet, item) {
+      var p = this.facetsCtrl.sParams[facet.name];
+      if (angular.isString(p)) {
+        p = [p];
+      }
+      return p && p.indexOf(item.name) >= 0;
+  };
+
+  FacetController.$inject = [
+    '$scope'
+  ];
 
   module.directive('esFacet', [
     'gnFacetConfigService', 'gnLangs',
@@ -191,12 +123,14 @@
         scope: {
           facet: '<esFacet'
         },
+        require: {
+          facetsCtrl: '^^esFacets'
+        },
         templateUrl: function (elem, attrs) {
           return attrs.template || '../../catalog/components/elasticsearch/directives/' +
             'partials/facet.html';
         },
         link: function (scope, element, attrs) {
-
         }
       };
     }]);
