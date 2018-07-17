794c881af5916f1c0eee7c388f0c4da7ae6035a4
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-07-17 14:58:21 +0200

    imlemented Matomo logic

--- web-ui/src/main/resources/catalog/views/sextant/services/emodnetDownload.js
+++ web-ui/src/main/resources/catalog/views/sextant/services/emodnetDownload.js
@@ -33,6 +33,34 @@
       var modal = null;
 
       return {
+        initialized: false,
+
+        /**
+         * This will add the Matomo/Piwik library to the page
+         * Must be called when opening the form for the first time
+         */
+        importLibrary() {
+          if (this.initialized) return;
+
+          // global tracking array
+          window._paq = window._paq || []
+
+          var url = "//piwik.vliz.be/";
+          _paq.push(['setTrackerUrl', url + 'piwik.php']);
+          _paq.push(['setSiteId', '23']);
+
+          var scriptTag = document.createElement('script');
+          scriptTag.type = 'text/javascript';
+          scriptTag.async = true;
+          scriptTag.defer = true;
+          scriptTag.src = url + 'piwik.js';
+          var firstScriptTag = document.getElementsByTagName('script')[0];
+          var header = firstScriptTag.parentNode;
+          header.insertBefore(scriptTag, firstScriptTag);
+
+          this.initialized = true;
+        },
+
         /**
          * Will show the download form and, when submitted, will open all urls
          * to download files
@@ -41,24 +69,31 @@
          * analytics service is done
          */
         openDownloadForm(urls) {
+          this.importLibrary();
+
           var scope = $rootScope.$new(true);
 
-          scope.values = {};
+          // hardcoded tracking values
+          scope.values = {
+            'DownloadForm-sender_id': 'sextant',
+            'DownloadForm-data_url': urls[0]
+          };
+
+          var category = 'Download_form';
 
           scope.submit = function() {
-            var defer = $q.defer();
+            Object.keys(scope.values).forEach(function(key) {
+              if (scope.values[key]) {
+                _paq.push(['trackEvent', category, key, scope.values[key]]);
+              }
+            });
+            _paq.push(['trackEvent', category, 'DownloadForm-jsondata', JSON.stringify(scope.values)]);
 
-            console.log(scope.values);
-
-            setTimeout(function() {
-              urls.forEach(function(url) {
-                console.log(url);
-              });
-              defer.resolve();
-              modal.modal('hide');
-            }, 1000);
-
-            return defer.promise;
+            // open each url for download
+            urls.forEach(function(url) {
+              window.open(url);
+            });
+            modal.modal('hide');
           };
 
           modal = gnPopup.createModal({
