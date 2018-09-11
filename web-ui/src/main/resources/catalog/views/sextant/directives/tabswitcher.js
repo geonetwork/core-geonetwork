0c56f3f784c6c1b7b01f1567c4dfbe0995531ab1
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-09-11 14:09:29 +0200

    #41968 restyle login button & put to the right & improve button when logged in

--- web-ui/src/main/resources/catalog/views/sextant/directives/tabswitcher.js
+++ web-ui/src/main/resources/catalog/views/sextant/directives/tabswitcher.js
@@ -5,8 +5,8 @@
   var module = angular.module('sxt_tabswitcher', []);
 
 
-  module.directive('sxtTabSwitcher', ['gnGlobalSettings',
-    function(gnGlobalSettings) {
+  module.directive('sxtTabSwitcher',
+    function() {
       return {
         restrict: 'E',
         replace: true,
@@ -14,19 +14,8 @@
             'partials/tabswitcher.html',
         link: function linkFn(scope, attrs) {
           scope.isVisible = Object.keys(scope.mainTabs).length > 1;
-          scope.allowLogin = typeof sxtSettings !== 'undefined' ?
-            sxtSettings.allowLogin : true;
-
-          // signin url is https://host/geonetwork/signin?service=<currentUrl>
-          // following #41968, we make sure there is always a query string in currentUrl
-          scope.getSignInUrl = function() {
-            var currentUrl = window.location.origin + window.location.pathname
-                + (window.location.search || '?') + window.location.hash;
-            return gnGlobalSettings.gnUrl + '../../signin?service=' +
-              encodeURIComponent(currentUrl);
-          }
         }
       };
     }
-  ]);
+  );
 })();
--- web-ui/src/main/resources/catalog/views/sextant/search.js
+++ web-ui/src/main/resources/catalog/views/sextant/search.js
@@ -445,6 +445,19 @@
         services: [
           ]
       };
+
+      // login button management
+      $scope.allowLogin = typeof sxtSettings !== 'undefined' ?
+      sxtSettings.allowLogin : true;
+
+      // signin url is https://host/geonetwork/signin?service=<currentUrl>
+      // following #41968, we make sure there is always a query string in currentUrl
+      $scope.getSignInUrl = function() {
+        var currentUrl = window.location.origin + window.location.pathname
+            + (window.location.search || '?') + window.location.hash;
+        return gnGlobalSettings.gnUrl + '../../signin?service=' +
+          encodeURIComponent(currentUrl);
+      }
     }]);
 
   module.controller('gnsSextantSearch', [
