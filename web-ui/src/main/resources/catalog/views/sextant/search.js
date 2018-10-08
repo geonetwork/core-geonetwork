114c33b18d74b2f9aa68a40d709125f35fee3005
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-10-08 17:35:30 +0200

    fixed url used for API login

--- web-ui/src/main/resources/catalog/views/sextant/search.js
+++ web-ui/src/main/resources/catalog/views/sextant/search.js
@@ -460,12 +460,12 @@
       $scope.allowLogin = typeof sxtSettings !== 'undefined' ?
       sxtSettings.allowLogin : true;
 
-      // signin url is https://host/geonetwork/signin?service=<currentUrl>
+      // signin url is https://host/geonetwork/casRedirect?service=<currentUrl>
       // following #41968, we make sure there is always a query string in currentUrl
       $scope.getSignInUrl = function() {
         var currentUrl = window.location.origin + window.location.pathname
             + (window.location.search || '?') + window.location.hash;
-        return gnGlobalSettings.gnUrl + '../../signin?service=' +
+        return gnGlobalSettings.gnUrl + '../../casRedirect?service=' +
           encodeURIComponent(currentUrl);
       }
     }]);
