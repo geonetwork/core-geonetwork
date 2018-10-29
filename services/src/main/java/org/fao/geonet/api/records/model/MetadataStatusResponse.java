cc09c72f9df77d4bb88cd770d61b574708f7aa22
Francois Prunayre <fx.prunayre@gmail.com>
2018-10-29 17:38:21 +0100

    Record history / Add user info in history directive.

---
+++ services/src/main/java/org/fao/geonet/api/records/model/MetadataStatusResponse.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+package org.fao.geonet.api.records.model;
+
+import org.fao.geonet.domain.MetadataStatus;
+
+public class MetadataStatusResponse extends MetadataStatus {
+
+    public MetadataStatusResponse(MetadataStatus s) {
+        this.setId(s.getId());
+        this.setOwner(s.getOwner());
+        this.setChangeMessage(s.getChangeMessage());
+        this.setStatusValue(s.getStatusValue());
+        this.setCloseDate(s.getCloseDate());
+        this.setDueDate(s.getDueDate());
+    }
+
+    String authorName;
+    String authorEmail;
+    String authorProfile;
+
+    String ownerName;
+    String ownerEmail;
+    String ownerProfile;
+
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+
+    public void setAuthorEmail(String authorEmail) {
+        this.authorEmail = authorEmail;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerProfile() {
+        return ownerProfile;
+    }
+
+    public void setOwnerProfile(String ownerProfile) {
+        this.ownerProfile = ownerProfile;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorProfile() {
+        return authorProfile;
+    }
+
+    public void setAuthorProfile(String authorProfile) {
+        this.authorProfile = authorProfile;
+    }
+}
