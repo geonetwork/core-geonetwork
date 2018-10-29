2f7d0e13944605b4ac2dfa2c97d7ff73321bb883
Francois Prunayre <fx.prunayre@gmail.com>
2018-10-29 11:49:13 +0100

    Record status / Add notification level to DB migration.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v350/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v350/migrate-default.sql
@@ -8,6 +8,11 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 UPDATE StatusValues SET type = 'workflow';
 
+UPDATE StatusValues SET notificationLevel = 'recordUserAuthor' WHERE name = 'approved';
+UPDATE StatusValues SET notificationLevel = 'recordUserAuthor' WHERE name = 'retired';
+UPDATE StatusValues SET notificationLevel = 'recordProfileReviewer' WHERE name = 'submitted';
+UPDATE StatusValues SET notificationLevel = 'recordUserAuthor' WHERE name = 'rejected';
+
 UPDATE Settings SET value='3.5.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
