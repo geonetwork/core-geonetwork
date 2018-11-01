7f5a47f62cc16f82607a7445bf35663768813c88
Antonio C <antonio.cerciello@geocat.net>
2018-11-01 14:28:28 +0100

    Add support for events in statusvalues table

--- web/src/main/webapp/WEB-INF/classes/setup/sql/data/data-db-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/data/data-db-default.sql
@@ -531,6 +531,19 @@ INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLe
 INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (5,'rejected','y', 4, 'workflow', 'recordUserAuthor');
 INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (100,'doiCreationTask','n', 100, 'task', 'statusUserOwner');
 
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (0,'recordcreated','y', 50, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (1,'recordupdated','y', 51, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (2,'attachementadded','y', 52, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (3,'attachementdeleted','y', 53, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (4,'recordownerchange','y', 54, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (5,'recordgroupownerchange','y', 55, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (6,'recordprivilegeschange','n', 56, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (7,'recordcategorychange','y', 57, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (8,'recordvalidationtriggered','y', 58, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (9,'recordstatuschange','y', 59, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (10,'recordprocessingchange','y', 60, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (11,'recorddeleted','n', 61, 'event', null);
+
 -- ======================================================================
 -- === Table: StatusValuesDes
 -- ======================================================================
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v350/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v350/migrate-default.sql
@@ -5,5 +5,18 @@ UPDATE StatusValues SET notificationLevel = 'recordUserAuthor' WHERE name = 'ret
 UPDATE StatusValues SET notificationLevel = 'recordProfileReviewer' WHERE name = 'submitted';
 UPDATE StatusValues SET notificationLevel = 'recordUserAuthor' WHERE name = 'rejected';
 
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (0,'recordcreated','y', 50, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (1,'recordupdated','y', 51, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (2,'attachementadded','y', 52, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (3,'attachementdeleted','y', 53, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (4,'recordownerchange','y', 54, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (5,'recordgroupownerchange','y', 55, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (6,'recordprivilegeschange','y', 56, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (7,'recordcategorychange','y', 57, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (8,'recordvalidationtriggered','y', 58, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (9,'recordstatuschange','y', 59, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (10,'recordprocessingchange','y', 60, 'event', null);
+INSERT INTO StatusValues (id, name, reserved, displayorder, type, notificationLevel) VALUES  (11,'recorddeleted','y', 61, 'event', null);
+
 UPDATE Settings SET value='3.5.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
