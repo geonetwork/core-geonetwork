4b7896342d87f5d164d366fa2c0a6b2e1f492475
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2018-11-19 11:27:11 +0100

    let index request stay synchronous

--- services/src/main/java/org/fao/geonet/api/records/MetadataIndexApi.java
+++ services/src/main/java/org/fao/geonet/api/records/MetadataIndexApi.java
@@ -148,12 +148,11 @@ public class MetadataIndexApi {
             }
         }
         index = ids.size();
-        String objectName = new BatchOpsMetadataReindexer(dataManager, ids).wrapAsyncProcess(false);
+        new BatchOpsMetadataReindexer(dataManager, ids).process(false);
 
         JSONObject res = new JSONObject();
         res.put("success", true);
         res.put("count", index);
-        res.put("objectName", objectName);
 
         return res;
     }
