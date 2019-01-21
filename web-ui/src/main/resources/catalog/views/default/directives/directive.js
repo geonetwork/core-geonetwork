3c9422dee45aa3ca98980514b30fc9469c178de2
Michel Gabriël <michel.gabriel@geocat.net>
2019-01-21 14:04:16 +0100

    Fix for 'Manage record' button dropdown (#3474)
    Fixed:
    - do not show divider when no tasks are shown
    - add `fa-fw` to icons to menu items

--- web-ui/src/main/resources/catalog/views/default/directives/directive.js
+++ web-ui/src/main/resources/catalog/views/default/directives/directive.js
@@ -98,13 +98,21 @@
           scope.md = scope.$eval(attrs.gnMdActionsMenu);
 
           scope.tasks = [];
+          scope.hasVisibletasks = false;
 
           function loadTasks() {
             return $http.get('../api/status/task', {cache: true}).
             success(function(data) {
               scope.tasks = data;
+              scope.getVisibleTasks();
             });
           };
+
+          scope.getVisibleTasks = function() {
+            $.each(scope.tasks, function(i,t) {
+              scope.hasVisibletasks = scope.taskConfiguration[t.name].isVisible();
+            });
+          }
 
           scope.taskConfiguration = {
             doiCreationTask: {
