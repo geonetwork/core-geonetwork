0fa270e161417d5530ecb663a06445e45141367d
Jose García <josegar74@gmail.com>
2019-01-22 12:20:33 +0100

    INSPIRE Remote validation not available in the metadata editor #3493

--- web-ui/src/main/resources/catalog/components/validationtools/GnmdInspireValidationDirective.js
+++ web-ui/src/main/resources/catalog/components/validationtools/GnmdInspireValidationDirective.js
@@ -28,21 +28,16 @@
 
   module.directive(
       'gnMdValidationTools', ['gnConfig', '$http', '$interval',
-        'gnAlertService', '$translate', 'gnPopup', 'gnCurrentEdit',
-        function(gnConfig, $http, $interval, 
-            gnAlertService, $translate, gnPopup, gnCurrentEdit) {
+      'gnAlertService', '$translate', 'gnPopup',
+      'gnCurrentEdit', 'gnConfigService',
+        function(gnConfig, $http, $interval, gnAlertService,
+                 $translate, gnPopup, gnCurrentEdit, gnConfigService) {
           return {
             restrict: 'AEC',
             replace: true,
             templateUrl:
             '../../catalog/components/validationtools/partials/mdValidationTools.html',
             link: function postLink(scope, element, attrs) {
-              // INSPIRE validator only support ISO19139 records.
-              // TODO: For other schema support we may need to convert the record
-              // to ISO19139 first. eg. ISO19115-3
-              scope.isInspireValidationEnabled =
-                gnConfig[gnConfig.key.isInspireEnabled]
-                && gnCurrentEdit.schema == 'iso19139';
               scope.isDownloadingRecord = false;
               scope.isDownloadedRecord = false;
               scope.isEnabled = false;
@@ -50,6 +45,16 @@
               scope.$watch('gnCurrentEdit.uuid', function(newValue, oldValue) {
                 scope.isEnabled = true;
                 scope.inspMdUuid = newValue;
+
+                gnConfigService.load().then(function(c) {
+                  // INSPIRE validator only support ISO19139 records.
+                  // TODO: For other schema support we may need to convert the record
+                  // to ISO19139 first. eg. ISO19115-3
+                  scope.isInspireValidationEnabled =
+                    gnConfig[gnConfig.key.isInspireEnabled] &&
+                    angular.isString(gnConfig['system.inspire.remotevalidation.url']) &&
+                    (gnCurrentEdit.schema === 'iso19139');
+                });
               });
 
               scope.validateInspire = function() {
