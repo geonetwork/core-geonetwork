890f95b25e9cfe9cd642bcdf7f5ef2afda7f46e7
François Prunayre <fx.prunayre@gmail.com>
2019-03-04 08:22:40 +0100

    Multiple UI config support (#3458)
    * Multiple UI config support / Add settings_ui table and API.
    
    * Multiple UI config support / Add admin config page.
    
    * Multiple UI config support / Load on startup, SQL migration, i18n.
    
    * Multiple UI config support / Apply filter config to editor board and admin.

--- web-ui/src/main/resources/catalog/js/admin/AdminToolsController.js
+++ web-ui/src/main/resources/catalog/js/admin/AdminToolsController.js
@@ -41,6 +41,7 @@
         sortbyValues: gnSearchSettings.sortbyValues,
         hitsperpageValues: gnSearchSettings.hitsperpageValues,
         selectionBucket: 'b101',
+        filters: gnSearchSettings.filters,
         params: {
           sortBy: 'changeDate',
           _isTemplate: 'y or n',
