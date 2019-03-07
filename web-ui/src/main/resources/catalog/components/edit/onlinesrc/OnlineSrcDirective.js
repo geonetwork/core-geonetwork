f838918c8aa19d9ed8e6b1096cd57d36724c6561
François Prunayre <fx.prunayre@gmail.com>
2019-03-07 13:47:06 +0100

    Editor / Online resource / Set default label and better multilingual support.

--- web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
+++ web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
@@ -1086,7 +1086,8 @@
                       scope.config.types[0];
                   scope.config.multilingualFields = [];
                   angular.forEach(typeConfig.fields, function(f, k) {
-                    if (f.isMultilingual !== false) {
+                    if (scope.isMdMultilingual &&
+                        f.isMultilingual !== false) {
                       scope.config.multilingualFields.push(k);
                     }
                   });
@@ -1243,13 +1244,12 @@
                  * @param {string} value of the attribute
                  */
                 function setParameterValue(pName, value) {
-                  var p = scope.params;
                   if (scope.isFieldMultilingual(pName)) {
                     $.each(scope.mdLangs, function(key, v) {
-                      p[pName][v] = value;
+                      scope.params[pName][v] = value;
                     });
                   } else {
-                    p[pName] = value;
+                    scope.params[pName] = value;
                   }
                 }
 
@@ -1446,7 +1446,7 @@
 
                   if (curUrl) {
                     scope.loadCurrentLink();
-                    scope.isImage = curUrl.match(/.*.(png|jpg|gif)$/i);
+                    scope.isImage = curUrl.match(/.*.(png|jpg|jpeg|gif)$/i);
                   }
 
                 };
@@ -1517,10 +1517,12 @@
                         }
                       });
                     }
+                    // Set a default label
                     if (!scope.isEditing &&
                         angular.isDefined(newValue.copyLabel)) {
-                      scope.params[newValue.copyLabel] =
-                          $translate(newValue.label);
+                      setParameterValue(
+                        newValue.copyLabel,
+                        $translate.instant(newValue.label));
                     }
 
                     if (newValue.sources && newValue.sources.thumbnailMaker) {
