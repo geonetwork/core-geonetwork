geonetwork/core-geonetwork

commit d9913ba682d7fe2556b3ef554bf59dfce549ded1

Removing unused parameters ownerID and groupID

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
@@ -263,7 +263,7 @@ public class Aligner extends BaseAligner<CswParams> {
 	}
 
 
-    private void addMetadata(RecordInfo ri, Integer ownerId, Integer groupId, String uuid) throws Exception {
+    private void addMetadata(RecordInfo ri, String uuid) throws Exception {
         if (cancelMonitor.get()) {
             return;
         }
@@ -301,7 +301,7 @@ public class Aligner extends BaseAligner<CswParams> {
         //
         AbstractMetadata metadata = new Metadata();
         metadata.setUuid(uuid);
-        ownerId = getOwner();
+        Integer ownerId = getOwner();
         metadata.getDataInfo().
             setSchemaId(schema).
             setRoot(md.getQualifiedName()).

commit 89de9394aa665a568aef6134d49604f1e845d645

Fixes compile error
Fixes compile error introduced by commit d9913ba682d7fe2556b3ef554bf59dfce549ded1
removing now unused parameters of `addMetadata` method calls.

See PR #3643.

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
@@ -195,7 +195,7 @@ public class Aligner extends BaseAligner<CswParams> {
                 if (id == null) {
                     //record doesn't exist (so it doesn't belong to this harvester)
                     log.debug("Adding record with uuid " + ri.uuid);
-                    addMetadata(ri, getOwnerId(params), getOwnerGroupId(params), ri.uuid);
+                    addMetadata(ri, ri.uuid);
                 } else if (localUuids.getID(ri.uuid) == null) {
                     //Record does not belong to this harvester
                     result.datasetUuidExist++;
@@ -208,7 +208,7 @@ public class Aligner extends BaseAligner<CswParams> {
                             break;
                         case RANDOM:
                             log.debug("Generating random uuid for remote record with uuid " + ri.uuid);
-                            addMetadata(ri, getOwnerId(params), getOwnerGroupId(params), UUID.randomUUID().toString());
+                            addMetadata(ri, UUID.randomUUID().toString());
                             break;
                         case SKIP:
                             log.debug("Skipping record with uuid " + ri.uuid);