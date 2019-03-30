7771e817472532b152f54515533976d1feb24f65
Juan Luis Rodríguez <juanluisrp@gmail.com>
2019-03-30 13:09:48 +0100

    Fixes bug in tooltip icon on template fields
    Fixes a problem that was adding the tooltip icon to multiple `gn-control` divs
    when a template with more than one field is used.

--- web-ui/src/main/resources/catalog/components/edit/FieldsDirective.js
+++ web-ui/src/main/resources/catalog/components/edit/FieldsDirective.js
@@ -256,7 +256,12 @@
                } else if (element.is('legend')) {
                  element.contents().first().after(tooltipIconCompiled);
                } else if (isDatePicker) {
-                 element.closest(".gn-field").find("div.gn-control").append(tooltipIconCompiled);
+                // first check if it is in a template (inside a class="row"
+                var control = element.closest(".gn-multi-field .row").find("div.gn-control");
+                if (control.length == 0) {
+                  control = element.closest(".gn-field").find("div.gn-control").first();
+                }
+                control.append(tooltipIconCompiled);
                } else if (element.is('label')) {
                  if (tooltipAfterLabel) {
                    element.parent().children('div')
