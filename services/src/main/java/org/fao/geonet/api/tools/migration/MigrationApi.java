9da59b66e04286793725b75c601497057875673c
Jose García <josegar74@gmail.com>
2019-04-08 14:20:27 +0200

    Migration API - NullPointerException when executing a database migration task #3729

--- services/src/main/java/org/fao/geonet/api/tools/migration/MigrationApi.java
+++ services/src/main/java/org/fao/geonet/api/tools/migration/MigrationApi.java
@@ -81,6 +81,7 @@ public class MigrationApi {
             if (DatabaseMigrationTask.class.isAssignableFrom(clazz)) {
                 DatabaseMigrationTask task =
                     (DatabaseMigrationTask) clazz.newInstance();
+                task.setContext(appContext);
                 task.update(connection);
             } else if (ContextAwareTask.class.isAssignableFrom(clazz)) {
                 ContextAwareTask task = (ContextAwareTask) clazz.newInstance();
