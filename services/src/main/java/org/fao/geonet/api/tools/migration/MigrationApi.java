c776c0f7371a9dec4295a61225da8f87b3bfc696
josegar74 <josegar74@gmail.com>
2019-04-10 15:18:56 +0200

    DatabaseMigration tasks doesn't save changes in the database #3732 (#3733)

--- services/src/main/java/org/fao/geonet/api/tools/migration/MigrationApi.java
+++ services/src/main/java/org/fao/geonet/api/tools/migration/MigrationApi.java
@@ -81,6 +81,7 @@ public class MigrationApi {
             if (DatabaseMigrationTask.class.isAssignableFrom(clazz)) {
                 DatabaseMigrationTask task =
                     (DatabaseMigrationTask) clazz.newInstance();
+                connection.setAutoCommit(true);
                 task.setContext(appContext);
                 task.update(connection);
             } else if (ContextAwareTask.class.isAssignableFrom(clazz)) {
