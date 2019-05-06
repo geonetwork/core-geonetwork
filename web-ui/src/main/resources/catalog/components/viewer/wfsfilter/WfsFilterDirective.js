f9db1e55349a96697783d555b284dba993230ef2
Olivier Guyot <olivier.guyot@camptocamp.com>
2019-05-06 13:43:38 +0200

    WFS facets / do not update the state of the last clicked facet

--- web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
+++ web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
@@ -326,8 +326,8 @@
 
             var fieldName = field.name;
             var facetKey = facet.value;
-
             var output = scope.output;
+            scope.lastClickedField = null;
 
             if (output[fieldName]) {
               if (output[fieldName].values[facetKey]) {
@@ -337,10 +337,12 @@
                 }
               }
               else {
+                scope.lastClickedField = fieldName;
                 output[fieldName].values[facetKey] = true;
               }
             }
             else {
+              scope.lastClickedField = fieldName;
               output[fieldName] = {
                 type: field.type,
                 query: facet.query,
@@ -518,6 +520,7 @@
            */
           scope.resetFacets = function() {
             scope.output = {};
+            scope.lastClickedField = null;
 
             scope.resetSLDFilters();
 
@@ -545,7 +548,16 @@
           function searchResponseHandler(resp) {
             indexObject.pushState();
             scope.count = resp.count;
-            scope.fields = resp.facets;
+
+            // if a facet was clicked, keep the previous facet object
+            var lastClickedFacet = scope.fields.filter(function(e){
+              return e.name === scope.lastClickedField;
+            })[0];
+            scope.fields = resp.facets.map(function(e){
+              return lastClickedFacet && lastClickedFacet.name === e.name ?
+                lastClickedFacet : e;
+            });
+
             scope.sortAggregation();
             resp.indexData.aggregations &&
             setFeatureExtent(resp.indexData.aggregations);
