9eeb603c7bffe884efec04ab73bf1cd6da9c39d9
María Arias de Reyna <maria.arias@geocat.net>
2019-05-07 12:05:44 +0200

    Unifying http requests

--- web-ui/src/main/resources/catalog/js/GnSearchModule.js
+++ web-ui/src/main/resources/catalog/js/GnSearchModule.js
@@ -51,15 +51,13 @@
   module.config(['$LOCALES', function($LOCALES) {
     $LOCALES.push('search');
     $LOCALES.push('/../api/0.1/tools/i18n/db?' +
-        'type=MetadataCategory&type=Operation&type=Group');
+        'type=MetadataCategory&type=Operation&type=Group&type=StatusValue');
     $LOCALES.push('/../api/0.1/standards/iso19139/' +
         'codelists/gmd%3AMD_TopicCategoryCode');
     $LOCALES.push('/../api/0.1/standards/iso19139/' +
         'codelists/gmd%3ADS_AssociationTypeCode');
     $LOCALES.push('/../api/0.1/standards/iso19139/' +
         'codelists/gmd%3ADS_InitiativeTypeCode');
-    $LOCALES.push('/../api/0.1/tools/i18n/db?' +
-        'type=StatusValue&type=Operation&type=Group');
   }]);
 
 })();
