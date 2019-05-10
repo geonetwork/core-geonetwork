e924bf0dd83540922b5ff9ab7041f4541ed13aeb
Jose García <josegar74@gmail.com>
2019-05-10 14:17:18 +0200

    Metadata selection pdf improvements: add documentation for GetSettingValue service

--- services/src/main/java/org/fao/geonet/guiservices/util/GetSettingValue.java
+++ services/src/main/java/org/fao/geonet/guiservices/util/GetSettingValue.java
@@ -1,3 +1,26 @@
+//=============================================================================
+//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: geonetwork@osgeo.org
+//==============================================================================
+
 package org.fao.geonet.guiservices.util;
 
 import jeeves.interfaces.Service;
@@ -9,6 +32,26 @@ import org.jdom.Element;
 
 import java.nio.file.Path;
 
+/**
+ * Service to retrieve a setting value to be used in internally by Jeeves services.
+ *
+ * Requires to use a parameter "settingValue" with the setting path to retrieve.
+ *
+ * Example:
+ *
+ * <service name="pdf.search">
+ *    ...
+ *    <output sheet="../xslt/services/pdf/portal-present-fop.xsl" file="true"
+ *               contentType="application/pdf">
+ *         ...
+ *         <call name="documentFileName" class=".guiservices.util.GetSettingValue">
+ *           <param name="setting" value="metadata/pdfReport/pdfName"/>
+ *         </call>
+ *    </output>
+ *     ...
+ * </service>
+ *
+ */
 public class GetSettingValue implements Service {
 
     private String setting;
@@ -35,5 +78,4 @@ public class GetSettingValue implements Service {
 
         return new Element("a").setText(settingValue);
     }
-
 }
