68d38db842ee34da61b5b46c04e670527b2d91f6
Francois Prunayre <fx.prunayre@gmail.com>
2019-09-20 13:57:21 +0200

    Fix spring mapping.

--- workers/camelPeriodicProducer/src/main/java/org/fao/geonet/camelPeriodicProducer/MessageProducerController.java
+++ workers/camelPeriodicProducer/src/main/java/org/fao/geonet/camelPeriodicProducer/MessageProducerController.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/msg_producers")
+@RequestMapping("/{portal}/api/msg_producers")
 public class MessageProducerController {
     @Autowired
     protected MessageProducerFactory messageProducerFactory;
