246ca52e9d10df4a9ca036a9c2e747a123d23777
Jose García <josegar74@gmail.com>
2019-09-25 12:09:41 +0200

    Field upload directive for metadata editor

--- web-ui/src/main/resources/catalog/components/edit/FieldsModule.js
+++ web-ui/src/main/resources/catalog/components/edit/FieldsModule.js
@@ -43,7 +43,7 @@
   goog.require('gn_record_fragment_selector');
   goog.require('gn_template_field_directive');
   goog.require('gn_anchor_switcher_directive')
-
+  goog.require('gn_field_upload_directive');
 
 
   angular.module('gn_fields', [
@@ -62,6 +62,7 @@
     'gn_checkbox_with_nilreason',
     'gn_md_validation_tools_directive',
     'gn_bounding',
-    'gn_anchor_switcher_directive'
+    'gn_anchor_switcher_directive',
+    'gn_field_upload_directive'
   ]);
 })();
