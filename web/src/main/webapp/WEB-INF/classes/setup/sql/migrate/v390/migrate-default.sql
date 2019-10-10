144cc192a4a20bd170ade39b766afceda1f4d33b
François Prunayre <fx.prunayre@gmail.com>
2019-10-10 16:10:53 +0200

    SQL /  Migration / Set source types
    See https://github.com/geonetwork/core-geonetwork/pull/4077

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v390/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v390/migrate-default.sql
@@ -1,2 +1,8 @@
+-- 3.8.2
+UPDATE Sources SET type = 'portal' WHERE type IS null AND uuid = (SELECT value FROM settings WHERE name = 'system/site/siteId');
+UPDATE Sources SET type = 'harvester' WHERE type IS null AND uuid != (SELECT value FROM settings WHERE name = 'system/site/siteId');
+
+
+
 UPDATE Settings SET value='3.9.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
