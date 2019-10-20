0b21be08423adfd5a53809c0258677e990b0d5d8
Francois Prunayre <fx.prunayre@gmail.com>
2019-10-20 11:00:36 +0200

    Editor / Flat mode / When adding free text keyword, insert keyword in existing one or create a new block if not. Fix insertion point ref. Add title. Propose first from thesaurus, then free text.

--- web-ui/src/main/resources/catalog/components/thesaurus/ThesaurusDirective.js
+++ web-ui/src/main/resources/catalog/components/thesaurus/ThesaurusDirective.js
@@ -62,6 +62,8 @@
              mode: '@gnThesaurusSelector',
              elementName: '@',
              elementRef: '@',
+             freekeywordElementName: '@',
+             freekeywordElementRef: '@',
              domId: '@',
              selectorOnly: '@',
              transformation: '@',
@@ -114,7 +116,11 @@
 
              scope.add = function() {
                return gnEditor.add(gnCurrentEdit.id,
-               scope.elementRef, scope.elementName, scope.domId, 'before');
+                   scope.freekeywordElementRef || scope.elementRef,
+                 scope.freekeywordElementName || scope.elementName,
+                        scope.domId, 'before').then(function() {
+                 gnEditor.save(gnCurrentEdit.id, true);
+               });
              };
 
              scope.addThesaurus = function(thesaurusIdentifier) {
