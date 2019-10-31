fd44c1fa14d818e6272e97b90f085fa98370fbe1
François Prunayre <fx.prunayre@gmail.com>
2019-10-31 10:44:52 +0100

    SQL / DOI password is internal. (#4162)
    * SQL / DOI password is internal.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v390/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v390/migrate-default.sql
@@ -2,7 +2,7 @@
 UPDATE Sources SET type = 'portal' WHERE type IS null AND uuid = (SELECT value FROM settings WHERE name = 'system/site/siteId');
 UPDATE Sources SET type = 'harvester' WHERE type IS null AND uuid != (SELECT value FROM settings WHERE name = 'system/site/siteId');
 
-
+UPDATE Settings SET internal = 'y' WHERE name = 'system/publication/doi/doipassword';
 
 UPDATE Settings SET value='3.9.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
