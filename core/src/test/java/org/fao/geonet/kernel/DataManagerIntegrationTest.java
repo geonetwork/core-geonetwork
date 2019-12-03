51665e123e17a2465c68c4ddc917bde6658441d5
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2019-12-03 14:01:12 +0100

    use failsafe plugin to run tagged abstractSpringDataTest as it tests once test es started

--- core/src/test/java/org/fao/geonet/kernel/DataManagerIntegrationTest.java
+++ core/src/test/java/org/fao/geonet/kernel/DataManagerIntegrationTest.java
@@ -62,7 +62,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.data.jpa.domain.Specifications.where;
 
 
