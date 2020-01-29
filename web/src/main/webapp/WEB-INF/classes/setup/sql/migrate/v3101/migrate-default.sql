116d86d66b9268e783fc637035a183c2d1a9295c
Juan Luis Rodríguez <juanluisrp@gmail.com>
2020-01-29 10:13:21 +0100

    Icrease length of Validation.valType column
    Sometimes the name of the schematron files are longer than 40 characters. This commit allow
    file names up to 128 characters long instead of 40.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3101/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3101/migrate-default.sql
@@ -2,5 +2,8 @@ DELETE FROM cswservercapabilitiesinfo;
 DELETE FROM Settings WHERE name = 'system/csw/contactId';
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/csw/capabilityRecordUuid', '-1', 0, 1220, 'y');
 
+-- Increase the length of Validation type (where the schematron file name is stored)
+ALTER TABLE Validation ALTER COLUMN valType TYPE varchar(128);
+
 UPDATE Settings SET value='3.10.1' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
