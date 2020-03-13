d984b53aec2aeb7b9eb639318eb9e22ab97632ae
Francois Prunayre <fx.prunayre@gmail.com>
2020-03-13 07:04:01 +0100

    DB / Migration / Add missing settings - not a blocker.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.7.x/migrate-db-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/6.7.x/migrate-db-postgres.sql
@@ -37,6 +37,7 @@ UPDATE Settings SET  position = position + 1 WHERE name = 'metadata/workflow/for
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/enable', 'false', 2, 100002, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/allowSumitApproveInvalidMd', 'true', 2, 100004, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/allowPublishNonApprovedMd', 'true', 2, 100005, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/harvester/disabledHarvesterTypes', '', 0, 9011, 'n');
 
 DELETE FROM cswservercapabilitiesinfo;
 DELETE FROM Settings WHERE name = 'system/csw/contactId';
