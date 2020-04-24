ea3b60d68195aefcb270f5dc4ebcc7a282a068db
josegar74 <josegar74@gmail.com>
2020-04-24 11:52:54 +0200

    Fix typo in file header comments

--- core/src/test/java/org/fao/geonet/kernel/security/shibboleth/ShibbolethUserUtilsTest.java
+++ core/src/test/java/org/fao/geonet/kernel/security/shibboleth/ShibbolethUserUtilsTest.java
@@ -4,7 +4,7 @@
  * and United Nations Environment Programme (UNEP)
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of thgroupLengthNotMatchProfileLengthe GNU General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
  *
