61711d41d4ecea0aa8e3e8d8cfa59d78a2f0f3eb
Francois Prunayre <fx.prunayre@gmail.com>
2020-05-05 16:40:43 +0200

    Merge remote-tracking branch 'origin/master' into 4.0.x / Missing files.

---
+++ workers/wfsfeature-harvester/src/main/java/org/fao/geonet/harvester/wfsfeatures/worker/WFSHarvesterExchangeState.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2001-2015 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+package org.fao.geonet.harvester.wfsfeatures.worker;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.fao.geonet.harvester.wfsfeatures.model.WFSHarvesterParameter;
+import org.geotools.data.wfs.WFSDataStore;
+import org.geotools.data.wfs.WFSDataStoreFactory;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by fgravin on 11/5/15.
+ */
+public class WFSHarvesterExchangeState {
+    Logger logger = Logger.getLogger(WFSHarvesterRouteBuilder.LOGGER_NAME);
+
+    private WFSHarvesterParameter parameters;
+
+    public WFSHarvesterParameter getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(WFSHarvesterParameter parameters) {
+        this.parameters = parameters;
+    }
+
+    private Map<String, String> fields = new LinkedHashMap<String, String>();
+
+    public void setFields(Map<String, String> fields) {
+        this.fields = fields;
+    }
+
+    public Map<String, String> getFields() {
+        return fields;
+    }
+
+    private WFSDataStore wfsDatastore = null;
+
+    public void setWfsDatastore(WFSDataStore wfsDatastore) {
+        this.wfsDatastore = wfsDatastore;
+    }
+
+    public WFSDataStore getWfsDatastore() {
+        return wfsDatastore;
+    }
+
+    WFSHarvesterExchangeState(WFSHarvesterParameter parameters) {
+        this.parameters = parameters;
+        checkTaskParameters();
+    }
+
+    private void checkTaskParameters() {
+        logger.info("Checking parameters ...");
+        if (StringUtils.isEmpty(parameters.getUrl())) {
+            String errorMsg = "Empty WFS server URL is not allowed.";
+            logger.error(errorMsg);
+            throw new IllegalArgumentException(errorMsg);
+        }
+        if (StringUtils.isEmpty(parameters.getTypeName())) {
+            String errorMsg = "Empty WFS type name is not allowed.";
+            logger.error(errorMsg);
+            throw new IllegalArgumentException(errorMsg);
+        }
+        logger.info("Parameters are accepted.");
+    }
+
+    /**
+     * Create a WFSDatastore for this featuretype and retrieve
+     * all schema infos (attributes names and types).
+     */
+    public void initDataStore() throws Exception {
+        WFSDataStoreFactory factory = new WFSDataStoreFactory();
+        Map m = new HashMap();
+
+        try {
+            String getCapUrl = OwsUtils.getGetCapabilitiesUrl(
+                    parameters.getUrl(), parameters.getVersion());
+            logger.info(String.format(
+                    "Connecting using GetCatapbilities URL '%s'.",
+                    getCapUrl));
+
+            m.put(WFSDataStoreFactory.URL.key, getCapUrl);
+            m.put(WFSDataStoreFactory.TIMEOUT.key, parameters.getTimeOut());
+            m.put(WFSDataStoreFactory.TRY_GZIP.key, true);
+            m.put(WFSDataStoreFactory.ENCODING.key, parameters.getEncoding());
+            m.put(WFSDataStoreFactory.USEDEFAULTSRS.key, true);
+            m.put(WFSDataStoreFactory.OUTPUTFORMAT.key, "GML3"); // seems to be mandatory with wfs 1.1.0 sources
+            m.put(WFSDataStoreFactory.LENIENT.key, true);
+
+            if (parameters.getMaxFeatures() != -1) {
+                m.put(WFSDataStoreFactory.MAXFEATURES.key, parameters.getMaxFeatures());
+            }
+
+            wfsDatastore = factory.createDataStore(m);
+
+            logger.info(String.format(
+                    "Reading feature type '%s' schema structure.",
+                    parameters.getTypeName()));
+            SimpleFeatureType sft = wfsDatastore.getSchema(parameters.getTypeName());
+            List<AttributeDescriptor> attributesDesc = sft.getAttributeDescriptors();
+
+            for (AttributeDescriptor desc : attributesDesc) {
+                fields.put(desc.getName().getLocalPart(), OwsUtils.getTypeFromFeatureType(desc));
+            }
+
+            logger.info(String.format(
+                    "Successfully analyzed %d attributes in schema.", fields.size()));
+        } catch (IOException e) {
+            String errorMsg = String.format(
+                    "Failed to create datastore from service using URL '%s'. Error is %s.", parameters.getUrl(), e.getMessage());
+            logger.error(errorMsg);
+            throw e;
+        } catch (Exception e) {
+            String errorMsg = String.format(
+                    "Failed to GetCapabilities from service using URL '%s'. Error is %s.",
+                    parameters.getUrl(), e.getMessage());
+            logger.error(errorMsg);
+            throw e;
+        }
+    }
+}
---
+++ workers/wfsfeature-harvester/src/main/java/org/fao/geonet/harvester/wfsfeatures/worker/WFSHarvesterRouteBuilder.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2001-2015 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+package org.fao.geonet.harvester.wfsfeatures.worker;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.XPathBuilder;
+import org.apache.camel.model.dataformat.JaxbDataFormat;
+import org.fao.geonet.harvester.wfsfeatures.model.WFSHarvesterParameter;
+import org.w3c.dom.Document;
+
+import javax.xml.bind.JAXBContext;
+
+/**
+ * Created by francois on 28/10/15.
+ */
+public class WFSHarvesterRouteBuilder extends RouteBuilder {
+    public static final String LOGGER_NAME = "geonetwork.harvest.wfs.features";
+    public static final String MESSAGE_HARVEST_WFS_FEATURES = "harvest-wfs-features";
+    public static final String MESSAGE_DELETE_WFS_FEATURES = "delete-wfs-features";
+
+    private boolean startsFromXMLConfigurationFile = false;
+
+    public boolean isStartsFromXMLConfigurationFile() {
+        return startsFromXMLConfigurationFile;
+    }
+
+    public void setStartsFromXMLConfigurationFile(boolean isStartingFromXMLConfigurationFile) {
+        this.startsFromXMLConfigurationFile = isStartingFromXMLConfigurationFile;
+    }
+
+
+    @Override
+    public void configure() throws Exception {
+        onException(Exception.class)
+                .handled(true)
+                .log(LoggingLevel.ERROR, LOGGER_NAME,
+                        "Exception occured: ${exception.message}")
+                .log(LoggingLevel.ERROR, LOGGER_NAME,
+                        "Harvesting task terminated due to previous exception (Exchange ${exchangeId}).");
+        JaxbDataFormat jaxb = new JaxbDataFormat(false);
+        jaxb.setContextPath(WFSHarvesterParameter.class.getPackage().getName());
+
+        /**
+         * Start the indexing from the XML configuration file.
+         * Used for testing mainly.
+         */
+        XPathBuilder xPathWfsConfigBuilder = new XPathBuilder("//wfs");
+        from("file:src/test/resources/?fileName=wfs.xml&noop=true")
+                .id("harvest-wfs-start-from-file")
+                .autoStartup(startsFromXMLConfigurationFile)
+                .log(LoggingLevel.INFO, LOGGER_NAME, "Harvest features from XML configuration file.")
+                .convertBodyTo(Document.class)
+                .log(LoggingLevel.DEBUG, LOGGER_NAME, "Content is: ${body}")
+                .split(xPathWfsConfigBuilder)
+                    .parallelProcessing()
+                    // Will not stop on exception if one of the splitted task fails
+                    .executorServiceRef("harvest-wfs-thread-pool")
+                        .unmarshal(jaxb)
+                        .setProperty("configuration", simple("${body}"))
+//                        .setProperty("url", xpath("wfs/@url", String.class))
+//                        .setProperty("typeName", xpath("wfs/@typeName", String.class))
+                        .log(LoggingLevel.INFO, LOGGER_NAME, "#${property.CamelSplitIndex}. Harvesting ${property.configuration.url} - start (Exchange ${exchangeId}).")
+                        .beanRef("WFSFeatureIndexer", "initialize(*, true)")
+                        .to("direct:delete-wfs-featuretype-features")
+                        .to("direct:index-wfs")
+                        .log(LoggingLevel.INFO, LOGGER_NAME, "#${property.CamelSplitIndex}. Harvesting ${property.configuration.url} - end (Exchange ${exchangeId}).")
+                .end()
+                .log(LoggingLevel.DEBUG, LOGGER_NAME, "All WFS harvested.");
+
+        /**
+         * This route get `uuid` `wfsUrl` and `featureType` properties from JSM message.
+         * It creates a bean FeatureTypeBean to store these properties, the attribute
+         * types and the WFSDatastore.
+         * This bean will be pass to next Route.
+         */
+        from("activemq:queue:" + MESSAGE_HARVEST_WFS_FEATURES + "?concurrentConsumers=5")
+                .id("harvest-wfs-start-from-message")
+                .log(LoggingLevel.INFO, LOGGER_NAME, "Harvest features message received.")
+                .log(LoggingLevel.INFO, LOGGER_NAME, "${body}")
+                .setProperty("configuration", simple("${body.parameters}"))
+                .beanRef("WFSFeatureIndexer", "initialize(*, true)")
+                .to("direct:delete-wfs-featuretype-features")
+                .to("direct:index-wfs");
+
+        from("activemq:queue:" + MESSAGE_DELETE_WFS_FEATURES + "?concurrentConsumers=5")
+                .id("harvest-wfs-delete-features-from-message")
+                .log(LoggingLevel.INFO, LOGGER_NAME, "Delete features message received.")
+                .setProperty("url", simple("${body.parameters.url}"))
+                .setProperty("typeName", simple("${body.parameters.typeName}"))
+                .to("direct:delete-wfs-featuretype-features");
+
+        from("direct:delete-wfs-featuretype-features")
+                .id("harvest-wfs-delete-features")
+                .log(LoggingLevel.INFO, "Removing features from ${property.url}#${property.typeName} ...")
+                .beanRef("WFSFeatureIndexer", "deleteFeatures")
+                .log(LoggingLevel.INFO, "All features from ${property.url}#${property.typeName} removed.");
+
+        from("direct:index-wfs")
+                .id("harvest-wfs-features")
+                .log(LoggingLevel.INFO, "Indexing features from ${property.url}#${property.typeName} ...")
+                .beanRef("WFSFeatureIndexer", "indexFeatures", false)
+                .log(LoggingLevel.INFO, "All features from ${property.url}#${property.typeName} indexed.");
+    }
+}
+
