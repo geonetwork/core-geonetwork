bbade80b647e8ee064e9cc62a518137f858438d3
Jose García <josegar74@gmail.com>
2020-05-11 11:04:31 +0200

    Add method to support HttpClientContext for HttpUriRequest requests

--- common/src/main/java/org/fao/geonet/utils/GeonetHttpRequestFactory.java
+++ common/src/main/java/org/fao/geonet/utils/GeonetHttpRequestFactory.java
@@ -33,6 +33,7 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.ConnectionRequest;
 import org.apache.http.conn.HttpClientConnectionManager;
@@ -183,6 +184,20 @@ public class GeonetHttpRequestFactory {
             return new AdaptingResponse(httpClient, httpClient.execute(request));
         }
 
+    }
+
+    public ClientHttpResponse execute(HttpUriRequest request,
+                                      Function<HttpClientBuilder, Void> configurator,
+                                      HttpClientContext r) throws IOException {
+        final HttpClientBuilder clientBuilder = getDefaultHttpClientBuilder();
+        configurator.apply(clientBuilder);
+        CloseableHttpClient httpClient = clientBuilder.build();
+
+        if (r != null) {
+            return new AdaptingResponse(httpClient, httpClient.execute(request, r));
+        } else {
+            return new AdaptingResponse(httpClient, httpClient.execute(request));
+        }
     }
 
     public HttpClientBuilder getDefaultHttpClientBuilder() {
