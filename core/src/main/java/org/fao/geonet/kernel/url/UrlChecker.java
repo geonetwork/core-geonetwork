2e067cba8bbd98acb10cf617ca9c8a5992dfdc88
david blasby <david.blasby@geocat.net>
2020-05-25 16:31:49 -0700

    add proxy for UrlChecker and change UserAgent

--- core/src/main/java/org/fao/geonet/kernel/url/UrlChecker.java
+++ core/src/main/java/org/fao/geonet/kernel/url/UrlChecker.java
@@ -28,18 +28,26 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.domain.LinkStatus;
+import org.fao.geonet.kernel.setting.SettingManager;
+import org.fao.geonet.lib.NetLib;
 import org.fao.geonet.utils.GeonetHttpRequestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.ClientHttpResponse;
 import sun.net.ftp.FtpLoginException;
+import org.fao.geonet.utils.Log;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class UrlChecker {
+
+    @Autowired
+    SettingManager settingManager;
 
     private static final Function<HttpClientBuilder, Void> HTTP_CLIENT_CONFIGURATOR = new Function<HttpClientBuilder, Void>() {
         @Nullable
@@ -51,10 +59,12 @@ public class UrlChecker {
                     .setSocketTimeout(10000);
             RequestConfig requestConfig = config.build();
             originalConfig.setDefaultRequestConfig(requestConfig);
-            originalConfig.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/73.0.3683.86 Chrome/73.0.3683.86 Safari/537.36");
+            originalConfig.setUserAgent("GeoNetwork Link Checker");
             return null;
         }
     };
+
+
 
     @Autowired
     protected GeonetHttpRequestFactory requestFactory;
@@ -64,7 +74,10 @@ public class UrlChecker {
             if (url.startsWith("ftp")) {
                 return getFTPStatus(url);
             }
-            return getUrlStatus(url, 5);
+            LinkStatus status =  getUrlStatus(url, 5);
+            Log.info(Geonet.GEONETWORK,"getUrlStatus for: "+url);
+            Log.info(Geonet.GEONETWORK,"result: "+status);
+            return status;
         } catch (Exception e) {
             return buildExceptionStatus(e);
         }
@@ -105,7 +118,29 @@ public class UrlChecker {
 
     private ClientHttpResponse getResponseFromServer(String url) throws IOException {
         HttpHead head = new HttpHead(url);
-        ClientHttpResponse response = requestFactory.execute(head, HTTP_CLIENT_CONFIGURATOR);
+
+        Function<HttpClientBuilder, Void> HTTP_CLIENT_CONFIGURATOR2 = new Function<HttpClientBuilder, Void>() {
+
+            @Nullable
+            @Override
+            public Void apply(@Nullable HttpClientBuilder originalConfig) {
+                HTTP_CLIENT_CONFIGURATOR.apply(originalConfig); // call the base one
+                //given a URL, find its host name
+                String hostname = "";
+                try {
+                    URL _url = new URL(url);
+                    hostname = _url.getHost();
+                } catch (MalformedURLException e) {
+                    Log.info(Geonet.GEONETWORK,"UrlChecker: cannot determine hostname from url: "+url);
+                 }
+                //now we have hostname, we can configure proxy
+                NetLib netLib = new NetLib();
+                netLib.setupProxy(settingManager, originalConfig, hostname);
+                return null;
+            }
+        };
+
+        ClientHttpResponse response = requestFactory.execute(head, HTTP_CLIENT_CONFIGURATOR2);
         if (!shouldTryGetInsteadOfHead(response.getRawStatusCode())) {
             return response;
         }
