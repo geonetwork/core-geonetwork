6e3c65f4c4f3eac3a7784e72f78d04eff52fa186
Jody Garnett <jody.garnett@gmail.com>
2020-06-01 15:28:41 -0700

    Add choice to unpack or copy schema plugins
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- add-schema.sh
+++ add-schema.sh
@@ -45,15 +45,24 @@ fi
 # Add schema module in schemas/pom.xml
 line=$(grep -n ${schema} schemas/pom.xml | cut -d: -f1)
 
-if [ ! $line ]
+if [ ! -n "$line" ]
 then
-  line=$(grep -n 'iso19139</module>' schemas/pom.xml | cut -d: -f1)
+  line=$(grep -n '</profiles>' schemas/pom.xml | cut -d: -f1)
+  insertLine=$(($line - 1))
 
   echo "Adding schema ${schema} to schemas/pom.xml"
 
   sed $sedopt -f /dev/stdin schemas/pom.xml << SED_SCRIPT
-  ${line} a\\
-\    <module>${schema}</module>
+  ${insertLine} a\\
+\    <profile>\\
+\      <id>schema-${schema}</id>\\
+\      <activation>\\
+\        <file><exists>${schema}</exists></file>\\
+\      </activation>\\
+\      <modules>\\
+\        <module>${schema}</module>\\
+\      </modules>\\
+\    </profile>
 SED_SCRIPT
 fi
 
@@ -61,23 +70,78 @@ fi
 # Add schema dependency in web/pom.xml
 line=$(grep -n "schema-${schema}" web/pom.xml | cut -d: -f1)
 
-if [ ! $line ]
+if [ ! -n "$line" ]
 then
-  line=$(grep -n 'schema-iso19139</artifactId>' web/pom.xml | cut -d: -f1)
-  insertLine=$(($line + 2))
-
   projectGroupId='${project.groupId}'
   gnSchemasVersion='${gn.schemas.version}'
+  basedir='${basedir}'
 
-  echo "Adding schema ${schema} dependency to web/pom.xml"
+  echo "Adding schema ${schema} dependency to web/pom.xml for schemaCopy"
 
+  line=$(grep -n 'schema-iso19115-3.2018</artifactId>' web/pom.xml | cut -d: -f1 | tail -1)
+  insertLine=$(($line + 2))
   sed $sedopt -f /dev/stdin web/pom.xml << SED_SCRIPT
   ${insertLine} a\\
-\    <dependency>\\
-\      <groupId>${projectGroupId}</groupId>\\
-\      <artifactId>schema-${schema}</artifactId>\\
-\      <version>${gnSchemasVersion}</version>\\
-\    </dependency>
+\        <dependency>\\
+\          <groupId>${projectGroupId}</groupId>\\
+\          <artifactId>schema-${schema}</artifactId>\\
+\          <version>${gnSchemasVersion}</version>\\
+\        </dependency>
+SED_SCRIPT
+
+  echo "Adding schema ${schema} dependency to web/pom.xml for schemaUnpack"
+
+  line=$(grep -n '</profiles>' web/pom.xml | cut -d: -f1)
+  insertLine=$(($line - 1))
+  sed $sedopt -f /dev/stdin web/pom.xml << SED_SCRIPT
+  ${insertLine} a\\
+\\
+\    <profile>\\
+\      <id>schema-${schema}</id>\\
+\      <activation>\\
+\        <property><name>schemasCopy</name><value>!true</value></property>\\
+\        <file><exists>../schemas/${schema}</exists></file>\\
+\      </activation>\\
+\      <dependencies>\\
+\        <dependency>\\
+\          <groupId>${projectGroupId}</groupId>\\
+\          <artifactId>schema-${schema}</artifactId>\\
+\          <version>${gnSchemasVersion}</version>\\
+\        </dependency>\\
+\      </dependencies>\\
+\      <build>\\
+\        <plugins>\\
+\          <plugin>\\
+\            <groupId>org.apache.maven.plugins</groupId>\\
+\            <artifactId>maven-dependency-plugin</artifactId>\\
+\            <executions>\\
+\              <execution>\\
+\                <id>${schema}-resources</id>\\
+\                <phase>process-resources</phase>\\
+\                <goals><goal>unpack</goal></goals>\\
+\                <configuration>\\
+\                  <artifactItems>\\
+\                    <artifactItem>\\
+\                      <groupId>${projectGroupId}</groupId>\\
+\                      <artifactId>schema-${schema}</artifactId>\\
+\                      <type>jar</type>\\
+\                      <overWrite>false</overWrite>\\
+\                      <outputDirectory>${basedir}/src/main/webapp/WEB-INF/data/config/schema_plugins</outputDirectory>\\
+\                      <includes>plugin/**</includes>\\
+\                      <fileMappers>\\
+\                        <fileMapper implementation="org.codehaus.plexus.components.io.filemappers.RegExpFileMapper">\\
+\                          <pattern>^plugin/</pattern><replacement>./</replacement>\\
+\                        </fileMapper>\\
+\                      </fileMappers>\\
+\                    </artifactItem>\\
+\                  </artifactItems>\\
+\                </configuration>\\
+\              </execution>\\
+\            </executions>\\
+\          </plugin>\\
+\        </plugins>\\
+\      </build>\\
+\    </profile>
 SED_SCRIPT
 fi
 
@@ -87,19 +151,20 @@ line=$(grep -n "schemas/${schema}/src/main/plugin</directory>" web/pom.xml | cut
 
 if [ ! $line ]
 then
-  line=$(grep -n 'schemas/iso19139/src/main/plugin</directory>' web/pom.xml | cut -d: -f1)
-  finalLine=$(($line + 3))
+  profileLine=$(grep -n '<id>schemas-copy</id>' web/pom.xml | cut -d: -f1)
+  lineOffset=$(tail -n +$profileLine web/pom.xml | grep -n '<resources>' | cut -d: -f1 | head -1)
+  finalLine=$(($profileLine + $lineOffset -1))
 
   projectBaseDir='${project.basedir}'
   baseDir='${basedir}'
 
-  echo "Adding schema ${schema} resources to web/pom.xml"
+  echo "Adding schema ${schema} resources to web/pom.xml for schemaCopy"
 
   sed $sedopt -f /dev/stdin web/pom.xml << SED_SCRIPT
   ${finalLine} a\\
-\                <resource>\\
-\                  <directory>${projectBaseDir}/../schemas/${schema}/src/main/plugin</directory>\\
-\                  <targetPath>${baseDir}/src/main/webapp/WEB-INF/data/config/schema_plugins</targetPath>\\
-\                </resource>
+\                    <resource>\\
+\                      <directory>${projectBaseDir}/../schemas/${schema}/src/main/plugin</directory>\\
+\                      <targetPath>${baseDir}/src/main/webapp/WEB-INF/data/config/schema_plugins</targetPath>\\
+\                    </resource>
 SED_SCRIPT
 fi
--- schemas/README.md
+++ schemas/README.md
@@ -1,17 +1,92 @@
 # Maven module containing schema plugins
 
-## Plugin structure
+## Schema Plugin Definition
 
-A schema plugin is composed off:
+1. `src/main/plugin/<folder>` contents:
+   
+   * `schema-ident.xml` required configuration file
+   * schema definition `xsd` files
+   * transformations `xsl` files
+  
+   See [GeoNetwork Manual](https://geonetwork-opensource.org/manuals/trunk/en/customizing-application/implementing-a-schema-plugin.html)
 
-* A configuration folder containing XML file for configuration,
-the schema as XSDs and a set of transformation (See http://geonetwork-opensource.org/manuals/trunk/eng/developer/schemaPlugins/index.html).
+2. `src/main/java` providing:
+  
+  * Optional: SchemaPlugin bean
+  * Optional: ApplicationListener<ServerStartup> to auto install plugin
 
-* (optional) A SchemaPlugin bean
+3. Schema plugin `pom.xml` must include `src/main/plugin` in resources section:
+   
+   ```xml
+    <resources>
+      <resource>
+        <directory>src/main/resources</directory>
+      </resource>
+      <resource>
+        <directory>src/main/plugin</directory>
+        <targetPath>plugin</targetPath>
+      </resource>
+    </resources>
+   ```
 
+4. If you need to depend on any modules please make use of:
+  
+   * `gn.project.version` property for geonetwork modules:
+   
+      ```xml
+     <dependency>
+       <groupId>${project.groupId}</groupId>
+       <artifactId>common</artifactId>
+       <version>${gn.project.version}</version>
+     </dependency>
+      ```
+   
+   * `project.version` property for other schemas (or schema-core):
+     
+     ```
+     <dependencies>
+       <dependency>
+         <groupId>${project.groupId}</groupId>
+         <artifactId>schema-core</artifactId>
+         <version>${project.version}</version>
+       </dependency>
+       <dependency>
+         <groupId>${project.groupId}</groupId>
+         <artifactId>schema-iso19139</artifactId>
+         <version>${project.version}</version>
+       </dependency>
+     </dependencies>
+     ```
+   
+5. Use `mvn install` to install your schema plugin in your local repository.
 
 ## Add a plugin to the build
 
-To include a new schema plugin in a build, copy the schema folder
-here and add it to the copy-schemas execution in web/pom.xml.
+While schema plugins can be built independently, they can be conditionally included in the build:
+
+1. Add schema plugin:
+
+   * As a folder, using `.gitignore` to avoid accidentally commiting.
+   
+   * As a git submodule if you are making a fork
+`
+2. Use a profile (activated by your schema plugin folder being present) to include your schema plugin to the list of modules in `pom.xml`.
+   
+   ```xml
+   <profiles>
+     <profile>
+       <id>schema-iso19139.xyz</id>
+       <activation>
+       <file>
+         <exists>iso19139.xyz</exists>
+       </file>
+       </activation>
+       <modules>
+         <module>iso19139.xyz</module>
+       </modules>
+     </profile>
+   </profiles>
+   ```
+   
+3. The `add-schema.sh` script automates these changes.
 
