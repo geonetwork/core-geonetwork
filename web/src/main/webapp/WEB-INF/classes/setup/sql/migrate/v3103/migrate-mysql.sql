254254071b2bfcf7d1858ef1cef9fba8308c2bac
davidblasby <48937730+davidblasby@users.noreply.github.com>
2020-06-21 23:21:06 -0700

    shorten index names for oracle 30 char limit (#4776)
    * shorten index names for oracle 30 char limit
    
    * remove old indexes (will be replaced by new index name)
    
    * add h2 migration
    
    * update migration files
    
    Co-authored-by: david blasby <david.blasby@geocat.net>

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3103/migrate-mysql.sql
@@ -0,0 +1,18 @@
+
+ALTER TABLE groupsdes ALTER COLUMN label TYPE varchar(255);
+ALTER TABLE sourcesdes ALTER COLUMN label TYPE varchar(255);
+ALTER TABLE schematrondes ALTER COLUMN label TYPE varchar(255);
+
+UPDATE Settings SET value='3.11.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+-- Increase the length of Validation type (where the schematron file name is stored)
+ALTER TABLE Validation ALTER COLUMN valType TYPE varchar(128);
+
+-- New setting for server timezone
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/server/timeZone', '', 0, 260, 'n');
+
+
+DROP INDEX idx_metadatafiledownloads_metadataid ON MetadataFileDownloads;
+DROP INDEX idx_metadatafileuploads_metadataid ON MetadataFileUploads;
+DROP INDEX idx_operationallowed_metadataid ON OperationAllowed;
