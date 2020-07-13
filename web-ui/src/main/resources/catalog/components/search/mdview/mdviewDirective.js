49586adf3d29d3d6bccc039c66092c5b20c33951
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-07-13 14:32:58 +0200

    Md View / prepend current URL path for MD links in search results
    Without this, opening a record in a webpage with a <base> element
    might route the user to a wrong URL.

--- web-ui/src/main/resources/catalog/components/search/mdview/mdviewDirective.js
+++ web-ui/src/main/resources/catalog/components/search/mdview/mdviewDirective.js
@@ -56,12 +56,13 @@
 
             var hyperlinkTagName = 'A';
             if (element.get(0).tagName === hyperlinkTagName) {
-              var url = '#/' +
+              var url = window.location.pathname + '#/' +
                 (scope.md.draft == 'y' ? 'metadraf' : 'metadata') +
                 '/' + scope.md.uuid +
                 (scope.formatter === undefined || scope.formatter == '' ?
                   '' :
                   formatter);
+
               element.attr('href', url);
             } else {
               element.on('click', function(e) {
