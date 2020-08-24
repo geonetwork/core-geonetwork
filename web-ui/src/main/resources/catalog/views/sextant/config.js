41784f7053dbd2b9fa784aec9d2f5c25a744bc4e
Francois Prunayre <fx.prunayre@gmail.com>
2020-08-24 13:42:12 +0200

    ISO19110 / Register formatter.

--- web-ui/src/main/resources/catalog/views/sextant/config.js
+++ web-ui/src/main/resources/catalog/views/sextant/config.js
@@ -242,6 +242,8 @@
             }
           } else if(md.getSchema() == 'dublin-core') {
             url = '../api/records/' + uuid + '/formatters/xsl-view';
+          } else if(md.getSchema() == 'iso19110') {
+            url = '../api/records/' + uuid + '/formatters/xsl-view';
           } else if(md.getSchema() == 'iso19115-3.2018') {
             url = '../api/records/' + uuid + '/formatters/xsl-view?root=div&header=false&view=earthObservation&related=';
           } else if(md.getSchema() == 'iso19115-3') {
