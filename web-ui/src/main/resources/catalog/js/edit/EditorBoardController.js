a2cff823ff35c54552a3aa64ceaef62a0bd62fe9
Francois Prunayre <fx.prunayre@gmail.com>
2020-08-25 15:08:34 +0200

    Do not apply UI filter in editor board.

--- web-ui/src/main/resources/catalog/js/edit/EditorBoardController.js
+++ web-ui/src/main/resources/catalog/js/edit/EditorBoardController.js
@@ -66,7 +66,8 @@
         sortbyValues: gnSearchSettings.sortbyValues,
         hitsperpageValues: gnSearchSettings.hitsperpageValues,
         selectionBucket: 'e101',
-        filters: gnSearchSettings.filters,
+        // Sextant only applies filter to main search. Not here
+        // filters: gnSearchSettings.filters,
         params: {
           sortBy: 'changeDate',
           _isTemplate: 'y or n',
