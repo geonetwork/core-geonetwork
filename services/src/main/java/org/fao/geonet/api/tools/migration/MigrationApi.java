7671b09e1dfab2516519380ca32cda31103228aa
François Prunayre <fx.prunayre@gmail.com>
2020-09-10 10:40:40 +0200

    4.0.x / API / Migration not in documentation

--- services/src/main/java/org/fao/geonet/api/tools/migration/MigrationApi.java
+++ services/src/main/java/org/fao/geonet/api/tools/migration/MigrationApi.java
@@ -35,10 +35,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -47,7 +44,7 @@ import java.sql.Connection;
     "/{portal}/api/tools/migration"
 })
 @Tag(name = "tools")
-@Controller("migration")
+@RestController
 public class MigrationApi {
 
 
