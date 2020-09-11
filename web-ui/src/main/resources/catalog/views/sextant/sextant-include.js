8074b21c5a348f91e6bb9ca1c75a893c58cc2965
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-09-11 15:49:17 +0200

    API / wait for libs to be loaded before loading app init script
     #59

--- web-ui/src/main/resources/catalog/views/sextant/sextant-include.js
+++ web-ui/src/main/resources/catalog/views/sextant/sextant-include.js
@@ -31,14 +31,22 @@
   root.appendChild(libs);
 
   var sextant = document.createElement('script');
-  sextant.src = staticUrl + 'sextant_api.js';
 
   // copy input attributes to the script which loads Sextant
-  copyScriptAttribute(sextant, 'catalog')
-  copyScriptAttribute(sextant, 'max-sm')
-  copyScriptAttribute(sextant, 'max-md')
-  copyScriptAttribute(sextant, 'size')
-  copyScriptAttribute(sextant, 'size-diff')
+  copyScriptAttribute(sextant, 'catalog');
+  copyScriptAttribute(sextant, 'max-sm');
+  copyScriptAttribute(sextant, 'max-md');
+  copyScriptAttribute(sextant, 'size');
+  copyScriptAttribute(sextant, 'size-diff');
+
+  // we're waiting for lib.js to be loaded before loading the actual app
+  // this is because when using dynamically generated <script> tags, we have
+  // no guarantee on the order of execution of scripts, and many times
+  // sextant_api.js will be executed before lib.js (since it is bigger)
+  // - this makes the app loading time a bit longer (~260 KB more to load before paint)
+  libs.onload = function() {
+    sextant.src = staticUrl + 'sextant_api.js';
+  };
 
   root.appendChild(sextant);
 })();
