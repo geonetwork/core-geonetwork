geonetwork/core-geonetwork

commit d08b9b2edbd035166dca611ef7d74bfc7654e0aa

update email in source code contact details to operational email address (#4853)
* update email in source code contact details to operational email address

* undo change on atom

schemas/iso19139/src/main/plugin/iso19139/extract-datasets.xsl
@@ -22,14 +22,18 @@
   ~ Rome - Italy. email: geonetwork@osgeo.org
   -->
 
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:gmd="http://www.isotc211.org/2005/gmd"
-                xmlns:srv="http://www.isotc211.org/2005/srv"
-                version="2.0">
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
   <xsl:template match="gmd:MD_Metadata">
     <datasets>
       <xsl:for-each select="gmd:identificationInfo/srv:SV_ServiceIdentification/srv:operatesOn">
+      <xsl:if test="@uuidref!=''">
         <dataset>
           <xsl:value-of select="@uuidref"/>
+        </dataset>
+      <xsl:if>
+        <dataset>
+          <xsl:value-of select="tokenize(tokenize(string(@xlink:href),'&amp;id=')[2],'&amp;')[1]"/>
+          <xsl:comment>fuu:<xsl:value-of select="tokenize(tokenize(string(@xlink:href),'&amp;id=')[2],'&amp;')[1]"/></xsl:comment>
         </dataset>
       </xsl:for-each>
     </datasets>

commit c9266c8060e3923bb4e0ce84c911245b2df3bdd7

fixes a bug accidentally introduced in #4853

schemas/iso19139/src/main/plugin/iso19139/extract-datasets.xsl
@@ -22,18 +22,23 @@
   ~ Rome - Italy. email: geonetwork@osgeo.org
   -->
 
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:gmd="http://www.isotc211.org/2005/gmd"
+                xmlns:srv="http://www.isotc211.org/2005/srv"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                version="2.0">
   <xsl:template match="gmd:MD_Metadata">
     <datasets>
       <xsl:for-each select="gmd:identificationInfo/srv:SV_ServiceIdentification/srv:operatesOn">
-      <xsl:if test="@uuidref!=''">
         <dataset>
-          <xsl:value-of select="@uuidref"/>
-        </dataset>
-      <xsl:if>
-        <dataset>
-          <xsl:value-of select="tokenize(tokenize(string(@xlink:href),'&amp;id=')[2],'&amp;')[1]"/>
-          <xsl:comment>fuu:<xsl:value-of select="tokenize(tokenize(string(@xlink:href),'&amp;id=')[2],'&amp;')[1]"/></xsl:comment>
+          <xsl:choose>
+            <xsl:when test="@uuidref!=''">
+              <xsl:value-of select="@uuidref"/>
+            </xsl:when>
+            <xsl:otherwise>
+              <!-- works only for csw requests for now -->
+              <xsl:value-of select="tokenize(tokenize(string(@xlink:href),'id=')[2],'&amp;')[1]"/>
+            </xsl:otherwise>
+          </xsl:choose>
         </dataset>
       </xsl:for-each>
     </datasets>