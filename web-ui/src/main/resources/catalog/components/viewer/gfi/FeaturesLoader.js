8c8effe231d92ab784e95b050ae39e310aee3050
François Prunayre <fx.prunayre@gmail.com>
2020-09-29 10:19:02 +0200

    Map / ESRI / Fix GetFeatureInfo

--- web-ui/src/main/resources/catalog/components/viewer/gfi/FeaturesLoader.js
+++ web-ui/src/main/resources/catalog/components/viewer/gfi/FeaturesLoader.js
@@ -516,7 +516,7 @@
 
     var uuid;
     if(layer.get('md')) {
-      uuid = layer.get('md').getUuid();
+      uuid = layer.get('md').uuid;
     } else if(layer.get('metadataUuid')) {
       uuid = layer.get('metadataUuid');
     }
