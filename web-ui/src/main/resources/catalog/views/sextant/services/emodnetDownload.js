b139eb1bcb921b691591397b95dd86abff6085f5
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-10-28 17:14:51 +0100

    Emodnet / use actual URL for analytics
    sextant/geonetwork#223

--- web-ui/src/main/resources/catalog/views/sextant/services/emodnetDownload.js
+++ web-ui/src/main/resources/catalog/views/sextant/services/emodnetDownload.js
@@ -4,7 +4,7 @@
 
   var module = angular.module('sxt_emodnetdownload', []);
 
-  var ANALYTICS_API_URL = 'https://nodc.inogs.it/emodnet-dev/extranet/analytics';
+  var ANALYTICS_API_URL = 'https://www.emodnet-chemistry.eu/extranet/analytics';
   var COUNTRIES_JSON_URL = 'https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json';
 
   /**
