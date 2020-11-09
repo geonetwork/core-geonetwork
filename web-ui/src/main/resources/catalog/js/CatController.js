762931e7a5d5a703cb023fa39baf10830f6f330e
François Prunayre <fx.prunayre@gmail.com>
2020-11-09 16:57:14 +0100

    Search / Aggs / Set a default_field for query_string to avoid field expansion error on large catalogs
    Execption:
    ```
    field expansion matches too many fields, limit: 1024, got: 2662"}
    ```

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -280,11 +280,13 @@ goog.require('gn_alert');
                 'filters': {
                   'availableInViewService': {
                     'query_string': {
+                      'default_field': 'linkProtocol',
                       'query': '+linkProtocol:/OGC:WMS.*/'
                     }
                   },
                   'availableInDownloadService': {
                     'query_string': {
+                      'default_field': 'linkProtocol',
                       'query': '+linkProtocol:/OGC:WFS.*/'
                     }
                   }
