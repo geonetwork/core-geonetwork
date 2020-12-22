01cdd87967ef228c81ad92324126ad09126be95a
christophe mangeat <christophe.mangeat@camptocamp.com>
2020-12-22 11:50:51 +0100

    fix build

--- services/src/main/java/org/fao/geonet/api/records/formatters/FormatterApi.java
+++ services/src/main/java/org/fao/geonet/api/records/formatters/FormatterApi.java
@@ -593,7 +593,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
             adjustedUrl = settingManager.getSiteURL(lang) + url;
         } else {
             final URI uri = new URI(url);
-            Set allowedRemoteHosts = context.getBean("formatterRemoteFormatAllowedHosts", Set.class);
+            Set allowedRemoteHosts = ApplicationContextHolder.get().getBean("formatterRemoteFormatAllowedHosts", Set.class);
             Assert.isTrue(allowedRemoteHosts.contains(uri.getHost()), "xml.format is not allowed to make requests to " + uri.getHost());
         }
 
