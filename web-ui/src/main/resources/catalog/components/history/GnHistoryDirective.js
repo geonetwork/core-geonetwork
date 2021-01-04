a2b3c1a8c76d8dd0d3e71959fd57820d0cdb021b
Jose García <josegar74@gmail.com>
2021-01-04 17:00:37 +0100

    [BP] Hide workflow option in versioning page when the workflow is not enabled

--- web-ui/src/main/resources/catalog/components/history/GnHistoryDirective.js
+++ web-ui/src/main/resources/catalog/components/history/GnHistoryDirective.js
@@ -33,8 +33,8 @@
   module
     .directive(
       'gnRecordHistory', [
-        '$http', 'gnRecordHistoryService',
-      function($http, gnRecordHistoryService) {
+        '$http', 'gnConfig', 'gnConfigService', 'gnRecordHistoryService',
+      function($http, gnConfig, gnConfigService, gnRecordHistoryService) {
         return {
           restrict: 'A',
           replace: true,
@@ -50,12 +50,23 @@
             scope.hasMoreRecords = false;
             var recordByPage = 5;
 
-            scope.filter = {
-              types: {'workflow': true, 'task': true, 'event': true},
-              recordFilter: null,
-              from: 0,
-              size: recordByPage
-            };
+            scope.filter = {};
+
+            gnConfigService.load().then(function(c) {
+              var types = {};
+              if (gnConfig['metadata.workflow.enable']) {
+                types.workflow = true;
+              }
+              types.task = true;
+              types.event = true;
+
+              scope.filter = {
+                types: types,
+                recordFilter: null,
+                from: 0,
+                size: recordByPage
+              };
+            });
 
             // Wait for metatada to be available
             scope.$watch('md', function(n, o) {
@@ -159,9 +170,9 @@
   module
     .directive(
       'gnHistory', [
-        '$http', '$filter', 'gnConfig', '$translate',
+        '$http', '$filter', 'gnConfig', 'gnConfigService', '$translate',
         'gnSearchManagerService', 'gnRecordHistoryService', 'gnRecordTaskService',
-        function($http, $filter, gnConfig, $translate,
+        function($http, $filter, gnConfig, gnConfigService, $translate,
                  gnSearchManagerService, gnRecordHistoryService, gnRecordTaskService) {
           return {
             restrict: 'A',
@@ -175,16 +186,28 @@
               scope.user = scope.$parent.user;
               var recordByPage = 20;
               scope.history = [];
-              scope.filter = {
-                types: {'workflow': true, 'task': true, 'event': true},
-                ownerFilter: null,
-                authorFilter: null,
-                recordFilter: null,
-                dateFromFilter: null,
-                dateToFilter: null,
-                from: 0,
-                size: recordByPage
-              };
+              scope.filter = {};
+
+              gnConfigService.load().then(function(c) {
+                var types = {};
+                if (gnConfig['metadata.workflow.enable']) {
+                  types.workflow = true;
+                }
+                types.task = true;
+                types.event = true;
+
+                scope.filter = {
+                  types: types,
+                  ownerFilter: null,
+                  authorFilter: null,
+                  recordFilter: null,
+                  dateFromFilter: null,
+                  dateToFilter: null,
+                  from: 0,
+                  size: recordByPage
+                };
+              });
+
               scope.hasMoreRecords = false;
 
               scope.getSuggestions = function(val) {
