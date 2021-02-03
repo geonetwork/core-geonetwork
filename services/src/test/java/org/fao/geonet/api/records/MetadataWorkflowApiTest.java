1eef41a898b2dec5f80725402da074e25ef291bf
wangf1122 <74916635+wangf1122@users.noreply.github.com>
2021-02-03 14:23:04 -0500

    Unit test fix.

--- services/src/test/java/org/fao/geonet/api/records/MetadataWorkflowApiTest.java
+++ services/src/test/java/org/fao/geonet/api/records/MetadataWorkflowApiTest.java
@@ -1,27 +1,41 @@
 package org.fao.geonet.api.records;
 
+import static org.fao.geonet.schema.iso19139.ISO19139Namespaces.GCO;
+import static org.fao.geonet.schema.iso19139.ISO19139Namespaces.GMD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.fao.geonet.domain.ISODate;
-import org.fao.geonet.domain.MetadataStatus;
-import org.fao.geonet.domain.StatusValue;
+import jeeves.server.context.ServiceContext;
+import org.fao.geonet.GeonetContext;
+import org.fao.geonet.domain.*;
+import org.fao.geonet.kernel.DataManager;
+import org.fao.geonet.kernel.SchemaManager;
+import org.fao.geonet.kernel.UpdateDatestamp;
+import org.fao.geonet.kernel.datamanager.IMetadataManager;
+import org.fao.geonet.kernel.metadata.StatusActionsFactory;
 import org.fao.geonet.repository.MetadataStatusRepository;
+import org.fao.geonet.repository.SourceRepository;
 import org.fao.geonet.repository.StatusValueRepository;
 import org.fao.geonet.repository.StatusValueRepositoryTest;
 import org.fao.geonet.services.AbstractServiceIntegrationTest;
+import org.fao.geonet.utils.Xml;
+import org.jdom.Element;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -34,19 +48,54 @@ public class MetadataWorkflowApiTest  extends AbstractServiceIntegrationTest {
     @Autowired
     StatusValueRepository statusValueRepo;
 
+
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private SourceRepository sourceRepository;
+    @Autowired
+    private SchemaManager schemaManager;
+
     private String uuid;
+    private ServiceContext context;
 
 
     @Before
     public void setUp() throws Exception {
+        this.context = createServiceContext();
         createTestData();
     }
 
-    private void createTestData() {
+    private void createTestData() throws Exception {
         this.uuid = UUID.randomUUID().toString();
 
+        loginAsAdmin(context);
+
+        final Element sampleMetadataXml = getSampleMetadataXml();
+        this.uuid = UUID.randomUUID().toString();
+        Xml.selectElement(sampleMetadataXml, "gmd:fileIdentifier/gco:CharacterString", Arrays.asList(GMD, GCO)).setText(this.uuid);
+
+        String source = sourceRepository.findAll().get(0).getUuid();
+        String schema = schemaManager.autodetectSchema(sampleMetadataXml);
+        final Metadata metadata = (Metadata) new Metadata()
+            .setDataAndFixCR(sampleMetadataXml)
+            .setUuid(uuid);
+        metadata.getDataInfo()
+            .setRoot(sampleMetadataXml.getQualifiedName())
+            .setSchemaId(schema)
+            .setType(MetadataType.METADATA)
+            .setPopularity(1000);
+        metadata.getSourceInfo()
+            .setOwner(1)
+            .setSourceId(source);
+        metadata.getHarvestInfo()
+            .setHarvested(false);
+
+        int id = dataManager.insertMetadata(context, metadata, sampleMetadataXml, false, true, false, UpdateDatestamp.NO,
+            false, false).getId();
+
         MetadataStatus metadataStatus = new MetadataStatus();
-        metadataStatus.setMetadataId(111);
+        metadataStatus.setMetadataId(id);
         metadataStatus.setChangeDate(new ISODate());
         metadataStatus.setUserId(1);
         metadataStatus.setChangeMessage("change message test");
@@ -62,12 +111,16 @@ public class MetadataWorkflowApiTest  extends AbstractServiceIntegrationTest {
 
     @Test
     public void testGetStatusByType() throws Exception {
+        StatusActionsFactory statusActionFactory = Mockito.mock(StatusActionsFactory.class);
+        Mockito.when(wac.getBean(StatusActionsFactory.class)).thenReturn(statusActionFactory);
+
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
         MockHttpSession mockHttpSession = loginAsAdmin();
 
         mockMvc.perform(get("/srv/api/records/status/search?type=&uuid="+uuid+"&from=0&size=100")
             .accept(MediaType.parseMediaType("application/json"))
             .session(mockHttpSession))
+            .andDo(MockMvcResultHandlers.print())
             .andExpect(status().is2xxSuccessful())
             .andExpect(content().contentType(API_JSON_EXPECTED_ENCODING))
             .andExpect(jsonPath("$.[0].uuid").value(uuid));
