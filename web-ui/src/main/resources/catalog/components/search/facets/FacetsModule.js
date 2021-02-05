7f19de8f30132874a73c998e731044a41254ebf4
Francois Prunayre <fx.prunayre@gmail.com>
2021-02-05 12:24:21 +0100

    AngularJS / Remove old unused facet code.

--- web-ui/src/main/resources/catalog/components/search/facets/FacetsModule.js
+++ web-ui/src/main/resources/catalog/components/search/facets/FacetsModule.js
@@ -24,23 +24,9 @@
 (function() {
   goog.provide('gn_facets');
 
-
-
-
-
-
-
-
-
-  goog.require('gn_facets_config_service');
-  goog.require('gn_facets_dimension_directive');
   goog.require('gn_facets_directive');
-  goog.require('gn_facets_service');
 
   angular.module('gn_facets', [
-    'gn_facets_directive',
-    'gn_facets_dimension_directive',
-    'gn_facets_config_service',
-    'gn_facets_service'
+    'gn_facets_directive'
   ]);
 })();
