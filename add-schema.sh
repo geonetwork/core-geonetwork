42a4eb45743f179370004992c7cb2bb0a0e492c2
Jody Garnett <jody.garnett@gmail.com>
2021-02-09 01:26:54 -0800

    Backport add-schema.sh from master and adapt for 3.10.x branch (#5403)
    * Backport add-schema.sh from master and adapt for 3.10.x branch. Note use of --force to allow reuse of
    previously added remote locations
    * Add profiles to schemas/pom.xml for add-schema.sh to use
    * Restore copy based on feedback
    * Backport use of Ant to process schema plugin resources
    
    This simplifies add-schema also.

--- add-schema.sh
+++ add-schema.sh
@@ -7,14 +7,14 @@ function showUsage
   echo -e "Usage: ./`basename $0 $1` schema_name git_schema_repository git_schema_branch"
   echo
   echo -e "Example:"
-  echo -e "\t./`basename $0 $1` iso19115-3 https://github.com/metadata101/iso19115-3 3.4.x"
+  echo -e "\t./`basename $0 $1` iso19139.ca.HNAP https://github.com/metadata101/iso19139.ca.HNAP 3.10.x"
   echo
 }
 
 if [ "$1" = "-h" ]
 then
-	showUsage
-	exit
+  showUsage
+  exit
 fi
 
 if [ $# -ne 3 ]
@@ -38,36 +38,44 @@ fi
 # Add submodule
 if [ ! -d "schemas/${schema}" ]; then
   echo "Adding schema from ${gitRepository}, branch  ${gitBranch} to schemas/${schema}"
-  git submodule add -b ${gitBranch} ${gitRepository} schemas/${schema}
+  git submodule add --force -b ${gitBranch} ${gitRepository} schemas/${schema}
 fi
 
 
 # Add schema module in schemas/pom.xml
 line=$(grep -n ${schema} schemas/pom.xml | cut -d: -f1)
 
-if [ ! $line ]
+if [ ! -n "$line" ]
 then
-  line=$(grep -n 'iso19139</module>' schemas/pom.xml | cut -d: -f1)
+  line=$(grep -n '</profiles>' schemas/pom.xml | cut -d: -f1)
+  insertLine=$(($line - 1))
 
   echo "Adding schema ${schema} to schemas/pom.xml"
 
   sed $sedopt -f /dev/stdin schemas/pom.xml << SED_SCRIPT
-  ${line} a\\
-\    <module>${schema}</module>
+  ${insertLine} a\\
+\    <profile>\\
+\      <id>schema-${schema}</id>\\
+\      <activation>\\
+\        <file><exists>${schema}</exists></file>\\
+\      </activation>\\
+\      <modules>\\
+\        <module>${schema}</module>\\
+\      </modules>\\
+\    </profile>
 SED_SCRIPT
 fi
-
 
 # Add schema dependency in web/pom.xml
 line=$(grep -n "schema-${schema}" web/pom.xml | cut -d: -f1)
 
-if [ ! $line ]
+if [ ! -n "$line" ]
 then
   line=$(grep -n 'schema-iso19139</artifactId>' web/pom.xml | cut -d: -f1)
   insertLine=$(($line + 2))
 
-  projectGroupId='${project.groupId}'
-  gnSchemasVersion='${gn.schemas.version}'
+  projectGroupId='org.geonetwork-opensource.schemas'
+  gnSchemasVersion='${project.version}'
 
   echo "Adding schema ${schema} dependency to web/pom.xml"
 
@@ -81,29 +89,6 @@ then
 SED_SCRIPT
 fi
 
-
-# Add schema resources in web/pom.xml
-line=$(grep -n "schemas/${schema}/src/main/plugin</directory>" web/pom.xml | cut -d: -f1)
-
-if [ ! $line ]
-then
-  line=$(grep -n 'schemas/iso19139/src/main/plugin</directory>' web/pom.xml | cut -d: -f1)
-  finalLine=$(($line + 3))
-
-  projectBaseDir='${project.basedir}'
-  baseDir='${basedir}'
-
-  echo "Adding schema ${schema} resources to web/pom.xml"
-
-  sed $sedopt -f /dev/stdin web/pom.xml << SED_SCRIPT
-  ${finalLine} a\\
-\                <resource>\\
-\                  <directory>${projectBaseDir}/../schemas/${schema}/src/main/plugin</directory>\\
-\                  <targetPath>${baseDir}/src/main/webapp/WEB-INF/data/config/schema_plugins</targetPath>\\
-\                </resource>
-SED_SCRIPT
-fi
-
 # Add schema resources in service/pom.xml with test scope for unit tests
 line=$(grep -n "<artifactId>schema-${schema}</artifactId>" services/pom.xml | cut -d: -f1)
 
@@ -112,8 +97,8 @@ then
   line=$(grep -n '</dependencies>' services/pom.xml | cut -d: -f1)
   finalLine=$(($line - 1))
 
-  projectGroupId='${project.groupId}'
-  gnSchemasVersion='${gn.schemas.version}'
+  projectGroupId='org.geonetwork-opensource.schemas'
+  gnSchemasVersion='${project.version}'
 
   echo "Adding schema ${schema} resources to service/pom.xml"
 
