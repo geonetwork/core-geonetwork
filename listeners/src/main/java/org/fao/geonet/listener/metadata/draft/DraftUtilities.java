db3fc05da286ba0a885d744fe49a6dd8ec3e72a0
Jose García <josegar74@gmail.com>
2021-02-11 11:01:34 +0100

    Fix for validation result gets lost when approving record and copy validation results when creating a draft. Fixes #5209

--- listeners/src/main/java/org/fao/geonet/listener/metadata/draft/DraftUtilities.java
+++ listeners/src/main/java/org/fao/geonet/listener/metadata/draft/DraftUtilities.java
@@ -10,6 +10,7 @@ import org.fao.geonet.domain.MetadataFileUpload;
 import org.fao.geonet.domain.MetadataStatus;
 import org.fao.geonet.domain.MetadataStatus_;
 import org.fao.geonet.domain.MetadataValidation;
+import org.fao.geonet.domain.MetadataValidationId;
 import org.fao.geonet.kernel.XmlSerializer;
 import org.fao.geonet.kernel.datamanager.IMetadataManager;
 import org.fao.geonet.kernel.datamanager.IMetadataOperations;
@@ -22,6 +23,7 @@ import org.fao.geonet.repository.MetadataStatusRepository;
 import org.fao.geonet.repository.MetadataValidationRepository;
 import org.fao.geonet.repository.SortUtils;
 import org.fao.geonet.repository.specification.MetadataFileUploadSpecs;
+import org.fao.geonet.repository.specification.MetadataValidationSpecs;
 import org.fao.geonet.utils.Log;
 import org.jdom.Element;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -100,9 +102,17 @@ public class DraftUtilities {
         Log.trace(Geonet.DATA_MANAGER, "Found draft with id " + draft.getId());
         // Reassign metadata validations
         List<MetadataValidation> validations = metadataValidationRepository.findAllById_MetadataId(draft.getId());
-        for (MetadataValidation mv : validations) {
-            mv.getId().setMetadataId(md.getId());
-            metadataValidationRepository.save(mv);
+        metadataValidationRepository.deleteAll(MetadataValidationSpecs.hasMetadataId(md.getId()));
+        for (MetadataValidation draftValidation : validations) {
+            MetadataValidation metadataValidation = new MetadataValidation()
+                .setId(new MetadataValidationId(md.getId(), draftValidation.getId().getValidationType()))
+                .setStatus(draftValidation.getStatus()).setRequired(draftValidation.isRequired())
+                .setValid(draftValidation.isValid()).setValidationDate(draftValidation.getValidationDate())
+                .setNumTests(draftValidation.getNumTests()).setNumFailures(draftValidation.getNumFailures())
+                .setReportUrl(draftValidation.getReportUrl()).setReportContent(draftValidation.getReportContent());
+
+            metadataValidationRepository.save(metadataValidation);
+            metadataValidationRepository.delete(draftValidation);
         }
 
         // Reassign metadata workflow statuses
