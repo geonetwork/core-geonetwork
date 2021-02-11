2ea94ae8adb1e289ecfb8b68c5315c8cfc792137
Juan Luis Rodríguez <juanluisrp@gmail.com>
2021-02-11 17:27:55 +0100

    Add support for authentication in WAF harvesters
    * Add the ability to harvest a remote WAF server protected with BASIC or
    DIGEST authentication.
    * User the server's proxy settings if enabled in the Admin Console.

--- harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/webdav/WAFRemoteFile.java
+++ harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/webdav/WAFRemoteFile.java
@@ -1,5 +1,5 @@
 //=============================================================================
-//===	Copyright (C) 2001-2012 Food and Agriculture Organization of the
+//===	Copyright (C) 2001-2021 Food and Agriculture Organization of the
 //===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
 //===	and United Nations Environment Programme (UNEP)
 //===
@@ -23,71 +23,90 @@
 
 package org.fao.geonet.kernel.harvest.harvester.webdav;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.domain.ISODate;
 import org.fao.geonet.kernel.SchemaManager;
 import org.fao.geonet.util.Sha1Encoder;
 import org.fao.geonet.utils.Xml;
 import org.jdom.Element;
+import org.jdom.JDOMException;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-//=============================================================================
-
 class WAFRemoteFile implements RemoteFile {
 
-    private static String outputSchema = "iso19139";
-
-    //---------------------------------------------------------------------------
-    //---
-    //--- Constructor
-    //---
-    //---------------------------------------------------------------------------
-    private String path;
+    private static final String OUTPUT_SCHEMA = "iso19139";
+    private final CloseableHttpClient httpClient;
+    private final String path;
     private ISODate changeDate;
 
-    //---------------------------------------------------------------------------
-    //---
-    //--- RemoteFile interface
-    //---
-    //---------------------------------------------------------------------------
-
-    public WAFRemoteFile(String path) {
+    public WAFRemoteFile(String path, CloseableHttpClient httpClient) {
         this.path = path;
+        this.httpClient = httpClient;
     }
 
-
-    //---------------------------------------------------------------------------
-    //---
-    //--- Get Metadata from GetCapabilities file (Service)
-    //---
-    //---------------------------------------------------------------------------
-
+    /**
+     * Get Metadata from GetCapabilities file (Service).
+     *
+     * @param schemaMan
+     * @return
+     * @throws Exception
+     */
     public Element getMetadata(SchemaManager schemaMan) throws Exception {
         String type = WAFRetriever.getFileType(this.path);
-        if (type.equals(WAFRetriever.type_GetCapabilities))
+        if (WAFRetriever.type_GetCapabilities.equals(type)) {
             return getMdFromService(path, schemaMan);
-        else if (type.equals(WAFRetriever.type_xml))
-            return Xml.loadFile(new URL(path));
-        else
+        } else if (WAFRetriever.type_xml.equals(type)) {
+            return getXmlFromUrl(new URL(path));
+        } else {
             return null;
+        }
+    }
+
+    private Element getXmlFromUrl(URL url) throws IOException, JDOMException {
+        HttpGet request = new HttpGet(url.toString());
+        Element result;
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            if (response.getStatusLine().getStatusCode() / 100 == 2) {// 2xx codes (integer division)
+                HttpEntity entity = response.getEntity();
+                try (InputStream responseInputStream = entity.getContent()) {
+                    result = Xml.loadStream(responseInputStream);
+                } finally {
+                    EntityUtils.consumeQuietly(response.getEntity());
+                }
+            } else {
+                EntityUtils.consumeQuietly(response.getEntity());
+                throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
+            }
+
+        }
+        return result;
     }
 
     private Element getMdFromService(String url, SchemaManager schemaMan) throws Exception {
-        Element el = null;
+        Element el;
         Path styleSheet = getStyleSheet(url, schemaMan);
         if (styleSheet == null) {
             return null;
         }
-        Element xml = Xml.loadFile(new URL(url));
+
+        Element xml = getXmlFromUrl(new URL(url));
 
         // md5 the full capabilities URL
         String uuid = Sha1Encoder.encodeString(url); // is the service identifier
 
-        Map<String, Object> param = new HashMap<String, Object>();
+        Map<String, Object> param = new HashMap<>();
         param.put("uuid", uuid);
 
         el = Xml.transform(xml, styleSheet, param);
@@ -104,10 +123,10 @@ class WAFRemoteFile implements RemoteFile {
         String serviceType = getServiceType(url);
         if (serviceType == null)
             return null;
-        return schemaMan.getSchemaDir(outputSchema).
-            resolve(Geonet.Path.CONVERT_STYLESHEETS).
-            resolve("OGCWxSGetCapabilitiesto19119").
-            resolve("OGC" + serviceType + "GetCapabilities-to-ISO19119_ISO19139.xsl");
+        return schemaMan.getSchemaDir(OUTPUT_SCHEMA).
+                resolve(Geonet.Path.CONVERT_STYLESHEETS).
+                resolve("OGCWxSGetCapabilitiesto19119").
+                resolve("OGC" + serviceType + "GetCapabilities-to-ISO19119_ISO19139.xsl");
     }
 
     //---------------------------------------------------------------------------
@@ -148,6 +167,7 @@ class WAFRemoteFile implements RemoteFile {
     public ISODate getChangeDate() {
         return changeDate;
     }
+
     public void setChangeDate(String date) {
         try {
             changeDate = new ISODate(ISODate.parseBasicOrFullDateTime(date).getMillis());
@@ -165,4 +185,3 @@ class WAFRemoteFile implements RemoteFile {
 
 }
 
-//=============================================================================
