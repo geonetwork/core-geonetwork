b46eacb3c7c35a3569d340173b5288ada2aadbc8
Francois Prunayre <fx.prunayre@gmail.com>
2021-03-08 19:09:11 +0100

    DOI / Add menu and load tasks.

--- web-ui/src/main/resources/catalog/views/sextant/directives/mdactionmenu.js
+++ web-ui/src/main/resources/catalog/views/sextant/directives/mdactionmenu.js
@@ -16,6 +16,43 @@
         link: function linkFn(scope, element, attrs) {
           scope.mdService = gnMetadataActions;
           scope.md = scope.$eval(attrs.sxtMdActionsMenu);
+
+
+          scope.tasks = [];
+          scope.hasVisibletasks = false;
+
+          function loadTasks() {
+            return $http.get('../api/status/task', {cache: true}).
+            success(function(data) {
+              scope.tasks = data;
+              scope.getVisibleTasks();
+            });
+          };
+
+          scope.getVisibleTasks = function() {
+            $.each(scope.tasks, function(i,t) {
+              scope.hasVisibletasks = scope.taskConfiguration[t.name] &&
+                scope.taskConfiguration[t.name].isVisible &&
+                scope.taskConfiguration[t.name].isVisible();
+            });
+          }
+
+          scope.taskConfiguration = {
+            doiCreationTask: {
+              isVisible: function(md) {
+                return gnConfig['system.publication.doi.doienabled'];
+              },
+              isApplicable: function(md) {
+                // TODO: Would be good to return why a task is not applicable as tooltip
+                // TODO: Add has DOI already
+                return md && md.isPublished()
+                  && md.isTemplate === 'n'
+                  && md.isHarvested === 'n';
+              }
+            }
+          };
+
+          loadTasks();
           if (!scope.md) {
             var url = $location.url();
             var uuid = url.substring(url.lastIndexOf('/') + 1);
