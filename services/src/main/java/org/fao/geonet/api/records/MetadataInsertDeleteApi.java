e9f8d2da4b115493d9420f18b3996a537958e4bb
josegar74 <josegar74@gmail.com>
2021-03-31 17:49:11 +0200

    [BP] Generic error  message in 'Upload a file from URL'. Fixes #5535 (#5553)

--- services/src/main/java/org/fao/geonet/api/records/MetadataInsertDeleteApi.java
+++ services/src/main/java/org/fao/geonet/api/records/MetadataInsertDeleteApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2021 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -47,7 +47,19 @@ import org.fao.geonet.api.processing.report.SimpleMetadataProcessingReport;
 import org.fao.geonet.api.records.attachments.Store;
 import org.fao.geonet.api.records.attachments.StoreUtils;
 import org.fao.geonet.constants.Geonet;
-import org.fao.geonet.domain.*;
+
+import org.fao.geonet.domain.AbstractMetadata;
+import org.fao.geonet.domain.ISODate;
+import org.fao.geonet.domain.Metadata;
+import org.fao.geonet.domain.MetadataCategory;
+import org.fao.geonet.domain.MetadataDraft;
+import org.fao.geonet.domain.MetadataResourceVisibility;
+import org.fao.geonet.domain.MetadataType;
+import org.fao.geonet.domain.Pair;
+import org.fao.geonet.domain.Profile;
+import org.fao.geonet.domain.ReservedGroup;
+import org.fao.geonet.domain.ReservedOperation;
+import org.fao.geonet.domain.UserGroup;
 import org.fao.geonet.domain.utils.ObjectJSONUtils;
 import org.fao.geonet.events.history.RecordCreateEvent;
 import org.fao.geonet.events.history.RecordDeletedEvent;
@@ -122,7 +134,10 @@ import static org.springframework.data.jpa.domain.Specifications.where;
 @PreAuthorize("hasRole('Editor')")
 @ReadWriteController
 public class MetadataInsertDeleteApi {
-
+    /**
+     * Logger name.
+     */
+    public static final String LOGGER = Geonet.GEONETWORK + ".api.metadatainsertdelete";
     public static final String API_PARAM_REPORT_ABOUT_IMPORTED_RECORDS = "Report about imported records.";
     public static final String API_PARAM_RECORD_GROUP = "The group the record is attached to.";
     public static final String API_PARAM_RECORD_UUID_PROCESSING = "Record identifier processing.";
@@ -320,7 +335,11 @@ public class MetadataInsertDeleteApi {
                 try {
                     xmlContent = Xml.loadFile(ApiUtils.downloadUrlInTemp(u));
                 } catch (Exception e) {
-                    report.addError(e);
+                    Log.error(LOGGER, String.format("Error importing metadata from '%s'.", url), e);
+                    report.addError(new Exception(
+                        String.format("Failed to import metadata from '%s'. Verify that the URL is correct " +
+                                "and contact your administrator if the problem persists to verify the details of " +
+                                "the error in the log files.", url)));
                 }
                 if (xmlContent != null) {
                     Pair<Integer, String> pair = loadRecord(metadataType, xmlContent, uuidProcessing, group, category,
