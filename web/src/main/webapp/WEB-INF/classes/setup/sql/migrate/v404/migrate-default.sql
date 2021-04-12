98c24049523fe33a86d4fd59ece4dae663c45c28
jit1582 <meetjeetendra@gmail.com>
2021-04-12 15:03:59 +0200

    Configurable sitemap url template for html render

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v404/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v404/migrate-default.sql
@@ -4,6 +4,7 @@ DELETE FROM Schematroncriteria WHERE group_name || group_schematronid IN (SELECT
 DELETE FROM Schematroncriteriagroup WHERE schematronid IN (SELECT id FROM schematron WHERE filename LIKE 'schematron-rules-inspire%');
 DELETE FROM Schematron WHERE filename LIKE 'schematron-rules-inspire%';
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/server/sitemapLinkUrl', NULL, 0, 270, 'y');
 
 UPDATE Settings SET value='4.0.4' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
