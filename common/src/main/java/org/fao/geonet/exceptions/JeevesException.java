d0c4ce2b7596d8f01c325214d58f82a7c16a3334
PRUNAYRE <fx.prunayre@titellus.net>
2021-04-13 14:49:52 +0000

    Update JeevesException.java

--- common/src/main/java/org/fao/geonet/exceptions/JeevesException.java
+++ common/src/main/java/org/fao/geonet/exceptions/JeevesException.java
@@ -78,7 +78,7 @@ public abstract class JeevesException extends RuntimeException {
         Element error = new Element(Constants.ERROR)
             .addContent(new Element("message").setText(
                 msg == null ? 
-                    "" : msg.replaceAll(XML10_ILLEGAL_CHAR_PATTERN, "")))
+                    "" : msg.replaceAll(XML11_ILLEGAL_CHAR_PATTERN, "")))
             .addContent(new Element("class").setText(cls))
             .addContent(getStackTrace(t, 10));
 
