0206e666400574ce013b5c4b34a53ba0e22fb66c
Juan Luis Rodríguez Ponce <juanluisrp@geocat.net>
2021-05-07 08:50:19 +0200

    [BP] Add a new directive for combining add online resource fields (#5559)
    * Add a new directive for combining add online resource fields
    
    Add a new gnMultientryCombinerOnlineResourcesDescription for use in the add online resource
    panel. It allows to show one original field as multiple fields, combining the values of
    these ones back into the original one when the resource is added.
    Check the comments in about the usage.
    
    * Manage the directive option in add resource panel html

--- web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
+++ web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2021 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -349,6 +349,9 @@
               post: function(scope, element, attrs) {
                 scope.clearFormOnProtocolChange = !(attrs.clearFormOnProtocolChange == "false"); //default to true (old behavior)
                 scope.popupid = attrs['gnPopupid'];
+                $(scope.popupid).on('hidden.bs.modal', function(){
+                  scope.$broadcast('onlineSrcDialogHidden', {"popupid": scope.popupid});
+                });
 
                 scope.config = null;
                 scope.linkType = null;
@@ -680,6 +683,7 @@
                       scope.params.desc= '';
                       initMultilingualFields();
                     }
+                    scope.$broadcast('onlineSrcDialogInited', {"popupid": scope.popupid});
                   };
                   function loadConfigAndInit(withInit) {
                     gnSchemaManagerService.getEditorAssociationPanelConfig(
