97cd76d5150755b655d84d7b1b30f2d93b614148
François Prunayre <fx.prunayre@gmail.com>
2021-05-20 08:26:02 +0200

    Map / Update protocol to fix add map to map action
    Protocol used when saving a map as a metadata record was `WWW:DOWNLOAD-OGC:OWS-C` and needs to be `OGC:OWS-C` to be supported by the map viewer.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v405/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v405/migrate-default.sql
@@ -1,2 +1,4 @@
+UPDATE metadata SET data = replace(data, 'WWW:DOWNLOAD-OGC:OWS-C', 'OGC:OWS-C') WHERE data LIKE '%WWW:DOWNLOAD-OGC:OWS-C%';
+
 UPDATE Settings SET value='4.0.5' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
