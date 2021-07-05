4285d2cd3c0fa5c5c2561e4680db5a22dc0c07f4
Francois Prunayre <fx.prunayre@gmail.com>
2021-07-05 14:58:15 +0200

    Record view / Conditional formatter.

--- web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
+++ web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
@@ -79,6 +79,53 @@
         }
       });
 
+      function getFormatterForRecord(record) {
+        console.log('getFormatterForRecord', record);
+        var list = [];
+        if (record == null) {
+          return list;
+        }
+        for (var i = 0; i < gnSearchSettings.formatter.list.length; i ++) {
+          var f = gnSearchSettings.formatter.list[i];
+          if (f.views === undefined) {
+            list.push(f);
+          } else {
+            // Check conditional views
+            var isViewSet = false;
+
+            viewLoop:
+            for (var j = 0; j < f.views.length; j ++) {
+              var v = f.views[j];
+
+              if (v.if) {
+                for (var key in v.if) {
+                  if (v.if.hasOwnProperty(key)) {
+                    var values = angular.isArray(v.if[key])
+                      ? v.if[key]
+                      : [v.if[key]]
+
+                    if (values.includes(record[key])) {
+                      list.push({label: f.label, url: v.url});
+                      isViewSet = true;
+                      break viewLoop;
+                    }
+                  }
+                }
+              } else {
+                console.warn('A conditional view MUST have a if property. ' +
+                  'eg. {"if": {"documentStandard": "iso19115-3.2018"}, "url": "..."}')
+              }
+            }
+            if (f.url !== undefined && !isViewSet) {
+              list.push(f);
+            }
+          }
+        }
+        return list;
+      }
+
+      $scope.recordFormatterList = getFormatterForRecord($scope.mdView.current.record);
+
       $scope.search = function(params) {
         $location.path('/search');
         $location.search(params);
@@ -183,8 +230,13 @@
       // in default mode.
       function loadFormatter(n, o) {
         if (n === true) {
-          var f = gnSearchLocation.getFormatterPath();
+          $scope.recordFormatterList = getFormatterForRecord($scope.mdView.current.record);
+
+          var f = gnSearchLocation.getFormatterPath($scope.recordFormatterList[0].url);
           $scope.currentFormatter = '';
+
+          gnMdViewObj.usingFormatter = f !== undefined;
+
           if (f != undefined) {
             $scope.currentFormatter = f.replace(/.*(\/formatters.*)/, '$1');
             $scope.loadFormatter(f);
--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -526,12 +526,30 @@ goog.require('gn_alert');
           'formatter': {
             'list': [{
               'label': 'defaultView',
+              'views': [ {
+                'if': {
+                  'standardName': 'ISO 19115-3 - Emodnet Checkpoint - Targeted Data Product'
+                },
+                'url' : '/formatters/xsl-view?root=div&view=advanced'
+              }, {
+                'if': {'documentStandard': 'iso19115-3.2018'},
+                'url' : '/dada'
+              }, {
+                'if': {
+                  'standardName': [
+                    'ISO 19115:2003/19139 - EMODNET - BATHYMETRY',
+                    'ISO 19115:2003/19139 - EMODNET - HYDROGRAPHY']
+                },
+                'url' : '/formatters/xsl-view?root=div&view=emodnetHydrography'
+              }],
               'url' : ''
             }, {
               'label': 'full',
-              'url' : '/formatters/xsl-view?root=div&view=advanced'
-            }],
-            defaultUrl: ''
+              'url' : '/formatters/xsl-view?root=div&view=xml'
+            }, {
+              'label': 'extra',
+              'url' : '/main'
+            }]
           },
           'downloadFormatter': [{
             'label': 'exportMEF',
