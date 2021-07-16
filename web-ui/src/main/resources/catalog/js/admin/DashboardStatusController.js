cc0db6fdcd2a42a3b7a00e71cfe3793adeea68ed
François Prunayre <fx.prunayre@gmail.com>
2021-07-16 09:39:22 +0200

    Admin / Status / Fix facet more

--- web-ui/src/main/resources/catalog/js/admin/DashboardStatusController.js
+++ web-ui/src/main/resources/catalog/js/admin/DashboardStatusController.js
@@ -47,8 +47,8 @@
    *
    */
   module.controller('GnDashboardStatusController', [
-    '$scope', '$routeParams', '$http', '$rootScope', '$translate',
-    function($scope, $routeParams, $http, $rootScope, $translate) {
+    '$scope', '$routeParams', '$http', '$rootScope', '$translate', 'gnESFacet',
+    function($scope, $routeParams, $http, $rootScope, $translate, gnESFacet) {
       $scope.healthy = undefined;
       $scope.nowarnings = undefined;
       $scope.threadSortField = undefined;
@@ -278,12 +278,16 @@
       };
       $scope.searchObj = {
         configId: 'recordsWithErrors',
-        params: {
+        defaultParams: {
           'indexingError': true,
           sortBy: 'changeDate',
           sortOrder: 'desc'
         }
       };
+
+      $scope.searchObj.params = angular.extend({},
+        $scope.searchObj.defaultParams);
+      $scope.facetConfig = gnESFacet.configs.recordsWithErrors.facets;
     }]);
 
 })();
