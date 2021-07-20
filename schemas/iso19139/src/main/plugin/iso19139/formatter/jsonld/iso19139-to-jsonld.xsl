geonetwork/core-geonetwork

commit 528a0732ac28f456ce996c8e55ad034e529bfb5b

iso19139 full view - codelist elements - display the codelistValue translation only if the element has a text also

schemas/iso19139/src/main/plugin/iso19139/formatter/xsl-view/view.xsl
@@ -361,7 +361,29 @@
        gco:Angle|gmx:FileName|
        gco:Scale|gco:Record|gco:RecordType|gmx:MimeFileType|gmd:URL|
        gco:LocalName|gmd:PT_FreeText|
-       gco:Date|gco:DateTime|*/@codeListValue]|*[gts:TM_PeriodDuration != '']"
+       gco:Date|gco:DateTime|*[gts:TM_PeriodDuration != '']"
+                priority="50">
+    <xsl:param name="fieldName" select="''" as="xs:string"/>
+
+    <xsl:if test="normalize-space(string-join(*, '')) != ''">
+      <dl>
+        <dt>
+          <xsl:call-template name="render-field-label">
+            <xsl:with-param name="fieldName" select="$fieldName"/>
+            <xsl:with-param name="languages" select="$allLanguages"/>
+          </xsl:call-template>
+        </dt>
+        <dd><xsl:comment select="name()"/>
+          <xsl:apply-templates mode="render-value" select="*|*/@codeListValue"/>
+          <xsl:apply-templates mode="render-value" select="@*"/>
+        </dd>
+      </dl>
+    </xsl:if>
+  </xsl:template>
+
+  <!-- Codelist elements -->
+  <xsl:template mode="render-field"
+                match="*[*/@codeListValue]"
                 priority="50">
     <xsl:param name="fieldName" select="''" as="xs:string"/>
 
@@ -374,7 +396,14 @@
           </xsl:call-template>
         </dt>
         <dd><xsl:comment select="name()"/>
-          <xsl:apply-templates mode="render-value" select="*|*/@codeListValue"/>
+          <xsl:choose>
+            <xsl:when test="normalize-space(*/@codeListValue) != ''">
+              <xsl:apply-templates mode="render-value" select="*/@codeListValue"/>
+            </xsl:when>
+            <xsl:otherwise>
+              <xsl:apply-templates mode="render-value" select="*"/>
+            </xsl:otherwise>
+          </xsl:choose>
           <xsl:apply-templates mode="render-value" select="@*"/>
         </dd>
       </dl>

commit bb71562710abcd656abcbc5f534969fac386c600

ISO19139 / Formatter / Support isoType in template match for plugins (#5853)
* ISO19139 / Support isoType in template match.

This makes life easier for plugin extending iso19139.

Also fix issue introduced in https://github.com/geonetwork/core-geonetwork/commit/528a0732ac28f456ce996c8e55ad034e529bfb5b.

* ISO19139 / Support isoType in template match.

Missing ns.

schemas/iso19139/src/main/plugin/iso19139/formatter/jsonld/iso19139-to-jsonld.xsl
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet 
+<xsl:stylesheet
 	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 	xmlns:gco="http://www.isotc211.org/2005/gco"
 	xmlns:gmd="http://www.isotc211.org/2005/gmd"
@@ -104,26 +104,27 @@
              select="''"/>
 
   <xsl:variable name="defaultLanguage"
-                select="//gmd:MD_Metadata/gmd:language/*/@codeListValue"/>
+                select="//(gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata'])/gmd:language/*/@codeListValue"/>
 
   <!-- TODO: Convert language code eng > en_US ? -->
- 
+
   <xsl:variable name="requestedLanguageExist"
                 select="$lang != ''
-                        and count(//gmd:MD_Metadata/gmd:locale/*[gmd:languageCode/*/@codeListValue = $lang]/@id) > 0"/>
+                        and count(//(gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata'])/gmd:locale/*[gmd:languageCode/*/@codeListValue = $lang]/@id) > 0"/>
 
   <xsl:variable name="requestedLanguage"
                 select="if ($requestedLanguageExist)
                         then $lang
-                        else //gmd:MD_Metadata/gmd:language/*/@codeListValue"/>
+                        else //(gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata'])/gmd:language/*/@codeListValue"/>
 
   <xsl:variable name="requestedLanguageId"
-                select="concat('#', //gmd:MD_Metadata/gmd:locale/*[gmd:languageCode/*/@codeListValue = $requestedLanguage]/@id)"/>
+                select="concat('#', //(gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata'])/gmd:locale/*[gmd:languageCode/*/@codeListValue = $requestedLanguage]/@id)"/>
 
 
 
   <xsl:template name="getJsonLD"
-                mode="getJsonLD" match="gmd:MD_Metadata">
+                mode="getJsonLD"
+                match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
 	{
 		"@context": "http://schema.org/",
     <xsl:variable name="hierarchyLevels"
@@ -155,7 +156,7 @@
     <xsl:for-each select="gmd:identificationInfo/*/gmd:citation/*/gmd:date[gmd:dateType/*/@codeListValue='revision']/*/gmd:date/*/text()">
 		"dateModified": "<xsl:value-of select="."/>",
     </xsl:for-each>
-    
+
 		"thumbnailUrl": [
     <xsl:for-each select="gmd:identificationInfo/*/gmd:graphicOverview/*/gmd:fileName/*[. != '']">
     "<xsl:value-of select="."/>"<xsl:if test="position() != last()">,</xsl:if>
@@ -294,8 +295,8 @@
 
 
     <xsl:if test="count(gmd:identificationInfo/*/gmd:extent/*/gmd:temporalElement/*/gmd:extent[normalize-space(.) != '']) > 0">
-      ,"temporalCoverage": [<xsl:for-each 
-        select="gmd:identificationInfo/*/gmd:extent/*/gmd:temporalElement/*/gmd:extent">"<xsl:value-of 
+      ,"temporalCoverage": [<xsl:for-each
+        select="gmd:identificationInfo/*/gmd:extent/*/gmd:temporalElement/*/gmd:extent">"<xsl:value-of
           select="concat(gml:TimePeriod/gml:beginPosition|gml320:TimePeriod/gml320:beginPosition,'/',
             gml:TimePeriod/gml:endPosition|gml320:TimePeriod/gml320:endPosition
       )"/>"<xsl:if test="position() != last()">,</xsl:if></xsl:for-each> ]
@@ -305,10 +306,10 @@
       "temporalCoverage" : "2013-12-19/.."
       "temporalCoverage" : "2008"
       -->
-    
+
     <!-- array of licenses is allowed, not multiple licenses-->
-    <xsl:if test="count(gmd:identificationInfo/*/gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints[normalize-space(.) != '']) > 0"> 
-      ,"license":  [<xsl:for-each 
+    <xsl:if test="count(gmd:identificationInfo/*/gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints[normalize-space(.) != '']) > 0">
+      ,"license":  [<xsl:for-each
         select="gmd:identificationInfo/*/gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints">
         <xsl:choose>
           <xsl:when test="starts-with(normalize-space(string-join(gco:CharacterString/text(),'')),'http') or starts-with(normalize-space(string-join(gco:CharacterString/text(),'')),'//')">

schemas/iso19139/src/main/plugin/iso19139/formatter/jsonld/view.xsl
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet 
+<xsl:stylesheet
 	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 	xmlns:gmd="http://www.isotc211.org/2005/gmd"
+	xmlns:gco="http://www.isotc211.org/2005/gco"
 	exclude-result-prefixes="#all"
 	version="2.0">
 
@@ -12,7 +13,7 @@
   <xsl:template match="/">
     <textResponse>
       <xsl:apply-templates mode="getJsonLD"
-                           select="*/gmd:MD_Metadata"/>
+                           select="*/(gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata'])"/>
     </textResponse>
   </xsl:template>
 </xsl:stylesheet>

schemas/iso19139/src/main/plugin/iso19139/formatter/xsl-view/view.xsl
@@ -74,7 +74,7 @@
 
   <!-- Define the metadata to be loaded for this schema plugin-->
   <xsl:variable name="metadata"
-                select="/root/gmd:MD_Metadata"/>
+                select="/root/(gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata'])"/>
 
   <xsl:variable name="langId" select="gn-fn-iso19139:getLangId($metadata, $language)"/>
 
@@ -89,7 +89,7 @@
                 priority="2000"/>
 
   <!-- Specific schema rendering -->
-  <xsl:template mode="getMetadataTitle" match="gmd:MD_Metadata">
+  <xsl:template mode="getMetadataTitle" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <xsl:for-each select="gmd:identificationInfo/*/gmd:citation/*/gmd:title">
       <xsl:call-template name="localised">
         <xsl:with-param name="langId" select="$langId"/>
@@ -97,7 +97,7 @@
     </xsl:for-each>
   </xsl:template>
 
-  <xsl:template mode="getMetadataAbstract" match="gmd:MD_Metadata">
+  <xsl:template mode="getMetadataAbstract" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <xsl:for-each select="gmd:identificationInfo/*/gmd:abstract">
 
       <xsl:variable name="txt">
@@ -112,7 +112,7 @@
   </xsl:template>
 
 
-  <xsl:template mode="getTags" match="gmd:MD_Metadata">
+  <xsl:template mode="getTags" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <xsl:param name="byThesaurus" select="false()"/>
 
     <section class="gn-md-side-social">
@@ -179,7 +179,7 @@
     </section>
   </xsl:template>
 
-  <xsl:template mode="getMetadataHierarchyLevel" match="gmd:MD_Metadata">
+  <xsl:template mode="getMetadataHierarchyLevel" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <xsl:value-of select="gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeListValue"/>
   </xsl:template>
 
@@ -187,7 +187,7 @@
     <xsl:value-of select="gmd:identificationInfo/*/gmd:graphicOverview[1]/gmd:MD_BrowseGraphic/gmd:fileName/gco:CharacterString"/>
   </xsl:template>
 
-  <xsl:template mode="getExtent" match="gmd:MD_Metadata">
+  <xsl:template mode="getExtent" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <section class="gn-md-side-extent">
       <h2>
         <i class="fa fa-fw fa-map-marker"><xsl:comment select="'image'"/></i>
@@ -209,7 +209,7 @@
     </section>
   </xsl:template>
 
-  <xsl:template mode="getOverviews" match="gmd:MD_Metadata">
+  <xsl:template mode="getOverviews" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <section class="gn-md-side-overview">
       <h2>
         <i class="fa fa-fw fa-image"><xsl:comment select="'image'"/></i>
@@ -236,7 +236,7 @@
     </section>
   </xsl:template>
 
-  <xsl:template mode="getMetadataHeader" match="gmd:MD_Metadata">
+  <xsl:template mode="getMetadataHeader" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <div class="gn-abstract">
       <xsl:for-each select="gmd:identificationInfo/*/gmd:abstract">
         <xsl:variable name="txt">
@@ -259,7 +259,7 @@
   </xsl:template>
 
 
-  <xsl:template mode="getMetadataCitation" match="gmd:MD_Metadata">
+  <xsl:template mode="getMetadataCitation" match="gmd:MD_Metadata|*[@gco:isoType = 'gmd:MD_Metadata']">
     <xsl:variable name="displayCitation"
                   select="true()"/>
     <xsl:variable name="doiUrl"
@@ -361,7 +361,7 @@
        gco:Angle|gmx:FileName|
        gco:Scale|gco:Record|gco:RecordType|gmx:MimeFileType|gmd:URL|
        gco:LocalName|gmd:PT_FreeText|
-       gco:Date|gco:DateTime|*[gts:TM_PeriodDuration != '']"
+       gco:Date|gco:DateTime|gts:TM_PeriodDuration != '']"
                 priority="50">
     <xsl:param name="fieldName" select="''" as="xs:string"/>
 