c84497a3d127209ee00617541a6054c2c720ea59
Francois Prunayre <fx.prunayre@gmail.com>
2021-08-02 17:21:20 +0200

    Record view / Refactor getFormatterForRecord and add support to get property by path eg. standardName.default.

--- web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
+++ web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
@@ -129,7 +129,8 @@
         return list;
       }
 
-      $scope.recordFormatterList = getFormatterForRecord($scope.mdView.current.record);
+      $scope.recordFormatterList =
+        gnMdFormatter.getFormatterForRecord($scope.mdView.current.record);
 
       $scope.search = function(params) {
         $location.path('/search');
@@ -235,7 +236,8 @@
       // in default mode.
       function loadFormatter(n, o) {
         if (n === true) {
-          $scope.recordFormatterList = getFormatterForRecord($scope.mdView.current.record);
+          $scope.recordFormatterList =
+            gnMdFormatter.getFormatterForRecord($scope.mdView.current.record);
 
           var f = gnSearchLocation.getFormatterPath($scope.recordFormatterList[0].url);
           $scope.currentFormatter = '';
