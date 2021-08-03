9ebd0ff4c16748820abb699b0d7dbe41a279ea81
Francois Prunayre <fx.prunayre@gmail.com>
2021-08-03 18:42:09 +0200

    INSPIRE / Validation from a portal endpoint.
    More and more often, end user define a custom INSPIRE portal for being harvested by INSPIRE geoportal. The INSPIRE portal first filter records in the scope of the Directive and may also define post processing to adapt record encoding to INSPIRE rules (see. CSW post processing https://github.com/geonetwork/core-geonetwork/pull/4493).
    
    If the nodeid parameter is not set, then the record XML is sent to the validator for validation. If defined, then the GetRecordById URL of the record in this node is sent to the validator which retrieve the XML (see https://github.com/geonetwork/core-geonetwork/pull/5227).
    
    Also add error message in case of validator return an empty token.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v406/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v406/migrate-default.sql
@@ -1,2 +1,4 @@
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/remotevalidation/nodeid', '', 0, 7212, 'n');
+
 UPDATE Settings SET value='4.0.6' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
