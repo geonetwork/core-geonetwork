1e34fce0443a0e3eff498c30fae4ddb9026af0eb
François Prunayre <fx.prunayre@gmail.com>
2021-08-24 13:15:27 +0200

    Admin / Languages and translations (#2)
    * Admin / Languages ui draft.
    
    * API / Language / Fix delete operation due to DB changes.
    
    * API / Languages / Add operation to list application languages.
    
    Those that can be initialized with and SQL scripts. Also fix PUT operation after JPA migration.
    
    * Admin / Language / Add panel to manage db languages.
    
    For end user, it is recommended to remove unused languages to keep admin form as simple as possible.
    
    * SQL / French / Capital letter ?
    
    * Admin / Languages / Don't allow deletion of last language.
    
    * Admin / Translations / Draft ui.
    
    * Admin / Translations / API improvements.
    
    * Admin / Translations / UI.
    
    * Admin / Translations / Conflict.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/template/language-delete.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/template/language-delete.sql
@@ -1,8 +1,9 @@
-DELETE FROM CswServerCapabilitiesInfo WHERE langid = '%s';
 DELETE FROM CategoriesDes WHERE langid = '%s';
 DELETE FROM GroupsDes WHERE langid = '%s';
 DELETE FROM IsoLanguagesDes WHERE langid = '%s';
 DELETE FROM OperationsDes WHERE langid = '%s';
 DELETE FROM StatusValuesDes WHERE langid = '%s';
 DELETE FROM SourcesDes WHERE langid = '%s';
+DELETE FROM SelectionsDes WHERE langid = '%s';
+DELETE FROM GUF_RatingCriteriaDes WHERE langid = '%s';
 DELETE FROM Languages WHERE id = '%s';
