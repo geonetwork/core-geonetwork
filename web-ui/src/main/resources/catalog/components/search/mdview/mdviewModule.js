3f7c22aec098c4c1d3f331f9af3869878ce0ae6e
François Prunayre <fx.prunayre@gmail.com>
2021-08-24 13:20:19 +0200

    Record view / Keywords sort (#7)
    * Metadata detail page - sort keywords / topic categories
    
    * Metadata detail page - sort GeoNetwork categories
    
    Co-authored-by: Jose García <josegar74@gmail.com>

--- web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
+++ web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
@@ -47,11 +47,11 @@
     '$scope', '$http', '$compile', 'gnSearchSettings', 'gnSearchLocation',
     'gnMetadataActions', 'gnAlertService', '$translate', '$location',
     'gnMdView', 'gnMdViewObj', 'gnMdFormatter', 'gnConfig',
-    'gnGlobalSettings', 'gnConfigService', '$rootScope',
+    'gnGlobalSettings', 'gnConfigService', '$rootScope', '$filter',
     function($scope, $http, $compile, gnSearchSettings, gnSearchLocation,
              gnMetadataActions, gnAlertService, $translate, $location,
              gnMdView, gnMdViewObj, gnMdFormatter, gnConfig,
-             gnGlobalSettings, gnConfigService, $rootScope) {
+             gnGlobalSettings, gnConfigService, $rootScope, $filter) {
 
       $scope.formatter = gnSearchSettings.formatter;
       $scope.gnMetadataActions = gnMetadataActions;
@@ -63,6 +63,7 @@
       $scope.isSocialbarEnabled = gnGlobalSettings.gnCfg.mods.recordview.isSocialbarEnabled;
       $scope.showStatusWatermarkFor = gnGlobalSettings.gnCfg.mods.recordview.showStatusWatermarkFor;
       $scope.showStatusTopBarFor = gnGlobalSettings.gnCfg.mods.recordview.showStatusTopBarFor;
+      $scope.sortKeywordsAlphabetically = gnGlobalSettings.gnCfg.mods.recordview.sortKeywordsAlphabetically;
 
       gnConfigService.load().then(function(c) {
         $scope.isRecordHistoryEnabled = gnConfig['system.metadata.history.enabled'];
@@ -251,6 +252,10 @@
         }
       }
       $scope.$watch('mdView.recordsLoaded', loadFormatter);
+
+      $scope.sortByCategory = function(cat) {
+        return $filter('translate')('cat-' + cat);
+      };
 
       // Know from what path we come from
       $scope.gnMdViewObj = gnMdViewObj;
--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -679,7 +679,8 @@ goog.require('gn_alert');
         'recordview': {
           'isSocialbarEnabled': true,
           'showStatusWatermarkFor': 'historicalArchive,obsolete,superseded',
-          'showStatusTopBarFor': ''
+          'showStatusTopBarFor': '',
+          'sortKeywordsAlphabetically': true
         },
         'editor': {
           'enabled': true,
