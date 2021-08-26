71cf63430ec64305cdf89b2d05c0da1a288299c5
Francois Prunayre <fx.prunayre@gmail.com>
2021-08-26 18:28:17 +0200

    Harvester / CSW / Add sortby option.
    This allows to fix issue while harvesting large set of records
    and when record may changed on remote node, they may appear in various
    pages (eg. a GetRecordById might increase popularity in GeoNework and
    the record is reindex which may alter its position when no criteria or
    sort options are defined).
    
    Fixing the order helps solving this issue.

--- harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Harvester.java
+++ harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Harvester.java
@@ -205,6 +205,7 @@ class Harvester implements IHarvester<HarvestResult> {
         return true;
     }
 
+
     /**
      * Does CSW GetRecordsRequest
      * @param server
@@ -221,6 +222,9 @@ class Harvester implements IHarvester<HarvestResult> {
 
         request.setResultType(ResultType.RESULTS);
         //request.setOutputSchema(OutputSchema.OGC_CORE);	// Use default value
+        if (StringUtils.isNotEmpty(params.sortBy)) {
+            request.addSortBy(params.sortBy);
+        }
         request.setElementSetName(ElementSetName.SUMMARY);
         request.setMaxRecords(GETRECORDS_REQUEST_MAXRECORDS);
         request.setDistribSearch(params.queryScope.equalsIgnoreCase("distributed"));
