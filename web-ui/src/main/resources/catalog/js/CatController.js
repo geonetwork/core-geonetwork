8b3020121f544bfd816f5bcec3a62ce68342af56
François Prunayre <fx.prunayre@gmail.com>
2021-09-08 16:31:53 +0200

    Record view / Fix full view formatter path.

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -570,7 +570,7 @@ goog.require('gn_alert');
               'url' : ''
             }, {
               'label': 'full',
-              'url' : '/formatters/xsl-view?root=div&view=xml'
+              'url' : '/formatters/xsl-view?root=div&view=advanced'
             }]
           },
           'downloadFormatter': [{
