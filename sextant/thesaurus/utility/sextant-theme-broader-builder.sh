5faa973ae80c0c44477868fe6ca520fcacfee22d
PRUNAYRE <fx.prunayre@titellus.net>
2021-09-17 07:42:40 +0000

    Update sextant-theme-broader-builder.sh

--- sextant/thesaurus/utility/sextant-theme-broader-builder.sh
+++ sextant/thesaurus/utility/sextant-theme-broader-builder.sh
@@ -2,6 +2,6 @@
 
 for thesaurus in sextant-theme.rdf mission-atlantic-bodc-parameters.rdf mission-atlantic-odemm.rdf odatis_variables.rdf type_jeux_donnee.rdf
 do
-#  cp ../../../web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/theme/$thesaurus $thesaurus
+  cp ../../../web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/theme/$thesaurus $thesaurus
   java -jar ~/.m2/repository/net/sf/saxon/saxon/9.1.0.8b-patch/saxon-9.1.0.8b-patch.jar -s:$thesaurus -xsl:sextant-theme-broader-builder.xsl -o:../../../web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/theme/$thesaurus
 done
