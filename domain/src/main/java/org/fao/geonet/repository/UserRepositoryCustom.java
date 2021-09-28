06cf66e4fa4df3b7f1f025f8d0f4654d1c41b288
Jose García <josegar74@gmail.com>
2021-09-28 11:42:21 +0200

    Fix workflow mail notifications for metadata with a working copy (#5971)

--- domain/src/main/java/org/fao/geonet/repository/UserRepositoryCustom.java
+++ domain/src/main/java/org/fao/geonet/repository/UserRepositoryCustom.java
@@ -57,13 +57,11 @@ public interface UserRepositoryCustom {
      *
      * @param metadataIds the metadataIds of the metadata to inspect.
      * @param profile     if non-null then filter the users by the given profile.
-     * @param sort        if non-null then Sort the results by the <em>User</em> property.  The sort
-     *                    object must contain user properties only.
      * @return all the users that are part of the ownerGroup of a particular set of metadata.
      */
     @Nonnull
     List<Pair<Integer, User>> findAllByGroupOwnerNameAndProfile(@Nonnull Collection<Integer> metadataIds,
-                                                                @Nullable Profile profile, @Nullable Sort sort);
+                                                                @Nullable Profile profil);
 
     /**
      * Find all the users that own at least one metadata element.
