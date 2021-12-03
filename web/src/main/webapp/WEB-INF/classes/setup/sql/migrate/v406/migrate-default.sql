a7d8c6488138373eb2c03c5911f72efbd7c079ce
Juan Luis Rodríguez <juanluisrp@gmail.com>
2021-12-03 12:30:03 +0100

    Add missing translations for the doiCreationTask status
    The statusvalue 100 - doiCreationTask doesn't have a translation in the DB if the database has
    been created before GeoNetwork v10.5.0. This migration add the missing translation fixing an
    option without label in the Manage Records menu in the record view.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v406/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v406/migrate-default.sql
@@ -5,3 +5,23 @@ DELETE FROM Settings WHERE name = 'system/server/securePort';
 
 UPDATE Settings SET value='4.0.6' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+-- Add missing translation for StatusValue 100 - doiCreationTask
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'tur','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'ara','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'ger','DOI-Erstellungsanfrage');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'pol','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'rus','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'nor','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'swe','Begäran att skapa DOI');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'fre','Demande de création de DOI');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'fin','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'vie','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'eng','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'chi','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'dut','DOI aanvragen');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'ita','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'slo','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'cat','DOI creation request');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'spa','Solicitud de creación de DOI');
+INSERT INTO StatusValuesDes  (iddes, langid, label) VALUES (100,'por','DOI creation request');
