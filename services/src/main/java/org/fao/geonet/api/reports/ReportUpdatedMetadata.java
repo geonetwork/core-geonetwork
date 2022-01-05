ee2fd5bb3eea1a76394bb7e343e257bc3f83fe0e
Jose García <josegar74@gmail.com>
2022-01-05 07:33:11 +0100

    [BP] Metadata update and internal reports updates (#6082)
    * Metadata update and internal reports:
    - Add metadata change date to the report.
    - Exclude metadata templates
    
    * Fix column header
    
    Co-authored-by: Juan Luis Rodríguez <juanluisrp@gmail.com>

--- services/src/main/java/org/fao/geonet/api/reports/ReportUpdatedMetadata.java
+++ services/src/main/java/org/fao/geonet/api/reports/ReportUpdatedMetadata.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2001-2022 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
 package org.fao.geonet.api.reports;
 
 import java.io.PrintWriter;
@@ -28,7 +51,7 @@ public class ReportUpdatedMetadata implements IReport {
     private ReportFilter reportFilter;
 
     /**
-     *  Creates a report for metadata file uploads instance.
+     * Creates a report for metadata file uploads instance.
      *
      * @param filter report filter.
      */
@@ -40,8 +63,8 @@ public class ReportUpdatedMetadata implements IReport {
      * Creates the users report and streams to a PrintWriter.
      *
      * @param context Service context.
-     * @param writer Writer.
-     * @throws Exception  Exception creating a report.
+     * @param writer  Writer.
+     * @throws Exception Exception creating a report.
      */
     public void create(final ServiceContext context,
                        final PrintWriter writer) throws Exception {
@@ -55,13 +78,13 @@ public class ReportUpdatedMetadata implements IReport {
 
             // Retrieve metadata
             final IMetadataUtils metadataRepository =
-                    context.getBean(IMetadataUtils.class);
+                context.getBean(IMetadataUtils.class);
             final List<? extends AbstractMetadata> records =
-                    metadataRepository.getMetadataReports().
-                            getUpdatedMetadata(
-                                    reportFilter.getBeginDate(),
-                                    reportFilter.getEndDate(),
-                                    reportFilter.getGroups());
+                metadataRepository.getMetadataReports().
+                    getUpdatedMetadata(
+                        reportFilter.getBeginDate(),
+                        reportFilter.getEndDate(),
+                        reportFilter.getGroups());
 
             //initialize CSVPrinter object
             csvFilePrinter = new CSVPrinter(writer, csvFileFormat);
@@ -71,15 +94,15 @@ public class ReportUpdatedMetadata implements IReport {
             csvFilePrinter.println();
 
             String[] entries = (
-                    "Metadata ID#Metadata Title#Metadata Create date#"
+                "Metadata ID#Metadata Title#Metadata Create date#Metadata Update date#"
                     + "Owner Username#Owner Surname#Owner Name#Owner Email#"
                     + "Group Owner#Group Owner Email").split("#");
             csvFilePrinter.printRecord(Arrays.asList(entries));
 
             List<User> users =
-                    context.getBean(UserRepository.class).findAll();
+                context.getBean(UserRepository.class).findAll();
             List<Group> groups =
-                    context.getBean(GroupRepository.class).findAll();
+                context.getBean(GroupRepository.class).findAll();
 
             // Process the records
             for (AbstractMetadata metadata : records) {
@@ -91,55 +114,56 @@ public class ReportUpdatedMetadata implements IReport {
                 String groupOwnerMail = "";
 
                 Integer mdUserOwner =
-                        metadata.getSourceInfo().getOwner();
+                    metadata.getSourceInfo().getOwner();
                 if (mdUserOwner != null) {
                     Optional<User> userOwnerFilter = users.stream().filter(
                             u -> u.getId() == mdUserOwner)
-                            .findFirst();
+                        .findFirst();
 
                     if (userOwnerFilter.isPresent()) {
                         User userOwner = userOwnerFilter.get();
 
                         userOwnerUsername = Optional.ofNullable(
-                                userOwner.getUsername()).orElse("");
+                            userOwner.getUsername()).orElse("");
                         userOwnerName = Optional.ofNullable(
-                                userOwner.getName()).orElse("");
+                            userOwner.getName()).orElse("");
                         userOwnerSurname = Optional.ofNullable(
-                                userOwner.getSurname()).orElse("");
+                            userOwner.getSurname()).orElse("");
                         userOwnerMail = Optional.ofNullable(
-                                userOwner.getEmail()).orElse("");
+                            userOwner.getEmail()).orElse("");
                     }
                 }
 
                 Integer mdGroupOwner =
-                        metadata.getSourceInfo().getGroupOwner();
+                    metadata.getSourceInfo().getGroupOwner();
                 if (mdGroupOwner != null) {
                     Optional<Group> groupOwnerFilter = groups.stream().filter(
                             g -> g.getId() == mdGroupOwner)
-                            .findFirst();
+                        .findFirst();
                     if (groupOwnerFilter.isPresent()) {
                         Group groupOwner = groupOwnerFilter.get();
 
                         String groupNameTranslation =
-                                groupOwner.getLabelTranslations().get(
-                                        context.getLanguage());
+                            groupOwner.getLabelTranslations().get(
+                                context.getLanguage());
                         groupOwnerName =
-                                Optional.ofNullable(groupNameTranslation)
-                                        .orElse(groupOwner.getName());
+                            Optional.ofNullable(groupNameTranslation)
+                                .orElse(groupOwner.getName());
 
                         groupOwnerMail = Optional.ofNullable(
-                                groupOwner.getEmail()).orElse("");
+                            groupOwner.getEmail()).orElse("");
                     }
                 }
 
                 String mdTitle = ReportUtils.retrieveMetadataTitle(
-                        context, metadata.getId());
+                    context, metadata.getId());
 
                 // Build the record element with the information for the report
                 List<String> record = new ArrayList<>();
                 record.add(metadata.getUuid());
                 record.add("" + mdTitle);
                 record.add("" + metadata.getDataInfo().getCreateDate());
+                record.add("" + metadata.getDataInfo().getChangeDate());
                 record.add(userOwnerUsername);
                 record.add(userOwnerSurname);
                 record.add(userOwnerName);
