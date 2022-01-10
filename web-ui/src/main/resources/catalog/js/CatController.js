e83a90e1242a3f5d4569e90c3682c93177b46d64
Francois Prunayre <fx.prunayre@gmail.com>
2022-01-10 11:33:41 +0100

    Facet / Publication group / Translate group properly.

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -752,9 +752,9 @@ goog.require('gn_alert');
                 'size': 10
               }
             },
-            'groupPublished': {
+            'groupPublishedId': {
               'terms': {
-                'field': 'groupPublished',
+                'field': 'groupPublishedId',
                 'size': 10
               }
             },
