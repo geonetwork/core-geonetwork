778e73ec5dff1665bf818914a4be7cfa5a77c44a
Jose García <josegar74@gmail.com>
2022-01-20 16:58:26 +0100

    [BP] DOI - add identifier / online resource url using the DOI proxy url (#6124)
    * DOI - add identifier / online resource url using the DOI proxy url (https://doi.org/)
    * DOI / Add public URL settings.
    
    Before the API url was used instead of the default https://doi.org prefix.
    Keep the setting empty to use the default. When using the test API, user can set a custom value.
    
    Backport of https://github.com/geonetwork/core-geonetwork/pull/6124
    
    Co-authored-by: Francois Prunayre <fx.prunayre@gmail.com>

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3123/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3123/migrate-default.sql
@@ -1,2 +1,5 @@
 UPDATE Settings SET value='3.12.3' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+-- New setting for DOI public URL
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/publication/doi/doipublicurl', '', 0, 100196, 'n');
