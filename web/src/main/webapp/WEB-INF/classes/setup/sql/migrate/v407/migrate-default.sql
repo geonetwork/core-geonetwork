246832a48555b842cc4f2435e32ba8c906304e76
Jose García <josegar74@gmail.com>
2022-02-12 10:47:42 +0100

    [FP] Support for non-editable settings

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v407/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v407/migrate-default.sql
@@ -16,3 +16,6 @@ INSERT INTO UserAddress (userid, addressid) VALUES  (0, 0);
 
 UPDATE Settings SET value='4.0.7' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+UPDATE Settings SET editable = 'n' WHERE name = 'system/userFeedback/lastNotificationDate';
+UPDATE Settings SET editable = 'n' WHERE name = 'system/security/passwordEnforcement/pattern';
