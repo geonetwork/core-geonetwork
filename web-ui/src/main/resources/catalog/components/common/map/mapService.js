geonetwork/core-geonetwork

commit 0d23940e0d36b0a0cf48fcca39fbd77bf708a047

Map / Layer / Preserve link to metadata record.
When saving or restoring map, the layer is not linked anymore to the corresponding metadata record.
Adding a uuid and label in the OWS context layer extension to preserve this link.
If not preserved, then layer contextual menu has no link to metadata and WFS filter directive does not work.

web-ui/src/main/resources/catalog/components/common/map/mapService.js
@@ -57,7 +57,7 @@
       '$q',
       '$translate',
       'gnWmsQueue',
-      'gnSearchManagerService',
+      'gnMetadataManager',
       'Metadata',
       'gnWfsService',
       'gnGlobalSettings',
@@ -68,7 +68,7 @@
       'gnEsriUtils',
       function(olDecorateLayer, gnOwsCapabilities, gnConfig, $log,
           gnSearchLocation, $rootScope, gnUrlUtils, $q, $translate,
-          gnWmsQueue, gnSearchManagerService, Metadata, gnWfsService,
+          gnWmsQueue, gnMetadataManager, Metadata, gnWfsService,
           gnGlobalSettings, gnViewerSettings, gnViewerService, gnAlertService,
           $http, gnEsriUtils) {
 
@@ -1401,10 +1401,14 @@
                         });
                   };
 
-                  var feedMdPromise = md ?
+                  var feedMdPromise =
+                    typeof md === 'object' ?
                     $q.resolve(md).then(function(md) {
                       olL.set('md', md);
-                    }) : $this.feedLayerMd(olL);
+                    }) : (
+                      typeof md === 'string'
+                        ? $this.feedLayerMd(olL, md)
+                        : $this.feedLayerMd(olL));
 
                   feedMdPromise.then(finishCreation);
                 }
@@ -2040,22 +2044,16 @@
            *
            * @param {ol.Layer} layer to feed
            */
-          feedLayerMd: function(layer) {
+          feedLayerMd: function(layer, uuid) {
             var defer = $q.defer();
             var $this = this;
 
             defer.resolve(layer);
 
-            if (layer.get('metadataUrl') && layer.get('metadataUuid')) {
-
-              return gnSearchManagerService.gnSearch({
-                uuid: layer.get('metadataUuid'),
-                fast: 'index',
-                draft: 'n or e',
-                _content_type: 'json'
-              }).then(function(data) {
-                if (data.metadata.length == 1) {
-                  var md = new Metadata(data.metadata[0]);
+            if (layer.get('metadataUuid') || uuid) {
+              return gnMetadataManager.getMdObjByUuid(layer.get('metadataUuid') || uuid)
+                .then(function(md) {
+                if (md) {
                   layer.set('md', md);
 
                   var mdLinks = md.getLinksByType('#OGC:WMTS',

web-ui/src/main/resources/catalog/components/viewer/owscontext/OwsContextService.js
@@ -278,42 +278,47 @@
               var currentStyle;
 
               // load extension content (JSON)
-              if (layer.extension && layer.extension.any) {
-                var extension = JSON.parse(layer.extension.any);
-                var loadingId = layer.name;
-                if (extension.style) {
-                  currentStyle = {Name: extension.style};
-                  loadingId += ' ' + extension.style;
-                }
+              var extension = layer.extension && layer.extension.any ? JSON.parse(layer.extension.any) : {};
 
-                // import saved filters if available
-                if (extension.filters && extension.wfsUrl) {
-                  var url = extension.wfsUrl;
-
-                  // get ES object and save filters on it
-                  // (will be used by the WfsFilterDirective
-                  // when initializing)
-                  var esObj =
-                      wfsFilterService.registerEsObject(url, layer.name);
-                  esObj.initialFilters = extension.filters;
-                }
-
-                // this object holds the WPS input values
-                var defaultInputs = extension.processInputs || {};
+              var loadingId = extension.label ? extension.label : layer.name;
+              if (extension.style) {
+                currentStyle = {Name: extension.style};
+                loadingId += ' ' + extension.style;
               }
+
+              // import saved filters if available
+              if (extension.filters && extension.wfsUrl) {
+                var url = extension.wfsUrl;
+
+                // get ES object and save filters on it
+                // (will be used by the WfsFilterDirective
+                // when initializing)
+                var esObj =
+                    wfsFilterService.registerEsObject(url, layer.name);
+                esObj.initialFilters = extension.filters;
+              }
+
+              // this object holds the WPS input values
+              var defaultInputs = extension.processInputs || {};
 
               // create WMS layer
               if (server.service == 'urn:ogc:serviceType:WMS') {
-
                 var loadingLayer = new ol.layer.Image({
                   loading: true,
-                  label: loadingId || 'loading',
+                  label: loadingId + '...',
                   url: '',
                   visible: false,
                   group: layer.group
                 });
 
                 loadingLayer.displayInLayerManager = true;
+
+                if (extension.label) {
+                  layer.title = extension.label;
+                }
+                if (extension.uuid)  {
+                  layer.metadataUuid = extension.uuid
+                }
 
                 var layerIndex = map.getLayers().push(loadingLayer) - 1;
                 var p = self.createLayer(layer, map, undefined, i, currentStyle);
@@ -527,27 +532,30 @@
 
 
           // apply filters & processes inputs in extension if needed
-          if (layer.get('currentStyle') || filters || processInputs) {
-            var extension = {};
+          var extension = {};
 
-            if (layer.get('currentStyle')) {
-              extension.style = layer.get('currentStyle').Name;
-            }
-
-            if (esObj) {
-              var wfsUrl = esObj.config.params.wfsUrl;
-              if (wfsUrl) {
-                extension.filters = filters;
-                extension.wfsUrl = wfsUrl;
-              }
-            }
-            if (processInputs) { extension.processInputs = processInputs; }
-
-            layerParams.extension = {
-              name: 'Extension',
-              any: JSON.stringify(extension)
-            };
+          if (layer.get('md') && layer.get('md')._id) {
+            extension.uuid = layer.get('md')._id;
+            extension.label = layer.get('label');
           }
+
+          if (layer.get('currentStyle')) {
+            extension.style = layer.get('currentStyle').Name;
+          }
+
+          if (esObj) {
+            var wfsUrl = esObj.config.params.wfsUrl;
+            if (wfsUrl) {
+              extension.filters = filters;
+              extension.wfsUrl = wfsUrl;
+            }
+          }
+          if (processInputs) { extension.processInputs = processInputs; }
+
+          layerParams.extension = {
+            name: 'Extension',
+            any: JSON.stringify(extension)
+          };
 
           resourceList.layer.push(layerParams);
         });
@@ -646,6 +654,7 @@
               olL.set('title', layer.title);
               olL.set('label', layer.title);
             }
+            olL.set('metadataUuid', layer.metadataUuid || '');
             if (bgIdx) {
               olL.set('bgIdx', bgIdx);
             } else if (index) {
@@ -659,7 +668,7 @@
           // even when loaded from a context.
           return gnMap.addWmsFromScratch(
               map, res.href, layer.name,
-              createOnly, null, server.version, style).
+              createOnly, layer.metadataUuid || null, server.version, style).
               then(function(olL) {
                 if (olL) {
                   try {
@@ -676,6 +685,7 @@
                   var title =  layer.title ? layer.title : olL.get('label');
                   olL.set('title', title || '');
                   olL.set('label', title || '');
+                  olL.set('metadataUuid', layer.metadataUuid || '');
                   $rootScope.$broadcast('layerAddedFromContext', olL);
                   return olL;
                 }

web-ui/src/main/resources/catalog/components/viewer/wfsfilter/WfsFilterDirective.js
@@ -283,7 +283,11 @@
            * @return {HttpPromise} promise
            */
           scope.checkWFSServerUrl = function() {
-            return $http.get(scope.url)
+            var url = scope.url;
+            if (url.indexOf('GetCapabilities') === -1) {
+              url = url + (url.indexOf('?') === -1 ? '?' : '&') + 'request=GetCapabilities';
+            }
+            return $http.get(url)
               .then(function() {
                 scope.isWfsAvailable = true;
               }, function() {

web-ui/src/main/resources/catalog/components/viewer/wfsfilter/partials/wfsfilterfacet.html
@@ -105,14 +105,14 @@
               data-ng-click="initIndexRequest()">
         <i class="fa fa-refresh"></i>
       </button>
-      <button data-ng-if="user.canEditRecord(md) || md.isHarvested == 'y'"
+      <button data-ng-if="user.canEditRecord(md) || md.isHarvested == 'true'"
               type="button" class="btn btn-default btn-xs dropdown-toggle"
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fa fa-cog"></i>
         <span class="caret"></span>
       </button>
       <ul class="dropdown-menu" aria-labelledby="dropdownMenu1"
-          data-ng-if="user.canEditRecord(md) || md.isHarvested == 'y'">
+          data-ng-if="user.canEditRecord(md) || md.isHarvested == 'true'">
         <li>
           <a href="" data-ng-click="indexWFSFeatures('1.1.0')">
             <i class="fa fa-pie-chart"></i>&nbsp;

commit 0b0558dbd5860ff2764198ba9b137ae6cf5c2b91

Map / call feedLayerMd when only a md uuid is present
This fixes a regression in https://github.com/geonetwork/core-geonetwork/pull/5307
where a layer with a metadata uuid (i.e. whose metadata was found in the catalog)
was not going through feedLayerMd and as such did not have a link to the
md record

web-ui/src/main/resources/catalog/components/common/map/mapService.js
@@ -1560,7 +1560,7 @@
                   };
 
                   var feedMdPromise =
-                    typeof md === 'object' ?
+                    md && typeof md === 'object' ?
                     $q.resolve(md).then(function(md) {
                       olL.set('md', md);
                     }) : (