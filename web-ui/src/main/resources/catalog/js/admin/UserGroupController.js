65eb2ccfa0e24bd8d7ff7ac82540d4005e0f48b1
davinciagf <arnaud.degroof@spacebel.be>
2022-02-25 12:29:34 +0100

    Fix typo aiming to hide reset password button for LDAP's users

--- web-ui/src/main/resources/catalog/js/admin/UserGroupController.js
+++ web-ui/src/main/resources/catalog/js/admin/UserGroupController.js
@@ -344,7 +344,7 @@
           return true;
         }
 
-        return (($scope.userSelected.security.authtype == 'LDAP') ||
+        return (($scope.userSelected.security.authType == 'LDAP') ||
             ($scope.userSelected.username !== $scope.user.username));
       };
 
