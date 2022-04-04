0d1bd4b728ee71166cd163fc625a60fb68f348b7
Francois Prunayre <fx.prunayre@gmail.com>
2022-04-04 16:49:03 +0200

    Sextant / Thesaurus utility / Add possibility to process external thesaurus.
    Script can now use the same XSLT as in the UI.
    https://gitlab.ifremer.fr/sextant/geonetwork/-/issues/487.

--- sextant/thesaurus/utility/thesaurus-build-hierarchy-from-slash-label.sh
+++ sextant/thesaurus/utility/thesaurus-build-hierarchy-from-slash-label.sh
@@ -1,10 +1,12 @@
-#xsltproc -o test.rdf sextant-theme-broader-builder.xsl /data/dev/gn/sextant/web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/theme/sextant-theme.rdf
+#xsltproc -o test.rdf sextant-theme-broader-builder.xsl /data/dev/gn/sextant/web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/thesauri/theme/sextant-theme.rdf
 
-#for thesaurus in place/dcsmm.area.rdf
-#for thesaurus in place/dcsmm.area.rdf theme/simm.thematiques.rdf
-for thesaurus in theme/sextant-theme.rdf theme/mission-atlantic-bodc-parameters.rdf theme/mission-atlantic-odemm.rdf theme/odatis_variables.rdf theme/type_jeux_donnee.rdf place/dcsmm.area.rdf theme/simm.thematiques.rdf
+#for thesaurus in local/thesauri/place/dcsmm.area.rdf
+#for thesaurus in local/thesauri/place/dcsmm.area.rdf local/thesauri/theme/simm.thematiques.rdf
+#for thesaurus in local/thesauri/theme/sextant-theme.rdf local/thesauri/theme/mission-atlantic-bodc-parameters.rdf local/thesauri/theme/mission-atlantic-odemm.rdf local/thesauri/theme/odatis_variables.rdf local/thesauri/theme/type_jeux_donnee.rdf place/dcsmm.area.rdf local/thesauri/theme/simm.thematiques.rdf
+for thesaurus in external/thesauri/theme/GCMDparameter.rdf
 do
-  THESAURUSFILE=`cut -d "/" -f2 <<< "$thesaurus"`
-  #cp ../../../web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/$thesaurus $THESAURUSFILE
-  java -jar ~/.m2/repository/net/sf/saxon/saxon/9.1.0.8b-patch/saxon-9.1.0.8b-patch.jar -s:$THESAURUSFILE -xsl:thesaurus-build-hierarchy-from-slash-label.xsl -o:../../../web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/$thesaurus
+  THESAURUSFILE=`cut -d "/" -f4 <<< "$thesaurus"`
+  echo $THESAURUSFILE
+  #cp ../../../web/src/main/webapp/WEB-INF/data/config/codelist/local/thesauri/thesauri/$thesaurus $THESAURUSFILE
+  java -jar ~/.m2/repository/net/sf/saxon/saxon/9.1.0.8b-patch/saxon-9.1.0.8b-patch.jar -s:$THESAURUSFILE -xsl:../../../web/src/main/webapp/xslt/services/thesaurus/build-hierarchy-from-label-with-separator.xsl -o:../../../web/src/main/webapp/WEB-INF/data/config/codelist/$thesaurus
 done
