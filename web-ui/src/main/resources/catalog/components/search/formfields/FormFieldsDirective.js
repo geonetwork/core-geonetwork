7840134974f782a4e704005d029c27e430880597
François Prunayre <fx.prunayre@gmail.com>
2022-04-06 15:00:02 +0200

    Admin / Harvester / Properly set group on page load
    Test:
    * Create a harvester with a group, save
    * Reload page, the group owner is wrong.

--- web-ui/src/main/resources/catalog/components/search/formfields/FormFieldsDirective.js
+++ web-ui/src/main/resources/catalog/components/search/formfields/FormFieldsDirective.js
@@ -327,9 +327,10 @@
                   });
                 }
 
-                if (angular.isNumber(scope.ownerGroup)) {
+                var groupId = parseInt(scope.ownerGroup);
+                if (groupId != NaN) {
                   scope.selectedGroup = scope.groups.find(function(v) {
-                    return v.id === scope.ownerGroup || v['@id'] === scope.ownerGroup
+                    return v.id === groupId || v['@id'] === groupId
                   });
 
                   if (scope.selectedGroup === undefined) {
