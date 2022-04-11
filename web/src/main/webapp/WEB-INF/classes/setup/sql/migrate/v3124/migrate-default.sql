7d9bb699a998c022fbea629ee7d2e438829934ba
Jose García <josegar74@gmail.com>
2022-04-11 11:28:09 +0200

    Update version to 3.12.4-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3124/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3124/migrate-default.sql
@@ -1,7 +1,7 @@
 UPDATE Settings SET value = '0 0 0 * * ?' WHERE name = 'system/inspire/atomSchedule' and value = '0 0 0/24 ? * *';
 
 UPDATE Settings SET value='3.12.4' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/remotevalidation/urlquery', '', 0, 7212, 'n');
 
