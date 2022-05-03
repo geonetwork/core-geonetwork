d910e895dc42c23f058b28f16e39f13f801a212d
Francois Prunayre <fx.prunayre@gmail.com>
2022-05-03 12:59:30 +0200

    Sextant / SQL / Add migration for https://gitlab.ifremer.fr/sextant/geonetwork/-/issues/470.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/7.0.1/maintenance-470-thesaurus.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadata/thesaurusNamespace', 'https://registry.ifremer.fr/scheme/{{type}}/{{filename}}', 0, 9161, 'n');
