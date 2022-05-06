b13a155d2d204208c637fb5dda4488f8f72ce699
Olivier Guyot <olivier.guyot@camptocamp.com>
2022-05-06 13:54:56 +0200

    Annotations / make sure both layers visibility are synced
     #504

--- web-ui/src/main/resources/catalog/views/sextant/annotations/AnnotationsDirective.js
+++ web-ui/src/main/resources/catalog/views/sextant/annotations/AnnotationsDirective.js
@@ -9,7 +9,6 @@
    */
   module.directive('sxtAnnotationsEditor', ['sxtAnnotationsService', '$rootScope', '$timeout',
     function(sxtAnnotationsService, $rootScope, $timeout) {
-      var listenerKey;
       /**
        * Copy-pasted from DrawDirective.js
        */
@@ -81,76 +80,106 @@
         return new ol.style.Style(styleObjCfg);
       };
 
-      var geojson = new ol.format.GeoJSON();
-      var init = function init (scope) {
-        scope.annotationsUuid = scope.layer.get('annotationsUuid');
-        scope.metadataObj = scope.layer.get('md');
-        scope.metadataUuid = scope.metadataObj ? scope.metadataObj.uuid : null;
+      var GEOJSON = new ol.format.GeoJSON();
 
-        var user = $rootScope.user;
-
-        if (!scope.annotationsUuid) {
-          console.error('Missing annotations UUID on layer');
-          return;
-        }
-        /**
-         * @type {ol.layer.Vector}
-         */
-        scope.annotationsLayer;
-
-        /**
-         * @type {boolean}
-         */
-        scope.loadingAnnotation = true;
-
-        /**
-         * @type {boolean}
-         */
-        scope.updatingAnnotation = false;
-
-        /**
-         * @type {Annotation}
-         */
-        scope.currentAnnotation = null;
-
-        /**
-         * @type {string}
-         */
-        scope.error = null;
-
-        /**
-         * Read-only if the annotations has an associated metadata record, but the user
-         * do not have edit rights on it
-         * @type {boolean}
-         */
-        scope.readOnly = scope.metadataObj && !(user.canEditRecord && user.canEditRecord(scope.metadataObj));
-
-        /**
-         * True if something has changed and should be saved
-         * @type {boolean}
-         */
-        scope.annotationsChanged = false;
-
-        /**
-         * Show a success notification
-         * @type {boolean}
-         */
-        scope.saveSuccess = false;
-
-
-        // initial loading of annotation entity
-        sxtAnnotationsService.getAnnotation(scope.annotationsUuid)
-          .then(scope.setCurrentAnnotation)
-
-      }
       return {
-        restrict: 'AE',
+        restrict: 'E',
         scope: {
           map: '=map',
           layer: '='
         },
         templateUrl: '../../catalog/views/sextant/annotations/partials/annotationsEditor.html',
-        link: function(scope, element, attrs) {
+        link: function(scope) {
+          var sourceListenerKey;
+          var visibilityListenerKey;
+
+          /**
+           * This will be created automatically by the gn-draw directive used in the HTML template
+           * @type {ol.layer.Vector}
+           */
+          scope.annotationsLayer;
+
+          /**
+           * @type {boolean}
+           */
+          scope.loadingAnnotation = true;
+
+          /**
+           * @type {boolean}
+           */
+          scope.updatingAnnotation = false;
+
+          /**
+           * @type {Annotation}
+           */
+          scope.currentAnnotation = null;
+
+          /**
+           * @type {string}
+           */
+          scope.error = null;
+
+          /**
+           * Read-only if the annotations has an associated metadata record, but the user
+           * do not have edit rights on it
+           * @type {boolean}
+           */
+          scope.readOnly = false;
+
+          /**
+           * True if something has changed and should be saved
+           * @type {boolean}
+           */
+          scope.annotationsChanged = false;
+
+          /**
+           * Show a success notification
+           * @type {boolean}
+           */
+          scope.saveSuccess = false;
+
+          var init = function (layer) {
+            scope.annotationsUuid = scope.layer.get('annotationsUuid');
+            scope.metadataObj = scope.layer.get('md');
+            scope.metadataUuid = scope.metadataObj ? scope.metadataObj.uuid : null;
+
+            var user = $rootScope.user;
+
+            if (!scope.annotationsUuid) {
+              console.error('Missing annotations UUID on layer');
+              return;
+            }
+
+            // reset scope vars
+            scope.loadingAnnotation = true;
+            scope.updatingAnnotation = false;
+            scope.currentAnnotation = null;
+            scope.error = null;
+            scope.readOnly = scope.metadataObj && !(user.canEditRecord && user.canEditRecord(scope.metadataObj));
+            scope.annotationsChanged = false;
+            scope.saveSuccess = false;
+
+            if (scope.annotationsLayer) {
+              var source = scope.annotationsLayer.getSource();
+              source.clear();
+              scope.bindLayerVisibility();
+            }
+
+            // initial loading of annotation entity
+            sxtAnnotationsService.getAnnotation(scope.annotationsUuid)
+              .then(scope.setCurrentAnnotation)
+          }
+
+          // teardown existing listeners if any
+          var teardown = function() {
+            if (sourceListenerKey) {
+              ol.Observable.unByKey(sourceListenerKey);
+            }
+            if (visibilityListenerKey) {
+              ol.Observable.unByKey(visibilityListenerKey);
+            }
+          }
+
           /**
            * Create an empty annotation entity with the given UUID
            * @param {string} uuid
@@ -203,7 +232,7 @@
             scope.currentAnnotation = annotation;
             scope.annotationsLayer.active = true;
             if (annotation.geometry) {
-              var features = geojson.readFeatures(annotation.geometry, {
+              var features = GEOJSON.readFeatures(annotation.geometry, {
                 dataProjection: 'EPSG:4326',
                 featureProjection: scope.map.getView().getProjection().getCode()
               });
@@ -218,36 +247,29 @@
               source.addFeatures(features);
             }
 
-            listenerKey = scope.annotationsLayer.getSource().on(['changefeature', 'addfeature', 'removefeature'], function() {
+            sourceListenerKey = scope.annotationsLayer.getSource().on(['changefeature', 'addfeature', 'removefeature'], function() {
               scope.annotationsChanged = true;
             })
           };
 
-          scope.layer.on('change:visible', function(e) {
-
-            if( scope.layer.get('annotationsUuid') != e.target.get('annotationsUuid')) {
-              return
+          // make sure that the main layer and annotations layer have the same visibility
+          scope.bindLayerVisibility = function () {
+            function syncVisible() {
+              scope.annotationsLayer.setVisible(scope.layer.getVisible());
             }
-            if (!e.oldValue)  {
+            syncVisible();
+            visibilityListenerKey = scope.layer.on('change:visible', syncVisible);
+          };
 
-              scope.annotationsLayer.setVisible(true);
-            }
-            else {
-              scope.annotationsLayer.setVisible(false);
-            }
-
-          });
-
-          scope.$watch("layer", function() {
-              init(scope)
+          scope.$watch("layer", function(newValue, oldValue) {
+            teardown();
+            init();
           });
 
           scope.$on('$destroy', function() {
-            scope.map.removeLayer(scope.annotationsLayer);
-            if (listenerKey) {
-              scope.annotationsLayer.getSource().un(listenerKey);
-            }
+            teardown();
             scope.annotationsLayer.active = false;
+            scope.map.removeLayer(scope.annotationsLayer);
           });
         }
       };
