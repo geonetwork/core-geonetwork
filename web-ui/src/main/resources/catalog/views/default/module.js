d50a9f6e62589b1e5f0ae008963491878fca9545
Florent gravin <florent.gravin@camptocamp.com>
2022-06-08 13:48:56 +0200

    [gn]: fix mdview mdLanguages
    avoid having duplicates, main lang is duplicated in otherLanguage

--- web-ui/src/main/resources/catalog/views/default/module.js
+++ web-ui/src/main/resources/catalog/views/default/module.js
@@ -271,6 +271,16 @@
       $scope.mdView = mdView;
       gnMdView.initMdView();
 
+      $scope.getMdLanguages = function() {
+        if (!mdView.current.record) {
+          return [];
+        }
+        return [mdView.current.record.mainLanguage].concat(
+          mdView.current.record.otherLanguage.filter(function(lang) {
+            return lang !== mdView.current.record.mainLanguage;
+          }) || []);
+      }
+
       $scope.goToSearch = function (any) {
         $location.path("/search").search({ any: any });
       };
