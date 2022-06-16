f8e078adc078c0ae215de7bf92bef4cec671c3c1
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2022-06-16 11:27:03 +0200

    fix(apache-proxy-ows): typo

--- sextant/apache-proxy-ows/apache-proxy-ows.sh
+++ sextant/apache-proxy-ows/apache-proxy-ows.sh
@@ -75,6 +75,6 @@ for identityMapping in "${identityMappings[@]}"; do
     ((configCounter++))
 
 done
-env LC_COLLATE=c cat temp.csv | sort -t"|" -k1 >> apache_conf.csv
+cat temp.csv | env LC_COLLATE=c sort -t"|" -k1 >> apache_conf.csv
 # supprime le fichier csv existant.
 rm temp.csv
