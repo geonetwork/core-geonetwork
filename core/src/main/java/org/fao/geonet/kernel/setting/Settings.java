91accd5d1facd901c61e250b72bcd90966e062be
Jose García <josegar74@gmail.com>
2022-06-21 12:36:57 +0200

    Configure the user profile allowed to delete published metadata (#6398)
    * Configure the user profile allowed to delete published metadata
    
    * Configure the user profile allowed to delete published metadata - migration
    
    * Update web-ui/src/main/resources/catalog/locales/en-admin.json
    
    Co-authored-by: François Prunayre <fx.prunayre@gmail.com>
    
    Co-authored-by: François Prunayre <fx.prunayre@gmail.com>

--- core/src/main/java/org/fao/geonet/kernel/setting/Settings.java
+++ core/src/main/java/org/fao/geonet/kernel/setting/Settings.java
@@ -126,6 +126,7 @@ public class Settings {
     public static final String METADATA_LINK_EXCLUDEPATTERN = "metadata/link/excludedUrlPattern";
     public static final String METADATA_IMPORT_RESTRICT = "metadata/import/restrict";
     public static final String METADATA_IMPORT_USERPROFILE = "metadata/import/userprofile";
+    public static final String METADATA_PUBLISHED_DELETE_USERPROFILE = "metadata/delete/profilePublishedMetadata";
     public static final String METADATA_BACKUPARCHIVE_ENABLE = "metadata/backuparchive/enable";
     public static final String METADATA_VCS = "metadata/vcs/enable";
     public static final String VIRTUAL_SETTINGS_SUFFIX_ISDEFINED = "IsDefined";
