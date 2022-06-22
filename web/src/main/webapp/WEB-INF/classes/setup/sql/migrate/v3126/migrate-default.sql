4fed94d1b548b9aa0c9054e6557e4bfd73e78db4
Jose García <josegar74@gmail.com>
2022-06-22 08:44:55 +0200

    Update version to 3.12.6-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3126/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3126/migrate-default.sql
@@ -1,4 +1,4 @@
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/delete/profilePublishedMetadata', 'Editor', 0, 12011, 'n');
 
 UPDATE Settings SET value='3.12.6' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
