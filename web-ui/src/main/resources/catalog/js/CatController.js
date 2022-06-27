6acbec566c1ea6c2b420e32b7b5d0bb3f0e311ce
Francois Prunayre <fx.prunayre@gmail.com>
2022-06-27 17:10:28 +0200

    Admin / Harvester / Fix paging - not really sure why the pagination directive does not work when in tab?

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -971,7 +971,8 @@ goog.require('gn_alert');
             },
             'indexingErrorMsg': {
               'terms': {
-                'field': 'indexingErrorMsg'
+                'field': 'indexingErrorMsg',
+                'size': 12
               }
             }
           }
