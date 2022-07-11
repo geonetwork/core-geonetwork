63cfe85afbaf7b9f4517813a2be666915b99f88d
david.blasby <david.blasby@geocat.net>
2022-07-11 13:37:13 -0700

    add quick=true/false to getstatus commands

--- services/src/main/java/org/fao/geonet/api/harvesting/RemoteHarvestersApi.java
+++ services/src/main/java/org/fao/geonet/api/harvesting/RemoteHarvestersApi.java
@@ -96,7 +96,7 @@ public class RemoteHarvestersApi {
         RemoteHarvesterApiClient client = new RemoteHarvesterApiClient(url);
 
         for(int i = 0; i < id.length; i++) {
-            OrchestratedHarvestProcessStatus harvesterProcessStatus = client.retrieveProgress(id[i], null);
+            OrchestratedHarvestProcessStatus harvesterProcessStatus = client.retrieveProgress(id[i], null, false);
             OrchestratedHarvestProcessState state = harvesterProcessStatus.getOrchestratedHarvestProcessState();
 
             RemoteHarvesterInfoStatus remoteHarvesterInfoStatus = new RemoteHarvesterInfoStatus();
@@ -151,7 +151,7 @@ public class RemoteHarvestersApi {
 
 
         RemoteHarvesterApiClient client = new RemoteHarvesterApiClient(url);
-        OrchestratedHarvestProcessStatus harvesterProcessStatus = client.retrieveProgress(processId, null);
+        OrchestratedHarvestProcessStatus harvesterProcessStatus = client.retrieveProgress(processId, null, false);
 
         RemoteHarvesterInfoStatus remoteHarvesterInfoStatus = new RemoteHarvesterInfoStatus();
         remoteHarvesterInfoStatus.processID = harvesterProcessStatus.getProcessID();
