dac0dccbdcf99a9f3d9d9492ed0eb79d3b34a026
François Prunayre <fx.prunayre@gmail.com>
2022-07-18 11:46:02 +0200

    Harvester / LocalFileSystem / Add batch edits. (#6442)
    * Harvester / LocalFileSystem / Add batch edits.
    
    * Sonarlint.
    
    * Harvester / Batch edit config / Handle invalid JSON config.

--- harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
+++ harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
@@ -316,7 +316,7 @@ public class Aligner extends BaseAligner<CswParams> {
             }
         }
 
-        applyBatchEdits(ri, md, schema);
+        applyBatchEdits(ri.uuid, md, schema, params.getBatchEdits(), context, log);
 
         //
         // insert metadata
@@ -355,11 +355,13 @@ public class Aligner extends BaseAligner<CswParams> {
         result.addedMetadata++;
     }
 
-    private void applyBatchEdits(RecordInfo ri, Element md, String schema) throws JDOMException, IOException {
-        if (StringUtils.isNotEmpty(params.getBatchEdits())) {
+    public static void applyBatchEdits(
+        String uuid, Element md, String schema,
+        String batchEdits, ServiceContext context, Logger log) throws JDOMException, IOException {
+        if (StringUtils.isNotEmpty(batchEdits)) {
             ObjectMapper mapper = new ObjectMapper();
 
-            BatchEditParameter[] listOfUpdates = mapper.readValue(params.getBatchEdits(), BatchEditParameter[].class);
+            BatchEditParameter[] listOfUpdates = mapper.readValue(batchEdits, BatchEditParameter[].class);
             if (listOfUpdates.length > 0) {
                 SchemaManager _schemaManager = context.getBean(SchemaManager.class);
                 EditLib editLib = new EditLib(_schemaManager);
@@ -395,8 +397,8 @@ public class Aligner extends BaseAligner<CswParams> {
                         ) || metadataChanged;
                     }
                 }
-                if (metadataChanged) {
-                    log.debug("  - Record updated by batch edit configuration:" + ri.uuid);
+                if (metadataChanged && log != null) {
+                    log.debug("  - Record updated by batch edit configuration:" + uuid);
                 }
             }
         }
@@ -448,7 +450,7 @@ public class Aligner extends BaseAligner<CswParams> {
             schema = newSchema;
         }
 
-        applyBatchEdits(ri, md, schema);
+        applyBatchEdits(ri.uuid, md, schema, params.getBatchEdits(), context, log);
 
         //
         // update metadata
