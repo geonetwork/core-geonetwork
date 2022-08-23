geonetwork/core-geonetwork

commit 7a77fa99e383506869c7e840fd2fe61cb93e2d05

API / Batch editing / Add support for attribute. (#6423)
* API / Batch editing / Add support for attribute.

* Update AddElemValue.java

core/src/main/java/org/fao/geonet/kernel/EditLib.java
@@ -500,13 +500,6 @@ public class EditLib {
                 value.getNodeValue().getName()
                     .equals(SpecialUpdateTags.CREATE);
 
-            if (isValueXml &&
-                xpathProperty.matches(".*@[^/\\]]+")) {
-                throw new AssertionError(String.format(
-                    "Cannot set Xml on an attribute. Xpath:'%s' value: '%s'.",
-                    xpathProperty, Xml.getString(value.getNodeValue())
-                ));
-            }
             LOGGER_ADD_ELEMENT.debug("Inserting at location {} the snippet or value {}", xpathProperty, value);
 
             xpathProperty = cleanRootFromXPath(xpathProperty, metadataRecord);

core/src/main/java/org/fao/geonet/kernel/AddElemValue.java
@@ -39,10 +39,9 @@ public class AddElemValue {
     private final Element nodeValue;
 
     public AddElemValue(String stringValue) throws JDOMException, IOException {
-        String finalStringVal = stringValue;
         Element finalNodeVal = null;
-
-        if (Xml.isXMLLike(stringValue)) {
+        String finalStringVal = stringValue.replaceAll("</?gn_(add|replace|delete)>", "");
+        if (Xml.isXMLLike(finalStringVal)) {
             try {
                 finalNodeVal = Xml.loadString(stringValue, false);
                 finalStringVal = null;

services/src/test/java/org/fao/geonet/services/metadata/BatchEditsServiceTest.java
@@ -40,6 +40,7 @@ import org.fao.geonet.domain.AbstractMetadata;
 import org.fao.geonet.kernel.datamanager.IMetadataUtils;
 import org.fao.geonet.kernel.mef.MEFLibIntegrationTest;
 import org.fao.geonet.services.AbstractServiceIntegrationTest;
+import org.jdom.Attribute;
 import org.jdom.Element;
 import org.junit.Assert;
 import org.junit.Before;
@@ -194,7 +195,134 @@ public class BatchEditsServiceTest extends AbstractServiceIntegrationTest {
         Assert.assertEquals(1, nodes.size());
     }
 
-        @Test
+
+    @Test
+    public void testUpdateRecordUpdateAttribute() throws Exception {
+        final String uuid = "db07463b-6769-401e-944b-f22e2e3bcc26";
+        BatchEditParameter[] listOfupdates = new BatchEditParameter[]{
+            new BatchEditParameter(
+                "/mdb:MD_Metadata/mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope/@codeListValue",
+                "newScope"
+            )
+        };
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+        this.mockHttpSession = loginAsAdmin();
+
+        Gson gson = new GsonBuilder()
+            .create();
+        JsonElement jsonEl = gson.toJsonTree(listOfupdates);
+
+        this.mockMvc.perform(put("/srv/api/records/batchediting?uuids=" + uuid)
+                .content(jsonEl.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(this.mockHttpSession)
+                .accept(MediaType.parseMediaType("application/json")))
+            .andExpect(status().is(201));
+
+        AbstractMetadata updatedRecord = repository.findOneByUuid(uuid);
+        Element xml = Xml.loadString(updatedRecord.getData(), false);
+
+        List attr = org.fao.geonet.utils.Xml.selectNodes(xml,
+            "./mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope/@codeListValue",
+            xml.getAdditionalNamespaces());
+        Assert.assertEquals("newScope", ((Attribute) attr.get(0)).getValue());
+
+
+        listOfupdates = new BatchEditParameter[]{
+            new BatchEditParameter(
+                "/mdb:MD_Metadata/mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope/@codeListValue",
+                "<gn_replace>anotherNewScope</gn_replace>"
+            )
+        };
+
+        jsonEl = gson.toJsonTree(listOfupdates);
+
+        this.mockMvc.perform(put("/srv/api/records/batchediting?uuids=" + uuid)
+                .content(jsonEl.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(this.mockHttpSession)
+                .accept(MediaType.parseMediaType("application/json")))
+            .andExpect(status().is(201));
+
+        updatedRecord = repository.findOneByUuid(uuid);
+        xml = Xml.loadString(updatedRecord.getData(), false);
+
+        attr = org.fao.geonet.utils.Xml.selectNodes(xml,
+            "./mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope/@codeListValue",
+            xml.getAdditionalNamespaces());
+        Assert.assertEquals("anotherNewScope", ((Attribute) attr.get(0)).getValue());
+    }
+
+
+    @Test
+    public void testUpdateRecordDeleteAttribute() throws Exception {
+        final String uuid = "db07463b-6769-401e-944b-f22e2e3bcc26";
+        BatchEditParameter[] listOfupdates = new BatchEditParameter[]{
+            new BatchEditParameter(
+                "/mdb:MD_Metadata/mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope/@codeListValue",
+                "<gn_delete/>"
+            )
+        };
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+        this.mockHttpSession = loginAsAdmin();
+
+        Gson gson = new GsonBuilder()
+            .create();
+        JsonElement jsonEl = gson.toJsonTree(listOfupdates);
+
+        this.mockMvc.perform(put("/srv/api/records/batchediting?uuids=" + uuid)
+                .content(jsonEl.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(this.mockHttpSession)
+                .accept(MediaType.parseMediaType("application/json")))
+            .andExpect(status().is(201));
+
+        AbstractMetadata updatedRecord = repository.findOneByUuid(uuid);
+        Element xml = Xml.loadString(updatedRecord.getData(), false);
+
+        List scope = org.fao.geonet.utils.Xml.selectNodes(xml,
+            "./mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope",
+            xml.getAdditionalNamespaces());
+        Assert.assertEquals(null, ((Element) scope.get(0)).getAttribute("codeListValue"));
+    }
+
+
+    @Test
+    public void testUpdateRecordAddAttribute() throws Exception {
+        final String uuid = "db07463b-6769-401e-944b-f22e2e3bcc26";
+        BatchEditParameter[] listOfupdates = new BatchEditParameter[]{
+            new BatchEditParameter(
+                "/mdb:MD_Metadata/mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope/@newAttribute",
+                "<gn_add>value</gn_add>"
+            )
+        };
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+        this.mockHttpSession = loginAsAdmin();
+
+        Gson gson = new GsonBuilder()
+            .create();
+        JsonElement jsonEl = gson.toJsonTree(listOfupdates);
+
+        this.mockMvc.perform(put("/srv/api/records/batchediting?uuids=" + uuid)
+                .content(jsonEl.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(this.mockHttpSession)
+                .accept(MediaType.parseMediaType("application/json")))
+            .andExpect(status().is(201));
+
+        AbstractMetadata updatedRecord = repository.findOneByUuid(uuid);
+        Element xml = Xml.loadString(updatedRecord.getData(), false);
+
+        List scope = org.fao.geonet.utils.Xml.selectNodes(xml,
+            "./mdb:metadataScope/mdb:MD_MetadataScope/mdb:resourceScope",
+            xml.getAdditionalNamespaces());
+        Assert.assertEquals("value", ((Element) scope.get(0)).getAttributeValue("newAttribute"));
+    }
+
+    @Test
     public void testUpdateRecordElement() throws Exception {
         final String uuid = "db07463b-6769-401e-944b-f22e2e3bcc26";
         // XPath has no match and same type as fragment, element is created

commit 199f16ae9bebe05d6912f7376e45475fc24dff5b

API / XPath editing / gn_replace_all mode may keep empty elements.
Issue introduced in
https://github.com/geonetwork/core-geonetwork/pull/6423.

Symptoms : edit a record with a topic category, switch to xml view
Empty `<gmd:topicCategory/>` elements are in the record.

When `gn_replace_all` is used the `gn_delete` steps was not take into
account.

core/src/main/java/org/fao/geonet/kernel/EditLib.java
@@ -396,7 +396,7 @@ public class EditLib {
 
             if (isReplaceAllMode) {
                 // Remove all
-                AddElemValue propertyValueToProcess = new AddElemValue("<gn_delete></gn_delete>");
+                AddElemValue propertyValueToProcess = new AddElemValue(new Element("gn_delete"));
 
                 addElementOrFragmentFromXpath(metadataRecord, metadataSchema, xpathProperty, propertyValueToProcess,
                     createXpathNodeIfNotExist);

core/src/test/java/org/fao/geonet/kernel/EditLibIntegrationTest.java
@@ -416,6 +416,32 @@ public class EditLibIntegrationTest extends AbstractCoreIntegrationTest {
         assertEquals(0, Xml.selectNodes(metadataElement, "gmd:referenceSystemInfo", Arrays.asList(GMD, GCO)).size());
     }
 
+
+    @Test
+    public void testReplaceAll() throws Exception {
+        MetadataSchema schema = _schemaManager.getSchema("iso19139");
+        String code1 = "code1";
+        String code2 = "code2";
+        String code3 = "code3";
+        final Element metadataElement = new Element("MD_Metadata", GMD).addContent(Arrays.asList(
+            createReferenceSystemInfo(code1),
+            createReferenceSystemInfo(code2),
+            createReferenceSystemInfo(code3)
+        ));
+        assertEquals(3, Xml.selectNodes(metadataElement, "gmd:referenceSystemInfo", Arrays.asList(GMD, GCO)).size());
+
+        final String refSysElemName = "gmd:referenceSystemInfo";
+        String updateConfig = "<gn_replace_all><gmd:referenceSystemInfo xmlns:gmd=\"http://www.isotc211.org/2005/gmd\"><gmd:code>NEW</gmd:code></gmd:referenceSystemInfo></gn_replace_all>";
+
+        LinkedHashMap<String, AddElemValue> updates = new LinkedHashMap<String, AddElemValue>();
+        final String attXPath = "gmd:referenceSystemInfo";
+        updates.put(attXPath, new AddElemValue(updateConfig));
+        new EditLib(_schemaManager).addElementOrFragmentFromXpaths(metadataElement, updates, schema, true);
+
+        assertEquals(1, Xml.selectNodes(metadataElement, "gmd:referenceSystemInfo", Arrays.asList(GMD, GCO)).size());
+        assertEqualsText("NEW", metadataElement, refSysElemName + "[1]/gmd:code", GMD, GCO);
+    }
+
     @Test
     public void testReplaceFragmentFromXpath_SpecialDeleteAllTag() throws Exception {
 