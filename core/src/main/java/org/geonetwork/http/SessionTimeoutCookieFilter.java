geonetwork/core-geonetwork

commit 6b6334d9052f8d49c88667c44fae033d688a0408

Configure HttpOnly for session timeout cookies and fix the cookie path. Fixes #6151

core/src/main/java/org/geonetwork/http/SessionTimeoutCookieFilter.java
@@ -65,7 +65,8 @@ public class SessionTimeoutCookieFilter implements javax.servlet.Filter {
             long currTime = System.currentTimeMillis();
 
             Cookie cookie = new Cookie("serverTime", "" + currTime);
-            cookie.setPath("/");
+            cookie.setPath(httpReq.getContextPath());
+            cookie.setHttpOnly(req.getServletContext().getSessionCookieConfig().isHttpOnly());
             cookie.setSecure(req.getServletContext().getSessionCookieConfig().isSecure());
             httpResp.addCookie(cookie);
 
@@ -83,7 +84,8 @@ public class SessionTimeoutCookieFilter implements javax.servlet.Filter {
             } else {
                 cookie = new Cookie("sessionExpiry", "" + currTime);
             }
-            cookie.setPath("/");
+            cookie.setPath(httpReq.getContextPath());
+            cookie.setHttpOnly(req.getServletContext().getSessionCookieConfig().isHttpOnly());
             cookie.setSecure(req.getServletContext().getSessionCookieConfig().isSecure());
             httpResp.addCookie(cookie);
         }

commit 1fca15a70ad1940ac447e323fa68a3ea38b2865c

Remove httpOnly from session timeout cookies (#6570)
Remove HttpOnly for session timeout cookies, otherwise can't be processed in the UI code.
Fix a problem introduced in #6163.

core/src/main/java/org/geonetwork/http/SessionTimeoutCookieFilter.java
@@ -66,7 +66,6 @@ public class SessionTimeoutCookieFilter implements javax.servlet.Filter {
 
             Cookie cookie = new Cookie("serverTime", "" + currTime);
             cookie.setPath(httpReq.getContextPath());
-            cookie.setHttpOnly(req.getServletContext().getSessionCookieConfig().isHttpOnly());
             cookie.setSecure(req.getServletContext().getSessionCookieConfig().isSecure());
             httpResp.addCookie(cookie);
 
@@ -85,7 +84,6 @@ public class SessionTimeoutCookieFilter implements javax.servlet.Filter {
                 cookie = new Cookie("sessionExpiry", "" + currTime);
             }
             cookie.setPath(httpReq.getContextPath());
-            cookie.setHttpOnly(req.getServletContext().getSessionCookieConfig().isHttpOnly());
             cookie.setSecure(req.getServletContext().getSessionCookieConfig().isSecure());
             httpResp.addCookie(cookie);
         }