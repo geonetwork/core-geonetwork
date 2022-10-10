ddeff78b8a7574c6eeea91b17a611561a979f905
Francois Prunayre <fx.prunayre@gmail.com>
2022-10-10 19:18:42 +0200

    Prettier.

--- web-ui/src/main/resources/catalog/webcomponents/webcomponents.js
+++ web-ui/src/main/resources/catalog/webcomponents/webcomponents.js
@@ -1,17 +1,21 @@
 function validURL(str) {
-  var urlPattern = new RegExp("^(https?:\\/\\/)?" +
-    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
-    "((\\d{1,3}\\.){3}\\d{1,3})|localhost)" +
-    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
-    "(\\?[;&a-z\\d%_.~+=-]*)?" +
-    "(\\#[-a-z\\d_]*)?$", "i");
+  var urlPattern = new RegExp(
+    "^(https?:\\/\\/)?" +
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3})|localhost)" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
   return !!urlPattern.test(str);
 }
 
 var DEFAULT_BASEURL = "http://localhost:8080/geonetwork";
 var DEFAULT_PORTAL = "srv";
 
-customElements.define("gn-app",
+customElements.define(
+  "gn-app",
   class extends HTMLElement {
     constructor() {
       super();
@@ -39,17 +43,19 @@ customElements.define("gn-app",
         app.setAttribute("data-ng-controller", "GnCatController");
 
         var div = document.createElement("div");
-        div.setAttribute("data-ng-include", "'" + baseUrl + "/catalog/views/default/templates/index.html'");
+        div.setAttribute(
+          "data-ng-include",
+          "'" + baseUrl + "/catalog/views/default/templates/index.html'"
+        );
         div.setAttribute("class", "gn-full");
         app.appendChild(div);
-
 
         var css = document.createElement("link");
         css.setAttribute("href", baseUrl + "/static/gn_search_default.css");
         css.setAttribute("rel", "stylesheet");
         app.appendChild(css);
 
-        ["/static/lib.js"].forEach(function(src) {
+        ["/static/lib.js"].forEach(function (src) {
           var script = document.createElement("script");
           script.setAttribute("src", baseUrl + src);
           script.setAttribute("onload", "loadDependency()");
@@ -59,44 +65,54 @@ customElements.define("gn-app",
         var script = document.createElement("script");
         script.setAttribute("type", "text/javascript");
 
-        script.textContent = "" +
+        script.textContent =
+          "" +
           "var gnShadowRoot = document.getElementsByTagName('gn-app')[0].shadowRoot;\n" +
           "function gnBootstrap() {" +
-          "    config = " + JSON.stringify(uiConfig) + " || {};\n" +
+          "    config = " +
+          JSON.stringify(uiConfig) +
+          " || {};\n" +
           "    var cfgModule = angular.module('gn_config', []);\n" +
           "    cfgModule.config(['gnViewerSettings', 'gnSearchSettings', 'gnGlobalSettings',\n" +
           "      function (gnViewerSettings, gnSearchSettings, gnGlobalSettings) {\n" +
-          "        gnGlobalSettings.init(config, '" + baseUrl + "/" + portal + "/', gnViewerSettings, gnSearchSettings);\n" +
+          "        gnGlobalSettings.init(config, '" +
+          baseUrl +
+          "/" +
+          portal +
+          "/', gnViewerSettings, gnSearchSettings);\n" +
           "      }]);" +
           "angular.bootstrap(gnShadowRoot, ['gn_search_default']);" +
           "};" +
           "function loadDependency() {" +
-          "  [\"" + baseUrl + "/static/gn_search_default.js\"].forEach(function(src) {\n" +
-          "    var script = document.createElement(\"script\");\n" +
-          "    script.setAttribute(\"src\", src);\n" +
-          "    script.setAttribute(\"onload\", \"gnBootstrap()\");\n" +
+          '  ["' +
+          baseUrl +
+          '/static/gn_search_default.js"].forEach(function(src) {\n' +
+          '    var script = document.createElement("script");\n' +
+          '    script.setAttribute("src", src);\n' +
+          '    script.setAttribute("onload", "gnBootstrap()");\n' +
           "    gnShadowRoot.appendChild(script);\n" +
           "  });\n" +
           "};";
         app.appendChild(script);
 
-
         var style = document.createElement("style");
         style.setAttribute("type", "text/css");
-        var css = "@charset \"UTF-8\";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}";
+        var css =
+          '@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}';
         style.textContent = css;
         app.appendChild(style);
 
         return app;
       }
 
-
-      this.attachShadow({ mode: "open" })
-        .appendChild(getTemplate(document, baseUrl, portal, uiConfig));
+      this.attachShadow({ mode: "open" }).appendChild(
+        getTemplate(document, baseUrl, portal, uiConfig)
+      );
 
       var style = document.createElement("style");
       style.setAttribute("type", "text/css");
-      var css = "\n" +
+      var css =
+        "\n" +
         "@font-face {\n" +
         "  font-family: 'FontAwesome';\n" +
         "  src: url('../lib/style/font-awesome/fonts/fontawesome-webfont.eot?v=4.4.0');\n" +
@@ -110,7 +126,8 @@ customElements.define("gn-app",
   }
 );
 
-customElements.define("gn-app-frame",
+customElements.define(
+  "gn-app-frame",
   class extends HTMLElement {
     constructor() {
       super();
@@ -129,7 +146,8 @@ customElements.define("gn-app-frame",
         return;
       }
       var uiConfig = this.getAttribute("config") || "";
-      var style = this.getAttribute("style") || "width:100%;height:80%;border:none";
+      var style =
+        this.getAttribute("style") || "width:100%;height:80%;border:none";
 
       function getTemplate(document, baseUrl, portal, uiConfig) {
         var app = document.createElement("iframe");
@@ -143,7 +161,9 @@ customElements.define("gn-app-frame",
             try {
               JSON.parse(uiConfig);
             } catch (e) {
-              console.warn("Invalid configuration: " + uiConfig + ". Using default.");
+              console.warn(
+                "Invalid configuration: " + uiConfig + ". Using default."
+              );
               console.warn(e);
               return;
             }
@@ -154,8 +174,9 @@ customElements.define("gn-app-frame",
         return app;
       }
 
-      this.attachShadow({ mode: "open" })
-        .appendChild(getTemplate(document, baseUrl, portal, uiConfig));
+      this.attachShadow({ mode: "open" }).appendChild(
+        getTemplate(document, baseUrl, portal, uiConfig)
+      );
     }
   }
 );
