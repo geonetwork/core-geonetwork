fcd495f8cdbbffb01914a6ded5f35a43317fa08a
Jose García <josegar74@gmail.com>
2022-12-02 14:28:54 +0100

    Update  request

--- web-ui/src/main/resources/catalog/views/default/directives/directive.js
+++ web-ui/src/main/resources/catalog/views/default/directives/directive.js
@@ -145,8 +145,8 @@
           function loadTasks() {
             return $http
               .get("../api/status/task", { cache: true })
-              .success(function (data) {
-                scope.tasks = data;
+              .then(function (response) {
+                scope.tasks = response.data;
                 scope.getVisibleTasks();
               });
           }
