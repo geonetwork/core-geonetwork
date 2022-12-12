b65665bef435af4c2cacc9fcc67ddadd7bfcc012
Jose García <josegar74@gmail.com>
2022-12-12 11:55:14 +0100

    Update version to 3.12.8-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3128/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v3128/migrate-default.sql
@@ -2,4 +2,4 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadataprivs/publication/notificationGroups', '', 0, 9183, 'n');
 
 UPDATE Settings SET value='3.12.8' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
