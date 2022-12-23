3e6150c55f3a2c03e9b8c758a9f3e901a13fcfcf
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2022-12-23 09:36:31 +0100

    chore(apache-proxy): ajout des fleches pour messages watning

--- sextant/apache-proxy-ows/python/src/apache_proxy.py
+++ sextant/apache-proxy-ows/python/src/apache_proxy.py
@@ -51,7 +51,7 @@ class ApacheProxy:
                     self.softwares.append(self.software)
                 else:
                     print(
-                        "{} existe déja et sera considéré comme un doublon, il ne sera pas rajouté dans la conf".format(
+                        "--> {} existe déja et sera considéré comme un doublon, il ne sera pas rajouté dans la conf".format(
                             self.interne_url
                         )
                     )
@@ -130,7 +130,7 @@ class ApacheProxy:
                 self.softwares.append(self.software)
             else:
                 print(
-                    "⚠ {} existe déja et sera considéré comme un doublon, il ne sera pas rajouté dans la conf".format(
+                    "--> {} existe déja et sera considéré comme un doublon, il ne sera pas rajouté dans la conf".format(
                         interne_url
                     )
                 )
