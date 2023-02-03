10ced4129a5f9b566f3a2a1fc9992ef5ceaa130b
Francois Prunayre <fx.prunayre@gmail.com>
2023-02-03 09:11:40 +0100

    Sextant / Permalink migration https://gitlab.ifremer.fr/sextant/geonetwork/-/issues/257.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/7.1.0/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-sextant/migrate/7.1.0/migrate-default.sql
@@ -72,7 +72,12 @@ DELETE FROM Settings WHERE name = 'system/downloadservice/leave';
 DELETE FROM Settings WHERE name = 'system/downloadservice/simple';
 DELETE FROM Settings WHERE name = 'system/downloadservice/withdisclaimer';
 
-INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/server/sitemapLinkUrl', NULL, 0, 270, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/url/sitemapLinkUrl', NULL, 0, 9165, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/url/sitemapDoiFirst', 'false', 2, 9166, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/url/dynamicAppLinkUrl', NULL, 0, 9167, 'y');
+
+
+
 
 UPDATE Settings SET value='4.2.2' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
