5bbc6be94253322538c50dd1cde2120c5c798257
Francois Prunayre <fx.prunayre@gmail.com>
2023-05-11 08:46:19 +0200

    Workflow / Fix typo in setting key.

--- core/src/main/java/org/fao/geonet/kernel/setting/Settings.java
+++ core/src/main/java/org/fao/geonet/kernel/setting/Settings.java
@@ -130,7 +130,7 @@ public class Settings {
     public static final String REGION_GETMAP_SUMMARY_WIDTH = "region/getmap/summaryWidth";
     public static final String METADATA_WORKFLOW_ENABLE = "metadata/workflow/enable";
     public static final String METADATA_WORKFLOW_DRAFT_WHEN_IN_GROUP = "metadata/workflow/draftWhenInGroup";
-    public static final String METADATA_WORKFLOW_ALLOW_SUBMIT_APPROVE_INVALID_MD = "metadata/workflow/allowSumitApproveInvalidMd";
+    public static final String METADATA_WORKFLOW_ALLOW_SUBMIT_APPROVE_INVALID_MD = "metadata/workflow/allowSubmitApproveInvalidMd";
     public static final String METADATA_WORKFLOW_ALLOW_PUBLISH_INVALID_MD = "metadata/workflow/allowPublishInvalidMd";
     public static final String METADATA_WORKFLOW_ALLOW_PUBLISH_NON_APPROVED_MD = "metadata/workflow/allowPublishNonApprovedMd";
     public static final String METADATA_LINK_EXCLUDEPATTERN = "metadata/link/excludedUrlPattern";
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v425/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v425/migrate-default.sql
@@ -1,2 +1,6 @@
+
+UPDATE settings SET name = 'metadata/workflow/allowSubmitApproveInvalidMd'
+WHERE name = 'metadata/workflow/allowSumitApproveInvalidMd';
+
 UPDATE Settings SET value='4.2.5' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
