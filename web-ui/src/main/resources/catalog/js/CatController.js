geonetwork/core-geonetwork

commit 3a8cf71f49bd984c134083a016d77e1004ab370a

Home page / Aggregations / Fix label for facet based on multilingual thesaurus.
Use the facet model for the facet cards as in the search facet
component.

web-ui/src/main/resources/catalog/js/CatController.js
@@ -1517,6 +1517,7 @@
     "$cookies",
     "gnExternalViewer",
     "gnAlertService",
+    "gnESFacet",
     function (
       $scope,
       $http,
@@ -1536,7 +1537,8 @@
       gnSearchSettings,
       $cookies,
       gnExternalViewer,
-      gnAlertService
+      gnAlertService,
+      gnESFacet
     ) {
       $scope.version = "0.0.1";
       var defaultNode = "srv";
@@ -1952,6 +1954,7 @@
                   $scope.searchInfo = r.data;
                   var keys = Object.keys(gnGlobalSettings.gnCfg.mods.home.facetConfig);
                   selectedFacet = keys[0];
+
                   for (var i = 0; i < keys.length; i++) {
                     if (
                       $scope.searchInfo.aggregations[keys[i]].buckets.length > 0 ||
@@ -1965,7 +1968,13 @@
                     list: keys,
                     key: selectedFacet,
                     lastKey: keys.length > 1 ? keys[keys.length - 1] : undefined,
-                    config: gnGlobalSettings.gnCfg.mods.home.facetConfig
+                    config: gnGlobalSettings.gnCfg.mods.home.facetConfig,
+                    facets: gnESFacet.createFacetModel(
+                      gnGlobalSettings.gnCfg.mods.home.facetConfig,
+                      r.data.aggregations,
+                      undefined,
+                      undefined
+                    )
                   };
                 });
             }

web-ui/src/main/resources/catalog/components/elasticsearch/directives/FacetDirective.js
@@ -449,9 +449,7 @@
         scope: {
           key: "=esFacetCards",
           homeFacet: "=homeFacet",
-          searchInfo: "=searchInfo",
-          aggResponse: "=aggResponse",
-          aggConfig: "=aggConfig"
+          searchInfo: "=searchInfo"
         },
         templateUrl: function (elem, attrs) {
           return (
@@ -468,6 +466,11 @@
               scope.homeFacet.config[scope.key].terms &&
               scope.homeFacet.config[scope.key].terms.missing;
             scope.isInspire = scope.key.indexOf("th_httpinspireeceuropaeutheme") === 0;
+
+            scope.aggregations = {};
+            scope.homeFacet.facets.forEach(function (facet) {
+              scope.aggregations[facet.key] = facet;
+            });
           }
 
           init();

web-ui/src/main/resources/catalog/components/elasticsearch/directives/partials/facet-cards.html
@@ -1,6 +1,6 @@
 <div
-  data-ng-repeat="(k, facet) in searchInfo.aggregations[key].buckets"
-  data-ng-init="facetValue = facet.key || k;"
+  data-ng-repeat="(k, facet) in aggregations[key].items"
+  data-ng-init="facetValue = facet.value;"
   data-ng-show="facetValue"
   data-ng-class="isInspire ? ('bg-iti-' + (facet.key | facetCssClassCode: isInspire)) : ''"
   class="col-xs-6 col-sm-4 col-md-3 col-lg-2 gn-topic"
@@ -14,9 +14,9 @@
         class="clearfix"
         title="{{::facetValue}}"
         role="link"
-        data-ng-init="response = searchInfo.aggregations[key];
+        data-ng-init="response = aggregations[key];
                       decorator = (response.meta && response.meta.decorator) || undefined"
-        data-ng-href="{{::facetValue | facetSearchUrlBuilder:key:searchInfo.aggregations[key]:homeFacet.config[key]:missingValue}}"
+        data-ng-href="{{::facetValue | facetSearchUrlBuilder:key:aggregations[key]:homeFacet.config[key]:missingValue}}"
       >
         <span
           data-ng-if="decorator"
@@ -38,7 +38,7 @@
       </a>
     </div>
     <div class="panel-footer">
-      <i class="fa fa-fw fa-file-text-o"></i>{{::facet.doc_count}}
+      <i class="fa fa-fw fa-file-text-o"></i>{{::facet.count}}
     </div>
   </div>
 </div>

commit 99cdd681ae4713db4ad523e7d401134779b17900

UI / Dependency / CatController depends on facets also on signin.
Issue introduced in https://github.com/geonetwork/core-geonetwork/pull/7066.

web-ui/src/main/resources/catalog/js/CatController.js
@@ -31,6 +31,8 @@
   goog.require("gn_search_manager");
   goog.require("gn_session_service");
   goog.require("gn_alert");
+  goog.require("gn_es");
+
 
   var module = angular.module("gn_cat_controller", [
     "gn_search_manager",
@@ -39,7 +41,8 @@
     "gn_saved_selections",
     "gn_external_viewer",
     "gn_history",
-    "gn_alert"
+    "gn_alert",
+    "gn_es"
   ]);
 
   module.constant("gnSearchSettings", {});

web/src/main/webapp/xslt/base-layout-cssjs-loader.xsl
@@ -163,17 +163,12 @@
           <script src="{$uiResourcesPath}lib/nv.d3.js?v={$buildNumber}"></script>
         </xsl:if>
 
-        <xsl:if test="$angularApp = 'gn_search' or
-                      $angularApp = 'gn_editor' or
-                      $angularApp = 'gn_formatter_viewer' or
-                      $angularApp = 'gn_admin'">
-          <script src="{$uiResourcesPath}lib/zip/zip.js?v={$buildNumber}"></script>
-          <!-- Jsonix resources (OWS Context) -->
-          <script src="{$uiResourcesPath}lib/jsonix/jsonix/Jsonix-all.js?v={$buildNumber}"></script>
-          <script type="text/javascript">
-            zip.workerScriptsPath = "../../catalog/lib/zip/";
-          </script>
-        </xsl:if>
+        <script src="{$uiResourcesPath}lib/zip/zip.js?v={$buildNumber}"></script>
+        <!-- Jsonix resources (OWS Context) -->
+        <script src="{$uiResourcesPath}lib/jsonix/jsonix/Jsonix-all.js?v={$buildNumber}"></script>
+        <script type="text/javascript">
+          zip.workerScriptsPath = "../../catalog/lib/zip/";
+        </script>
 
 
         <!--<xsl:if test="$isEditing">-->