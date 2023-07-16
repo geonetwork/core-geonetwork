0e876ea1fa2f241b670a8d4bc2ea66a7875d1398
Ian <ianwallen@hotmail.com>
2023-07-16 08:12:03 -0300

    [BP] Update status emails to contain link to metadata which does not contain anchor. (#7130)

--- core/src/main/java/org/fao/geonet/kernel/metadata/DefaultStatusActions.java
+++ core/src/main/java/org/fao/geonet/kernel/metadata/DefaultStatusActions.java
@@ -290,6 +290,11 @@ public class DefaultStatusActions implements StatusActions {
             textTemplate = messages.getString("status_change_default_email_text");
         }
 
+        // Replace link in message
+        ApplicationContext applicationContext = ApplicationContextHolder.get();
+        SettingManager sm = applicationContext.getBean(SettingManager.class);
+        textTemplate = textTemplate.replace("{{link}}", sm.getNodeURL()+ "api/records/{{index:_uuid}}");
+
         UserRepository userRepository = context.getBean(UserRepository.class);
         User owner = userRepository.findOne(status.getOwner());
 
