geonetwork/core-geonetwork

commit b4c77581ac3f54b7f1a08fb7705ae10f44e49728

Update status emails to contain link to metadata which does not contain anchor. (#7130)
This will make the link consistent with other emails such as publish/unpublish notifications.

core/src/main/java/org/fao/geonet/kernel/metadata/DefaultStatusActions.java
@@ -264,6 +264,11 @@ public class DefaultStatusActions implements StatusActions {
             textTemplate = messages.getString("status_change_default_email_text");
         }
 
+        // Replace link in message
+        ApplicationContext applicationContext = ApplicationContextHolder.get();
+        SettingManager sm = applicationContext.getBean(SettingManager.class);
+        textTemplate = textTemplate.replace("{{link}}", sm.getNodeURL()+ "api/records/{{index:uuid}}");
+
         UserRepository userRepository = context.getBean(UserRepository.class);
         User owner = userRepository.findById(status.getOwner()).orElse(null);
 

core/src/test/resources/org/fao/geonet/api/Messages.properties
@@ -101,7 +101,7 @@ Message: \n\
 {1} \n\
  \n\
 View record: \n\
-{7}
+{{link}}
 
 # SiteName / Task / recordTitle statusName by userName
 status_change_doiCreationTask_email_subject={0} / Task / {1} for '{{'index:resourceTitleObject'}}'
@@ -113,7 +113,7 @@ Message: \n\
 {1} \n\
 \n\
 View record: \n\
-{7}
+{{link}}
 # TODO: Link to DOI creation panel
 
 api.groups.group_not_found=Group with ID ''{0}'' not found in this catalog.

core/src/test/resources/org/fao/geonet/api/Messages_fre.properties
@@ -89,7 +89,7 @@ Message: \n\
 {1} \n\
  \n\
 Consulter la fiche : \n\
-{7}
+{{link}}
 
 # SiteName / Task / recordTitle statusName by userName
 status_change_doiCreationTask_email_subject={0} / Action / {1} pour '{{'index:resourceTitleObject'}}'
@@ -102,7 +102,7 @@ Message: \n\
 {1} \n\
 \n\
 Consulter la fiche : \n\
-{7}
+{{link}}
 # TODO: Link to DOI creation panel
 metadata_published_subject=%s / Publication de m\u00E9tadonn\u00E9es
 metadata_published_text=Les fiches suivantes ont \u00E9t\u00E9 trait\u00E9es:\n\

web/src/main/webapp/WEB-INF/classes/org/fao/geonet/api/Messages.properties
@@ -101,7 +101,7 @@ Message: \n\
 {1} \n\
  \n\
 View record: \n\
-{7}
+{{link}}
 
 # SiteName / Task / recordTitle statusName by userName
 status_change_doiCreationTask_email_subject={0} / Task / {1} for '{{'index:resourceTitleObject'}}'
@@ -113,7 +113,7 @@ Message: \n\
 {1} \n\
 \n\
 View record: \n\
-{7}
+{{link}}
 # TODO: Link to DOI creation panel
 metadata_published_subject=%s / Metadata publication
 metadata_published_text=The following records have been processed:\n\

web/src/main/webapp/WEB-INF/classes/org/fao/geonet/api/Messages_fre.properties
@@ -89,7 +89,7 @@ Message: \n\
 {1} \n\
  \n\
 Consulter la fiche : \n\
-{7}
+{{link}}
 
 # SiteName / Task / recordTitle statusName by userName
 status_change_doiCreationTask_email_subject={0} / Action / {1} pour '{{'index:resourceTitleObject'}}'
@@ -102,7 +102,7 @@ Message: \n\
 {1} \n\
 \n\
 Consulter la fiche : \n\
-{7}
+{{link}}
 # TODO: Link to DOI creation panel
 metadata_published_subject=%s / Publication de m\u00E9tadonn\u00E9es
 metadata_published_text=Les fiches suivantes ont \u00E9t\u00E9 trait\u00E9es:\n\

commit 16f4c910b52dc494dfde9ab8a6c77b66561330bf

Escape {{ to avoid error parsing argument. (#7237)
Regression from #7130
Fixes the following error.
    Error is: can't parse argument number: {index:_uuid}

core/src/main/java/org/fao/geonet/kernel/metadata/DefaultStatusActions.java
@@ -267,7 +267,7 @@ public class DefaultStatusActions implements StatusActions {
         // Replace link in message
         ApplicationContext applicationContext = ApplicationContextHolder.get();
         SettingManager sm = applicationContext.getBean(SettingManager.class);
-        textTemplate = textTemplate.replace("{{link}}", sm.getNodeURL()+ "api/records/{{index:uuid}}");
+        textTemplate = textTemplate.replace("{{link}}", sm.getNodeURL()+ "api/records/'{{'index:uuid'}}'");
 
         UserRepository userRepository = context.getBean(UserRepository.class);
         User owner = userRepository.findById(status.getOwner()).orElse(null);
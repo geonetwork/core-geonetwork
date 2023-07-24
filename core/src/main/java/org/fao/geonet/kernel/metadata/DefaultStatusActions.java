16f4c910b52dc494dfde9ab8a6c77b66561330bf
Ian <ianwallen@hotmail.com>
2023-07-24 08:36:50 -0300

    Escape {{ to avoid error parsing argument. (#7237)
    Regression from #7130
    Fixes the following error.
        Error is: can't parse argument number: {index:_uuid}

--- core/src/main/java/org/fao/geonet/kernel/metadata/DefaultStatusActions.java
+++ core/src/main/java/org/fao/geonet/kernel/metadata/DefaultStatusActions.java
@@ -267,7 +267,7 @@ public class DefaultStatusActions implements StatusActions {
         // Replace link in message
         ApplicationContext applicationContext = ApplicationContextHolder.get();
         SettingManager sm = applicationContext.getBean(SettingManager.class);
-        textTemplate = textTemplate.replace("{{link}}", sm.getNodeURL()+ "api/records/{{index:uuid}}");
+        textTemplate = textTemplate.replace("{{link}}", sm.getNodeURL()+ "api/records/'{{'index:uuid'}}'");
 
         UserRepository userRepository = context.getBean(UserRepository.class);
         User owner = userRepository.findById(status.getOwner()).orElse(null);
