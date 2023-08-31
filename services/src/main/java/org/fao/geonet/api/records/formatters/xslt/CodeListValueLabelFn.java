d126733e90a7c653d840f56284e080a01adbae27
sebr72 <sebastien_riollet@hotmail.com>
2023-08-31 12:00:36 +0200

    Refactored to avoid code duplication

--- services/src/main/java/org/fao/geonet/api/records/formatters/xslt/CodeListValueLabelFn.java
+++ services/src/main/java/org/fao/geonet/api/records/formatters/xslt/CodeListValueLabelFn.java
@@ -2,17 +2,15 @@ package org.fao.geonet.api.records.formatters.xslt;
 
 import net.sf.saxon.expr.XPathContext;
 import net.sf.saxon.lib.ExtensionFunctionCall;
-import net.sf.saxon.lib.ExtensionFunctionDefinition;
 import net.sf.saxon.om.Item;
 import net.sf.saxon.om.Sequence;
 import net.sf.saxon.om.StructuredQName;
 import net.sf.saxon.trans.XPathException;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
-import org.apache.commons.lang3.StringUtils;
 import org.fao.geonet.api.records.formatters.SchemaLocalizations;
 
-public class CodeListValueLabelFn extends ExtensionFunctionDefinition {
+public class CodeListValueLabelFn extends GnExtensionFunctionDefinition {
     @Override
     public StructuredQName getFunctionQName() {
         return new StructuredQName(
@@ -36,21 +34,12 @@ public class CodeListValueLabelFn extends ExtensionFunctionDefinition {
         return new ExtensionFunctionCall() {
             @Override
             public Sequence call(XPathContext context, Sequence[] arguments) throws XPathException {
-                Item schema = arguments[0].head();
-                String schemaVal = (schema != null) ? schema.getStringValue() : null;
-                Item lang = arguments[1].head();
-                String langVal = (lang != null && StringUtils.isNotEmpty(lang.getStringValue())) ? lang.getStringValue() : null;
                 Item codeListName = arguments[2].head();
                 String codeListNameVal = (codeListName != null) ? codeListName.getStringValue() : null;
                 Item codeListValue = arguments[3].head();
                 String codeListValueVal = (codeListValue != null) ? codeListValue.getStringValue() : null;
                 try {
-                    SchemaLocalizations schemaLocalizations;
-                    if (langVal == null) {
-                        schemaLocalizations = SchemaLocalizations.create(schemaVal);
-                    } else {
-                        schemaLocalizations = SchemaLocalizations.create(schemaVal, langVal);
-                    }
+                    SchemaLocalizations schemaLocalizations = getSchemaLocalizations(arguments);
                     String response = schemaLocalizations.codelistValueLabel(codeListNameVal, codeListValueVal);
                     return StringValue.makeStringValue(response);
                 } catch (Exception e) {
@@ -59,4 +48,5 @@ public class CodeListValueLabelFn extends ExtensionFunctionDefinition {
             }
         };
     }
+
 }
--- services/src/main/java/org/fao/geonet/api/records/formatters/xslt/NodeLabelFn.java
+++ services/src/main/java/org/fao/geonet/api/records/formatters/xslt/NodeLabelFn.java
@@ -2,17 +2,15 @@ package org.fao.geonet.api.records.formatters.xslt;
 
 import net.sf.saxon.expr.XPathContext;
 import net.sf.saxon.lib.ExtensionFunctionCall;
-import net.sf.saxon.lib.ExtensionFunctionDefinition;
 import net.sf.saxon.om.Item;
 import net.sf.saxon.om.Sequence;
 import net.sf.saxon.om.StructuredQName;
 import net.sf.saxon.trans.XPathException;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
-import org.apache.commons.lang3.StringUtils;
 import org.fao.geonet.api.records.formatters.SchemaLocalizations;
 
-public class NodeLabelFn extends ExtensionFunctionDefinition {
+public class NodeLabelFn extends GnExtensionFunctionDefinition {
     @Override
     public StructuredQName getFunctionQName() {
         return new StructuredQName(
@@ -46,21 +44,12 @@ public class NodeLabelFn extends ExtensionFunctionDefinition {
         return new ExtensionFunctionCall() {
             @Override
             public Sequence call(XPathContext context, Sequence[] arguments) throws XPathException {
-                Item schema = arguments[0].head();
-                String schemaVal = (schema != null) ? schema.getStringValue() : null;
-                Item lang = arguments[1].head();
-                String langVal = (lang != null && StringUtils.isNotEmpty(lang.getStringValue())) ? lang.getStringValue() : null;
                 Item nodeName = arguments[2].head();
                 String nodeNameVal = (nodeName != null) ? nodeName.getStringValue() : null;
                 Item parentNodeName = arguments[3].head();
                 String parentNodeNameVal = (parentNodeName != null) ? parentNodeName.getStringValue() : null;
                 try {
-                    SchemaLocalizations schemaLocalizations;
-                    if (langVal == null) {
-                        schemaLocalizations = SchemaLocalizations.create(schemaVal);
-                    } else {
-                        schemaLocalizations = SchemaLocalizations.create(schemaVal, langVal);
-                    }
+                    SchemaLocalizations schemaLocalizations = getSchemaLocalizations(arguments);
                     String response = schemaLocalizations.nodeLabel(nodeNameVal, parentNodeNameVal);
                     return StringValue.makeStringValue(response);
                 } catch (Exception e) {
