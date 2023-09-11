984294e91f58ef2d946cd0801e7c5fe61d85f3a5
Juan Luis Rodríguez Ponce <juanluisrp@geocat.net>
2023-09-11 10:04:03 +0200

    [BP] Fix robots.txt and sitemap 500 errors if no right content type is provided (#7327)
    * Return 200 OK for robots.txt and sitemap. Return 200 OK for /robots.xt and /srv/api/sitemap instead
    of 500 "service not found". Previously if the request didn't contain "Accept: text/plain" or
    "Accept: application/xml" the server returned a 500 error. Now the server accepts any "Accept" header
    without complaining, returning a 200 response with "Content-Type: text/plain" or
    "Content-Type: application/xml" and the right content.
    * Disallow /catalog and /static in robots.txt. Some search engines are indexing the Angular JS HTML
    template files. Exclude the ${context_path}/catalog and ${context_path}/static from being crawled by
    robots. Fix https://github.com/geonetwork/core-geonetwork/issues/3585.

--- services/src/main/java/org/fao/geonet/api/site/SitemapApi.java
+++ services/src/main/java/org/fao/geonet/api/site/SitemapApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2023 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -23,21 +23,10 @@
 
 package org.fao.geonet.api.site;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.fao.geonet.NodeInfo;
-import org.fao.geonet.api.API;
 import org.fao.geonet.api.ApiParams;
-import org.fao.geonet.domain.ISODate;
-import org.fao.geonet.domain.Metadata;
-import org.fao.geonet.domain.MetadataDataInfo_;
-import org.fao.geonet.domain.Metadata_;
-import org.fao.geonet.domain.OperationAllowed;
-import org.fao.geonet.domain.OperationAllowedId_;
-import org.fao.geonet.domain.ReservedOperation;
+import org.fao.geonet.domain.*;
 import org.fao.geonet.exceptions.SitemapDocumentNotFoundEx;
 import org.fao.geonet.kernel.GeonetworkDataDirectory;
 import org.fao.geonet.kernel.setting.SettingManager;
@@ -52,34 +41,30 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.domain.Specifications;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
 import static org.fao.geonet.api.ApiParams.API_CLASS_CATALOG_TAG;
 
-/**
- *
- */
-
 @RequestMapping(value = {
-    "/{portal}/api",
-    "/{portal}/api/" + API.VERSION_0_1
+        "/{portal}/api"
 })
 @Api(value = API_CLASS_CATALOG_TAG,
-    tags = API_CLASS_CATALOG_TAG,
-    description = ApiParams.API_CLASS_CATALOG_OPS)
+        tags = API_CLASS_CATALOG_TAG,
+        description = ApiParams.API_CLASS_CATALOG_OPS)
 @Controller("sitemap")
 public class SitemapApi {
     private static final String FORMAT_XML = "xml";
@@ -105,83 +90,84 @@ public class SitemapApi {
 
 
     @ApiOperation(
-        value = "robots.txt",
-        notes = "",
-        nickname = "getRobotsText")
+            value = "robots.txt",
+            notes = "",
+            nickname = "getRobotsText")
     @RequestMapping(
-        path = "/robots.txt",
-        produces = MediaType.TEXT_PLAIN_VALUE,
-        method = RequestMethod.GET)
+            path = "/robots.txt",
+            method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "robots.txt file for SEO.")
+            @ApiResponse(code = 200, message = "robots.txt file for SEO.")
     })
-    @ResponseBody
-    public String getRobotsText() throws Exception {
-        StringBuffer response = new StringBuffer();
-        response.append("sitemap: ").append(settingManager.getNodeURL()).append("api/sitemap").append("\n");
-        response.append("sitemap: ").append(settingManager.getNodeURL()).append("api/sitemap?format=rdf");
-        return response.toString();
+    public void getRobotsText(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        StringBuilder content = new StringBuilder(256);
+        String contextPath = request.getContextPath();
+        content.append("User-agent: *\n");
+        content.append("Disallow: ").append(contextPath).append("/catalog/\n");
+        content.append("Disallow: ").append(contextPath).append("/static/\n");
+        content.append("Sitemap: ").append(settingManager.getNodeURL()).append("api/sitemap\n");
+        content.append("Sitemap: ").append(settingManager.getNodeURL()).append("api/sitemap?format=rdf");
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+        response.getWriter().append(content);
     }
 
     @ApiOperation(
-        value = "Get sitemap",
-        notes = "",
-        nickname = "getSitemap")
+            value = "Get sitemap",
+            notes = "",
+            nickname = "getSitemap")
     @RequestMapping(
-        path = "/sitemap",
-        produces = MediaType.APPLICATION_XML_VALUE,
-        method = RequestMethod.GET)
+            path = "/sitemap",
+            method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "Site map.")
+            @ApiResponse(code = 200, message = "Site map.")
     })
-    public @ResponseBody
-    ResponseEntity<Element> getSitemap(
-        @ApiParam(
-            value = "Format (xml or html).",
-            required = false
-        )
-        @RequestParam(
-            required = false,
-            defaultValue = FORMAT_HTML
-        )
+    public void getSitemap(
+            @ApiParam(
+                    value = "Format (xml or html).",
+                    required = false
+            )
+            @RequestParam(
+                    required = false,
+                    defaultValue = FORMAT_HTML
+            )
             String format,
-        @ApiParam(
-            value = "page.",
-            required = false
-        )
-        @RequestParam(
-            required = false,
-            defaultValue = "0"
-        )
+            @ApiParam(
+                    value = "page.",
+                    required = false
+            )
+            @RequestParam(
+                    required = false,
+                    defaultValue = "0"
+            )
             Integer doc,
-        @ApiIgnore
-            HttpServletRequest request
+            @ApiIgnore
+            HttpServletResponse response
     ) throws Exception {
         if (!(format.equalsIgnoreCase(FORMAT_HTML) ||
-            format.equalsIgnoreCase(FORMAT_XML))) {
+                format.equalsIgnoreCase(FORMAT_XML))) {
             format = FORMAT_HTML;
         }
 
-        Integer allgroup = 1;
+        Integer allGroup = 1;
 
         Specifications<OperationAllowed> spec = Specifications.where(
-            OperationAllowedSpecs.hasOperation(ReservedOperation.view));
-        spec = spec.and(OperationAllowedSpecs.hasGroupId(allgroup));
+                OperationAllowedSpecs.hasOperation(ReservedOperation.view));
+        spec = spec.and(OperationAllowedSpecs.hasGroupId(allGroup));
 
         final List<Integer> list = operationAllowedRepository.findAllIds(
-            spec,
-            OperationAllowedId_.metadataId);
+                spec,
+                OperationAllowedId_.metadataId);
 
         Sort sortByChangeDateDesc = new Sort(
-            Sort.Direction.DESC,
-            Metadata_.dataInfo.getName() + "." + MetadataDataInfo_.changeDate.getName());
+                Sort.Direction.DESC,
+                Metadata_.dataInfo.getName() + "." + MetadataDataInfo_.changeDate.getName());
 
-        long metadatataCount = metadataRepository.count((Specification<Metadata>) MetadataSpecs.hasMetadataIdIn(list));
-        long pages = (long) Math.ceil((double) metadatataCount / MAX_ITEMS_PER_PAGE);
+        long metadataCount = metadataRepository.count((Specification<Metadata>) MetadataSpecs.hasMetadataIdIn(list));
+        long pages = (long) Math.ceil((double) metadataCount / MAX_ITEMS_PER_PAGE);
 
-        Element result = null;
+        Element result;
 
         if (doc > 0) {
             // Requesting a sitemap specific document
@@ -197,7 +183,7 @@ public class SitemapApi {
             }
         } else {
             // Request the sitemap (no specific document)
-            if (metadatataCount <= MAX_ITEMS_PER_PAGE) {
+            if (metadataCount <= MAX_ITEMS_PER_PAGE) {
                 // Request the full sitemap
                 result = metadataRepository.findAllUuidsAndChangeDatesAndSchemaId(list);
 
@@ -218,14 +204,14 @@ public class SitemapApi {
             }
         }
         Path xslt = dataDirectory.getWebappDir()
-            .resolve("xslt/services/sitemap/sitemap.xsl");
+                .resolve("xslt/services/sitemap/sitemap.xsl");
         Element root = new Element("root");
         Element requestElt = new Element("request");
         requestElt.addContent(new Element("format").setText(format));
         root.addContent(requestElt);
         root.addContent(result);
 
-        Element sitemap = Xml.transform(root, xslt);
-        return new ResponseEntity<>(sitemap, HttpStatus.OK);
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+        Xml.transform(root, xslt, response.getOutputStream());
     }
 }
