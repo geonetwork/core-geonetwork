80d6e57035a76d1a26275b9e9d028331825dbc92
Jose García <josegar74@gmail.com>
2023-10-03 14:01:22 +0200

    Jetty bundle / Add JAVA_OPTS configuration for the /monitor/metrics endpoint to work correctly

--- release/bin/startup.bat
+++ release/bin/startup.bat
@@ -7,7 +7,7 @@ del logs\*request.log*
 del logs\output.log
 move logs\geonetwork.log.* logs\archive
 
-java -Xms512m -Xmx1g -Djetty.httpConfig.requestHeaderSize=32768 -Dorg.eclipse.jetty.server.Request.maxFormContentSize=500000 -Dorg.eclipse.jetty.server.Request.maxFormKeys=4000 -Dmime-mappings=..\web\geonetwork\WEB-INF\mime-types.properties -DSTOP.PORT=8079 -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar
+java -Xms512m -Xmx1g --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED -Djetty.httpConfig.requestHeaderSize=32768 -Dorg.eclipse.jetty.server.Request.maxFormContentSize=500000 -Dorg.eclipse.jetty.server.Request.maxFormKeys=4000 -Dmime-mappings=..\web\geonetwork\WEB-INF\mime-types.properties -DSTOP.PORT=8079 -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar
 
 rem Try changing the Xmx parameter if having memory errors
 rem If you want to hide the dos window when GeoNetwork is started, comment the previous line and comment out the last line
