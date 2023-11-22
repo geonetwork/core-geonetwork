dbe8ace46eedb1ec831e8dcbf2310c4ee8fa9fbe
fxprunayre <fx.prunayre@gmail.com>
2023-11-22 11:02:32 +0000

    Update version to 4.4.1-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v441/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v441/migrate-default.sql
@@ -1,2 +1,2 @@
 UPDATE Settings SET value='4.4.1' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
