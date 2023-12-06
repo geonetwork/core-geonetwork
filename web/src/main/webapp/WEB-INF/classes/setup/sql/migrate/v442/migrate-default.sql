5c60a60c9e01aaa91090530855bc5baf07795b0f
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2023-12-06 10:16:02 +0100

    add icon to static pages, configurable

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v442/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v442/migrate-default.sql
@@ -1,2 +1,4 @@
 UPDATE Settings SET value='4.4.2' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+ALTER TABLE public.spg_page ADD icon varchar NULL;
