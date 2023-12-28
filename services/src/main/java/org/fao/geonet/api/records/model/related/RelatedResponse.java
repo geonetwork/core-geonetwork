64a45540a5d42212bfef478151f27dbac4ad7390
Ian Allen <ianwallen@hotmail.com>
2023-12-27 20:39:30 -0400

    Fix SpringDoc duplicate Schema name Without this fix springdoc would randomly pick between 2 conflicting schemas and this could produce incorrect results in the open api spec.

--- services/src/main/java/org/fao/geonet/api/records/model/related/RelatedResponse.java
+++ services/src/main/java/org/fao/geonet/api/records/model/related/RelatedResponse.java
@@ -31,6 +31,7 @@ import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  * <p>Java class for relatedResponse complex type.
@@ -1037,6 +1038,8 @@ public class RelatedResponse {
     @XmlType(name = "", propOrder = {
         "item"
     })
+    // Use different schema name for source to resolve conflict with org.fao.geonet.domain.Source
+    @Schema(name = "RelatedSource")
     public static class Source implements IListOnlyClassToArray {
 
         protected List<RelatedMetadataItem> item;
