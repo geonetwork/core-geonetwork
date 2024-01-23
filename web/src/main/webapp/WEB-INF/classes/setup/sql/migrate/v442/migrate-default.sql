a499d1eb57a0f4a0a6596ae91ccecbefe0e0ee9b
WANGF <feifei.wang@dfo-mpo.gc.ca>
2024-01-23 11:03:19 -0500

    revert 442 script merge junks

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v442/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v442/migrate-default.sql
@@ -2,5 +2,3 @@ UPDATE Settings SET value='4.4.2' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/useGeodesicExtents', 'false', 2, 9591, 'n');
-
-ALTER TABLE public.spg_page ADD icon varchar NULL;
