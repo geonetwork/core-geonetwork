38e31a5b458d380b722750a8922420e7c0ffe70b
WANGF <feifei.wang@dfo-mpo.gc.ca>
2024-01-23 10:57:07 -0500

    443 migration script

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
@@ -1,2 +1,5 @@
 UPDATE Settings SET value='4.4.3' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+UPDATE settings SET name='metadata/history/enabled' WHERE name='system/metadata/history/enabled'
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'metadata/history/accesslevel', 'Editor', 0, 12021, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'metadata/history/accesslevel');
