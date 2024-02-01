46281e9eb711f2e551842264fcf24e24070917f7
GeoNetwork opensource <59019313+geonetworkbuild@users.noreply.github.com>
2024-02-01 11:32:14 +0100

    [Backport 4.2.x] Metadata editor / Fix javascript error in the add thumbnail option when the metadata has 1 WMS layer (#7684)
    * Metadata editor / Fix javascript error in the add thumbnail option when the metadata has 1 WMS layer:
    
    Error: [orderBy:notarray] http://errors.angularjs.org/1.8.2/orderBy/notarray\?p0\=%7B%22resource%22%3A%7B%22ref%22%3A%22172%22%2C%22refParent%22%3A%22171%22%2C%22name%22%3A%222%22%2C%22url%22%3A%22https%3A%2F%2Fmaps-cartes.ec.gc.ca%2Farcgis%2Fservices%2FDMS%2FStressCumulatifLittorales%2FMapServer%2FWMSServer%3Frequest%3DGetCapabilities%26service%3DWMS%22%2C%22title%22%3A%22Metadata%20WMS%20layer%22%2C%22abstract%22%3A%22The%20ISO19115%20metadata%20standard%20is%20the%20preferred%20metadata%20standard%20to%20use.%20If%20unsure%20what%20templates%20to%20start%20with%2C%20use%20this%20one.%22%2C%22protocol%22%3A%22OGC%3AWMS%22%7D%7D
        at lib.js?v=65b94734a63ca8491dba95d7439baf1be02c2450:21:168
    
    * Metadata editor / Improve layer configuration init. (#96)
    
    * Metadata editor / Improve layer configuration init.
    
    Related to https://github.com/geonetwork/core-geonetwork/pull/7646.
    
    * Metadata editor / Improve layer configuration init.
    
    * Metadata editor / Remove log.
    
    ---------
    
    Co-authored-by: Jose García <josegar74@gmail.com>
    Co-authored-by: François Prunayre <fx.prunayre@gmail.com>

--- web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
+++ web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
@@ -680,7 +680,6 @@
                 //   });
                 // }
                 // Add each WMS layer to the map
-                scope.layers = scope.gnCurrentEdit.layerConfig;
                 angular.forEach(scope.gnCurrentEdit.layerConfig, function (layer) {
                   scope.map.addLayer(
                     new ol.layer.Tile({
