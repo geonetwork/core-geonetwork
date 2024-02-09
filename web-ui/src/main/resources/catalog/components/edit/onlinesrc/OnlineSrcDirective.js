2506c09e7c3fce9e8855135ce98fa546ac10cfd2
WANGF <feifei.wang@dfo-mpo.gc.ca>
2024-02-09 12:07:36 -0500

    Main.prevent.delete.multiple.online.resources.iso.19139.schema (#18)
    * Passing the index into remove process xslt for iso 19139
    
    * Updated resource logic to use 2 new field called idx and hash and revert id back to the old method.  This will ensure backward compatibility
    Also update xslt for add and remove thumbnail/onlines recources
    
    * Add missing <xsl:copy> that was causing bugs.
    
    * unit test fix
    
    * Fix issues based on code review.
      - Fix issue with ordering of elements.
      - Fix issue with md5Hex not working
    
    * Update schemas/iso19139/src/main/plugin/iso19139/process/thumbnail-remove.xsl
    
    Co-authored-by: Jose García <josegar74@gmail.com>
    
    * Update schemas/iso19139/src/main/plugin/iso19139/process/onlinesrc-add.xsl
    
    Co-authored-by: Jose García <josegar74@gmail.com>
    
    * Update schemas/iso19139/src/main/plugin/iso19139/process/onlinesrc-remove.xsl
    
    Co-authored-by: Jose García <josegar74@gmail.com>
    
    * Update schemas/iso19139/src/main/plugin/iso19139/process/onlinesrc-remove.xsl
    
    Co-authored-by: Ian <ianwallen@hotmail.com>
    
    * Update schemas/iso19139/src/main/plugin/iso19139/process/thumbnail-remove.xsl
    
    Co-authored-by: Ian <ianwallen@hotmail.com>
    
    ---------
    
    Co-authored-by: Ian Allen <ianwallen@hotmail.com>
    Co-authored-by: Jose García <josegar74@gmail.com>

--- web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
+++ web-ui/src/main/resources/catalog/components/edit/onlinesrc/OnlineSrcDirective.js
@@ -981,6 +981,8 @@
                       }
                     }
                     scope.editingKey = [keyUrl, linkToEdit.protocol, keyName].join("");
+                    scope.editingIdx = linkToEdit.idx;
+                    scope.editingHash = linkToEdit.hash;
 
                     scope.OGCProtocol = checkIsOgc(linkToEdit.protocol);
 
@@ -1029,6 +1031,8 @@
                     };
                   } else {
                     scope.editingKey = null;
+                    scope.editingIdx = null;
+                    scope.editingHash = null;
                     scope.params.linkType = typeConfig;
                     scope.params.protocol = null;
                     scope.params.mimeType = "";
@@ -1220,6 +1224,8 @@
 
                 if (scope.isEditing) {
                   processParams.updateKey = scope.editingKey;
+                  processParams.resourceIdx = scope.editingIdx;
+                  processParams.resourceHash = scope.editingHash;
                 }
 
                 // Add list of layers for WMS
