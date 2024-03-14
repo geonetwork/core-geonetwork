a99f09e716dc8106466eb13214a4cea06618516e
Jose García <josegar74@gmail.com>
2024-03-14 08:33:13 +0100

    Update version to 3.12.12-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v31212/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v31212/migrate-default.sql
@@ -1,2 +1,2 @@
 UPDATE Settings SET value='3.12.12' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
