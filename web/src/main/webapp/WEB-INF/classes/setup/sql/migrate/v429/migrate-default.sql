a500988ee1506900c51d243ca998bf406d39eb3c
Jose García <josegar74@gmail.com>
2024-03-14 09:19:41 +0100

    Update version to 4.2.9-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v429/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v429/migrate-default.sql
@@ -1,4 +1,4 @@
 UPDATE Settings SET value='4.2.9' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/userFeedback/metadata/enable', 'false', 2, 1913, 'n');
