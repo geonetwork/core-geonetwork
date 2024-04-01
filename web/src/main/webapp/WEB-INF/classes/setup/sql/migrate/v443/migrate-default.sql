7d8e03d18482180ef63181732dff3ad207b473bb
WANGF <feifei.wang@dfo-mpo.gc.ca>
2024-04-01 09:21:18 -0400

    remove page creation script

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
@@ -4,13 +4,3 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 UPDATE Settings SET value='4.4.3' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
-
-CREATE TABLE spg_page_group
-(
-    language VARCHAR(255) not null,
-    linktext VARCHAR(255) not null,
-    groupid int not null,
-    primary key(language,linktext, groupid),
-    foreign key(language, linktext) references spg_page(language, linktext),
-    foreign key(groupid) references groups(id)
-);
