d61ee082d656e462d217bca9637b6519bb7157ea
Jure Mekiš <jure.mekis@igea.si>
2024-04-05 09:12:53 +0200

    fix #geonetwork/core-geonetwork/issues/7930

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v441/migrate-oracle.sql
@@ -0,0 +1,6 @@
+ALTER TABLE groups MODIFY name varchar(255);
+
+UPDATE Settings SET value='4.4.1' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
+
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'metadata/pdfReport/headerLogoFileName', '', 0, 12508, 'y' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'metadata/pdfReport/headerLogoFileName');
