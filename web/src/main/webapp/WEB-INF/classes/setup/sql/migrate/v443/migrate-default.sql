9fa5a4b540d0f2b598be8f2e969fc9e192ce9982
wangf1122 <74916635+wangf1122@users.noreply.github.com>
2024-04-05 09:37:19 -0400

    Update migrate-default.sql

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
@@ -3,5 +3,4 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 
 UPDATE Settings SET value='4.4.3' WHERE name='system/platform/version';
-
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
