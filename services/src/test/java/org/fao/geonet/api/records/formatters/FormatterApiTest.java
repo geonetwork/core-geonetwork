817ac6c13a039c416b7b09d5cb00bebf4051239d
Francois Prunayre <fx.prunayre@gmail.com>
2024-04-17 13:33:38 +0200

    Standard / ISO / DCAT formatters / SHACL / Rules are now working. Then we have to fix input document or decide to lose information on constraints.

--- services/src/test/java/org/fao/geonet/api/records/formatters/FormatterApiTest.java
+++ services/src/test/java/org/fao/geonet/api/records/formatters/FormatterApiTest.java
@@ -166,11 +166,13 @@ public class FormatterApiTest extends AbstractServiceIntegrationTest {
                     if (isRdf) {
                         String[] shaclValidation = {};
                         if ("eu-dcat-ap".equalsIgnoreCase(formatter)) {
-//                            shaclValidation = new String[]{"shacl/dcat-ap-2.1.1-base-SHACL.ttl"};
-//                            shaclValidation = new String[]{"shacl/dcat-ap-3.0.0-full.ttl"};
                             // https://github.com/ISAITB/validator-resources-dcat-ap/blob/master/resources/config.properties#L117-L128
-                            //+ shapes.ttl + range.ttl + shapes_recommended.ttl + imports.ttl + deprecateduris.ttl+
-                            // shaclValidation = new String[]{"shacl/eu-dcat-ap-3.0.0/shapes.ttl"};
+//                             shaclValidation = new String[]{
+//                                     "shacl/eu-dcat-ap-3.0.0/shapes.ttl",
+//                                     "shacl/eu-dcat-ap-3.0.0/range.ttl",
+//                                     "shacl/eu-dcat-ap-3.0.0/shapes_recommended.ttl",
+//                                     "shacl/eu-dcat-ap-3.0.0/imports.ttl",
+//                                     "shacl/eu-dcat-ap-3.0.0/deprecateduris.ttl"};
 //                        } else  if("eu-dcat-ap-hvd".equalsIgnoreCase(formatter)){
 //                            shaclValidation = new String[]{"shacl/dcat-ap-hvd-2.2.0-SHACL.ttl"};
 //                        } else  if("eu-geodcat-ap".equalsIgnoreCase(formatter)){
@@ -213,8 +215,14 @@ public class FormatterApiTest extends AbstractServiceIntegrationTest {
             fail(String.format("%s. RDF model error. %s.",
                     file, rdfException.getMessage()));
         }
-        String[] shaclValidation = new String[]{"dcat-ap-2.1.1-base-SHACL.ttl"};
-//        String[] shaclValidation = new String[]{"dcat-ap-3-SHACL.ttl"};
+//        String[] shaclValidation = new String[]{"shacl/dcat-ap-2.1.1-base-SHACL.ttl"};
+        String[] shaclValidation = new String[]{
+                "shacl/eu-dcat-ap-3.0.0/shapes.ttl",
+                "shacl/eu-dcat-ap-3.0.0/range.ttl",
+                "shacl/eu-dcat-ap-3.0.0/shapes_recommended.ttl",
+                "shacl/eu-dcat-ap-3.0.0/imports.ttl",
+                "shacl/eu-dcat-ap-3.0.0/deprecateduris.ttl"
+        };
 //        String[] shaclValidation = new String[]{"dcat-ap-hvd-2.2.0-SHACL.ttl"};
 //        String[] shaclValidation = new String[]{"geodcat-ap-2.0.1-SHACL.ttl"};
         for (String shaclShapes : shaclValidation) {
