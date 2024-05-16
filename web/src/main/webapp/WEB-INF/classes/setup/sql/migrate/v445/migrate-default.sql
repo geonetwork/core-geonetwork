a8f4d1126e1d5aa7a431fbfb0e53eee76a148a0e
tylerjmchugh <Tyler.McHugh@dfo-mpo.gc.ca>
2024-05-16 09:14:35 -0400

    Update migration script to only insert settings fields if not present

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v445/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v445/migrate-default.sql
@@ -1,5 +1,5 @@
 UPDATE Settings SET value='4.4.5' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
-INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/feedback/languages', '', 0, 646, 'n');
-INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/feedback/translationFollowsText', '', 0, 647, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/feedback/languages', '', 0, 646, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/feedback/languages');
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/feedback/translationFollowsText', '', 0, 647, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/feedback/translationFollowsText');
