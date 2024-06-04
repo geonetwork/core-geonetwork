8471646c1591263e16b6581b8bba30e83ef47192
François Prunayre <fx.prunayre@gmail.com>
2024-06-04 10:14:42 +0200

    Standard / ISO19115-3 / Only search for associated record with UUID
    Sources or feature catalogue reference may be described using description or uuidref.
    
    Do not consider text description while searching for associated records. eg.
    
    ```xml
       <mdb:resourceLineage >
          <mrl:LI_Lineage>
             <mrl:source>
                <mrl:LI_Source>
                   <mrl:description xsi:type="lan:PT_FreeText_PropertyType">
                      <gco:CharacterString>Le référentiel utilisé comme trait de côte est celui de Ifremer-Shom au 1/25 000 (date de révision 1995).</gco:CharacterString>
    ```
    
    This is not really visible but avoid to do search with an empty id in `MetadataUtils#getAssociated`

--- schemas/iso19115-3.2018/src/main/java/org/fao/geonet/schema/iso19115_3_2018/ISO19115_3_2018SchemaPlugin.java
+++ schemas/iso19115-3.2018/src/main/java/org/fao/geonet/schema/iso19115_3_2018/ISO19115_3_2018SchemaPlugin.java
@@ -181,7 +181,7 @@ public class ISO19115_3_2018SchemaPlugin
 
     @Override
     public Set<AssociatedResource> getAssociatedFeatureCatalogues(Element metadata) {
-        return collectAssociatedResources(metadata, "*//mrc:featureCatalogueCitation[@uuidref]");
+        return collectAssociatedResources(metadata, "*//mrc:featureCatalogueCitation[@uuidref != '']");
     }
 
     public Set<String> getAssociatedSourceUUIDs(Element metadata) {
@@ -193,7 +193,7 @@ public class ISO19115_3_2018SchemaPlugin
 
     @Override
     public Set<AssociatedResource> getAssociatedSources(Element metadata) {
-        return collectAssociatedResources(metadata, "*//mrl:source");
+        return collectAssociatedResources(metadata, "*//mrl:source[@uuidref != '']");
     }
 
     private Set<AssociatedResource> collectAssociatedResources(Element metadata, String xpath) {
