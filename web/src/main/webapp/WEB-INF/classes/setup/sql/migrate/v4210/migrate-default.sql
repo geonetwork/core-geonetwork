680b48cd156778c1beaf78917153e470f9d80736
Jose García <josegar74@gmail.com>
2024-06-14 09:09:08 +0200

    Update version to 4.2.10-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4210/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4210/migrate-default.sql
@@ -1,7 +1,7 @@
 UPDATE Metadata SET data = replace(data, 'http://standards.iso.org/iso/19115/-3/srv/2.1', 'http://standards.iso.org/iso/19115/-3/srv/2.0') WHERE data LIKE '%http://standards.iso.org/iso/19115/-3/srv/2.1%' AND schemaId = 'iso19115-3.2018';
 
 UPDATE Settings SET value='4.2.10' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/documentation/url', 'https://docs.geonetwork-opensource.org/{{version}}/{{lang}}', 0, 570, 'n');
 
