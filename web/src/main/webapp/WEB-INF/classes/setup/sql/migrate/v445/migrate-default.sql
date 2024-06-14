fa4210d63bcf82b2421717c106938002463c6136
Jose García <josegar74@gmail.com>
2024-06-14 11:34:46 +0200

    Register user / allow to configured allowed email domains - update migration scripts

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v445/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v445/migrate-default.sql
@@ -4,5 +4,3 @@ UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/provider', '', 0, 7301, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/serviceUrl', '', 0, 7302, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/apiKey', '', 0, 7303, 'y');
-
-INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/userSelfRegistration/domainsAllowed', '', 0, 1911, 'y');
