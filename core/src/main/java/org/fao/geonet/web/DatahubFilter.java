567e4e92e5ed4c70248b28911494c801411e47b6
Olivia <olivia.guyot@camptocamp.com>
2024-06-18 15:55:55 +0200

    add configuration for default datahub

--- core/src/main/java/org/fao/geonet/web/DatahubFilter.java
+++ core/src/main/java/org/fao/geonet/web/DatahubFilter.java
@@ -1,9 +1,18 @@
 package org.fao.geonet.web;
 
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.log4j.Logger;
+import org.fao.geonet.ApplicationContextHolder;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.kernel.setting.SettingManager;
+import org.fao.geonet.kernel.setting.Settings;
+import org.fao.geonet.utils.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,19 +23,20 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.GZIPOutputStream;
 
 public class DatahubFilter implements Filter {
     private static final Logger log = Logger.getLogger(DatahubFilter.class);
-//    final String PATH_TO_DIST_FOLDER = "/"; // this is relative to the context root
 
     @Override
     public void init(FilterConfig config) {
@@ -41,6 +51,13 @@ public class DatahubFilter implements Filter {
         String reqPath = req.getPathInfo();
         if (reqPath == null || !reqPath.startsWith("/srv/datahub")) {
             chain.doFilter(request, res);
+            return;
+        }
+
+        SettingManager settingManager = ApplicationContextHolder.get().getBean(SettingManager.class);
+        String isDatahubEnabled = settingManager.getValue(Settings.GEONETWORK_UI_DATAHUB_ENABLED);
+        if (!isDatahubEnabled.equals("true")) {
+            res.setStatus(404);
             return;
         }
 
@@ -60,6 +77,7 @@ public class DatahubFilter implements Filter {
             res.setHeader(HttpHeaders.PRAGMA, "no-cache");
             res.setHeader(HttpHeaders.EXPIRES, "0");
         }
+
         res.setStatus(200);
         String extension = FilenameUtils.getExtension(actualFile.getName()).toLowerCase();
         String contentType;
@@ -70,7 +88,14 @@ public class DatahubFilter implements Filter {
         }
         res.setContentType(contentType);
 
-        InputStream inStream = new FileInputStream(actualFile);
+        InputStream inStream;
+
+        // the config is read from the settings
+        if (actualFile.getName().equals("default.toml")) {
+            inStream = readConfiguration(portalName);
+        } else {
+            inStream = new FileInputStream(actualFile);
+        }
         OutputStream outStream = res.getOutputStream();
 
         // handle gzip compression
@@ -81,6 +106,21 @@ public class DatahubFilter implements Filter {
 
         IOUtils.copy(inStream, outStream);
         outStream.close();
+    }
+
+    private InputStream readConfiguration(String portalName) {
+        SettingManager settingManager = ApplicationContextHolder.get().getBean(SettingManager.class);
+        String datahubConfiguration = settingManager.getValue(Settings.GEONETWORK_UI_DATAHUB_CONFIGURATION);
+
+        Toml toml = new Toml().read(datahubConfiguration);
+        Map<String, Object> tomlMap = toml.toMap();
+
+        // Force the "gn4_api_url" field to a value including the portal name
+        tomlMap.put("geonetwork4_api_url", "/geonetwork/" + portalName + "/api");
+
+        TomlWriter tomlWriter = new TomlWriter();
+        String configuration = tomlWriter.write(tomlMap);
+        return new ByteArrayInputStream(configuration.getBytes());
     }
 
     @Override
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v445/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v445/migrate-default.sql
@@ -4,3 +4,79 @@ UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/provider', '', 0, 7301, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/serviceUrl', '', 0, 7302, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/apiKey', '', 0, 7303, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('geonetwork-ui/datahub/enabled', 'false', 2, 7304, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('geonetwork-ui/datahub/configuration', '
+[global]
+geonetwork4_api_url = "/geonetwork/srv/api"
+proxy_path = ""
+# metadata_language = "current"
+# login_url = "/cas/login?service=${current_url}"
+# web_component_embedder_url = "/datahub/wc-embedder.html"
+# languages = [''en'', ''fr'', ''de'']
+# contact_email = "opendata@mycompany.com"
+
+[theme]
+primary_color = "red"
+secondary_color = "blue"
+main_color = "#555"
+background_color = "#fdfbff"
+header_background = "var(--color-gray-500)"
+# header_foreground_color = ''white''
+# thumbnail_placeholder = ''assets/img/my_custom_placeholder.png''
+# main_font = "''My Custom Font'', fallback-font"
+# title_font = "''My Custom Title Font'', fallback-font-title"
+# fonts_stylesheet_url = "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Permanent+Marker&display=swap"
+# favicon = "assets/favicon.ico"
+
+[search]
+# filter_geometry_url = "https://my.domain.org/assets/boundary.geojson"
+# filter_geometry_data = ''{ "coordinates": [...], "type": "Polygon" }''
+# advanced_filters = [''publisher'', ''format'', ''publicationYear'', ''topic'', ''isSpatial'', ''license'']
+
+# [[search_preset]]
+#     name = ''filterByName''
+#     filters.q = ''Full text search''
+#     filters.publisher = [''Org 1'', ''Org 2'']
+#     filters.format = [''format 1'', ''format 2'']
+#     filters.documentStandard = [''iso19115-3.2018'']
+#     filters.inspireKeyword = [''keyword 1'', ''keyword 2'']
+#     filters.topic = [''boundaries'']
+#     filters.publicationYear = [''2023'', ''2022'']
+#     filters.isSpatial = [''yes'']
+#     filters.license = [''unknown'']
+#     sort = ''createDate''
+
+[metadata-quality]
+# enabled = true
+
+[map]
+# max_zoom = 10
+# max_extent = [-418263.418776, 5251529.591305, 961272.067714, 6706890.609855]
+# external_viewer_url_template = ''https://dev.geo2france.fr/mapstore/#/?actions=[{"type":"CATALOG:ADD_LAYERS_FROM_CATALOGS","layers":["${layer_name}"],"sources":[{"url":"${service_url}","type":"${service_type}"}]}]''
+# external_viewer_open_new_tab = false
+# do_not_tile_wms = false
+# do_not_use_default_basemap = false
+
+# [[map_layer]]
+# type = "xyz"
+# url = "https://{a-c}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+# [[map_layer]]
+# type = "wfs"
+# url = "https://www.geo2france.fr/geoserver/cr_hdf/ows"
+# name = "masque_hdf_ign_carto_latin1"
+# [[map_layer]]
+# type = "geojson"
+# data = """
+# {
+#   "type": "FeatureCollection",
+#   "features": [{"type": "Feature", "geometry": {"type": "Point", "coordinates": [125.6, 10.1]}}]
+# }
+# """
+
+# [translations.en]
+# results.sortBy.dateStamp = "Last time someone changed something"
+# [translations.fr]
+# results.sortBy.dateStamp = "Dernière fois que quelqu''un a modifié quelque chose"
+
+', 0, 7305, 'n');
