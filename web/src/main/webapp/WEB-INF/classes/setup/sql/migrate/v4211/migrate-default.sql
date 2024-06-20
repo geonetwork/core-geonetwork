eaa297da0f2c39db9149e273066873f818d5c231
Ian <ianwallen@hotmail.com>
2024-06-20 14:35:24 -0300

    Multilingual Emails (#8044) (#8210)
    * Add settings fields for localized emails
    
    * Add spring bean to initialize feedback email locales from settings field
    
    * Modify settings manager to update feedback locales when the settings fields are saved
    
    * Create classes for localized emails, their components, and the components' parameters
    
    * Implement lang in getIndexField
    
    * Localize workflow status emails
    
    * Localize metadata publication emails
    
    * Localize user feedback emails
    
    * Localize RegisterApi emails
    
    * Localize PasswordApi emails
    
    * Localize WatchListNotifier emails
    
    * Localize MailApi emails
    
    * Update migration script to only insert settings fields if not present
    
    * Add static enum imports for readability
    
    * Log a warning when a locale is invalid or missing
    
    * Update log modules and messages
    
    * Trim language codes defined in settings to handle spaces after commas
    
    * Rename translation follows label to translation follows text for consistency
    
    * Add back resource bundle 'messages' that was unused before merging main
    
    * Add logic to break from loop when email subject and text messages fail
    
    Co-authored-by: tylerjmchugh <163562062+tylerjmchugh@users.noreply.github.com>

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4211/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4211/migrate-default.sql
@@ -2,3 +2,6 @@ UPDATE Settings SET value='4.2.11' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/userSelfRegistration/domainsAllowed', '', 0, 1911, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/feedback/languages', '', 0, 646, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/feedback/languages');
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/feedback/translationFollowsText', '', 0, 647, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/feedback/translationFollowsText');
