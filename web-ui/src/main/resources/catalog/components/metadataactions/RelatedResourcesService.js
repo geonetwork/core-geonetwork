b1caf6b998a0dfa30875f553f2c71f9041405da1
Jose García <josegar74@gmail.com>
2024-08-27 12:02:38 +0200

    Record view / don't display the add to map button for WFS resources when the map viewer is disabled. (#8314)

--- web-ui/src/main/resources/catalog/components/metadataactions/RelatedResourcesService.js
+++ web-ui/src/main/resources/catalog/components/metadataactions/RelatedResourcesService.js
@@ -129,55 +129,9 @@
       var addEsriRestToMap =
         gnViewerSettings.resultviewFns && gnViewerSettings.resultviewFns.addMdLayerToMap;
 
-      var addWFSToMap = function (link, md) {
-        var url = $filter("gnLocalized")(link.url) || link.url;
-
-        var isServiceLink =
-          gnSearchSettings.mapProtocols.services.indexOf(link.protocol) > -1;
-
-        var isGetFeatureLink = url.toLowerCase().indexOf("request=getfeature") > -1;
-
-        var featureName;
-        if (isGetFeatureLink) {
-          var name = "typename";
-          var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-          var results = regex.exec(url);
-
-          if (results) {
-            featureName = decodeURIComponent(results[1].replace(/\+/g, " "));
-          }
-        } else {
-          featureName = $filter("gnLocalized")(link.title) || link.name;
-        }
-
-        // if an external viewer is defined, use it here
-        if (gnExternalViewer.isEnabled()) {
-          gnExternalViewer.viewService(
-            {
-              id: md ? md.id : null,
-              uuid: md ? md.uuid : null
-            },
-            {
-              type: "wfs",
-              url: url,
-              name: featureName
-            }
-          );
-          return;
-        }
-        if (featureName && (!isServiceLink || isGetFeatureLink)) {
-          gnMap.addWfsFromScratch(
-            gnSearchSettings.viewerMap,
-            url,
-            featureName,
-            false,
-            md
-          );
-        } else {
-          gnMap.addOwsServiceToMap(url, "WFS");
-        }
-        gnSearchLocation.setMap();
-      };
+      var addWFSToMap =
+        gnViewerSettings.resultviewFns &&
+        gnViewerSettings.resultviewFns.addMdWFSLayerToMap;
 
       var addWMTSToMap =
         gnViewerSettings.resultviewFns && gnViewerSettings.resultviewFns.addMdLayerToMap;
