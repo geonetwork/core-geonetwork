05b2e43dd5ce5a340081269461560af480b85af1
Jose García <josegar74@gmail.com>
2024-09-05 15:40:44 +0200

    CSW Harvester / Avoid increment 2 metrics for a single metadata in certain conditions (#8069)
    * CSW Harvester / Avoid increment 2 metrics for a single metadata in certain conditions.
    
    Retrieve metadata method increments metrics under certain conditions and returns null. Methods calling the retrieve metadata method increments additionally the unretrievable metric if null is returned. Sonarlint fixeas are already applied. Fixes #8039
    
    * CSW Harvester / Use primitive boolean for 'force' parameter

--- harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
+++ harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
@@ -1,5 +1,5 @@
 //=============================================================================
-//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===	Copyright (C) 2001-2024 Food and Agriculture Organization of the
 //===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
 //===	and United Nations Environment Programme (UNEP)
 //===
@@ -232,7 +232,7 @@ public class Aligner extends BaseAligner<CswParams> {
                 }
 
                 result.totalMetadata++;
-            } catch (Throwable t) {
+            } catch (Exception t) {
                 errors.add(new HarvestError(this.context, t));
                 log.error("Unable to process record from csw (" + this.params.getName() + ")");
                 log.error("   Record failed: " + ri.uuid + ". Error is: " + t.getMessage());
@@ -285,7 +285,6 @@ public class Aligner extends BaseAligner<CswParams> {
         Element md = retrieveMetadata(ri.uuid);
 
         if (md == null) {
-            result.unretrievable++;
             return;
         }
 
@@ -404,7 +403,7 @@ public class Aligner extends BaseAligner<CswParams> {
                     if (StringUtils.isNotEmpty(batchEditParameter.getCondition())) {
                         applyEdit = false;
                         final Object node = Xml.selectSingle(md, batchEditParameter.getCondition(), metadataSchema.getNamespaces());
-                        if (node != null && node instanceof Boolean && (Boolean)node == true) {
+                        if (node instanceof Boolean && Boolean.TRUE.equals(node)) {
                             applyEdit = true;
                         }
                     }
@@ -424,7 +423,7 @@ public class Aligner extends BaseAligner<CswParams> {
             }
         }
     }
-    private void updateMetadata(RecordInfo ri, String id, Boolean force) throws Exception {
+    private void updateMetadata(RecordInfo ri, String id, boolean force) throws Exception {
         String date = localUuids.getChangeDate(ri.uuid);
 
         if (date == null && !force) {
@@ -443,11 +442,10 @@ public class Aligner extends BaseAligner<CswParams> {
         }
     }
     @Transactional(value = TxType.REQUIRES_NEW)
-    boolean updatingLocalMetadata(RecordInfo ri, String id, Boolean force) throws Exception {
+    boolean updatingLocalMetadata(RecordInfo ri, String id, boolean force) throws Exception {
         Element md = retrieveMetadata(ri.uuid);
 
         if (md == null) {
-            result.unchangedMetadata++;
             return false;
         }
 
@@ -500,8 +498,11 @@ public class Aligner extends BaseAligner<CswParams> {
     }
 
     /**
-     * Does CSW GetRecordById request. If validation is requested and the metadata does not
-     * validate, null is returned.
+     * Does CSW GetRecordById request. Returns null on error conditions:
+     *  - If validation is requested and the metadata does not validate.
+     *  - No metadata is retrieved.
+     *  - If metadata resource is duplicated.
+     *  - An exception occurs retrieving the metadata.
      *
      * @param uuid uuid of metadata to request
      * @return metadata the metadata
@@ -524,6 +525,7 @@ public class Aligner extends BaseAligner<CswParams> {
             //--- maybe the metadata has been removed
 
             if (list.isEmpty()) {
+                result.unretrievable++;
                 return null;
             }
 
@@ -544,11 +546,10 @@ public class Aligner extends BaseAligner<CswParams> {
                 return null;
             }
 
-            if (params.rejectDuplicateResource) {
-                if (foundDuplicateForResource(uuid, response)) {
+            if (params.rejectDuplicateResource && (foundDuplicateForResource(uuid, response))) {
                     result.unchangedMetadata++;
                     return null;
-                }
+
             }
 
             return response;
@@ -612,7 +613,7 @@ public class Aligner extends BaseAligner<CswParams> {
                         }
                     }
                 }
-            } catch (Throwable e) {
+            } catch (Exception e) {
                 log.warning("      - Error when searching for resource duplicate " + uuid + ". Error is: " + e.getMessage());
             }
         }
