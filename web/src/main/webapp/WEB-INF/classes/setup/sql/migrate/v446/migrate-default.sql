832a023532758c933374ceb590b0b5e2e93707fa
Jose García <josegar74@gmail.com>
2024-10-09 18:56:34 +0200

    Add configuration to display an application banner

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v446/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v446/migrate-default.sql
@@ -2,3 +2,6 @@ UPDATE Settings SET value='4.4.6' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/userSelfRegistration/domainsAllowed', '', 0, 1911, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/banner/enable', 'false', 2, 1920, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/banner/message', '', 0, 1921, 'n');
