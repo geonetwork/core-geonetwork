2e55b577807206f3fa9c79346c31dea2c5dd61f7
WANGF <feifei.wang@dfo-mpo.gc.ca>
2024-11-22 14:44:53 -0500

    Revert "isoLang variable, help wording"
    This reverts commit 0b000ce2

--- web-ui/src/main/resources/catalog/components/common/needhelp/NeedHelpDirective.js
+++ web-ui/src/main/resources/catalog/components/common/needhelp/NeedHelpDirective.js
@@ -155,7 +155,6 @@
               baseUrl = scope.helpBaseUrl.replace("/{{lang}}", "");
             }
 
-            baseUrl = baseUrl.replace("{{isoLang}}", scope.applicationVersion);
             baseUrl = baseUrl.replace("{{version}}", scope.applicationVersion);
 
             var helpPageUrl;
