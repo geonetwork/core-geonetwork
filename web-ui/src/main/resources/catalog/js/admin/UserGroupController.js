f4890d56b0a61427ea5c9731cc8315ba7e247125
Jose García <josegar74@gmail.com>
2025-01-16 14:15:06 +0100

    Audit user changes (#8137)
    * Audit user changes
    
    * Audit user changes - update translations
    
    * Add README.md file
    
    * Update UserAuditable  model to track user address changes and add unit test for UserAuditable
    
    * Audit user changes - change to use Hibernate Envers due to Javers dependency conflict with Geotools for the picocontainer version
    
    * Audit user changes - update README.md documentation
    
    * Code formatting and styling
    
    * Update UsersApi to remove invalid user group assignment in the user update endpoint
    
    ---------
    
    Co-authored-by: Juan Luis Rodríguez <juanluisrp@gmail.com>

--- web-ui/src/main/resources/catalog/js/admin/UserGroupController.js
+++ web-ui/src/main/resources/catalog/js/admin/UserGroupController.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2024 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -27,10 +27,12 @@
   goog.require("gn_dbtranslation");
   goog.require("gn_multiselect");
   goog.require("gn_mdtypewidget");
+  goog.require("gn_auditable");
 
   var module = angular.module("gn_usergroup_controller", [
     "gn_dbtranslation",
     "gn_multiselect",
+    "gn_auditable",
     "gn_mdtypewidget",
     "blueimp.fileupload",
     "ngMessages"
@@ -47,8 +49,10 @@
     "$rootScope",
     "$translate",
     "$timeout",
+    "$log",
     "gnConfig",
     "gnConfigService",
+    "gnAuditableService",
     function (
       $scope,
       $routeParams,
@@ -56,8 +60,10 @@
       $rootScope,
       $translate,
       $timeout,
+      $log,
       gnConfig,
-      gnConfigService
+      gnConfigService,
+      gnAuditableService
     ) {
       $scope.searchObj = {
         params: {
@@ -117,6 +123,7 @@
 
       $scope.isLoadingUsers = false;
       $scope.isLoadingGroups = false;
+      $scope.auditableEnabled = false;
 
       gnConfigService.load().then(function (c) {
         // take the bigger of the two values
@@ -135,6 +142,8 @@
             gnConfig["system.security.passwordEnforcement.pattern"]
           );
         }
+
+        $scope.auditableEnabled = gnConfig["system.auditable.enable"];
       });
 
       // This is to force IE11 NOT to cache json requests
@@ -312,9 +321,21 @@
                 // TODO
               }
             );
+
+            // Load user changes
+            gnAuditableService.getEntityHistory("user", u.id).then(
+              function (response) {
+                $scope.userHistory = response.data;
+              },
+              function (response) {
+                // TODO
+                $log.error("Error retrieving the audit history of user " + u.id);
+              }
+            );
           },
           function (response) {
             // TODO
+            $log.error("Error retrieving the info of user " + u.id);
           }
         );
 
