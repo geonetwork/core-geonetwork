a35d7726f3c059d54460de8a7da5ce66e3200b6f
tylerjmchugh <Tyler.McHugh@dfo-mpo.gc.ca>
2025-01-29 14:52:40 -0500

    Update getFilenameFromUrl to url decode the file name

--- core/src/main/java/org/fao/geonet/api/records/attachments/AbstractStore.java
+++ core/src/main/java/org/fao/geonet/api/records/attachments/AbstractStore.java
@@ -48,8 +48,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -170,6 +173,7 @@ public abstract class AbstractStore implements Store {
         if (fileName.contains("?")) {
             fileName = fileName.substring(0, fileName.indexOf("?"));
         }
+        fileName = URLDecoder.decode(fileName, StandardCharsets.UTF_8);
         return fileName;
     }
 
