87924e64af7bc38c707af39cd38a30110e1affba
Olivia <olivia.guyot@camptocamp.com>
2025-03-20 12:13:18 +0100

    better Sources admin UI for Datahub

--- web-ui/src/main/resources/catalog/js/admin/SourcesController.js
+++ web-ui/src/main/resources/catalog/js/admin/SourcesController.js
@@ -39,7 +39,12 @@
       $scope.source = null;
       $scope.filteredSources = null;
       $scope.filter = {
-        types: { portal: true, subportal: true, externalportal: true, harvester: true }
+        types: {
+          portal: true,
+          subportal: true,
+          externalportal: true,
+          harvester: true
+        }
       };
 
       $scope.serviceRecordSearchObj = {
@@ -87,16 +92,21 @@
         },
         true
       );
+
+      $scope.defaultConfig = "";
+
       function loadDatahubStatus() {
         $http
           .get("../datahub/status")
-          .then(function () {
+          .then(function (response) {
             $scope.datahubAvailable = true;
+            $scope.defaultConfig = response.data.defaultConfig;
           })
           .catch(function () {
             $scope.datahubAvailable = false;
           });
       }
+
       function loadSources() {
         var url = "../api/sources";
         if ($scope.user.profile === "UserAdmin") {
@@ -105,7 +115,9 @@
         $http.get(url).then(function (response) {
           $scope.sources = response.data;
           if ($scope.source && $scope.source.uuid !== null) {
-            var selectedSource = _.find($scope.sources, { uuid: $scope.source.uuid });
+            var selectedSource = _.find($scope.sources, {
+              uuid: $scope.source.uuid
+            });
             if (selectedSource) {
               $scope.source = selectedSource;
             }
@@ -144,76 +156,7 @@
           groupOwner: null,
           listableInHeaderSelector: true,
           datahubEnabled: false,
-          datahubConfiguration: `[global]
-proxy_path = ""
-# metadata_language = "current"
-# login_url = "/cas/login?service=http://localhost:8080/"
-# web_component_embedder_url = "/datahub/wc-embedder.html"
-# languages = [''en'', ''fr'', ''de'']
-# contact_email = "opendata@mycompany.com"
-
-[theme]
-primary_color = "#c82850"
-secondary_color = "#001638"
-main_color = "#555"
-background_color = "#fdfbff"
-# header_background = "center /cover url(''assets/img/header_bg.webp'')" or "var(--color-gray-500)"
-# header_foreground_color = ''white''
-# thumbnail_placeholder = ''assets/img/my_custom_placeholder.png''
-# main_font = "''My Custom Font'', fallback-font"
-# title_font = "''My Custom Title Font'', fallback-font-title"
-# fonts_stylesheet_url = "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Permanent+Marker&display=swap"
-# favicon = "assets/favicon.ico"
-
-[search]
-# filter_geometry_url = "https://my.domain.org/assets/boundary.geojson"
-# filter_geometry_data = ''{ "coordinates": [...], "type": "Polygon" }''
-# advanced_filters = [''publisher'', ''format'', ''publicationYear'', ''topic'', ''isSpatial'', ''license'']
-
-# [[search_preset]]
-#     name = ''filterByName''
-#     filters.q = ''Full text search''
-#     filters.publisher = [''Org 1'', ''Org 2'']
-#     filters.format = [''format 1'', ''format 2'']
-#     filters.documentStandard = [''iso19115-3.2018'']
-#     filters.inspireKeyword = [''keyword 1'', ''keyword 2'']
-#     filters.topic = [''boundaries'']
-#     filters.publicationYear = [''2023'', ''2022'']
-#     filters.isSpatial = [''yes'']
-#     filters.license = [''unknown'']
-#     sort = ''createDate''
-
-[metadata-quality]
-# enabled = true
-
-[map]
-# max_zoom = 10
-# max_extent = [-418263.418776, 5251529.591305, 961272.067714, 6706890.609855]
-# external_viewer_url_template = ''https://dev.geo2france.fr/mapstore/#/?actions=[{"type":"CATALOG:ADD_LAYERS_FROM_CATALOGS","layers":["geonetwork"],"sources":[{"url":"http://localhost:8080/srv/datahub","type":"gzip"}]}]''
-# external_viewer_open_new_tab = false
-# do_not_tile_wms = false
-# do_not_use_default_basemap = false
-
-# [[map_layer]]
-# type = "xyz"
-# url = "https://{a-c}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
-# [[map_layer]]
-# type = "wfs"
-# url = "https://www.geo2france.fr/geoserver/cr_hdf/ows"
-# name = "masque_hdf_ign_carto_latin1"
-# [[map_layer]]
-# type = "geojson"
-# data = """
-# {
-#   "type": "FeatureCollection",
-#   "features": [{"type": "Feature", "geometry": {"type": "Point", "coordinates": [125.6, 10.1]}}]
-# }
-# """
-
-# [translations.en]
-# results.sortBy.dateStamp = "Last time someone changed something"
-# [translations.fr]
-# results.sortBy.dateStamp = "Dernière fois que quelqu''un a modifié quelque chose"`
+          datahubConfiguration: $scope.defaultConfig
         };
       };
 
