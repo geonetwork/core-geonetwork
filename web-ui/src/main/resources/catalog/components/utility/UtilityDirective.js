82967178035a097d2235979fe9ac5a702b8fed94
Jody Garnett <jody.garnett@gmail.com>
2025-04-09 16:08:21 -0700

    Update version to 4.4.8-SNAPSHOT

--- web-ui/src/main/resources/catalog/components/utility/UtilityDirective.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityDirective.js
@@ -2420,7 +2420,7 @@
           scope.$on("$locationChangeSuccess", checkActive);
 
           // Watch for changes in the link attribute
-          attrs.$observe('gnActiveTbItem', function (newLink) {
+          attrs.$observe("gnActiveTbItem", function (newLink) {
             link = newLink;
             updateHref();
             checkActive();
