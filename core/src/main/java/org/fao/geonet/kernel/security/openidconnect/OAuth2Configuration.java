1deff2b019c71db748c6a8d67c6f3dd2b91d47ca
Xie <Changning.Xie@dfo-mpo.gc.ca>
2025-04-25 14:08:28 -0400

    remove @Configuration in OAuth2Config

--- core/src/main/java/org/fao/geonet/kernel/security/openidconnect/OAuth2Configuration.java
+++ core/src/main/java/org/fao/geonet/kernel/security/openidconnect/OAuth2Configuration.java
@@ -3,11 +3,9 @@ package org.fao.geonet.kernel.security.openidconnect;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
-@Configuration
 public class OAuth2Configuration {
 
     /**
