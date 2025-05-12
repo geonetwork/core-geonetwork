d0e560b2ae02021ddd5304ec0ec756b587162b10
Jose García <josegar74@gmail.com>
2025-05-12 12:55:12 +0200

    Revert Record view / fix permalink option.

--- web-ui/src/main/resources/catalog/components/search/mdview/mdviewDirective.js
+++ web-ui/src/main/resources/catalog/components/search/mdview/mdviewDirective.js
@@ -576,27 +576,6 @@
     }
   ]);
 
-  module.directive("gnMetadataSocialLink", [
-    "gnMetadataActions",
-    "$http",
-    function (gnMetadataActions, $http) {
-      return {
-        templateUrl: "../../catalog/components/search/mdview/partials/social.html",
-        scope: {
-          md: "=gnMetadataSocialLink"
-        },
-        link: function (scope, element, attrs) {
-          scope.mdService = gnMetadataActions;
-          $http
-            .get("../api/records/" + scope.md.getUuid() + "/permalink")
-            .then(function (r) {
-              scope.socialMediaLink = r.data;
-            });
-        }
-      };
-    }
-  ]);
-
   module.directive("gnQualityMeasuresTable", [
     function () {
       return {
