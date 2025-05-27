42984e723d2c8e7b62e85915c730a617d5041ad1
Jose García <josegar74@gmail.com>
2025-05-27 12:19:29 +0200

    Update version to 4.2.13-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4213/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4213/migrate-default.sql
@@ -1,4 +1,4 @@
 UPDATE Settings SET value='4.2.13' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/delete/backupOptions', 'UseAPIParameter', 0, 12012, 'n');
