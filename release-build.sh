91cb90d2ac41a30ab4c61c1835d389507fde15e8
Jose García <josegar74@gmail.com>
2025-05-27 13:11:18 +0200

    Update version to 4.2.14-SNAPSHOT

--- release-build.sh
+++ release-build.sh
@@ -79,7 +79,7 @@ fi
 
 if [[ ${OSTYPE:0:6} == 'darwin' ]]; then
   md5 -r web/target/geonetwork.war > web/target/geonetwork.war.MD5
-  md5 -r release/target/GeoNetwork-$version/geonetwork-bundle-$newversion.zip > release/target/GeoNetwork-$nextVersionNumber/geonetwork-bundle-$newversion.zip.MD5
+  md5 -r release/target/GeoNetwork-$version/geonetwork-bundle-$newversion.zip > release/target/GeoNetwork-$version/geonetwork-bundle-$newversion.zip.MD5
 else
   (cd web/target && md5sum geonetwork.war > geonetwork.war.MD5)
   (cd release/target/GeoNetwork-$version && md5sum geonetwork-bundle-$newversion.zip > geonetwork-bundle-$newversion.zip.MD5)
