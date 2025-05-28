be429a7016f98425530df50785682b6482992884
tylerjmchugh <163562062+tylerjmchugh@users.noreply.github.com>
2025-05-28 11:33:02 -0400

    [Backport 4.2.x] Hide enable workflow button when group owner does not match workflow group matching regex (#8699)
    * add isProfileOrMoreForGroup method
    
    * Hide enable workflow button if group name does not match regex
    
    * Move workflow option display logic to function
    
    * Hide workflow label when no options are shown
    
    * Move configuration variables to CatController
    
    * Hide workflow status label from editor board
    
    * Prevent enabling workflow for groups with workflow disabled in API
    
    * Refactor
    
    * Hide warnPublishDraft confirmation when workflow is not enabled
    
    * Add message
    
    * Display status label if workflow is already enabled
    
    * Prevent setting status from null to any status for records owned by groups with workflow disabled
    
    * Add dutch messages
    
    * Remove ES6 code

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -1979,6 +1979,17 @@
                     }
                     return false;
                   };
+                  me["is" + profile + "OrMoreForGroup"] = function (groupId) {
+                    if ("Administrator" == profile) {
+                      return me.admin;
+                    }
+                    var allowedProfiles = $scope.profiles.slice(
+                      $scope.profiles.indexOf(profile)
+                    );
+                    return allowedProfiles.some(function(p) {
+                      return me["is" + p + "ForGroup"](groupId);
+                    });
+                  };
                 });
                 angular.extend($scope.user, me);
                 angular.extend($scope.user, userFn);
