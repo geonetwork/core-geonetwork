2608ba32bb4fee912701b863e427c48f5f547a54
Francois Prunayre <fx.prunayre@gmail.com>
2025-06-03 17:30:53 +0200

    OAIPMH server / Preserve _ in conversion file name. The default oai_dc value is using it.

--- core/src/main/java/org/fao/geonet/kernel/oaipmh/OaiPmhParams.java
+++ core/src/main/java/org/fao/geonet/kernel/oaipmh/OaiPmhParams.java
@@ -102,8 +102,9 @@ public class OaiPmhParams {
         }
 
         if (StringUtils.isNotEmpty(metadataPrefix)) {
-            //--- remove any non letter, number, dot or dash character
-            String processedMetadataPrefix = metadataPrefix.replaceAll("[^a-zA-Z0-9.-]", "");
+            // remove any non letter, number, dot, underscore or dash character
+            // (metadata prefix correspond to schema plugin identifiers or conversion filename eg. oai_dc)
+            String processedMetadataPrefix = metadataPrefix.replaceAll("[^a-zA-Z0-9.-_]", "");
             params.put(OaiPmh.ParamNames.METADATA_PREFIX, processedMetadataPrefix);
         }
 
