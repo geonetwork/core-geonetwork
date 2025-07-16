e32c02e36668d813c427d3fbcdc7e32654d1854b
François Prunayre <fx.prunayre@gmail.com>
2025-07-16 10:23:08 +0200

    Migrate OAIPMH server to Elasticsearch / Spring MVC (#8841)
    * Migrate OAIPMH server to Elasticsearch
    
    * Migrate OAIPMH server to Elasticsearch / Update documentation
    
    * Migrate OAIPMH server API to Spring MVC
    
    * Migrate OAIPMH server to Elasticsearch / fix total calculation
    
    * OAIPMH server / Sonarlint improvements
    
    * OAIPMH server / Fix base URL
    
    * OAIPMH server / Model API parameters and Sonarlint improvements
    
    * OAIPMH server / Remove non valid chars from the value to match the xslt filename to apply
    
    * OAIPMH server / Preserve _ in conversion file name. The default oai_dc value is using it.
    
    * OAIPMH server / Documentation with more examples.
    
    * OAIPMH server / Refactor / Move conversion oai_dc to formatter.
    
    * OAIPMH server improvements
    
    * As in v3, do not add a filter on schema depending on prefix (the prefix was only defining the conversion to use)
    * Update to current Elasticsearch client
    * Better error message
    * Improve conversion by using formatters:
      * if metadataPrefix is the record schema plugin identifier, use the XML as is
      * if metadataPrefix is available as a formatter, use the formatter
      * if not, skip the record
    
    * OAIPMH server / Documentation with more examples and details.
    
    * OAIPMH server / Update test and fix date format in response.
    
    * OAIPMH server / Add new formatter to test.
    
    * OAIPMH server / Display API in SpringDoc
    
    * OAIPMH server / Converter / Log exception if any.
    
    ---------
    
    Co-authored-by: Jose García <josegar74@gmail.com>

---
+++ services/src/main/java/org/fao/geonet/api/oaipmh/OaiPmhApi.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2001-2023 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+package org.fao.geonet.api.oaipmh;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jeeves.server.context.ServiceContext;
+import org.fao.geonet.api.ApiUtils;
+import org.fao.geonet.kernel.oaipmh.OaiPmhDispatcher;
+import org.fao.geonet.kernel.oaipmh.OaiPmhParams;
+import org.jdom.Element;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping(value = {
+    "/{portal}/api/oaipmh"
+})
+@Tag(name = "oaipmh",
+    description = "OAI-PMH server")
+@RestController("oaipmh-server")
+public class OaiPmhApi {
+
+    @Autowired
+    private OaiPmhDispatcher oaiPmhDispatcher;
+
+    @Operation(
+        summary = "OAI-PMH server endpoint",
+        description = "[More information](https://docs.geonetwork-opensource.org/4.4/api/oai-pmh/)")
+    @GetMapping(
+        produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OAI-PMH server response.")
+    })
+    @ResponseBody
+    public Element dispatch(
+        @ParameterObject final OaiPmhParams oaiPmhParams,
+        final HttpServletRequest request
+    ) {
+        ServiceContext serviceContext = ApiUtils.createServiceContext(request);
+        // Set the service name, used in OaiPmhDispatcher to build the oaiphm endpoint URL
+        serviceContext.setService("api/oaipmh");
+
+        return oaiPmhDispatcher.dispatch(oaiPmhParams, serviceContext);
+    }
+}
