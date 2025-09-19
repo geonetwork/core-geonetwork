f5e770a52eccce3f6d8f1fa00a538a6c7ce6cba3
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-09-19 12:29:43 +0200

    nominal test

--- core/src/main/java/org/fao/geonet/security/GrantViewMdAuthorityFilter.java
+++ core/src/main/java/org/fao/geonet/security/GrantViewMdAuthorityFilter.java
@@ -1,5 +1,7 @@
 package org.fao.geonet.security;
 
+import org.fao.geonet.repository.AnonymousAccessLinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -10,6 +12,9 @@ import javax.servlet.ServletResponse;
 import java.io.IOException;
 
 public class GrantViewMdAuthorityFilter extends GenericFilterBean {
+
+    @Autowired
+    AnonymousAccessLinkRepository anonymousAccessLinkRepository;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
