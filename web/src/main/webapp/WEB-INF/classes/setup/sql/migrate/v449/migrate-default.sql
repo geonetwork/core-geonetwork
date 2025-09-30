eff7d2f68518f07fac1d6362efbbcf458826a132
Jose García <josegar74@gmail.com>
2025-09-30 09:26:21 +0200

    Always delete the old DOI configuration in the DB migration (#9016)
    Remove the old DOI configuration, which was not removed during the migration to version 4.4.5 if the DOI server was disabled (#9016)

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v449/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v449/migrate-default.sql
@@ -1,4 +1,8 @@
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/publication/doi/doimailnotification', 'false', 2, 100192, 'n');
 
+-- Migration to 4.4.5 only removed the old DOI settings, when the DOI server was defined.
+-- Related to https://github.com/geonetwork/core-geonetwork/pull/8098
+DELETE FROM Settings WHERE name LIKE 'system/publication/doi%' and name != 'system/publication/doi/doienabled';
+
 UPDATE Settings SET value='4.4.9' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
