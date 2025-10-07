c7e60066d6e112498fd4525497090bb6c1840ff1
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-10-07 10:28:35 +0200

    rename test method

--- services/src/test/java/org/fao/geonet/api/anonymousAccessLink/AnonymousAccessLinkApiTest.java
+++ services/src/test/java/org/fao/geonet/api/anonymousAccessLink/AnonymousAccessLinkApiTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jeeves.server.context.ServiceContext;
 import org.fao.geonet.domain.AbstractMetadata;
 import org.fao.geonet.domain.AnonymousAccessLink;
-import org.fao.geonet.kernel.schema.AssociatedResourcesSchemaPlugin;
 import org.fao.geonet.repository.AnonymousAccessLinkRepository;
 import org.fao.geonet.services.AbstractServiceIntegrationTest;
 import org.junit.Test;
@@ -34,7 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class AnonymousAccessLinkTest extends AbstractServiceIntegrationTest {
+public class AnonymousAccessLinkApiTest extends AbstractServiceIntegrationTest {
 	@Autowired
 	private WebApplicationContext wac;
 
