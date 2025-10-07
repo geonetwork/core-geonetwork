506b7dee5dc048a7eda631fd8c7ce6cf77db3eda
josegar74 <josegar74@gmail.com>
2025-10-07 09:21:14 +0200

    Update version to 4.2.14-0

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4214/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4214/migrate-default.sql
@@ -1,2 +1,2 @@
 UPDATE Settings SET value='4.2.14' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
