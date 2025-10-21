52ac088313c8641a84a8f663777eaa904e172fa1
Ian Allen <ianwallen@hotmail.com>
2025-10-21 11:21:38 -0300

    Move system/metadata/edit/supportedFileMimetypes setting to from v444 to v4410 migration script. Fixes changes from PR #7710

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4410/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4410/migrate-default.sql
@@ -1,2 +1,4 @@
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/metadata/edit/supportedFileMimetypes', 'image/png|image/gif|image/jpeg|text/plain|application/xml|application/pdf', 0, 9107, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/metadata/edit/supportedFileMimetypes');
+
 UPDATE Settings SET value='4.4.10' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
