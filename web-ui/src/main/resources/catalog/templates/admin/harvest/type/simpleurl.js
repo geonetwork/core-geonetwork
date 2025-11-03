b6d8ef8bf02940284d3cb6521cdc8be1718a27ee
Antonio Cerciello <antonio.cerciello@gmail.com>
2025-11-03 22:26:50 +0100

    UI changes to manage param for SimpleURL harvester

--- web-ui/src/main/resources/catalog/templates/admin/harvest/type/simpleurl.js
+++ web-ui/src/main/resources/catalog/templates/admin/harvest/type/simpleurl.js
@@ -18,6 +18,7 @@ var gnHarvestersimpleurl = {
           "password" : []
         },
         "url" : "http://",
+        "apiKey" : "",
         "loopElement" : "",
         "numberOfRecordPath": "",
         "pageSizeParam": "",
@@ -87,6 +88,7 @@ var gnHarvestersimpleurl = {
       + '      <username>' + h.site.account.username + '</username>'
       + '      <password>' + h.site.account.password + '</password>'
       + '    </account>'
+      + '    <apiKey>' + h.site.apiKey + '</apiKey>'
       + '    <loopElement>' + h.site.loopElement + '</loopElement>'
       + '    <numberOfRecordPath>' + h.site.numberOfRecordPath + '</numberOfRecordPath>'
       + '    <recordIdPath>' + h.site.recordIdPath + '</recordIdPath>'
