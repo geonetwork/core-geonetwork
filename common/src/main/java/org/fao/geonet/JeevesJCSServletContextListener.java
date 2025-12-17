4543dcd940846eff628d88bae8dd34b11267eed5
Jody Garnett <jody.garnett@gmail.com>
2025-12-17 12:08:49 -0800

    Register JCS.shutdown with out own JeevesJCSServletContextListener

--- common/src/main/java/org/fao/geonet/JeevesJCSServletContextListener.java
+++ common/src/main/java/org/fao/geonet/JeevesJCSServletContextListener.java
@@ -1,4 +1,13 @@
 package org.fao.geonet;
 
+import jakarta.servlet.ServletContextEvent;
+import org.apache.commons.jcs3.JCS;
+
 public class JeevesJCSServletContextListener implements jakarta.servlet.ServletContextListener {
+
+    @Override
+    public void contextDestroyed( final ServletContextEvent arg0 )
+    {
+        JCS.shutdown();
+    }
 }
