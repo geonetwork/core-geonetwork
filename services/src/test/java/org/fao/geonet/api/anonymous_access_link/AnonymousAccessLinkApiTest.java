dede449bd53b97f671a35953af0c247208464471
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-12-24 11:02:01 +0100

    hyphenated package name

--- services/src/test/java/org/fao/geonet/api/anonymous_access_link/AnonymousAccessLinkApiTest.java
+++ services/src/test/java/org/fao/geonet/api/anonymous_access_link/AnonymousAccessLinkApiTest.java
@@ -21,7 +21,7 @@
  * Rome - Italy. email: geonetwork@osgeo.org
  */
 
-package org.fao.geonet.api.anonymousAccessLink;
+package org.fao.geonet.api.anonymous_access_link;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jeeves.server.context.ServiceContext;
