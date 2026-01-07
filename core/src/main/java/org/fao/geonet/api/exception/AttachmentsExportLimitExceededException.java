922a80c4afdf8d164b637b3f008f955319091419
tylerjmchugh <Tyler.McHugh@dfo-mpo.gc.ca>
2026-01-07 10:39:58 -0500

    Update header to 2026

--- core/src/main/java/org/fao/geonet/api/exception/AttachmentsExportLimitExceededException.java
+++ core/src/main/java/org/fao/geonet/api/exception/AttachmentsExportLimitExceededException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2024 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2026 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
