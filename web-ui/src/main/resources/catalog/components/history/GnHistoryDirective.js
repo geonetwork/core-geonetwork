geonetwork/core-geonetwork

commit 3fd0c87214fce344b4e91933efe5a3a9a6f29da4

Allow to restore a metadata record from deleted records (Part 3) (#4817)
* Restore from history functionality

* Behavior when draft exists

* Fixed migration merge

* Removed MetadataStatusId Serialization and Deserialization and used existing format of /{metadataUuid}/status/{statusId:[0-9]+}.{userId:[0-9]+}.{changeDate}
Removed rollback option as a rollback is the same as a restore.  Also removed logic to delete status of rollback status.
Renamed view to view previous version and changed version and added new showStatusAfter api to support this.
Database
Added missing fre language for insert
Move migration inserts to 3110 and also added missing translation.

* Allow capability to recover deleted records
Changed PK key for metadatastatus so that it is a single key.
Added groupownname to geonet:info to assist with the recovery.
Add metadataStatus entry for deleted objects.
    Added UUID and Titles to the status so that the data is available for deleted records as well.

* removed duplicate status_id which is the same as the statusvalue.

* Fixed improper resolved conflict which accidentally converted file to ascci causing script to fail.

* Updated migration script
attempted to load titles but failed.

* More versioning changes
Changed titles to LinkedHashMap so that the order is static.
Updated status search api so that we can query based on id and uuid.
Updated API results
   Changed RelatedMetadataStatus to only return the id instead of the object.
   Return titles as a json object (applied temp hack until we upgrade jpa)
   renamed *statusEmpty to *stateEmpty for consistency
   Fixed bug with some fields being null when they should have values.
   Removed *statusDetail as we already had the *state fields from the parent.  If we want them named statusDetail then we should rename the fields in MetadataStatus.
Also updated migration script to attempt to fix primary key issue - but may need more work.

web-ui/src/main/resources/catalog/components/history/GnHistoryDirective.js
@@ -101,14 +101,16 @@
 
   module
     .directive(
-      'gnRecordHistoryStep', ['gnRecordTaskService', 'gnRecordHistoryService',
-        function(gnRecordTaskService, gnRecordHistoryService) {
+      'gnRecordHistoryStep', ['gnRecordTaskService', 'gnRecordHistoryService', '$translate', '$window',
+        function(gnRecordTaskService, gnRecordHistoryService, $translate, $window) {
           return {
             restrict: 'A',
             replace: true,
             scope: {
               h: '=gnRecordHistoryStep',
               noTitle: '@noTitle',
+              noSourceViewOption: '@noTitle',
+              noRecoverOption: '@noTitle',
               allowRemoval: '=allowRemoval'
             },
             templateUrl:
@@ -142,6 +144,36 @@
                 gnRecordHistoryService.delete(s).then(function(r) {
                   scope.$parent.loadHistory();
                 });
+              };
+
+              scope.restoreHistoryElement = function(statusId) {
+                confirmMessage = $translate.instant('confirmRestore');
+                if($window.confirm(confirmMessage)) {
+                    return gnRecordHistoryService.restoreHistoryElement(statusId).then(function(r) {
+                          message = $translate.instant('recordRestored');
+                          scope.$emit('StatusUpdated', {
+                              msg: message,
+                              timeout: 0,
+                              type: 'info'});
+                          // Reload the page to load the new value
+                          $window.location.reload();
+                    }, function(response) {
+                          message = '';
+                          if(response.status === 403) {
+                              message = $translate.instant('notAllowedError');
+                          } else if(response.status === 404) {
+                              message = $translate.instant('notFoundError');
+                          } else if(response.status === 500) {
+                              message = $translate.instant('internalServerError');
+                          } else {
+                              message = $translate.instant('internalServerError');
+                          }
+                          scope.$emit('StatusUpdated', {
+                              msg: message,
+                              timeout: 0,
+                              type: 'danger'});
+                    });
+                }
               };
 
               scope.closeTask = function(status) {

commit 6a390c6a3f0f2d40d99a00fd6e15f5b5eb595215

Record history / Fix view and restore buttons not showing
The noSourceViewOption and noRecoverOption scope properties in the
gnRecordHistoryStep directive were both incorrectly bound to the
noTitle HTML attribute instead of their own respective attributes.

Since the recordHistory.html template passes data-no-title="true",
both properties evaluated to true, causing the View previous version,
View changed version and Restore buttons to be permanently hidden
via data-ng-hide.

Bug introduced in PR #4817 (November 2020).

web-ui/src/main/resources/catalog/components/history/GnHistoryDirective.js
@@ -122,6 +122,17 @@
     }
   ]);
 
+  /**
+   * @ngdoc directive
+   * @name gnRecordHistoryStep
+   * @attr {Object} gnRecordHistoryStep - The history step object to display.
+   * @attr {boolean} [noTitle] - When true, hides the record title link.
+   * @attr {boolean} [noSourceViewOption] - When true, hides the "View previous
+   *   version" and "View changed version" buttons.
+   * @attr {boolean} [noRecoverOption] - When true, hides the "Restore" button.
+   * @attr {boolean} [allowRemoval] - When true, shows the remove step button
+   *   (restricted to administrators).
+   */
   module.directive("gnRecordHistoryStep", [
     "gnDoiService",
     "gnRecordHistoryService",
@@ -134,8 +145,8 @@
         scope: {
           h: "=gnRecordHistoryStep",
           noTitle: "@noTitle",
-          noSourceViewOption: "@noTitle",
-          noRecoverOption: "@noTitle",
+          noSourceViewOption: "@noSourceViewOption",
+          noRecoverOption: "@noRecoverOption",
           allowRemoval: "=allowRemoval"
         },
         templateUrl: "../../catalog/components/history/partials/historyStep.html",