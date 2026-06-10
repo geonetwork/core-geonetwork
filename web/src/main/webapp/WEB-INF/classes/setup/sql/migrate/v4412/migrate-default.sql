c7be4e4278114655938a3de117eb5167d4e96630
Jose García <josegar74@gmail.com>
2026-06-10 15:04:50 +0200

    CSW GetRecords fails when requesting an outputSchema not supported by a metadata schema. (#6941)
    * CSW GetRecords fails when requesting an outputSchema not supported by a metadata schema. Fixes #6940
    
    * Setting to configure the behaviour
    
    * Update documentation

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4412/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4412/migrate-default.sql
@@ -1,2 +1,4 @@
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/csw/getRecordsIgnoreMetadataNotSupported', 'true', 2, 1321, 'y');
+
 UPDATE Settings SET value='4.4.12' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
