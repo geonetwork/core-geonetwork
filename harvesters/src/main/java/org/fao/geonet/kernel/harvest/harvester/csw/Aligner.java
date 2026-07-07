geonetwork/core-geonetwork

commit 86c8dc5eb5e1a272e13144fb644f80384d5c7797

Harvesters / Check if the metadata schema of a metadata to update has changed (#9052)
Not a usual case, but some metadata schemas like Dutch datasets / services have different versions
of the schemas and users can upgrade to the newer versions the existing metadata.

The harvesters code when updating a metadata, was not checking if the schema version changed, 
only updating the XML.

This pull request updates the following harvesters to deal with this use case:
* CSW
* Database
* GeoNetwork
* OAI-PMH
* SFTP
* WebDav

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
@@ -1,5 +1,5 @@
 //=============================================================================
-//===	Copyright (C) 2001-2024 Food and Agriculture Organization of the
+//===	Copyright (C) 2001-2025 Food and Agriculture Organization of the
 //===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
 //===	and United Nations Environment Programme (UNEP)
 //===
@@ -66,8 +66,6 @@ import org.jdom.xpath.XPath;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -309,7 +307,7 @@ public class Aligner extends BaseAligner<CswParams> {
         // If the xslfilter process changes the metadata uuid,
         // use that uuid (newMdUuid) for the new metadata to add to the catalogue.
         String newMdUuid = null;
-        if (!params.xslfilter.equals("")) {
+        if (!params.xslfilter.isEmpty()) {
             md = applyXSLTProcessToMetadata(context, md, processName, processParams, log);
             schema = dataMan.autodetectSchema(md);
             // Get new uuid if modified by XSLT process
@@ -467,6 +465,11 @@ public class Aligner extends BaseAligner<CswParams> {
             String newSchema = dataMan.autodetectSchema(md);
             updateSchema = !newSchema.equals(schema);
             schema = newSchema;
+        } else {
+            if (!ri.schema.equals(schema)) {
+                log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + ri.schema + "'. Using the detected one.");
+                updateSchema = true;
+            }
         }
 
         applyBatchEdits(ri.uuid, md, schema, params.getBatchEdits(), context, log);

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/sftp/Aligner.java
@@ -367,6 +367,11 @@ public class Aligner extends BaseAligner<SftpParams> {
             String newSchema = metadataSchemaUtils.autodetectSchema(md);
             updateSchema = !newSchema.equals(schema);
             schema = newSchema;
+        } else {
+            if (!ri.schema.equals(schema)) {
+                log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + ri.schema + "'. Using the detected one.");
+                updateSchema = true;
+            }
         }
 
         boolean validate = false;

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/database/DatabaseHarvesterAligner.java
@@ -236,7 +236,7 @@ class DatabaseHarvesterAligner extends BaseAligner<DatabaseHarvesterParams> impl
 
             switch (params.getOverrideUuid()) {
                 case OVERRIDE:
-                    updateMetadata(metadataElement, Integer.toString(metadataUtils.findOneByUuid(uuid).getId()), true);
+                    updateMetadata(metadataElement, metadataUtils.findOneByUuid(uuid), true);
                     log.debug(String.format("Overriding record with uuid %s", uuid));
                     result.updatedMetadata++;
                     break;
@@ -253,14 +253,15 @@ class DatabaseHarvesterAligner extends BaseAligner<DatabaseHarvesterParams> impl
             }
         } else {
             //record exists and belongs to this harvester
-            updateMetadata(metadataElement, id, false);
+            updateMetadata(metadataElement, metadataUtils.findOne(id), false);
             result.updatedMetadata++;
         }
 
         return id;
     }
 
-    private void updateMetadata(Element xml, String id, boolean force) throws Exception {
+    private void updateMetadata(Element xml, AbstractMetadata originalMetadata, boolean force) throws Exception {
+        String id = Integer.toString(originalMetadata.getId());
         log.info("Updating metadata with id: " + id);
 
         //
@@ -288,6 +289,11 @@ class DatabaseHarvesterAligner extends BaseAligner<DatabaseHarvesterParams> impl
             String newSchema = metadataSchemaUtils.autodetectSchema(xml);
             updateSchema = (newSchema != null) && !newSchema.equals(schema);
             schema = newSchema;
+        } else {
+            if (!originalMetadata.getDataInfo().getSchemaId().equals(schema)) {
+                log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + originalMetadata.getDataInfo().getSchemaId() + "'. Using the detected one.");
+                updateSchema = true;
+            }
         }
 
         applyBatchEdits(uuid, xml, schema, params.getBatchEdits(), context, log);

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/geonet/BaseGeoNetworkAligner.java
@@ -435,10 +435,22 @@ public abstract class BaseGeoNetworkAligner<P extends BaseGeonetParams> extends 
                 MetadataResourceDatabaseMigration.updateMetadataResourcesLink(md, null, settingManager);
             }
 
+            String schema = dataMan.autodetectSchema(md, null);
+            boolean updateSchema = false;
+
             if (!params.xslfilter.isEmpty()) {
                 md = HarvesterUtil.processMetadata(metadataSchemaUtils.getSchema(ri.schema),
                     md, processName, processParams);
+                String newSchema = dataMan.autodetectSchema(md);
+                updateSchema = !newSchema.equals(schema);
+                schema = newSchema;
+            } else {
+                if (!ri.schema.equals(schema)) {
+                    log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + ri.schema + "'. Using the detected one.");
+                    updateSchema = true;
+                }
             }
+
             // update metadata
             if (log.isDebugEnabled()) {
                 log.debug("  - Updating local metadata with id=" + id);
@@ -452,10 +464,16 @@ public abstract class BaseGeoNetworkAligner<P extends BaseGeonetParams> extends 
                 updateDateStamp, IndexingMode.none);
             metadata = metadataRepository.findOneById(Integer.parseInt(id));
             result.updatedMetadata++;
-            if (force) {
-                //change ownership of metadata to new harvester
-                metadata.getHarvestInfo().setUuid(params.getUuid());
-                metadata.getSourceInfo().setSourceId(params.getUuid());
+            if (force || updateSchema) {
+                if (force) {
+                    //change ownership of metadata to new harvester
+                    metadata.getHarvestInfo().setUuid(params.getUuid());
+                    metadata.getSourceInfo().setSourceId(params.getUuid());
+                }
+
+                if (updateSchema) {
+                    metadata.getDataInfo().setSchemaId(schema);
+                }
 
                 metadataManager.save(metadata);
             }

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/oaipmh/Harvester.java
@@ -1,5 +1,5 @@
 //=============================================================================
-//===	Copyright (C) 2001-2007 Food and Agriculture Organization of the
+//===	Copyright (C) 2001-2025 Food and Agriculture Organization of the
 //===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
 //===	and United Nations Environment Programme (UNEP)
 //===
@@ -28,7 +28,6 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -64,7 +63,6 @@ import org.fao.geonet.kernel.search.IndexingMode;
 import org.fao.geonet.lib.Lib;
 import org.fao.geonet.repository.MetadataValidationRepository;
 import org.fao.geonet.repository.OperationAllowedRepository;
-import org.fao.geonet.repository.Updater;
 import org.fao.geonet.repository.specification.MetadataValidationSpecs;
 import org.fao.geonet.utils.GeonetHttpRequestFactory;
 import org.fao.geonet.utils.Xml;
@@ -81,7 +79,6 @@ import org.jdom.JDOMException;
 
 import jeeves.server.context.ServiceContext;
 
-import javax.annotation.Nonnull;
 
 //=============================================================================
 
@@ -317,7 +314,7 @@ class Harvester extends BaseAligner<OaiPmhParams> implements IHarvester<HarvestR
                     switch (params.getOverrideUuid()) {
                         case OVERRIDE:
                             processParams.put("mdChangeDate", ri.changeDate);
-                            updateMetadata(t, ri, Integer.toString(metadataUtils.findOneByUuid(ri.id).getId()),
+                            updateMetadata(t, ri, metadataUtils.findOneByUuid(ri.id),
                                 processName, processParams, true);
                             result.updatedMetadata++;
                             break;
@@ -340,7 +337,7 @@ class Harvester extends BaseAligner<OaiPmhParams> implements IHarvester<HarvestR
                     //record exists and belongs to this harvester
                     String id = localUuids.getID(ri.id);
                     processParams.put("mdChangeDate", ri.changeDate);
-                    updateMetadata(t, ri, id, processName, processParams, false);
+                    updateMetadata(t, ri, metadataUtils.findOne(id), processName, processParams, false);
                 }
                 result.totalMetadata++;
             } catch (Throwable tr) {
@@ -534,7 +531,8 @@ class Harvester extends BaseAligner<OaiPmhParams> implements IHarvester<HarvestR
         }
     }
 
-    private void updateMetadata(XmlRequest t, RecordInfo ri, String id, String processName, Map<String, Object> processParams, boolean force) throws Exception {
+    private void updateMetadata(XmlRequest t, RecordInfo ri, AbstractMetadata originalMetadata, String processName, Map<String, Object> processParams, boolean force) throws Exception {
+        String id = Integer.toString(originalMetadata.getId());
         String date = localUuids.getChangeDate(ri.id);
 
         if (!force && !ri.isMoreRecentThan(date)) {
@@ -565,6 +563,11 @@ class Harvester extends BaseAligner<OaiPmhParams> implements IHarvester<HarvestR
 
                 schema = dataMan.autodetectSchema(md);
                 updateSchema = true;
+            } else {
+                if (!originalMetadata.getDataInfo().getSchemaId().equals(schema)) {
+                    log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + originalMetadata.getDataInfo().getSchemaId() + "'. Using the detected one.");
+                    updateSchema = true;
+                }
             }
 
             // Translate metadata
@@ -585,12 +588,7 @@ class Harvester extends BaseAligner<OaiPmhParams> implements IHarvester<HarvestR
                     context.getBean(MetadataValidationRepository.class);
 
                 final String newSchema = schema;
-                metadataManager.update(Integer.parseInt(id), new Updater<AbstractMetadata>() {
-                    @Override
-                    public void apply(@Nonnull AbstractMetadata entity) {
-                        entity.getDataInfo().setSchemaId(newSchema);
-                    }
-                });
+                metadataManager.update(Integer.parseInt(id), entity -> entity.getDataInfo().setSchemaId(newSchema));
 
                 metadataValidationRepository.deleteAll(MetadataValidationSpecs.hasMetadataId(Integer.parseInt(id)));
             }

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/webdav/Harvester.java
@@ -489,9 +489,18 @@ class Harvester extends BaseAligner<WebDavParams> implements IHarvester<HarvestR
                 md = translateMetadataContent(context, md, schema);
             }
 
+            boolean updateSchema = false;
             if (StringUtils.isNotEmpty(params.xslfilter)) {
                 md = HarvesterUtil.processMetadata(dataMan.getSchema(schema),
                     md, processName, processParams);
+                String newSchema = dataMan.autodetectSchema(md);
+                updateSchema = !newSchema.equals(schema);
+                schema = newSchema;
+            } else {
+                if (!recordInfo.schema.equals(schema)) {
+                    log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + recordInfo.schema + "'. Using the detected one.");
+                    updateSchema = true;
+                }
             }
 
             //
@@ -504,10 +513,16 @@ class Harvester extends BaseAligner<WebDavParams> implements IHarvester<HarvestR
             final AbstractMetadata metadata = metadataManager.updateMetadata(context, recordInfo.id, md, validate, ufo, language,
                 date, true, IndexingMode.none);
 
-            if(force) {
-                //change ownership of metadata to new harvester
-                metadata.getHarvestInfo().setUuid(params.getUuid());
-                metadata.getSourceInfo().setSourceId(params.getUuid());
+            if(force || updateSchema) {
+                if (force) {
+                    //change ownership of metadata to new harvester
+                    metadata.getHarvestInfo().setUuid(params.getUuid());
+                    metadata.getSourceInfo().setSourceId(params.getUuid());
+                }
+
+                if (updateSchema) {
+                    metadata.getDataInfo().setSchemaId(schema);
+                }
 
                 context.getBean(IMetadataManager.class).save(metadata);
             }

commit 616b8500a65546d8b69313b4df28bb8945df6841

Fix CSW harvester NullPointerException when RecordInfo schema is null
Reverse the schema comparison in Aligner.updatingLocalMetadata so the
non-null autodetected schema is the receiver, avoiding an NPE when the
catalog record's RecordInfo.schema is null (regression from #9052).

harvesters/src/main/java/org/fao/geonet/kernel/harvest/harvester/csw/Aligner.java
@@ -466,7 +466,7 @@ public class Aligner extends BaseAligner<CswParams> {
             updateSchema = !newSchema.equals(schema);
             schema = newSchema;
         } else {
-            if (!ri.schema.equals(schema)) {
+            if (!schema.equals(ri.schema)) {
                 log.warning("  - Detected schema '" + schema + "' is different from the one of the metadata in the catalog '" + ri.schema + "'. Using the detected one.");
                 updateSchema = true;
             }