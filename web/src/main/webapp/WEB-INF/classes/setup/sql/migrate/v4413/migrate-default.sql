d4bee61514a9d42117bb1af5daa2f20402a0d097
tylerjmchugh <Tyler.McHugh@dfo-mpo.gc.ca>
2026-07-14 11:58:48 -0400

    db migration

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4413/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4413/migrate-default.sql
@@ -1,2 +1,6 @@
+ALTER TABLE spg_page ADD COLUMN IF NOT EXISTS showOnNonApproved boolean DEFAULT true NOT NULL;
+ALTER TABLE spg_page ADD COLUMN IF NOT EXISTS showOnApproved boolean DEFAULT true NOT NULL;
+ALTER TABLE spg_page ADD COLUMN IF NOT EXISTS showWhenWorkflowDisabled boolean DEFAULT true NOT NULL;
+
 UPDATE Settings SET value='4.4.13' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
