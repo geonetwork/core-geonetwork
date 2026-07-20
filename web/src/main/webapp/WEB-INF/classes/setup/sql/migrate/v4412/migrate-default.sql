70edf0fbe3aadded7ac722913a79b99f89a54dcd
Francois Prunayre <fx.prunayre@gmail.com>
2026-07-20 12:37:27 +0200

    Set/unset metadata publication date when a metadata is published / SQL migration in 4.4.13.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4412/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4412/migrate-default.sql
@@ -1,7 +1,4 @@
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/csw/getRecordsIgnoreMetadataNotSupported', 'true', 2, 1321, 'y');
-INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/metadataprivs/publication/managepublicationdate', 'false', 2, 9182, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/metadataprivs/publication/managepublicationdate');
-UPDATE Settings SET position=9183 WHERE name='system/metadataprivs/publication/notificationLevel';
-UPDATE Settings SET position=9184 WHERE name='system/metadataprivs/publication/notificationGroups';
 
 INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/oai/enable', 'true', 2, 7000, 'n'  from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/oai/enable');
 
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4413/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v4413/migrate-default.sql
@@ -1,2 +1,6 @@
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/metadataprivs/publication/managepublicationdate', 'false', 2, 9182, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/metadataprivs/publication/managepublicationdate');
+UPDATE Settings SET position=9183 WHERE name='system/metadataprivs/publication/notificationLevel';
+UPDATE Settings SET position=9184 WHERE name='system/metadataprivs/publication/notificationGroups';
+
 UPDATE Settings SET value='4.4.13' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
