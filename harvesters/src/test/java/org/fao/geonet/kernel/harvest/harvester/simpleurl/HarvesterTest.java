f06ee4567c5948553ae21f63cb35bb4c0686dc93
Tobias Hotz <tobias.hotz@conet-isb.de>
2026-08-11 09:27:55 +0200

    Fix tests

--- harvesters/src/test/java/org/fao/geonet/kernel/harvest/harvester/simpleurl/HarvesterTest.java
+++ harvesters/src/test/java/org/fao/geonet/kernel/harvest/harvester/simpleurl/HarvesterTest.java
@@ -29,7 +29,6 @@ public class HarvesterTest {
         assertEquals("http://dados.gov.br/api/3/action/package_search?q=&rows=1&start=21", list.get(2));
 
 
-
         params.url = "http://dados.gov.br/api/3/action/package_search?q=&rows=10&start=0";
         list = harvester.buildListOfUrl(params, numberOfResult);
         assertEquals(3, list.size());
@@ -76,10 +75,10 @@ public class HarvesterTest {
 
         JsonNode json = objectMapper.readTree("{\"records\":[{\"id\":\"a\"},{\"id\":\"b\"}]}");
 
-        List<JsonNode> pointerNodes = harvester.selectJsonRecords(json, "/records");
+        List<JsonNode> pointerNodes = harvester.selectJsonRecords(json, SimpleUrlPathMode.JSONPOINTER, "/records");
         assertEquals(2, pointerNodes.size());
 
-        List<JsonNode> pathNodes = harvester.selectJsonRecords(json, "$.records[*]");
+        List<JsonNode> pathNodes = harvester.selectJsonRecords(json, SimpleUrlPathMode.JSONPATH, "$.records[*]");
         assertEquals(2, pathNodes.size());
     }
 }
