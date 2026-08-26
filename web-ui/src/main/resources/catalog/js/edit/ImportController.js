81f38e7390ac7bbc952a4390d598306455b925d4
Olivia <olivia.guyot@camptocamp.com>
2026-08-26 15:28:31 +0200

    Add an option to completely replace a record with identical UUID when importing

--- web-ui/src/main/resources/catalog/js/edit/ImportController.js
+++ web-ui/src/main/resources/catalog/js/edit/ImportController.js
@@ -78,6 +78,31 @@
       };
       $scope.importing = false;
 
+      var overwriteReplaceEnabled = false;
+      function setUuidProcessingOverwrite() {
+        $scope.params.uuidProcessing = overwriteReplaceEnabled
+          ? "REMOVE_AND_REPLACE"
+          : "OVERWRITE";
+      }
+      $scope.uuidProcessingOverwriteGetSet = function (uuidProcessing) {
+        if (uuidProcessing === undefined) {
+          // is getter
+          return $scope.params.uuidProcessing === "OVERWRITE" ||
+            $scope.params.uuidProcessing === "REMOVE_AND_REPLACE"
+            ? "OVERWRITE_OR_REMOVE_AND_REPLACE"
+            : "";
+        }
+        setUuidProcessingOverwrite();
+      };
+      $scope.overwriteReplaceRecordGetSet = function (enableReplace) {
+        if (enableReplace === undefined) {
+          // is getter
+          return overwriteReplaceEnabled;
+        }
+        overwriteReplaceEnabled = enableReplace;
+        setUuidProcessingOverwrite();
+      };
+
       gnConfigService.load().then(function (c) {
         $scope.isMdWorkflowEnable = gnConfig["metadata.workflow.enable"];
         $scope.params.group = gnConfig["system.metadatacreate.preferredGroup"];
