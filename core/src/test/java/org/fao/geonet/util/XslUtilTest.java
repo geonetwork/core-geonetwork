c082ae50cd01c2027589fae67f68246ae23cc572
josegar74 <josegar74@gmail.com>
2026-09-01 16:58:54 +0200

    Add unit tests for XslUtil.getRecordIfViewable privilege checks

--- core/src/test/java/org/fao/geonet/util/XslUtilTest.java
+++ core/src/test/java/org/fao/geonet/util/XslUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2022 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2026 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -23,13 +23,145 @@
 
 package org.fao.geonet.util;
 
+import jeeves.server.UserSession;
+import jeeves.server.context.ServiceContext;
+import org.fao.geonet.ApplicationContextHolder;
+import org.fao.geonet.domain.Operation;
+import org.fao.geonet.domain.ReservedOperation;
+import org.fao.geonet.kernel.AccessManager;
+import org.fao.geonet.kernel.DataManager;
+import org.jdom.Element;
+import org.junit.After;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Collections;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class XslUtilTest {
+
+    private static final String UUID = "test-uuid";
+    private static final String METADATA_ID = "42";
+
+    @After
+    public void tearDown() {
+        ApplicationContextHolder.clear();
+    }
+
+    private DataManager mockDataManagerWithRecord() throws Exception {
+        ConfigurableApplicationContext applicationContext = mock(ConfigurableApplicationContext.class);
+        DataManager dataManager = mock(DataManager.class);
+        when(applicationContext.getBean(DataManager.class)).thenReturn(dataManager);
+        when(dataManager.getMetadataId(UUID)).thenReturn(METADATA_ID);
+        when(dataManager.getMetadataSchema(METADATA_ID)).thenReturn("iso19139");
+        when(dataManager.getMetadata(METADATA_ID)).thenReturn(new Element("MD_Metadata"));
+
+        ApplicationContextHolder.set(applicationContext);
+        return dataManager;
+    }
+
+    private ServiceContext mockServiceContextGrantingOperations(Set<Operation> operations) throws Exception {
+        ServiceContext context = mock(ServiceContext.class);
+        AccessManager accessManager = mock(AccessManager.class);
+        UserSession userSession = mock(UserSession.class);
+
+        when(context.getBean(AccessManager.class)).thenReturn(accessManager);
+        when(context.getIpAddress()).thenReturn("127.0.0.1");
+        when(context.getUserSession()).thenReturn(userSession);
+        when(userSession.isAuthenticated()).thenReturn(false);
+        when(accessManager.getOperations(eq(context), anyString(), anyString())).thenReturn(operations);
+
+        return context;
+    }
+
+    private static Set<Operation> viewAllowed() {
+        return Collections.singleton(new Operation().setId(ReservedOperation.view.getId()));
+    }
+
+    @Test
+    public void testGetRecordDoesNotCheckPrivileges() throws Exception {
+        DataManager dataManager = mockDataManagerWithRecord();
+
+        // No ServiceContext at all bound to the thread (eg. async indexing) -
+        // the unchecked getRecord() must still return the record.
+        try (MockedStatic<ServiceContext> serviceContextMock = mockStatic(ServiceContext.class)) {
+            serviceContextMock.when(ServiceContext::get).thenReturn(null);
+
+            org.w3c.dom.Node node = XslUtil.getRecord(UUID);
+
+            assertNotNullRecord(node);
+        }
+    }
+
+    @Test
+    public void testGetRecordUnknownUuidReturnsNull() throws Exception {
+        mockDataManagerWithRecord();
+
+        assertNull(XslUtil.getRecord("does-not-exist"));
+        assertNull(XslUtil.getRecordIfViewable("does-not-exist"));
+    }
+
+    @Test
+    public void testGetRecordIfViewableReturnsRecordWhenViewAllowed() throws Exception {
+        DataManager dataManager = mockDataManagerWithRecord();
+        ServiceContext context = mockServiceContextGrantingOperations(viewAllowed());
+
+        try (MockedStatic<ServiceContext> serviceContextMock = mockStatic(ServiceContext.class)) {
+            serviceContextMock.when(ServiceContext::get).thenReturn(context);
+
+            org.w3c.dom.Node node = XslUtil.getRecordIfViewable(UUID);
+
+            assertNotNullRecord(node);
+        }
+    }
+
+    @Test
+    public void testGetRecordIfViewableReturnsNullWhenViewNotAllowed() throws Exception {
+        DataManager dataManager = mockDataManagerWithRecord();
+        ServiceContext context = mockServiceContextGrantingOperations(Collections.emptySet());
+
+        try (MockedStatic<ServiceContext> serviceContextMock = mockStatic(ServiceContext.class)) {
+            serviceContextMock.when(ServiceContext::get).thenReturn(context);
+
+            org.w3c.dom.Node node = XslUtil.getRecordIfViewable(UUID);
+
+            assertNull(node);
+            verify(dataManager, never()).getMetadata(anyString());
+        }
+    }
+
+    @Test
+    public void testGetRecordIfViewableReturnsNullWhenNoServiceContext() throws Exception {
+        DataManager dataManager = mockDataManagerWithRecord();
+
+        try (MockedStatic<ServiceContext> serviceContextMock = mockStatic(ServiceContext.class)) {
+            serviceContextMock.when(ServiceContext::get).thenReturn(null);
+
+            org.w3c.dom.Node node = XslUtil.getRecordIfViewable(UUID);
+
+            assertNull(node);
+            verify(dataManager, never()).getMetadata(anyString());
+        }
+    }
+
+    private static void assertNotNullRecord(org.w3c.dom.Node node) {
+        org.junit.Assert.assertNotNull(node);
+        org.w3c.dom.Document domDocument = (org.w3c.dom.Document) node;
+        assertEquals("MD_Metadata", domDocument.getDocumentElement().getTagName());
+    }
 
     @Test
     public void testHtml2text() {
